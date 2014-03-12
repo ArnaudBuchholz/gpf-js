5cde5c6e5d7164858f3c5c9fbe1ab7d6d279dfb3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-03-12 16:34:12 -0400

    Pattern parser
    Changed test case to make it more flexible

--- test/parser.js
+++ test/parser.js
@@ -2,11 +2,14 @@
     "use strict";
 
     var
-        identifiers = {
-            "abc": 3,
-            "abc!": 3,
-            "0123": -1,
-            "_ABcD_0": 7
+        _patterns = {
+            identifiers: {
+                _expression: "[_a-zA-Z][_a-zA-Z0-9]*",
+                "abc": 3,
+                "abc!": 3,
+                "0123": -1,
+                "_ABcD_0": 7
+            }
         };
 
     gpf.declareTests({
@@ -29,7 +32,8 @@
                 // Then verify that matching works
                 ctx2 = pattern.allocate();
                 test.equal(pattern.write(ctx2,"ab"), 0, "Need more input");
-                test.equal(ctx2.push("c"), 3, "Matching with the correct length");
+                test.equal(ctx2.push("c"), 3,
+                    "Matching with the correct length");
                 test.equal(ctx2.push("d"), 3,
                     "Any other char doesn't change the result");
                 // Check that immediate match works
@@ -39,21 +43,20 @@
             },
 
             function (test) {
-                test.title("Identifiers");
+                test.title("Range");
                 var
-                    pattern = new gpf.Pattern("[_a-zA-Z][_a-zA-Z0-9]*"),
-                    key, expectedResult;
-                for (key in identifiers) {
-                    if (identifiers.hasOwnProperty(key)) {
-                        expectedResult = identifiers[key];
-                        test.equal(pattern.write(pattern.allocate(), key),
-                            expectedResult, key);
-                    }
-                }
+                    pattern = new gpf.Pattern("[a-zA-Z^d-fJ]"),
+                    ctx1, ctx2, ctx3;
+                ctx1 = pattern.allocate();
+                test.equal(pattern.write(ctx1, "L"), 1, "Match");
+                ctx2 = pattern.allocate();
+                test.equal(pattern.write(ctx2, "e"), -1, "Not match");
+                ctx3 = pattern.allocate();
+                test.equal(pattern.write(ctx2, "0"), -1, "Not match");
             },
 
             function (test) {
-                test.title("Keywords");
+                test.title("Alternative");
                 var
                     pattern = new gpf.Pattern("if|else|then");
                 test.equal(pattern.write(pattern.allocate(), "if"), 2, "if");
@@ -61,8 +64,33 @@
                     "else");
                 test.equal(pattern.write(pattern.allocate(), "then"), 4,
                     "then");
-            }
+            },
 
+            function (test) {
+                test.title("Advanced expressions");
+                var
+                    name,
+                    patternTest,
+                    pattern,
+                    testExpr;
+                for (name in _patterns) {
+                    if (_patterns.hasOwnProperty(name)) {
+                        patternTest = _patterns[name];
+                        test.log(name);
+                        pattern = new gpf.Pattern(patternTest._expression);
+                        for (testExpr in patternTest) {
+                            if (patternTest.hasOwnProperty(testExpr)) {
+                                if (patternTest === "_expression") {
+                                    continue;
+                                }
+                                test.equal(pattern.write(pattern.allocate(),
+                                    testExpr), patternTest[testExpr],
+                                    "\t" + testExpr);
+                            }
+                        }
+                    }
+                }
+            }
 
         ]
 
