20a3bad73be031a26168b967feb476976cbc3425
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-07-18 00:43:59 -0400

    WIP

--- html.js
+++ html.js
@@ -9,8 +9,7 @@
         _MARKDOWN_ISTATE_INIT        = 0,
         _MARKDOWN_ISTATE_INPROGRESS  = 1,
         _MARKDOWN_ISTATE_WAITING     = 2,
-        _MARKDOWN_ISTATE_EOS         = 3,
-        _MARKDOWN_PARSER_INIT        = 0;
+        _MARKDOWN_ISTATE_EOS         = 3;
 
     gpf.html = {};
 
@@ -33,6 +32,7 @@
             constructor: function (input) {
                 this._iStream = gpfI.query(input, gpfI.IReadableStream, true);
                 this._outputBuffer = [];
+                this._pState = this._parseInit;
             },
 
             //region gpf.interfaces.IReadableStream
@@ -163,7 +163,128 @@
              * Parser state
              * @type {Number} see _MARKDOWN_PARSER_xxx
              */
-            _pState: _MARKDOWN_PARSER_INIT,
+            _pState: null,
+
+            /*
+             * 'Grammar'
+             * init
+             *      : '\n' init
+             *      | '#' title1
+             *      | '*' list
+             *      | ' ' init
+             *      | content
+             *
+             * title1
+             *      : '#' title2
+             *      | text \n
+             * title2
+             *      : '#' title3
+             *      | text \n
+             * title3
+             *       : text \n
+             *
+             * list
+             *      : ' ' content // confirmed
+             *      | content
+             *
+             * content
+             *      : '\n' init
+             *      | '*' italic
+             *      | '`' monospace
+             *      | '[' link
+             *      | '&' content
+             *      | '<' content
+             *      | '>' content
+             *      | content
+             *
+             * italic
+             *      : '*' text '*' '*' // bold
+             *      : text '*' // italic
+             *
+             * monospace
+             *      : text '`'
+             *
+             * link
+             *      : (text) ']' '(' url ')'
+             */
+
+            _pTagOpened: false,
+            _numberOfNL: 0,
+            _indentLevel: 0,
+
+            /**
+             * init
+             * @param {String} char
+             * @private
+             */
+            _parseInit: function (char) {
+                var
+                    closeP = false;
+                if ("\n" === char) {
+                    // If the second time and a <p> is opened, close it
+                    if (2 === ++this._numberOfNL && this._pTagOpened) {
+                        closeP = true;
+                    }
+                    this._indentLevel = 0;
+                    // this._pState = this._parseInit;
+                } else if ("#" === char) {
+                    // If a <p> is opened, close it
+                    closeP = true;
+                    this._hLevel = 1;
+                    this._pState = this._parseTitle;
+                } else if ("*" === char) {
+                    // If a <p> is opened, close it
+                    closeP = true;
+                    this._pState = this._parseList;
+                } else if (" " === char || "\t" === char) {
+                    ++this._indentLevel;
+                    // this._pState = this._pStateInit;
+                } else {
+                    // If no <p> and - at least - two \n were used, open one
+                    if (1 < this._numberOfNL) {
+                        this._output("<p>");
+                        this._pTagOpened = true;
+                    }
+                    this._parseContent(char);
+                }
+                if (closeP) {
+                    this._output("</p>");
+                    this._pTagOpened = false;
+                }
+            },
+
+            _hLevel: 1,
+
+            /**
+             * title1, ... N
+             * @param {String} char
+             * @private
+             */
+            _parseTitle: function (char) {
+                if ("#" === char) {
+                    ++this._hLevel;
+//                    this._pState = this._parseTitle;
+                } else {
+                    this._output("<h" + this._hLevel + ">");
+                    this._pState = this._parseText;
+                }
+            },
+
+            /**
+             * list
+             * @param {String} char
+             * @private
+             */
+            _parseList: function (char) {
+                if (" " === char) {
+                    // Start or append list
+                }
+                this._pState = this._parseContent;
+            },
+
+            _parseContent: function (char) {
+
+            },
 
             /**
              * Parses the received buffer
@@ -178,72 +299,8 @@
                     len = buffer.length;
                 while (pos < len) {
                     char = buffer.charAt(pos);
-/*
- * Special chars:
- *  Anywhere:
- *      & => &amp;
- *      > => &gt;
- *      < => &lt;
- *      \n End of line
- *      * italic until *
- *      ** bold until **
- *      ` => monospace
- *      [ => link
- *
- *
- *  At the begin of a line:
- *      # H1
- *      ## H2
- *      ### H3
- *      ' ' (link is blanked until something else is found)
- *      *' ' => list until \n
- *
- * 'Grammar'
- * init
- *      : '\n' init
- *      | '#' title1
- *      | '*' list
- *      | ' ' init
- *      | content
- *
- * title1
- *      : '#' title2
- *      | text \n
- * title2
- *      : '#' title3
- *      | text \n
- * title3
-*       : text \n
- *
- * list
- *      : ' ' content // confirmed
- *      | content
- *
- * content
- *      : '\n' init
- *      | '*' italic
- *      | '`' monospace
- *      | '[' link
- *      | '&' content
- *      | '<' content
- *      | '>' content
- *      | content
- *
- * italic
- *      : '*' text '*' '*' // bold
- *      : text '*' // italic
- *
- * monospace
- *      : text '`'
- *
- * link
- *      : (text) ']' '(' url ')'
- */
-
-
+                    this._pState(char);
                 }
-
-
             },
 
             /**
