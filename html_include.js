2c707b4c13574f2dcad012d4c5bb4b1e03eaac56
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-08-05 23:49:17 -0400

    Include helper for external project

---
+++ html_include.js
@@ -0,0 +1,8 @@
+(function () { /* Begin of privacy scope */
+    "use strict";
+
+    window.gpfSourcesPath = "../gpf-js/";
+    document.write("<script language=\"javascript\" src=\"../gpf-js/boot.js\">"
+        + "</script>");
+
+}()); /* End of privacy scope */
