a2eff6cc355f8c7689a4b74cdccf53f01429da66
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-09-03 01:01:19 -0400

    Created gpf.BufferedOnReadStream

--- i_stream.js
+++ i_stream.js
@@ -4,7 +4,12 @@
 /*#endif*/
 
     var
-        gpfI = gpf.interfaces;
+        gpfI = gpf.interfaces,
+        _BUFREADSTREAM_READ_SIZE        = 256,
+        _BUFREADSTREAM_ISTATE_INIT        = 0,
+        _BUFREADSTREAM_ISTATE_INPROGRESS  = 1,
+        _BUFREADSTREAM_ISTATE_WAITING     = 2,
+        _BUFREADSTREAM_ISTATE_EOS         = 3;
 
     /**
      * The Readable stream interface is the abstraction for a source of data
@@ -115,7 +120,7 @@
         write: function (int8buffer, eventsHandler) {
             gpf.interfaces.ignoreParameter(int8buffer);
             gpf.interfaces.ignoreParameter(eventsHandler);
-        },
+        }
 
     });
 
@@ -148,6 +153,223 @@
             this.write_(null);
         }
     };
+
+    /**
+     * Handles a buffered stream that depends on another stream
+     *
+     * @class gpf.BufferedOnReadStream
+     * @implements gpf.interfaces.IReadableStream
+     */
+    gpf.define("gpf.BufferedOnReadStream", {
+
+        "[Class]": [gpf.$InterfaceImplement(gpfI.IReadableStream)],
+
+        protected: {
+
+            /**
+             * Underlying stream default read size
+             *
+             * @type {Number}
+             * @protected
+             */
+            _readSize: _BUFREADSTREAM_READ_SIZE,
+
+            /**
+             * Process underlying stream buffer
+             *
+             * @param {Array} buffer
+             * @protected
+             */
+            _addToBuffer: function (buffer) {
+                gpf.interfaces.ignoreParameter(buffer);
+            },
+
+            /**
+             * Underlying stream reached its end
+             *
+             * @protected
+             */
+            _endOfInputStream: function () {
+            },
+
+            /**
+             * Send buffer part to the caller of this stream.
+             * This default implementation checks the buffer type to switch
+             * between string and byte array.
+             *
+             * @param {Array} buffer
+             * @param {Number} size
+             * @returns {Array}
+             * @protected
+             */
+            _extractFromBuffer: function (buffer, size) {
+                gpf.ASSERT(buffer.length, "Buffer is not empty");
+                if ("string" === typeof buffer[0]) {
+                    return gpf.stringExtractFromStringArray(buffer, size);
+                } else {
+                    if (size > buffer.length) {
+                        size = buffer.length;
+                    }
+                    return buffer.splice(0, size);
+                }
+            }
+
+        },
+
+        public: {
+
+            /**
+             * @param {gpf.interfaces.IReadableStream} input
+             * @constructor
+             */
+            constructor: function (input) {
+                this._iStream = gpfI.query(input, gpfI.IReadableStream, true);
+                this._outputBuffer = [];
+            },
+
+            //region gpf.interfaces.IReadableStream
+
+            /**
+             * @implements gpf.interfaces.IReadableStream:read
+             */
+            read: function (size, eventsHandler) {
+                var
+                    iState = this._iState,
+                    buffer,
+                    length = this._outputBufferLength;
+                if (_BUFREADSTREAM_ISTATE_INPROGRESS === iState) {
+                    // A read call is already in progress
+                    throw gpfI.IReadableStream.EXCEPTION_READ_IN_PROGRESS;
+
+                } else if (size < length
+                    || length && _BUFREADSTREAM_ISTATE_EOS === iState) {
+                    // Enough chars in the output buffer to do the read
+                    // OR there won't be any more chars
+                    buffer = this._extractFromBuffer(
+                        this._outputBuffer, size
+                    );
+                    this._outputBufferLength -= buffer.length;
+                    // Can output something
+                    gpf.events.fire.apply(this, [
+                        gpfI.IReadableStream.EVENT_DATA,
+                        {
+                            buffer: buffer
+                        },
+                        eventsHandler
+                    ]);
+
+                } else if (_BUFREADSTREAM_ISTATE_EOS === iState) {
+                    // No more input and output buffer is empty
+                    gpf.events.fire.apply(this, [
+                        gpfI.IReadableStream.EVENT_END_OF_STREAM,
+                        eventsHandler
+                    ]);
+
+                } else {
+                    // Read input
+                    if (_BUFREADSTREAM_ISTATE_INIT === this._iState) {
+                        // Very first call, create callback for input reads
+                        this._cbRead = new gpf.Callback(this._onRead, this);
+                    }
+                    this._iState = _BUFREADSTREAM_ISTATE_INPROGRESS;
+                    // Backup parameters
+                    this._size = size;
+                    this._eventsHandler = eventsHandler;
+                    this._iStream.read(this._readSize, this._cbRead);
+                }
+            }
+
+            //endregion
+        },
+
+        //region Implementation
+
+        private: {
+
+            /**
+             * Input stream
+             * @type {gpf.interfaces.IReadableStream}
+             */
+            _iStream: null,
+
+            /**
+             * Input stream read callback (pointing to this:_onRead)
+             * @type {gpf.Callback}
+             */
+            _cbRead: null,
+
+            /**
+             * Output buffer contains items to be read
+             * @type {*[]}
+             */
+            _outputBuffer: [],
+
+            /**
+             * Size of the output buffer (number of bytes)
+             * @type {Number}
+             */
+            _outputBufferLength: 0,
+
+            /**
+             * Input state
+             * @type {Number} see _BUFREADSTREAM_ISTATE_xxx
+             */
+            _iState: _BUFREADSTREAM_ISTATE_INIT,
+
+            /**
+             * Pending read call size
+             * @type {Number}
+             */
+            _size: 0,
+
+            /**
+             * Pending read call event handlers
+             * @type {gpf.events.Handler}
+             */
+            _eventsHandler: null,
+
+            /**
+             * Handles input stream read event
+             *
+             * @param {gpf.events.Event} event
+             * @private
+             */
+            _onRead: function (event) {
+                var
+                    type = event.type();
+                if (type === gpfI.IReadableStream.EVENT_END_OF_STREAM) {
+                    this._iState = _BUFREADSTREAM_ISTATE_EOS;
+                    this._endOfInputStream();
+                    // Redirect to read with backed parameters
+                    return this.read(this._size, this._eventsHandler);
+
+                } else if (type === gpfI.IReadableStream.EVENT_ERROR) {
+                    // Forward the event
+                    gpf.events.fire.apply(this, [
+                        event,
+                        this._eventsHandler
+                    ]);
+
+                } else {
+                    this._iState = _BUFREADSTREAM_ISTATE_WAITING;
+                    this._addToBuffer(event.get("buffer"));
+                    if (0 < this._outputBufferLength) {
+                        // Redirect to read with backed parameters
+                        return this.read(this._size, this._eventsHandler);
+                    } else {
+                        // Try to read source again
+                        this._iStream.read(_BUFREADSTREAM_READ_SIZE,
+                            this._cbRead);
+                    }
+                }
+            }
+
+        }
+
+        //endregion
+    });
+
+    //endregion
 
 /*#ifndef(UMD)*/
 }()); /* End of privacy scope */
