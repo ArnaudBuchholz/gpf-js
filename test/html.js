3525ffa1ae4c9c8ef052bab9b4566f88a6d48527
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-09-04 23:11:23 -0400

    Removed JSHint errors (not used)

--- test/html.js
+++ test/html.js
@@ -52,56 +52,56 @@
             "",
             "The rain---not the reign---in",
             "Spain."
-        ].join("\r\n"),
+        ].join("\r\n");
 
-        _sampleHTMLobj = {
-            h1: "Heading",
-            h2: "Sub-heading",
-            p:[
-                "Paragraphs are separated by a blank line.",
-                {
-                    $: "Text attributes ",
-                    em: "italic",
-                    $$1: ", ",
-                    b: "bold",
-                    $$2: ", ",
-                    code: "monospace",
-                    $$3: "."
-                },
-                {
-                    $: "A ",
-                    a: {
-                        href: "http://example.com",
-                        $: "link"
-                    },
-                    $$1: "."
-                },
-                "Shopping list:"
-            ],
-            ul: {
-                li: [
-                    "apples",
-                    "oranges",
-                    "pears"
-                ]
-            },
-            p$1: "Numbered list:",
-            ol: {
-                li: [
-                    "apples",
-                    "oranges",
-                    "pears"
-                ]
-            },
-            p$2: {
-                $: "The rain",
-
-                $$1: "not the reign",
-
-                $$2: "in Spain."
-            }
-
-        };
+//        _sampleHTMLobj = {
+//            h1: "Heading",
+//            h2: "Sub-heading",
+//            p:[
+//                "Paragraphs are separated by a blank line.",
+//                {
+//                    $: "Text attributes ",
+//                    em: "italic",
+//                    $$1: ", ",
+//                    b: "bold",
+//                    $$2: ", ",
+//                    code: "monospace",
+//                    $$3: "."
+//                },
+//                {
+//                    $: "A ",
+//                    a: {
+//                        href: "http://example.com",
+//                        $: "link"
+//                    },
+//                    $$1: "."
+//                },
+//                "Shopping list:"
+//            ],
+//            ul: {
+//                li: [
+//                    "apples",
+//                    "oranges",
+//                    "pears"
+//                ]
+//            },
+//            p$1: "Numbered list:",
+//            ol: {
+//                li: [
+//                    "apples",
+//                    "oranges",
+//                    "pears"
+//                ]
+//            },
+//            p$2: {
+//                $: "The rain",
+//
+//                $$1: "not the reign",
+//
+//                $$2: "in Spain."
+//            }
+//
+//        }
 
     gpf.declareTests({
 
