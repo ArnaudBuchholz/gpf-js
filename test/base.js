24a896d953a41681db2305a7b804d2aa1a439980
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-09-05 13:26:17 -0400

    Improved test coverage for base

--- test/base.js
+++ test/base.js
@@ -283,6 +283,147 @@
                 test.equal(result.join(""), "013456789", "Value removed");
             }
 
+        ],
+
+        xor: [
+
+            function (test) {
+                test.title("XOR truth table");
+                test.equal(gpf.xor(false, false), false, "0^0=0");
+                test.equal(gpf.xor(false, true), true, "0^1=1");
+                test.equal(gpf.xor(true, false), true, "1^0=1");
+                test.equal(gpf.xor(true, true), false, "1^1=0");
+            }
+
+        ],
+
+        capitalize: [
+
+            function (test) {
+                test.title("Capitalize basic tests");
+                test.equal(gpf.capitalize("word"), "Word", "one word");
+                test.equal(gpf.capitalize("two words"), "Two words",
+                    "Two words");
+                test.equal(gpf.capitalize("Two words"), "Two words",
+                    "Already capitalized");
+                test.equal(gpf.capitalize("éric"), "Éric",
+                    "Accent capitalization");
+            }
+
+        ],
+
+        callback: [
+
+            function (test) {
+                test.title("Basic callback");
+                var callback = new gpf.Callback(function() {
+                    test.equal(true, true, "Callback called");
+                    test.done();
+                });
+                test.wait(1);
+                callback.apply();
+            },
+
+            function (test) {
+                test.title("Scoped callback (1)");
+                var scope = {};
+                test.wait(1);
+                var callback = new gpf.Callback(function() {
+                    test.equal(this, scope, "Scope inside definition");
+                    test.done();
+                }, scope);
+                callback.apply();
+            },
+
+            function (test) {
+                test.title("Scoped callback (2)");
+                var scope = {};
+                var callback = new gpf.Callback(function() {
+                    test.equal(this, scope, "Scope inside apply");
+                    test.done();
+                });
+                test.wait(1);
+                callback.apply(scope);
+            },
+
+            function (test) {
+                test.title("Callback with parameters");
+                var scope = {};
+                var callback = new gpf.Callback(function() {
+                    test.equal(arguments.length, 2,
+                        "Correct number of arguments");
+                    test.equal(arguments[0], "string",
+                        "First argument is correct");
+                    test.equal(arguments[1], 123,
+                        "Second argument is correct");
+                    test.done();
+                });
+                test.wait(1);
+                callback.apply(scope, ["string", 123]);
+            },
+
+            function (test) {
+                test.title("Scope resolution");
+                var scope = {};
+                test.equal(gpf.Callback.resolveScope(scope), scope,
+                    "When specified, scope is not altered");
+                test.equal(gpf.Callback.resolveScope(false), false,
+                    "When specified (false), scope is not altered");
+                test.equal(gpf.Callback.resolveScope(0), 0,
+                    "When specified (0), scope is not altered");
+                test.equal(gpf.Callback.resolveScope(null), gpf.context(),
+                    "When null, global scope is returned");
+                test.equal(gpf.Callback.resolveScope(undefined), gpf.context(),
+                    "When undefined, global scope is returned");
+            },
+
+
+            function (test) {
+                test.title("Build param array");
+                var result = gpf.Callback.buildParamArray(2);
+                test.equal(result.length, 2,
+                    "Size is based on count and parameter");
+                test.equal(result[0], undefined,
+                    "First item is not initialized");
+                test.equal(result[1], undefined,
+                    "Second item is not initialized");
+            },
+
+            function (test) {
+                test.title("Build param array with parameters");
+                var result = gpf.Callback.buildParamArray(2, [0, 1]);
+                test.equal(result.length, 4,
+                    "Size is based on count and parameter");
+                test.equal(result[0], undefined,
+                    "First item is not initialized");
+                test.equal(result[1], undefined,
+                    "Second item is not initialized");
+                test.equal(result[2], 0,
+                    "First additional parameter is set");
+                test.equal(result[3], 1,
+                    "First additional parameter is set");
+            },
+
+            function (test) {
+                test.title("Use of param array with doApply");
+                var result = gpf.Callback.buildParamArray(2, [0, 1]);
+                test.wait(1);
+                // can use a gpf.Callback or a function
+                gpf.Callback.doApply(function() {
+                    test.equal(arguments.length, 4,
+                        "Correct number of arguments");
+                    test.equal(arguments[0], "string",
+                        "First argument is correct");
+                    test.equal(arguments[1], 123,
+                        "Second argument is correct");
+                    test.equal(arguments[2], 0,
+                        "Third argument is correct");
+                    test.equal(arguments[3], 1,
+                        "Fourth argument is correct");
+                    test.done();
+                }, null, result, "string", 123);
+            }
+
         ]
 
     });
