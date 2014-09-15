acda80cc9fd215ddf9d13c5ebcb8693c9129b3d3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-09-15 13:58:16 -0400

    Inserted UNICODE equivalent chars

--- string.js
+++ string.js
@@ -25,11 +25,11 @@
                 "&": "&amp;",
                 "<": "&lt;",
                 ">": "&gt;",
-                "é": "&eacute;",
-                "è": "&egrave;",
-                "ê": "&ecirc;",
-                "á": "&aacute;",
-                "à": "&agrave;"
+                "\u00E9": "&eacute;",
+                "\u00E8": "&egrave;",
+                "\u00EA": "&ecirc;",
+                "\u00E1": "&aacute;",
+                "\u00E0": "&agrave;"
             }
 
         },
