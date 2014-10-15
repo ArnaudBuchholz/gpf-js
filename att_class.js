a6287a4017444d98de996c89f2ccf39a106f256c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-10-14 23:25:38 -0400

    Documentation

--- att_class.js
+++ att_class.js
@@ -4,20 +4,36 @@
 /*#endif*/
 
     var
-        /*
-         * 2013-12-15 ABZ
-         *  Decided to make it as simple as possible
+        /**
+         * Generates a read-only property accessor
+         *
+         * @param {String} member
+         * @return {Function}
+         * @closure
          */
         _roProperty = function (member) {
             return gpf._func("return this." + member + ";");
         },
 
+        /**
+         * Generates a property accessor
+         *
+         * @param {String} member
+         * @return {Function}
+         * @closure
+         */
         _rwProperty = function (member) {
             return gpf._func("var r = this." + member
                 + "; if (0 < arguments.length) { this." + member
                 + " = arguments[0]; } return r;");
         },
 
+        /**
+         * Base class for class-specific attributes
+         *
+         * @class gpf.attributes.ClassAttribute
+         * @extends gpf.attributes.Attribute
+         */
         _base = gpf._defAttr("ClassAttribute");
 
     /**
@@ -33,45 +49,83 @@
      */
     gpf._defAttr("$ClassProperty", _base, {
 
-        _writeAllowed: false,
-        _publicName: undefined,
-        _visibility: undefined,
+        private: {
 
-        /**
-         * @param {Boolean} writeAllowed
-         * @param {String} [publicName=undefined] publicName When not specified,
-         * the member name (without _) is applied
-         * @param {String} [visibility=undefined] visibility When not specified,
-         * public is used
-         * @constructor
-         */
-        constructor: function (writeAllowed, publicName, visibility) {
-            if (writeAllowed) {
-                this._writeAllowed = true;
-            }
-            if ("string" === typeof publicName) {
-                this._publicName = publicName;
-            }
-            if ("string" === typeof visibility) {
-                this._visibility = visibility;
-            }
+            /**
+             * If true, generates a write wrapper
+             *
+             * @type {Boolean}
+             * @private
+             */
+            _writeAllowed: false,
+
+            /**
+             * If set, provides the member name. Otherwise, name is based on
+             * member.
+             *
+             * @type {String|undefined}
+             * @private
+             */
+            _publicName: undefined,
+
+            /**
+             * If set, provides the member visibility.
+             * Default is 'public'
+             * member.
+             *
+             * @type {String|undefined}
+             * @private
+             */
+            _visibility: undefined
+
         },
 
-        _alterPrototype: function (objPrototype) {
-            var
-                member = this._member,
-                publicName = this._publicName,
-                classDef = gpf.classDef(objPrototype.constructor),
-                accessor;
-            if (!publicName) {
-                publicName = member.substr(1); // Considering it starts with _
+        protected: {
+
+            /**
+             * @inheritdoc gpf.attributes.Attribute:_alterPrototype
+             */
+            _alterPrototype: function (objPrototype) {
+                var
+                    member = this._member,
+                    publicName = this._publicName,
+                    classDef = gpf.classDef(objPrototype.constructor),
+                    accessor;
+                if (!publicName) {
+                    publicName = member.substr(1); // starts with _
+                }
+                if (this._writeAllowed) {
+                    accessor = _rwProperty(member);
+                } else {
+                    accessor = _roProperty(member);
+                }
+                classDef.addMember(publicName, accessor, this._visibility);
             }
-            if (this._writeAllowed) {
-                accessor = _rwProperty(member);
-            } else {
-                accessor = _roProperty(member);
+
+        },
+
+        public: {
+
+            /**
+             * @param {Boolean} writeAllowed
+             * @param {String} [publicName=undefined] publicName When not
+             * specified, the member name (without _) is applied
+             * @param {String} [visibility=undefined] visibility When not
+             * specified, public is used
+             * @constructor
+             */
+            constructor: function (writeAllowed, publicName, visibility) {
+                if (writeAllowed) {
+                    this._writeAllowed = true;
+                }
+                if ("string" === typeof publicName) {
+                    this._publicName = publicName;
+                }
+                if ("string" === typeof visibility) {
+                    this._visibility = visibility;
+                }
             }
-            classDef.addMember(publicName, accessor, this._visibility);
+
         }
 
     });
@@ -79,11 +133,11 @@
     /**
      * Used to flag a method which owns a last parameter being an event handler
      *
-     * @class gpf.attributes.ClassEventMethodAttribute
+     * @class gpf.attributes.ClassEventHandlerAttribute
      * @extends gpf.attributes.ClassAttribute
-     * @alias gpf.$ClassEventMethod
+     * @alias gpf.$ClassEventHandler
      */
-    gpf._defAttr("$ClassEventMethod", _base, {});
+    gpf._defAttr("$ClassEventHandler", _base, {});
 
     /**
      * Defines a class extension (internal)
@@ -98,16 +152,41 @@
      */
     gpf._defAttr("$ClassExtension", _base, {
 
-//        "[Class]": [gpf.$Alias("ClassExtension")],
+        private: {
 
-        _ofClass: 0,
-        _publicName: "",
+            /**
+             * Constructor of the class to extend
+             *
+             * @type {Function}
+             * @private
+             */
+            _ofClass: 0,
 
-        constructor: function (ofClass, publicName) {
-            this._ofClass = ofClass;
-            if ("string" === typeof publicName) {
-                this._publicName = publicName;
+            /**
+             * Name of the method if added to the class
+             *
+             * @type {String}
+             * @private
+             */
+            _publicName: ""
+
+        },
+
+        public: {
+
+            /**
+             * @param {Function} ofClass Constructor of the class to extend
+             * @param {String} publicName Name of the method if added to the
+             * class
+             * @constructor
+             */
+            constructor: function (ofClass, publicName) {
+                this._ofClass = ofClass;
+                if ("string" === typeof publicName) {
+                    this._publicName = publicName;
+                }
             }
+
         }
 
     });
