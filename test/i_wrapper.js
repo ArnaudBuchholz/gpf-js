5fe33bb3ddf97af839168c26a28d47d76beeb668
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-10-21 00:35:28 -0400

    Test case for Interface Wrapper

---
+++ test/i_wrapper.js
@@ -0,0 +1,74 @@
+(function () { /* Begin of privacy scope */
+    "use strict";
+
+    var
+        IExample = gpf.define("IExample", gpf.interfaces.Interface, {
+
+            "[test1]": [gpf.$ClassEventHandler()],
+            test1: function (param1, eventsHandler) {
+                gpf.interfaces.ignoreParameter(param1);
+                gpf.interfaces.ignoreParameter(eventsHandler);
+            },
+
+            test2: function (param1) {
+                gpf.interfaces.ignoreParameter(param1);
+            }
+
+        }),
+
+        Example = gpf.define("Example", {
+
+            "[Class]": [gpf.$InterfaceImplement(IExample)],
+
+            private: {
+
+                _sequences: []
+
+            },
+
+            public: {
+
+                constructor: function () {
+                    this._sequences = [];
+                },
+
+                test1: function (param1, eventsHandler) {
+                    this._sequences.push(param1);
+                    gpf.events.fire.apply(this, ["TEST1", {
+                        param1: param1,
+                        param2: "2"
+                    }, eventsHandler]);
+                },
+
+                test2: function (param1) {
+                    this._sequences.push(param1);
+                }
+
+            }
+
+        });
+
+
+    gpf.declareTests({
+
+        "wrapper": [
+
+            function (test) {
+                var WExample = new gpf.interfaces.Wrapper(IExample),
+                    example = new Example(),
+                    wrapped = new WExample(example);
+                test.wait();
+                wrapped
+                    .test1("a")
+                    .test2("b")
+                    .test1("c")
+                    ._then(function (event) {
+                        test.done();
+                    });
+
+            }
+        ]
+
+    });
+
+})(); /* End of privacy scope */
