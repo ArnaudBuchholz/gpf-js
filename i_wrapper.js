53d580dbe04956148d903004185da478162c83da
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-10-22 13:44:33 -0400

    WIP

--- i_wrapper.js
+++ i_wrapper.js
@@ -6,31 +6,58 @@
     var
         _wrappers = {},
 
+        /**
+         * Build the synchronous member (no event)
+         *
+         * @param {String} name
+         * @return {Function}
+         * @closure
+         * @private
+         */
         _sync = function (name) {
             return function () {
                 return this._sync(name, arguments);
             };
         },
 
-        _async = function (name) {
+        /**
+         * Build the asynchronous member (i.e. event related)
+         *
+         * @param {String} name
+         * @param {Number} length argument count in the function (used to deduce
+         * where the event handler is)
+         * @return {Function}
+         * @closure
+         * @private
+         */
+        _async = function (name, length) {
             return function () {
-                return this._async(name, arguments);
+                return this._async(name, length, arguments);
             };
         },
 
-
         _buildMembers = function (interfaceDef) {
-            var result = {},
+            var
+                result = {
+                    public: {},
+                    static: {
+                        interface: interfaceDef
+                    }
+                },
+                publicMembers = result.public,
                 attributes = new gpf.attributes.Map(interfaceDef),
                 prototype = interfaceDef.prototype,
-                member;
+                member,
+                method;
             for (member in prototype) {
                 if (prototype.hasOwnProperty(member)) {
+                    method = prototype[member];
+                    gpf.ASSERT('function' === typeof method, "Only methods");
                     if (attributes.member(member)
                         .has(gpf.attributes.ClassEventHandlerAttribute)) {
-                        result[member] = _async(member);
+                        publicMembers[member] = _async(member, method.length);
                     } else {
-                        result[member] = _sync(member);
+                        publicMembers[member] = _sync(member);
                     }
                 }
             }
@@ -42,21 +69,70 @@
         private: {
 
             /**
-             * Interface instance
+             * Interface handler
              *
              * @type {gpf.interfaces.Interface}
              * @private
              */
-            _instance: null,
+            _iHandler: null,
 
-            _sync: function (name, args) {
-                var instance = this._instance;
-                instance[name].apply(instance, args);
+            /**
+             * Callback to be used as event handler
+             *
+             * @type {gpf.Callback}
+             * @private
+             */
+            _callback: null,
+
+            /**
+             * Event handler
+             *
+             * @param {gpf.Event} event
+             * @private
+             */
+            _asyncResult: function (event) {
+
+            },
+
+            /**
+             * Call a synchronous method
+             *
+             * @param {String} name
+             * @param {Array} incomingArguments
+             * @return {WrapInterface}
+             * @private
+             */
+            _sync: function (name, incomingArguments) {
+                var
+                    iHandler = this._iHandler;
+                iHandler[name].apply(iHandler, incomingArguments);
                 return this;
             },
 
-            _async: function (name, args) {
-
+            /**
+             * Call an asynchronous method
+             *
+             * @param {String} name
+             * @param {Number} length
+             * @param {Array} incomingArguments
+             * @return {WrapInterface}
+             * @private
+             */
+            _async: function (name, length, incomingArguments) {
+                var
+                    iHandler = this._iHandler,
+                    finalArguments = new Array(length),
+                    count = length - 1,
+                    idx;
+                if (count > incomingArguments.length) {
+                    count = incomingArguments.length - 1;
+                }
+                for (idx = 0; idx < count; ++idx) {
+                    finalArguments[idx] = incomingArguments[idx];
+                }
+                finalArguments[length - 1] = this._callback;
+                iHandler[name].apply(iHandler, finalArguments);
+                return this;
             }
 
         },
@@ -64,13 +140,13 @@
         public: {
 
             /**
-             * Wrap an interface
-             *
-             * @param {gpf.interfaces.Interface} instance
+             * @param {Object} instance
              * @constructor
              */
             constructor: function (instance) {
-                this._instance = instance;
+                this._iHandler = gpf.interfaces.query(instance,
+                    this.constructor.interface);
+                this._callback = new gpf.Callback(this._callback, this);
             }
 
         }
