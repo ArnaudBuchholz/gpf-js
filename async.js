c476f108f083df62158d3068f30dcf8cb777ef71
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-13 00:12:33 -0500

    Leverage browser setTimeout

--- async.js
+++ async.js
@@ -5,15 +5,19 @@
 
     var
         /**
-         * Defer the execution of the callback
+         * Creates a function that wraps the callback, the scope and its
+         * arguments
          *
-         * @param {Function} callback
+         * @param {Function|String} callback
          * @param {Object} [scope=null] scope
          * @param {Array} [args=[]] args
          * @return {Function}
          * @closure
          */
         _callback = function (callback, scope, args) {
+            if ("string" === typeof callback) {
+                callback = gpf._func(callback);
+            }
             if (!scope) {
                 scope = null;
             }
@@ -27,7 +31,54 @@
 
         _sortOnDt;
 
-    if ("wscript" === gpf.host()) {
+    /**
+     * Run the asynchronous queue (mandatory for some environments)
+     */
+    gpf.runAsyncQueue = gpf._func();
+
+/*#ifdef(DEBUG)*/
+    /**
+     * Defer the execution of the callback
+     *
+     * @param {Function} callback
+     * @param {Number} [timeout=0] timeout
+     * @param {Object} [scope=null] scope
+     * @param {Array} [args=[]] args
+     */
+    gpf.defer = gpf._func();
+/*#endif*/
+
+    if ("browser" === gpf.host()) {
+        // Leverage the use of setTimeout(func, delay, [param1, param2, ...])
+        // as it avoids creating closures
+
+        _callback = function (parameters) {
+            parameters[0].apply(gpf.Callback.resolveScope(parameters[1]),
+                parameters[2]);
+        };
+
+        gpf.defer = function (callback, timeout, scope, args) {
+            if ("string" === typeof callback) {
+                callback = gpf._func(callback);
+            }
+            if (!timeout) {
+                timeout = 0;
+            }
+            setTimeout(_callback, timeout, [callback, scope, args]);
+        };
+
+    } else if("undefined" !== typeof setTimeout) {
+        // Consider the use of setTimeout(func, delay)
+
+        gpf.defer = function (callback, timeout, scope, args) {
+            if (!timeout) {
+                timeout = 0;
+            }
+            setTimeout(_callback(callback, scope, args), timeout);
+        };
+
+    } else {
+        // Custom mechanism
 
         gpf._asyncQueue = [];
 
@@ -58,29 +109,6 @@
             }
         };
 
-    } else {
-
-        /**
-         * Defer the execution of the callback
-         *
-         * @param {Function} callback
-         * @param {Number} [timeout=0] timeout
-         * @param {Object} [scope=null] scope
-         * @param {Array} [args=[]] args
-         */
-        gpf.defer = function (callback, timeout, scope, args) {
-            if (!timeout) {
-                timeout = 0;
-            }
-            setTimeout(_callback(callback, scope, args), timeout);
-        };
-
-        /**
-         * Run the asynchronous queue (mandatory for some environments)
-         */
-        gpf.runAsyncQueue = function () {
-            // Nothing to do
-        };
     }
 
 /*#ifndef(UMD)*/
