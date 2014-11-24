d0381599d826e328546ac794ec3884b8d84d2d03
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-24 13:52:04 -0500

    Lite web server (for testing purposes)

---
+++ websvr.js
@@ -0,0 +1,39 @@
+/*#ifndef(UMD)*/
+(function () { /* Begin of privacy scope */
+    "use strict";
+/*#endif*/
+
+    /**
+     * Run the GPF web server
+     *
+     * @param {String[]|Object|undefined} options
+     */
+    gpf.runWebServer = function (options) {
+        console.log("GPF " + gpf.version() + " web server");
+
+    };
+
+    /**
+     * Detect if the library was run as a standalone file, in that case, run
+     * the web server.
+     */
+    (function () {
+        var
+            path = require("path"),
+            scriptName;
+        if ("nodejs" !== gpf.host()) {
+            return; // Not inside nodejs
+        }
+        scriptName = path.basename(process.argv[1], ".js");
+        if (scriptName === "boot"
+            || scriptName === "gpf"
+            || scriptName === "gpf-debug") {
+            gpf.runWebServer(process.argv.slice(2));
+        } else {
+            console.log(scriptName);
+        }
+    }());
+
+/*#ifndef(UMD)*/
+}()); /* End of privacy scope */
+/*#endif*/
