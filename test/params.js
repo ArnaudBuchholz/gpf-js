fa95b381d3936f9ad05251b744a5fc7ace9d0c04
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-25 13:04:55 -0500

    Started a test case

---
+++ test/params.js
@@ -0,0 +1,30 @@
+(function () { /* Begin of privacy scope */
+    "use strict";
+
+    gpf.declareTests({
+
+        create: [
+
+            function (test) {
+                test.title("Simple creation");
+                var params = gpf.Parameter.create([{
+                        name: "StringParameter",
+                        description: "This is a string parameter"
+                    }, {
+
+                    }]),
+                    param;
+                test.equal(params.length, 1, "Correct number of parameters");
+                param = params[0];
+                test.log("First parameter");
+                test.equal(param.name(), "StringParameter", "Name");
+                test.equal(param.description(), "This is a string parameter",
+                    "Description");
+                test.equal(param.type(), gpf.Parameter.TYPE_STRING, "Type");
+            }
+
+        ]
+
+    });
+
+})(); /* End of privacy scope */
