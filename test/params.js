2432abd5bf4725ed9e3d8ad1d04b08b9b56c6fbd
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-27 23:26:35 -0500

    Added a boolean parameter

--- test/params.js
+++ test/params.js
@@ -10,17 +10,26 @@
                 var params = gpf.Parameter.create([{
                         name: "StringParameter",
                         description: "This is a string parameter"
-                    //}, {
-                    //
+                    }, {
+                        name: "BooleanParameter",
+                        description: "This is a boolean parameter",
+                        type: "boolean"
                     }]),
                     param;
-                test.equal(params.length, 1, "Correct number of parameters");
+                test.equal(params.length, 2, "Correct number of parameters");
                 param = params[0];
                 test.log("First parameter");
                 test.equal(param.name(), "StringParameter", "Name");
                 test.equal(param.description(), "This is a string parameter",
                     "Description");
                 test.equal(param.type(), gpf.Parameter.TYPE_STRING, "Type");
+                param = params[1];
+                test.log("Second parameter");
+                test.equal(param.name(), "BooleanParameter", "Name");
+                test.equal(param.description(), "This is a boolean parameter",
+                    "Description");
+                test.equal(param.type(), gpf.Parameter.TYPE_BOOLEAN, "Type");
+                test.equal(param.defaultValue(), false, "Default value");
             }
 
         ]
