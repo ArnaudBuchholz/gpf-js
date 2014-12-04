db479d90079b98920f41c90228ccc5aa5ff2e796
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-12-04 12:42:37 -0500

    FIX #28 Support of PhantomJS

--- make/UMD.js
+++ make/UMD.js
@@ -3,12 +3,18 @@
 (function (root, factory) {
     "use strict";
 
-    // Universal Module Definition (UMD) to support AMD, CommonJS/Node.js,
-    // Rhino, and plain browser loading.
+    /**
+     * Universal Module Definition (UMD) to support AMD, CommonJS/Node.js,
+     * Rhino, and plain browser loading.
+     *
+     * 2014-12-04 ABZ Extended for PhantomJS
+     */
     if (typeof define === "function" && define.amd) {
         define(["exports"], factory);
     } else if (typeof exports !== "undefined") {
         factory(exports);
+    } else if (typeof module !== "undefined" && module.exports) {
+        factory(module.exports);
     } else {
         factory((root.gpf = {}));
     }
