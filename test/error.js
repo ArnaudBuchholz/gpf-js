3a1f4fd89a74f3f0ddbf1885bd193aab8051bf76
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-20 12:57:10 -0500

    Simple test cases

---
+++ test/error.js
@@ -0,0 +1,42 @@
+(function () { /* Begin of privacy scope */
+    "use strict";
+
+    gpf.declareTests({
+
+        "documentation": [
+
+            function (test) {
+                test.title("Checking error content");
+                try {
+                    gpf.Error.Abstract();
+                } catch (e) {
+                    test.assert(e instanceof gpf.Error, "Got a gpf.Error");
+                    test.equal(e.code, gpf.Error.CODE_ABSTRACT,
+                        "Error code is mapped on a constant");
+                    test.equal(e.name, "Abstract", "Error name");
+                    test.equal(e.message, "Abstract", "Error message");
+                }
+            },
+
+            function (test) {
+                test.title("Checking parameter use in message");
+                try {
+                    gpf.Error.InterfaceExpected({
+                        name: "Test"
+                    });
+                } catch (e) {
+                    test.assert(e instanceof gpf.Error, "Got a gpf.Error");
+                    test.equal(e.code, gpf.Error.CODE_INTERFACEEXPECTED,
+                        "Error code is mapped on a constant");
+                    test.equal(e.name, "InterfaceExpected", "Error name");
+                    test.equal(e.message,
+                        "Expected interface not implemented: Test",
+                        "Error message");
+                }
+            }
+
+        ]
+
+    });
+
+})(); /* End of privacy scope */
