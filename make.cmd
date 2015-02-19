617d6bbe972ecc145b1a9cfa1743a72dd92b0a10
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-02-19 13:07:20 -0500

    Make now includes Plato statistics

--- make.cmd
+++ make.cmd
@@ -5,5 +5,5 @@ cd ..
 if not exist ..\ArnaudBuchholz.github.io goto end
 copy build\gpf.js ..\ArnaudBuchholz.github.io\
 copy build\gpf-debug.js ..\ArnaudBuchholz.github.io\
-plato -d ..\ArnaudBuchholz.github.io\plato\gpf-js -t GPF-JS -l .jshintrc *.js
+plato -x coding_convention.js -d ..\ArnaudBuchholz.github.io\plato\gpf-js -t GPF-JS -l .jshintrc *.js
 :end
