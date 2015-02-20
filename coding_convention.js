187f8520e912036b4a5abff12b6192ddbc2f9d63
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-02-20 13:57:00 -0500

    Refactoring sources
    To be able to share private variables, lower code complexity (and
    increase maintanability)

--- coding_convention.js
+++ coding_convention.js
@@ -1,66 +1,59 @@
-/*jshint ignore: start*/ // No need to validate
-
 /*#ifndef(UMD)*/
-(function () { /* Begin of privacy scope */
-    "use strict";
+"use strict";
 /*#endif*/
 
-    /*                                                                         |
-     * First of all, apply JSlint with:                                        |
-     * - Tolerate continue                                                     |
-     * - Tolerate dangling _ in identifiers                                    |
-     * - Tolerate ++ and --                                                    |
-     *                                                                         |
-     * Tabs must be replaced with 4 spaces                                     |
-     * *NEVER* go over column 80                                               |
-     *                                                                         |
-     * try to lower the use of closures                                        |
-     */
-    function actionWithAdditionalKeywords (firstParameter, secondParameter) {
-        var
-            firstVariable,
-            secondVariable,
-            // Initialized variables must be the last ones
-            // Try to avoid function calls in there
-            thirdVariable = 0; // Initialized
-    }
+/* Global variables are shared within GPF sources so they might conflict */
+var
+    _gpfVariableName;
 
-    /**
-     * Any function triggering events
-     * 
-     * @param {*} param1
-     * @param {Object/function} eventsHandler
-     * @return {undefined}
-     *
-     * @event sample This is the sample event
-     * @eventParam {any} ctx1 the first event parameter
-     */
-    function triggerEvent(param1, eventsHandler) {
-        gpf.events.fire.apply(this, ['sample', {ctx1: param1}, eventsHandler]);
-    }
 
-    namspace.newClass = gpf.define("newClass", {
-
-        /*
-         * Modifier can be: abstract, const
-         */
-        method: function /*modifier*/ () {
-
-        }
-
-    });
-
-/*
- * gpf          base functions & helpers
- *   event      event model
- *   http       HTTP related
- *   bin        BINARY related
- *   xml        XML related
- *   js         JavaScript language related
- *   Class      the base class of gpf classes
- *   attributes ATTRIBUTES related
+/*                                                                             |
+ * First of all, apply JSHint with:                                            |
+ * - Tolerate continue                                                         |
+ * - Tolerate dangling _ in identifiers                                        |
+ * - Tolerate ++ and --                                                        |
+ *                                                                             |
+ * Tabs must be replaced with 4 spaces                                         |
+ * *NEVER* go over column 80                                                   |
+ *                                                                             |
+ * try to limit the use of closures                                            |
  */
+gpf.actionWithAdditionalKeywords = function (firstParameter, secondParameter) {
+    var
+        firstVariable,
+        secondVariable,
+        // Initialized variables must be the last ones
+        // Try to avoid function calls in there
+        thirdVariable = 0; // Initialized
+    gpf.interfaces.ignoreParameter(firstParameter);
+    gpf.interfaces.ignoreParameter(secondParameter);
+    gpf.interfaces.ignoreParameter(firstVariable);
+    gpf.interfaces.ignoreParameter(secondVariable);
+    gpf.interfaces.ignoreParameter(thirdVariable);
+    _gpfVariableName = 0;
+};
 
-/*#ifndef(UMD)*/
-}()); /* End of privacy scope */
-/*#endif*/
+/**
+ * Any function triggering events
+ *
+ * @param {*} param1
+ * @param {Object/function} eventsHandler
+ * @return {undefined}
+ *
+ * @event sample This is the sample event
+ * @eventParam {any} ctx1 the first event parameter
+ */
+gpf.AnyClass.triggerEvent = function (param1, eventsHandler) {
+    gpf.events.fire.apply(this, ["sample", {ctx1: param1}, eventsHandler]);
+};
+
+gpf.define("newClass", {
+
+    /*
+     * Modifier can be: abstract, const
+     */
+    method: function /*modifier*/ () {
+
+    }
+
+});
