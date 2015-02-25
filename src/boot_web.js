409ec5cd0ce60ecc64b594e2b77ebc6905e8c14e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-02-25 00:20:41 -0500

    Rewrote with comments

--- src/boot_web.js
+++ src/boot_web.js
@@ -1,71 +1,78 @@
 /*jshint browser: true*/
 /*global gpfSourcesPath*/ // Global source path
+/*global _gpfRawHttpInclude*/ // Raw HTTP include
 /*global _gpfFinishLoading*/ // Ends the loading (declared in boot.js)
 (function () {
     "use strict";
 
     var
         sources,
+        length,
         idx = 0,
-        loadSources = function () {
-            var src;
-            if (idx < sources.length) {
-                src = sources[idx] + ".js";
-                if ("undefined" !== typeof gpfSourcesPath) {
-                    src = gpfSourcesPath + src;
-                }
-                gpf.http.include(src, {
-                    load: loadSources
-                });
+
+        /**
+         * Iterate on remaining sources using idx as the current index in the
+         * sources array and include them using gpf.http.include.
+         * The function is also the callback to handle the loaded event.
+         *
+         * @param {gpf.events.Event} event (unused)
+         */
+        loadSources = function (/*event*/) {
+            if (idx < length) {
+                gpf.http.include(gpfSourcesPath + sources[idx] + ".js",
+                    loadSources);
                 ++idx;
+
             } else {
+                // Trigger finish loading
                 _gpfFinishLoading();
             }
         },
-    // Ugly but fast way to insert sources
-        head = document.getElementsByTagName("head")[0]
-            || document.documentElement,
 
-        include = function (src) {
-            if ("undefined" !== typeof gpfSourcesPath) {
-                src = gpfSourcesPath + src;
-            }
-            var script = document.createElement("script");
-            script.language = "javascript";
-            script.src = src;
-            head.insertBefore(script, head.firstChild);
-        },
-    // Sequence of things to load (followed by test)
-        bootList = ["sources", "sources",
-            "compatibility", "setReadOnlyProperty",
-            "constants", "_constants",
-            "base", "each",
-            "events", "events",
-            "http", "http"],
+        /**
+         * Sequence of things to load (followed by the gpf member to test)
+         *
+         * @type {string[]}
+         */
+        bootList = [
+            "sources",          "sources",
+            "compatibility",    "setReadOnlyProperty",
+            "constants",        "_constants",
+            "base",             "each",
+            "events",           "events",
+            "http",             "http"
+        ],
+
+        /**
+         * Load a predefined static list of files and test if they are
+         * loaded before moving to the next one.
+         */
         boot = function () {
             if (0 === idx % 2) {
-                include(bootList[idx] + ".js");
+                _gpfRawHttpInclude(bootList[idx] + ".js");
                 ++idx;
-            } else {
-                if (undefined !== gpf[bootList[idx]]) {
-                    ++idx;
-                }
+            } else if (undefined !== gpf[bootList[idx]]) {
+                ++idx;
             }
             if (idx === bootList.length) {
                 /*
-                 * Now that include & sources are loaded,
-                 * load the missing sources
+                 * Now that all initial sources are loaded,
+                 * load the rest using gpf.http.include
                  */
                 sources = gpf.sources().split(",");
+                length = sources.length;
                 idx = gpf.test(sources, "http") + 1;
                 loadSources();
+
             } else {
                 /**
-                 * Use an aggressive setting but will be used only for
-                 * the source version
+                 * Use an aggressive setting as it will be used only for
+                 * the non UMD version
                  */
-                window.setTimeout(boot, 0);
+                setTimeout(boot, 0);
             }
         };
+
     boot();
+
 }());
