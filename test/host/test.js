fed3f460cd21f5d6deea4b08732c4688981032fb
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-03-03 00:24:32 -0500

    Tree structures with nodes

--- test/host/test.js
+++ test/host/test.js
@@ -2,14 +2,62 @@
     "use strict";
 
     var
-        _tests = {},
-        _currentTest = _tests;
+        /**
+         * Test node
+         *
+         * @constructor
+         * @private
+         */
+        _TestNode = function (label) {
+            this.label = label;
+            this.children = [];
+        },
 
-    context.describe = function (name, callback) {
-        var lastTest = _currentTest;
-        _currentTest = lastTest[name] = {};
+        /**
+         * Root test node
+         *
+         * @type {_TestNode}
+         * @private
+         */
+        _root = null,
+
+        /**
+         * Current test node
+         *
+         * @type {_TestNode}
+         * @private
+         */
+        _current;
+
+    // _TestNode prototype
+    _TestNode.prototype = {
+
+        /**
+         * Label of the test node
+         *
+         * @type {String}
+         * @read-only
+         */
+        label: "",
+
+        /**
+         * Children of the test node
+         *
+         * @type {_TestNode[]}
+         */
+        children: []
+    };
+
+    context.describe = function (label, callback) {
+        if (null === _root) {
+            _root = new _TestNode();
+            _current = _root;
+        }
+        var lastCurrent = _current;
+        _current = new _TestNode(label);
+        lastCurrent.children.push(_current);
         callback();
-        _currentTest = lastTest;
+        _current = lastCurrent;
     };
 
     context.it = function (label, callback) {
