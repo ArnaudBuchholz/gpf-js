caf268fc10a036812ff4e86772f9e8dfce6869a2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-03-04 14:05:56 -0500

    Just make sure to test it

--- test/callback.js
+++ test/callback.js
@@ -3,3 +3,15 @@
 
 describe("callback", function () {
 });
+
+/*
+function (test) {
+    test.title("Fire on a Callback");
+    var callback = new gpf.Callback(function (event) {
+        test.equal(event.type(), "test", "Event type");
+        test.done();
+    });
+    test.wait();
+    gpf.events.fire("test", callback);
+},
+*/
