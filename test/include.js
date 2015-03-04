fca84352f21adec24daa2e20f5d66c8aace8a075
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-03-04 16:25:41 -0500

    I may rename this feature

--- test/include.js
+++ test/include.js
@@ -1,5 +1,14 @@
 "use strict";
-/*global describe*/ // , it, assert*/
+/*global describe, it, assert*/
 
 describe("include", function () {
+
+    describe("gpf.http.include", function () {
+
+        it("allows injection of any script");
+        it("signals successful script injection");
+        it("signals failed script injection");
+
+    });
+
 });
