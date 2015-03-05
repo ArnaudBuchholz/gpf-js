509510787e370e758286c52a20a096f5891aad71
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-03-05 15:31:24 -0500

    'Simple' BDD implementation

--- test/host/bdd.js
+++ test/host/bdd.js
@@ -2,20 +2,30 @@
     "use strict";
 
     /**
-     * Simple test implementation
+     * Simple BDD implementation
      */
 
     var
         /**
-         * Test folder
+         * Abstract item
+         *
+         * @param {String} label
+         * @constructor
+         */
+        BDDAbstract = function (label) {
+            this.label = label;
+        },
+
+        /**
+         * Test description
          *
          * @constructor
          * @param {String} label
-         * @class TestFolder
-         * @private
+         * @class BDDDescribe
+         * @extends BDDAbstract
          */
-        TestFolder = function (label) {
-            this.label = label;
+        BDDDescribe = function (label) {
+            BDDAbstract.apply(this, [label]);
             this.children = [];
         },
 
@@ -25,88 +35,228 @@
          * @constructor
          * @param {String} label
          * @param {Function} callback
-         * @class TestCase
-         * @private
+         * @class BDDIt
+         * @extends BDDAbstract
          */
-        TestCase = function (label, callback) {
-            this.label = label;
+        BDDIt = function (label, callback) {
+            BDDAbstract.apply(this, [label]);
             this.callback = callback;
         };
 
-    TestFolder.prototype = {
+    BDDAbstract.prototype = {
 
         /**
-         * Label of the test folder
+         * Label of the item
          *
          * @type {String}
          * @read-only
          */
-        label: "",
+        label: ""
 
-        /**
-         * Children of the test folder
-         *
-         * @type {TestFolder[]}
-         * @read-only
-         */
-        children: []
     };
+
+    BDDDescribe.prototype = new BDDAbstract();
+    /**
+     * Children of the description
+     *
+     * @type {BDDDescribe[]}
+     * @read-only
+     */
+    BDDDescribe.prototype.children = [];
 
     /**
      * Root test folder
      *
-     * @type {TestFolder}
+     * @type {BDDDescribe}
      * @static
      */
-    TestFolder.root = null;
+    BDDDescribe.root = null;
 
     /**
      * Current test folder
      *
-     * @type {TestFolder}
+     * @type {BDDDescribe}
      * @static
      */
-    TestFolder.current = null;
+    BDDDescribe.current = null;
 
-    TestCase.prototype = {
+    BDDIt.prototype = new BDDAbstract();
+    /**
+     * Test case callback
+     *
+     * @type {Function}
+     * @read-only
+     */
+    BDDIt.prototype.callback = null;
 
-        /**
-         * Label of the test case
-         *
-         * @type {String}
-         * @read-only
-         */
-        label: "",
-
-        /**
-         * Test case callback
-         *
-         * @type {Function}
-         * @read-only
-         */
-        callback: null
-
-    };
+    //region BDD interface
 
     context.describe = function (label, callback) {
-        if (null === TestFolder.root) {
-            TestFolder.current
-                = TestFolder.root
-                = new TestFolder();
+        if (null === BDDDescribe.root) {
+            BDDDescribe.current
+                = BDDDescribe.root
+                = new BDDDescribe();
         }
-        var lastCurrent = TestFolder.current;
-        TestFolder.current = new TestFolder(label);
-        lastCurrent.children.push(TestFolder.current);
+        var lastCurrent = BDDDescribe.current;
+        BDDDescribe.current = new BDDDescribe(label);
+        lastCurrent.children.push(BDDDescribe.current);
         callback();
-        TestFolder.current = lastCurrent;
+        BDDDescribe.current = lastCurrent;
     };
 
     context.it = function (label, callback) {
-        TestFolder.current.children.push(new TestCase(label, callback));
+        BDDDescribe.current.children.push(new BDDIt(label, callback));
     };
 
-    context.run = function () {
-        console.log("OK");
+    //endregion
+
+    //region Running the tests
+
+    var
+
+        /**
+         * @type {Function}
+         * @private
+         */
+        _callback = null,
+
+        /**
+         * @type {BDDDescribe[]}
+         * @private
+         */
+        _stackOfDescribe,
+
+        /**
+         * @type {BDDDescribe}
+         * @private
+         */
+        _describe,
+
+        /**
+         * @type {Number[]}
+         * @private
+         */
+        _stackOfChildIdx,
+
+        /**
+         * @type {Number}
+         * @private
+         */
+        _childIdx,
+
+        /**
+         * @type {BDDIt}
+         * @private
+         */
+        _it,
+
+        /**
+         * Test statistics
+         *
+         * @type {Object}
+         * @private
+         */
+        _stats;
+
+    if (!context.assert) {
+        /**
+         * Fails by throwing an exception if the value is falsy
+         *
+         * @param {*} condition
+         */
+        context.assert = function (condition) {
+            if (!condition) {
+                throw {
+                    message: "ASSERTION failed"
+                };
+            }
+        };
+    }
+
+    function _next() {
+        var item;
+        if (_childIdx < _describe.children.length) {
+            item = _describe.children[_childIdx];
+            ++_childIdx;
+            if (item instanceof BDDDescribe) {
+                _callback("describe", {
+                    depth: _stackOfDescribe.length,
+                    label: item.label
+                });
+                _stackOfDescribe.push(_describe);
+                _stackOfChildIdx.push(_childIdx);
+                _describe = item;
+                _childIdx = 0;
+                _next();
+            } else if (item instanceof BDDIt) {
+                _it = item;
+                ++_stats.count;
+                if (item.callback) {
+                    try {
+                        item.callback(_success);
+                        if (0  === item.callback.length) {
+                            _success();
+                        }
+                    } catch (e) {
+                        _fail(e);
+                    }
+                } else {
+                    ++_stats.pending;
+                    _callback("it", {
+                        depth: _stackOfDescribe.length,
+                        label: _it.label,
+                        pending: true
+                    });
+                    _next();
+                }
+            }
+        } else if (0 < _stackOfDescribe.length) {
+            // No more children, go up
+            _describe = _stackOfDescribe.pop();
+            _childIdx = _stackOfChildIdx.pop();
+            _next();
+        } else {
+            // DONE!
+            _callback("results", _stats);
+        }
+    }
+
+    function _success() {
+        ++_stats.success;
+        _callback("it", {
+            depth: _stackOfDescribe.length,
+            label: _it.label,
+            result: true
+        });
+        _next();
+    }
+
+    function _fail(e) {
+        ++_stats.fail;
+        _callback("it", {
+            depth: _stackOfDescribe.length,
+            label: _it.label,
+            result: false,
+            exception: e
+        });
+        _next();
+    }
+
+    context.run = function (callback) {
+        _callback = callback;
+        _stackOfDescribe = [];
+        _describe = BDDDescribe.root;
+        _stackOfChildIdx = [];
+        _childIdx = 0;
+        _stats = {
+            count: 0,
+            success: 0,
+            fail: 0,
+            pending: 0
+        };
+        _next();
     };
+
+    //endregion
 
 }(this));
