0ce09bf44abb66e17a123059a3aab80dcbcb687c
Arnaud Buchholz <buchholz@free.fr>
2015-03-09 15:34:35 -0400

    Documentation

--- src/sources.js
+++ src/sources.js
@@ -8,6 +8,8 @@
             "events",               // Event management         gpf.events.Event
             "include",              // WEB include               gpf.web.include
 
+                                    // --- Minimum required for boot_web.js ---
+
             "base",                 // Basic functions
             "like",                 // Comparison helper                gpf.like
             "callback",             // Callback object              gpf.Callback
