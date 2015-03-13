41ba2b6c10f766a8efd97c821abf1dbc3e42af8e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-03-13 00:10:09 -0400

    Test case for dispatch

--- test/dispatch.js
+++ test/dispatch.js
@@ -33,35 +33,94 @@ describe("dispatch", function () {
 
         });
 
-        describe("after registering the event type", function () {
-
-            var
-                _done;
+        describe("after registering a listener", function () {
 
             function eventHandler(event) {
+                var done;
                 assert(event.type === "test");
                 assert(event.scope === dispatcher);
                 assert(event.get("param1") === "first");
                 assert(event.get("param2") === true);
                 assert(event.get("param3") === 0);
-                assert(_done);
-                _done();
+                done = event.get("done");
+                assert(done);
+                done();
+            }
+
+            function eventHandler2(event) {
+                var done;
+                assert(event.type === "test2");
+                assert(event.get("expected"));
+                assert(event.scope === dispatcher);
+                done = event.get("done");
+                assert(done);
+                done();
             }
 
             beforeEach(function () {
                 dispatcher.addEventListener("test", eventHandler);
             });
 
-            it("receives the correct event", function (done) {
-                _done = done;
+            it("receives the event", function (done) {
                 dispatcher._dispatchEvent("test", {
                     param1: "first",
                     param2: true,
-                    param3: 0
+                    param3: 0,
+                    done: done
                 });
             });
 
-        })
+            describe("and registering another one", function () {
+
+                beforeEach(function () {
+                    dispatcher.addEventListener("test2", eventHandler2);
+                });
+
+                it("still receives the initial event", function (done) {
+                    dispatcher._dispatchEvent("test", {
+                        param1: "first",
+                        param2: true,
+                        param3: 0,
+                        done: done
+                    });
+                });
+
+                it("receives the new event", function (done) {
+                    dispatcher._dispatchEvent("test2", {
+                        expected: true,
+                        done: done
+                    });
+                });
+
+                describe("to finally remove it", function () {
+
+                    beforeEach(function () {
+                        dispatcher.removeEventListener("test2", eventHandler2);
+                    });
+
+                    it("ignores the last event", function (done) {
+                        dispatcher._dispatchEvent("test2", {
+                            expected: false,
+                            done: done
+                        });
+                        // Use promise to know when the event is thrown
+                        done();
+                    });
+
+                    it("still receives the initial event", function (done) {
+                        dispatcher._dispatchEvent("test", {
+                            param1: "first",
+                            param2: true,
+                            param3: 0,
+                            done: done
+                        });
+                    });
+
+                });
+
+            });
+
+        });
 
     });
 
