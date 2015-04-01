ae42472a7158185de0c4805f62c803a08f071d0d
Arnaud Buchholz <buchholz@free.fr>
2015-04-01 12:34:58 -0400

    Additional CSV testing

--- test/csv.js
+++ test/csv.js
@@ -41,6 +41,30 @@ describe("csv", function () {
             assert("MN\r\nO" === records[4].VALUE);
         });
 
+        it("supports quoting the separator as well", function () {
+            var records = gpf.csv.parse([
+                "LINE;VALUE",
+                "0;\"A;BC\""
+            ].join("\r\n"));
+            assert(1 === records.length);
+            assert("0" === records[0].LINE);
+            assert("A;BC" === records[0].VALUE);
+        });
+
+        it("supports header being specified as an option", function () {
+            var records = gpf.csv.parse([
+                "0;ABC",
+                "1;DEF"
+            ].join("\r\n"), {
+                header: "LINE;VALUE"
+            });
+            assert(2 === records.length);
+            assert("0" === records[0].LINE);
+            assert("ABC" === records[0].VALUE);
+            assert("1" === records[1].LINE);
+            assert("DEF" === records[1].VALUE);
+        });
+
     });
 
 });
