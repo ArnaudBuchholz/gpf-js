ce4a5a29b5d0fa186d16133e622cf0ef50e63c71
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-04-09 23:58:40 -0400

    Rewrote for consistency

--- test/define.js
+++ test/define.js
@@ -3,89 +3,93 @@
 
 describe("define", function () {
 
-    var
-        A = gpf.define("A", {
+    describe("gpf.define", function () {
 
-            protected: {
+        var
+            A = gpf.define("A", {
 
-                _a: 0
+                protected: {
 
-            },
+                    _a: 0
 
-            public: {
-
-                constructor: function (value) {
-                    this._a = value;
                 },
 
-                a: function () {
-                    return this._a;
+                public: {
+
+                    constructor: function (value) {
+                        this._a = value;
+                    },
+
+                    a: function () {
+                        return this._a;
+                    }
+
                 }
 
-            }
+            }),
 
-        }),
+            B = gpf.define("B", A, {
 
-        B = gpf.define("B", A, {
+                private: {
 
-            private: {
+                    _b: 0
 
-                _b: 0
-
-            },
-
-            public: {
-
-                constructor: function (value) {
-                    this._b = value;
-                    this._super(value + 1);
                 },
 
-                b: function () {
-                    return this._a + this._b;
+                public: {
+
+                    constructor: function (value) {
+                        this._b = value;
+                        this._super(value + 1);
+                    },
+
+                    b: function () {
+                        return this._a + this._b;
+                    }
+
                 }
 
-            }
+            });
+
+        describe("validate definitions", function () {
+
+            it("generates constructor functions", function () {
+                assert(A instanceof Function);
+                assert(B instanceof Function);
+            });
 
         });
 
-    describe("Validate definitions", function () {
+        describe("basic inheritance tests", function () {
 
-        it("generates constructor functions", function () {
-            assert(A instanceof Function);
-            assert(B instanceof Function);
-        });
+            var a = new A(1),
+                b = new B(1);
 
-    });
+            it("handles instanceof", function () {
+                assert(a instanceof A);
+                assert(!(a instanceof B));
+                assert(b instanceof B);
+            });
 
-    describe("Basic inheritance tests", function () {
+            it("handles inherited instanceof", function () {
+                assert(b instanceof A);
+            });
 
-        var a = new A(1),
-            b = new B(1);
+            it("exposes method A::a and provides access to A::_a", function () {
+                assert(!a.hasOwnProperty("a"));
+                assert(a.a() === 1);
+            });
 
-        it("handles instanceof", function () {
-            assert(a instanceof A);
-            assert(!(a instanceof B));
-            assert(b instanceof B);
-        });
+            it("provides access to A::_a inside constructor of B", function () {
+                assert(!b.hasOwnProperty("a"));
+                assert(b.a() === 2);
+            });
 
-        it("handles inherited instanceof", function () {
-            assert(b instanceof A);
-        });
+            it("provides access to A::_a inside B::b", function () {
+                assert(!b.hasOwnProperty("b"));
+                assert(b.b() === 3);
+            });
 
-        it("exposes method A::a and provides access to A::_a", function () {
-            assert(!a.hasOwnProperty("a"));
-            assert(a.a() === 1);
-        });
-
-        it("provides access to A::_a inside constructor of B", function () {
-            assert(!b.hasOwnProperty("a"));
-            assert(b.a() === 2);
-        });
-
-        it("provides access to A::_a inside B::b", function () {
-            assert(!b.hasOwnProperty("b"));
-            assert(b.b() === 3);
         });
 
     });
