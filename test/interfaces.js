0da07141a1e13aea8db4ea30bc443517207064e7
Arnaud Buchholz <buchholz@free.fr>
2015-04-21 13:54:11 -0400

    Tests for interfaces

---
+++ test/interfaces.js
@@ -0,0 +1,24 @@
+"use strict";
+/*global describe, it, assert*/
+
+describe("interfaces", function () {
+
+  describe("gpf.interfaces.isImplementedBy", function () {
+
+    it("verifies that the object implements the current interface");
+
+  });
+
+  describe("gpf.interfaces.query", function () {
+
+    it("retrieves an object implementing the expected interface");
+
+  });
+
+  describe("$InterfaceImplement", function () {
+
+    it("")
+
+  });
+
+});
