f95208b8a91f7a3c643732e68c1226476262f871
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-04-21 00:03:07 -0400

    Mockable console compatible with all env

--- test/host/console.js
+++ test/host/console.js
@@ -6,45 +6,40 @@
      * expected. Relies on GPF.
      */
 
-    var realConsole = gpf.context().console,
+    var consoleMethods = {},
         member,
-        expected = [],
-        newConsole = {};
+        expected = [];
 
     function genMember(name) {
         return function (text) {
-            if (0 === expected.length
-                || expected[0] !== name
-                || expected[1] !== text) {
+            var
+                expectedName,
+                expectedText;
+            consoleMethods[name].apply(console, arguments);
+
+            if (0 !== expected.length) {
+                expectedName = expected.shift();
+                expectedText = expected.shift();
+            }
+            if (expectedName !== name
+                || expectedText !== text) {
                 throw {
                     message: "Unexpected use of console." + name
                 };
             }
-            expected.shift();
-            expected.shift();
-            realConsole[name].apply(realConsole, arguments);
         };
     }
 
-    for (member in realConsole) {
-        if (realConsole.hasOwnProperty(member)
-            || "function" === typeof realConsole[member]) {
-            newConsole[member] = genMember(member);
+    for (member in console) {
+        if (console.hasOwnProperty(member)
+            || "function" === typeof console[member]) {
+            consoleMethods[member] = console[member];
+            console[member] = genMember(member);
         }
     }
 
-    newConsole.expects = function (method, text) {
+    console.expects = function (method, text) {
         expected.push(method, text);
     };
 
-    module.exports = function (enable) {
-        if (enable) {
-            gpf.context().console = newConsole;
-        } else {
-            gpf.context().console = realConsole;
-        }
-    };
-
 }());
-
-
