73d253b479771e2809d5b05be058264b7cd1426a
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-05-13 13:42:57 -0400

    Reformatting

--- test/a_attributes.js
+++ test/a_attributes.js
@@ -3,133 +3,133 @@
 
 describe("a_attributes", function () {
 
-  describe("gpf.$UniqueAttribute", function () {
+    describe("gpf.$UniqueAttribute", function () {
 
-    it("can't be used on non-attribute classes", function () {
-      var caught = null;
-      try {
-        gpf.define("TestClass", {
-          "[Class]": [gpf.$UniqueAttribute()]
-        });
-      } catch (e) {
-        caught = e;
-      }
-      assert(null !== caught);
-      assert(caught.name === "OnlyForAttributeClass");
-    });
-
-    describe("(true) for the whole class", function () {
-
-      it("should allow at least one instance", function () {
-        var caught = null;
-        try {
-          gpf.define("TestAttribute", AttributeClass, {
-            "[Class]": [gpf.$UniqueAttribute(true)]
-          });
-        } catch (e) {
-          caught = e;
-        }
-        assert(null === caught);
-      });
-
-      var
-        AttributeClass = gpf.attributes.Attribute,
-        TestAttribute = gpf.define("TestAttribute", AttributeClass, {
-          "[Class]": [gpf.$UniqueAttribute(true)]
-        }),
-        TestClass = gpf.define("TestClass", {
-          "[Class]": [new TestAttribute()]
+        it("can't be used on non-attribute classes", function () {
+            var caught = null;
+            try {
+                gpf.define("TestClass", {
+                    "[Class]": [gpf.$UniqueAttribute()]
+                });
+            } catch (e) {
+                caught = e;
+            }
+            assert(null !== caught);
+            assert(caught.name === "OnlyForAttributeClass");
         });
 
-      it("prevents defining the attribute twice (define)", function () {
-        var caught = null;
-        try {
-          gpf.define("TestClass2", TestClass, {
-            "[Class]": [new TestAttribute()]
-          });
-        } catch (e) {
-          caught = e;
-        }
-        assert(null !== caught);
-        assert(caught.name === "UniqueAttributeConstraint");
-      });
+        describe("(true) for the whole class", function () {
 
-      it("prevents defining the attribute twice (add)", function () {
-        var caught = null;
-        try {
-          gpf.attributes.add(TestClass, "Class", new TestAttribute());
-        } catch (e) {
-          caught = e;
-        }
-        assert(null !== caught);
-        assert(caught.name === "UniqueAttributeConstraint");
-      });
+            it("should allow at least one instance", function () {
+                var caught = null;
+                try {
+                    gpf.define("TestAttribute", AttributeClass, {
+                        "[Class]": [gpf.$UniqueAttribute(true)]
+                    });
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null === caught);
+            });
 
-    });
+            var
+                AttributeClass = gpf.attributes.Attribute,
+                TestAttribute = gpf.define("TestAttribute", AttributeClass, {
+                    "[Class]": [gpf.$UniqueAttribute(true)]
+                }),
+                TestClass = gpf.define("TestClass", {
+                    "[Class]": [new TestAttribute()]
+                });
 
-    describe("(false) for members", function () {
+            it("prevents defining the attribute twice (define)", function () {
+                var caught = null;
+                try {
+                    gpf.define("TestClass2", TestClass, {
+                        "[Class]": [new TestAttribute()]
+                    });
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null !== caught);
+                assert(caught.name === "UniqueAttributeConstraint");
+            });
 
-      it("should allow at least one instance", function () {
-        var caught = null;
-        try {
-          gpf.define("TestAttribute", AttributeClass, {
-            "[Class]": [gpf.$UniqueAttribute(false)]
-          });
-        } catch (e) {
-          caught = e;
-        }
-        assert(null === caught);
-      });
+            it("prevents defining the attribute twice (add)", function () {
+                var caught = null;
+                try {
+                    gpf.attributes.add(TestClass, "Class", new TestAttribute());
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null !== caught);
+                assert(caught.name === "UniqueAttributeConstraint");
+            });
 
-      var
-        AttributeClass = gpf.attributes.Attribute,
-        TestAttribute = gpf.define("TestAttribute", AttributeClass, {
-          "[Class]": [gpf.$UniqueAttribute(false)]
-        }),
-        TestClass = gpf.define("TestClass", {
-          "[a]": [new TestAttribute()],
-          a: 0
         });
 
-      it("prevents defining the attribute twice (define)", function () {
-        var caught = null;
-        try {
-          gpf.define("TestClass2", TestClass, {
-            "[a]": [new TestAttribute()]
-          });
-        } catch (e) {
-          caught = e;
-        }
-        assert(null !== caught);
-        assert(caught.name === "UniqueMemberAttributeConstraint");
-      });
+        describe("(false) for members", function () {
 
-      it("prevents defining the attribute twice (add)", function () {
-        var caught = null;
-        try {
-          gpf.attributes.add(TestClass, "a", new TestAttribute());
-        } catch (e) {
-          caught = e;
-        }
-        assert(null !== caught);
-        assert(caught.name === "UniqueMemberAttributeConstraint");
-      });
+            it("should allow at least one instance", function () {
+                var caught = null;
+                try {
+                    gpf.define("TestAttribute", AttributeClass, {
+                        "[Class]": [gpf.$UniqueAttribute(false)]
+                    });
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null === caught);
+            });
 
-      it("allows use on different members", function () {
-        var caught = null;
-        try {
-          gpf.define("TestClass2", TestClass, {
-            "[b]": [new TestAttribute()],
-            b: 0
-          });
-        } catch (e) {
-          caught = e;
-        }
-        assert(null === caught);
-      });
+            var
+                AttributeClass = gpf.attributes.Attribute,
+                TestAttribute = gpf.define("TestAttribute", AttributeClass, {
+                    "[Class]": [gpf.$UniqueAttribute(false)]
+                }),
+                TestClass = gpf.define("TestClass", {
+                    "[a]": [new TestAttribute()],
+                    a: 0
+                });
+
+            it("prevents defining the attribute twice (define)", function () {
+                var caught = null;
+                try {
+                    gpf.define("TestClass2", TestClass, {
+                        "[a]": [new TestAttribute()]
+                    });
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null !== caught);
+                assert(caught.name === "UniqueMemberAttributeConstraint");
+            });
+
+            it("prevents defining the attribute twice (add)", function () {
+                var caught = null;
+                try {
+                    gpf.attributes.add(TestClass, "a", new TestAttribute());
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null !== caught);
+                assert(caught.name === "UniqueMemberAttributeConstraint");
+            });
+
+            it("allows use on different members", function () {
+                var caught = null;
+                try {
+                    gpf.define("TestClass2", TestClass, {
+                        "[b]": [new TestAttribute()],
+                        b: 0
+                    });
+                } catch (e) {
+                    caught = e;
+                }
+                assert(null === caught);
+            });
+
+        });
 
     });
-
-  });
 
 });
