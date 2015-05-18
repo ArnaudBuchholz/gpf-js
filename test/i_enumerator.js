d01e64d9bcb797e81f7f9e17b9871ce8ead1a9d5
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-05-18 10:09:30 -0400

    Tested all cases

--- test/i_enumerator.js
+++ test/i_enumerator.js
@@ -4,6 +4,8 @@
 describe("i_enumerator", function () {
 
     var
+        gpfI = gpf.interfaces,
+        gpfE = gpf.events,
         ArrayEnumerable = gpf.define("ArrayEnumerable", {
 
             private: {
@@ -30,8 +32,7 @@ describe("i_enumerator", function () {
                 instance,
                 enumerator;
             instance = new ArrayEnumerable([1, 2, 3]);
-            enumerator = gpf.interfaces.query(instance,
-                gpf.interfaces.IEnumerator);
+            enumerator = gpfI.query(instance, gpfI.IEnumerator);
             assert(null !== enumerator);
             enumerator.reset();
             assert(true === enumerator.moveNext());
@@ -44,6 +45,117 @@ describe("i_enumerator", function () {
             enumerator.reset();
             assert(true === enumerator.moveNext());
             assert(1 === enumerator.current());
+        });
+
+    });
+
+    describe("IEnumerator.each", function () {
+
+        function getEnum (moveNext) {
+            return {
+                _current: -1,
+                reset: function () {
+                    assert(false); // Should not be called
+                },
+                moveNext: moveNext,
+                current: function () {
+                    return this._current;
+                }
+            };
+        }
+
+        function syncMoveNext (eventsHandler) {
+            /*jshint -W040*/
+            if (++this._current < 10) {
+                return true;
+            }
+            gpfE.fire(gpfE.EVENT_END_OF_DATA, eventsHandler);
+            return false;
+        }
+
+        function asyncMoveNext (eventsHandler) {
+            /*jshint -W040*/
+            if (++this._current < 10) {
+                gpfE.fire(gpfE.EVENT_DATA, eventsHandler);
+            } else {
+                gpfE.fire(gpfE.EVENT_END_OF_DATA, eventsHandler);
+            }
+            return false;
+        }
+
+        describe("synchronous callback", function () {
+
+            it("works with synchronous interface", function (done) {
+                var expected = 0;
+                gpfI.IEnumerator.each(getEnum(syncMoveNext),
+                    function (element) {
+                        assert(expected++ === element);
+                    }, function (event) {
+                        assert(gpfE.EVENT_END_OF_DATA === event.type);
+                        assert(10 === expected);
+                        done();
+                    });
+            });
+
+            it("works with asynchronous interface", function (done) {
+                var expected = 0;
+                gpfI.IEnumerator.each(getEnum(asyncMoveNext),
+                    function (element) {
+                        assert(expected++ === element);
+                    }, function (event) {
+                        assert(gpfE.EVENT_END_OF_DATA === event.type);
+                        assert(10 === expected);
+                        done();
+                    });
+            });
+
+        });
+
+        describe("asynchronous callback", function () {
+
+            it("works with synchronous interface", function (done) {
+                var expected = 0;
+                gpfI.IEnumerator.each(getEnum(syncMoveNext),
+                    function (element, eventsHandler) {
+                        assert(expected++ === element);
+                        gpfE.fire(gpfE.EVENT_CONTINUE, eventsHandler);
+                    }, function (event) {
+                        assert(gpfE.EVENT_END_OF_DATA === event.type);
+                        assert(10 === expected);
+                        done();
+                    });
+            });
+
+            it("works with asynchronous interface", function (done) {
+                var expected = 0;
+                gpfI.IEnumerator.each(getEnum(asyncMoveNext),
+                    function (element, eventsHandler) {
+                        assert(expected++ === element);
+                        gpfE.fire(gpfE.EVENT_CONTINUE, eventsHandler);
+                    }, function (event) {
+                        assert(gpfE.EVENT_END_OF_DATA === event.type);
+                        assert(10 === expected);
+                        done();
+                    });
+            });
+
+            it("allows stopping the process", function (done) {
+                var expected = 0;
+                gpfI.IEnumerator.each(getEnum(asyncMoveNext),
+                    function (element, eventsHandler) {
+                        assert(expected++ === element);
+                        if (5 === expected) {
+                            gpfE.fire(gpfE.EVENT_STOP, eventsHandler);
+                        } else {
+                            gpfE.fire(gpfE.EVENT_CONTINUE, eventsHandler);
+                        }
+                    }, function (event) {
+                        assert(gpfE.EVENT_STOPPED === event.type);
+                        assert(5 === expected);
+                        done();
+                    });
+            });
+
         });
 
     });
