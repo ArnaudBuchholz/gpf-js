00400ae2f5d4230049891eaee7d0538d70881265
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-05-19 23:02:07 -0400

    Created simple test case

--- test/i_array.js
+++ test/i_array.js
@@ -1,18 +1,41 @@
-(function () { /* Begin of privacy scope */
-    "use strict";
+"use strict";
+/*global describe, it, assert*/
 
-    // Nothing to test right now
+describe("i_array", function () {
 
-    //gpf.declareTests({
-    //
-    //    "": [
-    //
-    //        function (test) {
-    //            test.title("");
-    //        }
-    //
-    //    ]
-    //
-    //});
+    describe("$ClassIArray", function () {
 
-})(); /* End of privacy scope */
+        var
+            gpfI = gpf.interfaces,
+            A = gpf.define("A", {
+
+            private: {
+
+                "[_items]": [gpf.$ClassIArray(false)],
+                _items: []
+
+            },
+
+            public: {
+
+                constructor: function (items) {
+                    this._items = items;
+                }
+
+            }
+        });
+
+        it("exposes an array member to IReadOnlyArray interface", function () {
+            assert(gpfI.isImplementedBy(A, gpfI.IReadOnlyArray));
+            var a = new A([1, 2, 3]);
+            assert(gpfI.isImplementedBy(a, gpfI.IReadOnlyArray));
+            assert(3 === a.getItemsCount());
+            assert(1 === a.getItem(0));
+            assert(2 === a.getItem(1));
+            assert(3 === a.getItem(2));
+            assert(3 === a.getItem().length);
+        });
+
+    });
+
+});
