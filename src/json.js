a8d7b343f998a0b880d4f4d3c63f20dafa2dcadf
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-05-19 23:25:16 -0400

    JSON load/save

--- src/json.js
+++ src/json.js
@@ -2,6 +2,9 @@
 "use strict";
 /*global _gpfFunc*/ // Create a new function using the source
 /*global _gpfStringEscapeFor*/ // Make the string content compatible with lang
+/*global _gpfA*/ // _gpfA
+/*global _gpfANoSerial*/ // gpf.attributes.ClassNonSerializedAttribute
+/*global _gpfI*/ // gpf.interfaces
 // /*#endif*/
 
 var
@@ -78,10 +81,98 @@ gpf.json = {
     /**
      * The JSON.parse() method parses a string as JSON
      *
-     * @param {*} text Tthe string to parse as JSON
+     * @param {*} text The string to parse as JSON
      * @return {Object}
      */
-    parse: _gpfJsonParse
+    parse: _gpfJsonParse,
+
+    /**
+     * TODO there must be a way to make it less specific to JSON
+     * TODO handle object sub members
+     * TODO handle before/after load methods
+     * TODO handle before/after save methods
+     */
+
+    /**
+     * Load the object properties from the json representation.
+     *
+     * @param {Object} object
+     * @param {Object|string} json
+     * @return {Object}
+     * @chainable
+     */
+    load: function (object, json) {
+        var
+            prototype = object.constructor.prototype,
+            attributes = new _gpfA.Map(object),
+            member,
+            jsonMember;
+        /*jshint -W089*/ // Actually, I want all properties
+        for (member in prototype) {
+            if ("function" === typeof prototype[member]
+                || attributes.member(member).has(_gpfANoSerial)) {
+                continue; // Ignore functions & unwanted members
+            }
+            /*
+             * We have a member that must be serialized,
+             * by default members with a starting _ will be initialized from
+             * the corresponding member (without _) of the json object
+             */
+            if (0 === member.indexOf("_")) {
+                jsonMember = member.substr(1);
+            } else {
+                jsonMember = member;
+            }
+            if (jsonMember in json) {
+                object[member] = json[jsonMember];
+            } else {
+                // Reset the value coming from the prototype
+                object[member] = prototype[member];
+            }
+        }
+        return object;
+    },
+    /*jshint +W089*/
+
+    /**
+     * Save the object properties into a json representation.
+     *
+     * @param {Object} object
+     * @return {Object}
+     */
+    save: function (object) {
+        var
+            result = {},
+            prototype = object.constructor.prototype,
+            attributes = new _gpfA.Map(object),
+            member,
+            jsonMember,
+            value;
+        /*jshint -W089*/ // Actually, I want all properties
+        for (member in prototype) {
+            if ("function" === typeof prototype[member]
+                || attributes.member(member).has(_gpfANoSerial)) {
+                continue; // Ignore functions & unwanted members
+            }
+            /*
+             * We have a member that must be serialized,
+             * by default members with a starting _ will be initialized from
+             * the corresponding member (without _) of the json object
+             */
+            if (0 === member.indexOf("_")) {
+                jsonMember = member.substr(1);
+            } else {
+                jsonMember = member;
+            }
+            value = object[member];
+            if (value !== prototype[member]) {
+                result[jsonMember] = value;
+            }
+        }
+        return result;
+    }
+    /*jshint +W089*/
+
 };
 
 
