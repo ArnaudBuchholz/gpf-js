5bed5062d12191f75e648e471696a4e5d922d553
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-06-03 23:44:43 -0400

    Empty test for stream_node

---
+++ test/stream_node.js
@@ -0,0 +1,13 @@
+"use strict";
+/*global describe, it, assert*/
+
+describe("stream_node", function () {
+
+    /*jshint -W027*/ // because not relevant
+    return;
+
+    it("contains no test", function () {
+        assert(true);
+    });
+
+});
