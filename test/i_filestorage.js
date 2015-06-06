18e18e518913271717eaf075f2f04fc55e6070b5
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-06-06 11:03:04 -0400

    IFileStorage interface validation

---
+++ test/i_filestorage.js
@@ -0,0 +1,20 @@
+"use strict";
+/*global describe, it, assert*/
+
+describe("i_filestorage", function () {
+
+    describe("IFileStorage", function () {
+        it("is a known and static interface", function () {
+            /* jshint unused:false */
+            assert(gpf.interfaces.isImplementedBy({
+                getInfo: function (path, eventsHandler) {},
+                readAsBinaryStream: function (path, eventsHandler) {},
+                writeAsBinaryStream: function (path, eventsHandler) {},
+                close: function (stream) {},
+                explore: function (path, eventsHandler) {}
+            }, gpf.interfaces.IFileStorage));
+            /* jshint unused:true */
+        });
+    });
+
+});
