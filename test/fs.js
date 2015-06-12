0c46e8e45164c77a971c97498f3acd213fbef0ec
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-06-12 09:05:57 -0400

    Tests for fs

--- test/fs.js
+++ test/fs.js
@@ -1,74 +1,25 @@
-(function () { /* Begin of privacy scope */
-    "use strict";
+"use strict";
+/*global describe, it, assert*/
 
-    /*jshint -W027*/ // Done on purpose until gpf.declareTests is removed
-    return;
+describe("fs", function () {
 
-    if ("nodejs" === gpf.host() || "phantomjs" === gpf.host()) {
+    describe("gpf.fs.host", function () {
 
-        gpf.declareTests({
+        var
+            gpfI = gpf.interfaces,
+            iFs;
 
-            "NodeJS": [
-
-                function (test) {
-                    test.title("Get file info");
-                    test.wait();
-                    gpf.fs.getInfo("../fs.js", function (event) {
-                        test.equal(event.type(), "ready", "getInfo");
-                        var info = event.get("info");
-                        test.assert(info, "Info provided");
-                        test.equal(info.type, gpf.fs.TYPE_FILE, "File type");
-                        test.done();
-                    });
-                },
-
-                function (test) {
-                    test.title("Read a file (full)");
-                    var
-                        stream,
-                        decoder;
-                    test.wait();
-                    gpf.fs.readAsBinaryStream("../fs.js", function (event) {
-                        test.equal(event.type(), "ready", "readAsBinaryStream");
-                        stream = event.get("stream");
-                        decoder = gpf.encoding.createDecoder(stream,
-                            gpf.encoding.UTF_8);
-                        gpf.stringFromStream(decoder, function (event) {
-                            if (event.type() === "error") {
-                                console.error(event.get("error"));
-                            }
-                            test.equal(event.type(), "data", "Data event");
-                            var content = event.get("buffer"),
-                                index = content.indexOf("MyPrivateData");
-                            test.notEqual(index, -1, "Found MyPrivateData");
-                            test.done();
-                        });
-                    });
-                },
-
-                function (test) {
-                    test.title("Read a file (stringFromFile)");
-                    test.wait();
-                    gpf.stringFromFile("../fs.js", gpf.encoding.UTF_8,
-                        function (event) {
-                            if (event.type() === "error") {
-                                console.error(event.get("error"));
-                            }
-                            test.equal(event.type(), "data", "Data event");
-                            var content = event.get("buffer"),
-                                index = content.indexOf("MyPrivateData");
-                            test.notEqual(index, -1, "Found MyPrivateData");
-                            test.done();
-                        }
-                    );
-                }
-            ]
-
+        beforeEach(function () {
+            iFs = gpf.fs.host();
         });
 
-    }
+        it("gives access to a IFileStorage interface", function () {
+            assert(null !== iFs);
+            if (null !== iFs) {
+                assert(gpfI.isImplementedBy(iFs, gpfI.IFileStorage));
+            }
+        });
 
-    /*MyPrivateData*/
+    });
 
-})(); /* End of privacy scope */
-
+});
--- test/fs_node.js
+++ test/fs_node.js
@@ -1,6 +1,91 @@
 "use strict";
-/*global describe*/ // , it, assert*/
+/*global describe, it, assert, beforeEach*/
 
 describe("fs_node", function () {
 
+    if ("nodejs" !== gpf.host()) {
+        return; // Skip
+    }
+
+    var
+        gpfI = gpf.interfaces,
+        iNodeFs;
+
+    beforeEach(function () {
+        iNodeFs = new gpf.fs.NodeFileStorage();
+    });
+
+    describe("gpf.fs.NodeFileStorage", function () {
+
+        it("implements IFileStorage", function () {
+            assert(gpfI.isImplementedBy(iNodeFs, gpfI.IFileStorage));
+        });
+
+    });
+
 });
+
+
+/*
+        gpf.declareTests({
+
+            "NodeJS": [
+
+                function (test) {
+                    test.title("Get file info");
+                    test.wait();
+                    gpf.fs.getInfo("../fs.js", function (event) {
+                        test.equal(event.type(), "ready", "getInfo");
+                        var info = event.get("info");
+                        test.assert(info, "Info provided");
+                        test.equal(info.type, gpf.fs.TYPE_FILE, "File type");
+                        test.done();
+                    });
+                },
+
+                function (test) {
+                    test.title("Read a file (full)");
+                    var
+                        stream,
+                        decoder;
+                    test.wait();
+                    gpf.fs.readAsBinaryStream("../fs.js", function (event) {
+                        test.equal(event.type(), "ready", "readAsBinaryStream");
+                        stream = event.get("stream");
+                        decoder = gpf.encoding.createDecoder(stream,
+                            gpf.encoding.UTF_8);
+                        gpf.stringFromStream(decoder, function (event) {
+                            if (event.type() === "error") {
+                                console.error(event.get("error"));
+                            }
+                            test.equal(event.type(), "data", "Data event");
+                            var content = event.get("buffer"),
+                                index = content.indexOf("MyPrivateData");
+                            test.notEqual(index, -1, "Found MyPrivateData");
+                            test.done();
+                        });
+                    });
+                },
+
+                function (test) {
+                    test.title("Read a file (stringFromFile)");
+                    test.wait();
+                    gpf.stringFromFile("../fs.js", gpf.encoding.UTF_8,
+                        function (event) {
+                            if (event.type() === "error") {
+                                console.error(event.get("error"));
+                            }
+                            test.equal(event.type(), "data", "Data event");
+                            var content = event.get("buffer"),
+                                index = content.indexOf("MyPrivateData");
+                            test.notEqual(index, -1, "Found MyPrivateData");
+                            test.done();
+                        }
+                    );
+                }
+            ]
+
+        });
+
+    }
+    /*MyPrivateData*/
