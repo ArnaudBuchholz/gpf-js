107b08f72447dd15201edb2eb262208908439b63
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-06-19 13:51:30 -0400

    Tried reading (need DEBUG)

--- test/fs_ms.js
+++ test/fs_ms.js
@@ -22,17 +22,33 @@ describe("fs_ms", function () {
         });
 
         it("saves binary files", function (done) {
-            iWScriptFs.writeAsBinaryStream("tmp\\fs_ms.bin", function (event) {
-                assert(gpf.events.EVENT_READY === event.type);
-                var wStream = event.get("stream");
-                wStream.write([0, 34, 75, 0, 128, 255], function (event) {
+            iWScriptFs.writeAsBinaryStream("tmp\\test\\fs_ms.bin",
+                function (event) {
                     assert(gpf.events.EVENT_READY === event.type);
-                    iWScriptFs.close(wStream);
-                    done();
+                    var wStream = event.get("stream");
+                    wStream.write([0, 34, 75, 0, 128, 255], function (event) {
+                        assert(gpf.events.EVENT_READY === event.type);
+                        iWScriptFs.close(wStream);
+                        done();
+                    });
                 });
-            });
         });
 
+        it("reads binary files", function (done) {
+            iWScriptFs.readAsBinaryStream("test\\data\\file.bin",
+                function (event) {
+                    assert(gpf.events.EVENT_READY === event.type);
+                    var rStream = event.get("stream");
+                    rStream.read(1, function (event) {
+                        assert(gpf.events.EVENT_DATA === event.type);
+                        var buffer = event.get("buffer");
+                        assert(1 === buffer.length);
+                        assert(0 === buffer[0]);
+                        iWScriptFs.close(rStream);
+                        done();
+                    });
+                });
+        });
     });
 
 });
