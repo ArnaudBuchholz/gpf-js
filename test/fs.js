88ed56c61dbe343fe1b66fa4dbd5e376a676993a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-07-23 00:22:57 -0400

    Started to test IFileStorage

--- test/fs.js
+++ test/fs.js
@@ -7,25 +7,6 @@ describe("fs", function () {
         return; // No host
     }
 
-    describe("gpf.fs.host", function () {
-
-        var
-            gpfI = gpf.interfaces,
-            iFs;
-
-        beforeEach(function () {
-            iFs = gpf.fs.host();
-        });
-
-        it("gives access to a IFileStorage interface", function () {
-            assert(null !== iFs);
-            if (null !== iFs) {
-                assert(gpfI.isImplementedBy(iFs, gpfI.IFileStorage));
-            }
-        });
-
-    });
-
     describe("Data generator", function () {
 
         it("file.bin", function (done) {
@@ -33,7 +14,7 @@ describe("fs", function () {
             fs.getInfo("test/data/file.bin", function (event) {
                 if (event.type === gpf.events.EVENT_READY
                     && event.get("info").type === gpf.fs.TYPE_FILE
-                   ) {
+                ) {
                     done(); // Already exists
                     return;
                 }
@@ -58,6 +39,73 @@ describe("fs", function () {
                     }
                 );
             });
+        });
+
+    });
+
+    describe("gpf.fs.host", function () {
+
+        var
+            gpfI = gpf.interfaces,
+            iFs;
+
+        beforeEach(function () {
+            iFs = gpf.fs.host();
+        });
+
+        it("gives access to a IFileStorage interface", function () {
+            assert(null !== iFs);
+            if (null !== iFs) {
+                assert(gpfI.isImplementedBy(iFs, gpfI.IFileStorage));
+            }
+        });
+
+        describe("getInfo", function () {
+
+            function checkInfo(info) {
+                assert(null !== info);
+                assert("object" === typeof info);
+                assert("number" === typeof info.type);
+                assert("string" === typeof info.fileName);
+                assert("string" === typeof info.filePath);
+                assert("number" === typeof info.size);
+                assert("object" === typeof info.createdDateTime);
+                assert("object" === typeof info.modifiedDateTime);
+            }
+
+            it("tells if a file exists", function (done) {
+                iFs.getInfo("src/fs.js", function (event) {
+                    assert(event.type === gpf.events.EVENT_READY);
+                    var info = event.get("info");
+                    checkInfo(info);
+                    assert(info.type === gpf.fs.TYPE_FILE);
+                    assert(info.size > 0);
+                    done();
+                });
+            });
+
+            it("tells if a directory exists", function (done) {
+                iFs.getInfo("src", function (event) {
+                    assert(event.type === gpf.events.EVENT_READY);
+                    var info = event.get("info");
+                    checkInfo(info);
+                    assert(info.type === gpf.fs.TYPE_DIRECTORY);
+                    // size is not relevant
+                    done();
+                });
+            });
+
+            it("tells if a path does not exists", function (done) {
+                iFs.getInfo("null", function (event) {
+                    assert(event.type === gpf.events.EVENT_READY);
+                    var info = event.get("info");
+                    assert(null !== info);
+                    assert("object" === typeof info);
+                    assert(info.type === gpf.fs.TYPE_NOT_FOUND);
+                    done();
+                });
+            });
+
         });
 
     });
