a798feb10a7be44e365d5c1c800f7deeda3f9104
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-07-22 23:40:02 -0400

    Data generator part

--- test/fs.js
+++ test/fs.js
@@ -26,30 +26,40 @@ describe("fs", function () {
 
     });
 
-    //describe("data", function () {
-    //
-    //    it("generates file.bin", function (done) {
-    //        var fs = gpf.fs.host(),
-    //            count = 0,
-    //            wStream;
-    //        function loop(event) {
-    //            assert(!event || gpf.events.EVENT_READY === event.type);
-    //            if (256 === count) {
-    //                fs.close(wStream);
-    //                done();
-    //            }
-    //            wStream.write([count++], loop);
-    //        }
-    //        // Current path is always root of gpf-js
-    //        fs.writeAsBinaryStream("test/data/file.bin",
-    //            function (event) {
-    //                assert(gpf.events.EVENT_READY === event.type);
-    //                wStream = event.get("stream");
-    //                loop();
-    //            }
-    //        );
-    //    });
-    //
-    //});
+    describe("Data generator", function () {
+
+        it("generates file.bin", function (done) {
+            var fs = gpf.fs.host();
+            fs.getInfo("test/data/file.bin", function (event) {
+                if (event.type === gpf.events.EVENT_READY
+                    && event.get("type") === gpf.fs.TYPE_FILE
+                   ) {
+                    done(); // Already exists
+                    return;
+                }
+                // File does not exist, generates
+                var
+                    count = 0,
+                    wStream;
+                function loop(event) {
+                    assert(!event || gpf.events.EVENT_READY === event.type);
+                    if (256 === count) {
+                        fs.close(wStream);
+                        done();
+                    }
+                    wStream.write([count++], loop);
+                }
+                // Current path is always root of gpf-js
+                fs.writeAsBinaryStream("test/data/file.bin",
+                    function (event) {
+                        assert(gpf.events.EVENT_READY === event.type);
+                        wStream = event.get("stream");
+                        loop();
+                    }
+                );
+            });
+        });
+
+    });
 
 });
