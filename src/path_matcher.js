4b0dedf007ad1afeb972b741c1fdfc6bae3fd952
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-08-31 13:56:54 -0400

    Respecting codestyle

--- src/path_matcher.js
+++ src/path_matcher.js
@@ -4,160 +4,141 @@
 /*global _gpfPathDecompose*/ // Normalize path and returns an array of parts
 /*#endif*/
 
-var
-    /**
-     * Split the part to be processed in _GpfPathMatcher#_matchName
-     *
-     * @param {String} part
-     * @private
-     */
-    _gpfPatternPartSplit = function (part) {
-        return part.split("*");
-    },
+/**
+ * Split the part to be processed in _GpfPathMatcher#_matchName
+ *
+ * @param {String} part
+ */
+function _gpfPatternPartSplit (part) {
+    return part.split("*");
+}
+
+/**
+ * @param {String} pattern
+ *
+ * @class _GpfLikeContext
+ * @constructor
+ */
+function _GpfPathMatcher (pattern) {
+    /*jshint validthis:true*/
+/*#ifdef(DEBUG)*/
+    this._dbgSource = pattern;
+/*#endif*/
+
+    if ("!" === pattern.charAt(0)) {
+        this.negative = true;
+        pattern = pattern.substr(1);
+    }
 
     /**
-     *
-     * @param {String} pattern
-     *
-     * @class _GpfLikeContext
-     * @constructor
-     * @private
+     * if any use of "**", split the pattern in two:
+     * - the before part: start
+     * - the after part: end
+     * (otherwise, it is only the before part)
      */
-    _GpfPathMatcher = function (pattern) {
-        /*#ifdef(DEBUG)*/
-        this._dbgSource = pattern;
-        /*#endif*/
-
-        if ("!" === pattern.charAt(0)) {
-            this.negative = true;
-            pattern = pattern.substr(1);
-        }
-        /**
-         * if any use of "**", split the pattern in two:
-         * - the before part: start
-         * - the after part: end
-         * (otherwise, it is only the before part)
-         */
-        var pos = pattern.indexOf("**");
-        if (-1 === pos) {
+    var pos = pattern.indexOf("**");
+    if (-1 === pos) {
+        this.start = pattern
+            .split("/")
+            .map(_gpfPatternPartSplit);
+    } else {
+        if (0 < pos) {
+            gpf.ASSERT(pattern.charAt(pos - 1) === "/", "** must be preceded by /");
             this.start = pattern
+                .substr(0, pos - 1) // skip /
                 .split("/")
                 .map(_gpfPatternPartSplit);
-        } else {
-            if (0 < pos) {
-                gpf.ASSERT(pattern.charAt(pos - 1) === "/",
-                    "** must be preceded by /");
-                this.start = pattern
-                    .substr(0, pos - 1) // skip /
-                    .split("/")
-                    .map(_gpfPatternPartSplit);
-            }
-            if (pos < pattern.length - 2) {
-                gpf.ASSERT(pattern.charAt(pos + 2) === "/",
-                    "** must be followed by /");
-                this.end = pattern
-                    .substr(pos + 3) // skip /
-                    .split("/")
-                    .map(_gpfPatternPartSplit)
-                    .reverse();
-            }
         }
-    },
-
-
-    /**
-     * Convert - if necessary - the pattern parameter
-     *
-     * @param {_GpfPathMatcher|String} pattern
-     * @return {_GpfPathMatcher}
-     * @private
-     */
-    _gpfPathMatchCompilePattern = function (pattern) {
-        if (pattern instanceof _GpfPathMatcher) {
-            return pattern;
-        } else {
-            return new _GpfPathMatcher(pattern);
+        if (pos < pattern.length - 2) {
+            gpf.ASSERT(pattern.charAt(pos + 2) === "/", "** must be followed by /");
+            this.end = pattern
+                .substr(pos + 3) // skip /
+                .split("/")
+                .map(_gpfPatternPartSplit)
+                .reverse();
         }
-    },
+    }
+}
 
-    /**
-     * Convert - if necessary - the pattern parameter
-     *
-     * @param {Array|String} pattern
-     * @return {_GpfPathMatcher[]}
-     * @private
-     */
-    _gpfPathMatchCompilePatterns = function (pattern) {
-        if (pattern instanceof Array) {
-            return pattern.map(_gpfPathMatchCompilePattern);
-        } else {
-            return [_gpfPathMatchCompilePattern(pattern)];
-        }
-    },
+/**
+ * Convert - if necessary - the pattern parameter
+ *
+ * @param {_GpfPathMatcher|String} pattern
+ * @return {_GpfPathMatcher}
+ */
+function _gpfPathMatchCompilePattern (pattern) {
+    if (pattern instanceof _GpfPathMatcher) {
+        return pattern;
+    } else {
+        return new _GpfPathMatcher(pattern);
+    }
+}
 
-    /**
-     * Match a path item
-     *
-     * @param pathMatcher
-     * @this An object containing
-     * - {String[]} parts the path being tested split in parts
-     * - {Boolean} [result=undefined] result the result
-     *
-     * @private
-     */
-    _gpfPathMatchApply = function (pathMatcher) {
-        var negative = pathMatcher.negative;
-        if (pathMatcher.match(this.parts)) {
-            this.result = !negative;
-            return false; // Stop the main loop
-        }
-        if (negative) {
-            this.result = true;
-            return false; // Stop the main loop
-        }
-        return true; // continue
-    },
+/**
+ * Convert - if necessary - the pattern parameter
+ *
+ * @param {Array|String} pattern
+ * @return {_GpfPathMatcher[]}
+ */
+ function _gpfPathMatchCompilePatterns (pattern) {
+    if (pattern instanceof Array) {
+        return pattern.map(_gpfPathMatchCompilePattern);
+    } else {
+        return [_gpfPathMatchCompilePattern(pattern)];
+    }
+}
 
-    /**
-     * Match the pattern with the path
-     * @param {Array|String} pattern
-     * @param {String} path
-     * @return {Boolean|undefined}
-     * @private
-     */
-    _gpfPathMatch = function (pattern, path) {
-        var parts = _gpfPathDecompose(path),
-            matchers = _gpfPathMatchCompilePatterns(pattern),
-            scope = {
-                parts: parts
-            };
-        matchers.every(_gpfPathMatchApply, scope);
-        return scope.result;
-    };
+/**
+ * Match a path item
+ *
+ * @param pathMatcher
+ * @this An object containing
+ * - {String[]} parts the path being tested split in parts
+ * - {Boolean} [result=undefined] result the result
+ */
+function _gpfPathMatchApply (pathMatcher) {
+    /*jshint validthis:true*/
+    var negative = pathMatcher.negative;
+    if (pathMatcher.match(this.parts)) {
+        this.result = !negative;
+        return false; // Stop the main loop
+    }
+    if (negative) {
+        this.result = true;
+        return false; // Stop the main loop
+    }
+    return true; // continue
+}
+
+/**
+ * Match the pattern with the path
+ * @param {Array|String} pattern
+ * @param {String} path
+ * @return {Boolean|undefined}
+ */
+function _gpfPathMatch (pattern, path) {
+    var parts = _gpfPathDecompose(path),
+        matchers = _gpfPathMatchCompilePatterns(pattern),
+        scope = {
+            parts: parts
+        };
+    matchers.every(_gpfPathMatchApply, scope);
+    return scope.result;
+}
 
 _GpfPathMatcher.prototype = {
 
     constructor: _GpfPathMatcher,
 
-    /*#ifdef(DEBUG)*/
+/*#ifdef(DEBUG)*/
 
-    /**
-     * Source of the pattern
-     *
-     * @type {String}
-     * @private
-     */
+    // Source of the pattern
     _dbgSource: "",
 
-    /*#endif*/
+/*#endif*/
 
 
-    /**
-     * Indicate the pattern started with !
-     *
-     * @type {Boolean}
-     * @private
-     */
+    // Indicate the pattern started with !
     negative: false,
 
     /**
@@ -168,8 +149,7 @@ _GpfPathMatcher.prototype = {
     start: null,
 
     /**
-     * List of name patterns to be applied on the end of the path
-     * (note they are in the reverse order)
+     * List of name patterns to be applied on the end of the path (note they are in the reverse order)
      *
      * @type {String[]}
      */
@@ -186,7 +166,6 @@ _GpfPathMatcher.prototype = {
      *
      * @param {String[]} fixedPatterns
      * @param {String} part
-     * @private
      */
     _matchName: function (fixedPatterns, part) {
         var
@@ -270,21 +249,17 @@ _gpfExtend(gpf.path, {
      * PATTERN FORMAT
      * - An optional prefix "!" negates the pattern
      * - Path separator are /
-     * - In a DOS environment, path is transformed to lowercase and path
-     * separators are converted to / (hence the pattern remains the same)
-     * - A leading slash matches the beginning of the pathname. For example,
-     * "/*.c" matches "cat-file.c" but not "mozilla-sha1/sha1.c".
-     * - Two consecutive asterisks ("**") in patterns matched against full
-     * pathname may have special meaning:
+     * - In a DOS environment, path is transformed to lowercase and path separators are converted to / (hence the
+     *   pattern remains the same)
+     * - A leading slash matches the beginning of the pathname. For example, "/*.c" matches "cat-file.c" but not
+     *   "mozilla-sha1/sha1.c".
+     * - Two consecutive asterisks ("**") in patterns matched against full pathname may have special meaning:
      *   - A leading "**" followed by a slash means match in all directories.
-     *   For example, "**" + "/foo" matches file or directory "foo" anywhere,
-     *   the same as pattern "foo". "**" + "/foo/bar" matches file or directory
-     *   "bar" anywhere that is directly under directory "foo".
-     *   - A trailing "/**" matches everything inside. For example, "abc/**"
-     *   matches all files inside directory "abc"
-     *   - A slash followed by two consecutive asterisks then a slash matches
-     *   zero or more directories. For example, "a/**" + "/b" matches "a/b",
-     *   "a/x/b", "a/x/y/b" and so on.
+     *     For example, "**" + "/foo" matches file or directory "foo" anywhere, the same as pattern "foo".
+     *     "**" + "/foo/bar" matches file or directory "bar" anywhere that is directly under directory "foo".
+     *   - A trailing "/**" matches everything inside. For example, "abc/**" matches all files inside directory "abc"
+     *   - A slash followed by two consecutive asterisks then a slash matches zero or more directories. For example,
+     *     "a/**" + "/b" matches "a/b", "a/x/b", "a/x/y/b" and so on.
      * Other consecutive asterisks are considered invalid.
      *
      * @param {Array|String} pattern
@@ -296,9 +271,8 @@ _gpfExtend(gpf.path, {
     },
 
     /**
-     * Process the pattern and return an array that can be used in match
-     * NOTE this is not mandatory but if you reuse the same pattern multiple
-     * times, this will make it more efficient.
+     * Process the pattern and return an array that can be used in match.
+     * NOTE this is not mandatory but if you reuse the same pattern multiple times, this will make it more efficient
      *
      * @param {Array|String} pattern
      * @return {Array}
@@ -308,4 +282,3 @@ _gpfExtend(gpf.path, {
     }
 
 });
-
