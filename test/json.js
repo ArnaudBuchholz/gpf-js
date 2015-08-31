f8802a76f4d0c4d9e413a2f01c1e4a840cc5b14a
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-08-31 13:29:07 -0400

    JSON refactoring

--- test/json.js
+++ test/json.js
@@ -45,11 +45,19 @@ describe("json", function () {
         len = tests.length,
         idx;
 
-    describe("gpf.json.stringify", function () {
+    describe("JSON object", function () {
+
+        it("exists", function () {
+            assert("undefined" !== typeof JSON);
+        });
+
+    });
+
+    describe("JSON.stringify", function () {
 
         function makeIt(test) {
             it("works on " + test.label, function () {
-                assert(gpf.json.stringify(test.obj) === test.json);
+                assert(JSON.stringify(test.obj) === test.json);
             });
         }
 
@@ -59,11 +67,11 @@ describe("json", function () {
 
     });
 
-    describe("gpf.json.parse", function () {
+    describe("JSON.parse", function () {
 
         function makeIt(test) {
             it("works on " + test.label, function () {
-                var obj = gpf.json.parse(test.json);
+                var obj = JSON.parse(test.json);
                 assert(true === gpf.like(obj, test.obj));
             });
         }
@@ -71,62 +79,6 @@ describe("json", function () {
         for (idx = 0; idx < len; ++idx) {
             makeIt(tests[idx]);
         }
-
-    });
-
-    describe("gpf.json.load and gpf.json.save", function () {
-
-        var A = gpf.define("A", {
-
-                private: {
-                    "[_member1]": [gpf.$ClassProperty(true)],
-                    _member1: 34,
-
-                    "[_member2]": [gpf.$ClassProperty(true)],
-                    _member2: "abc",
-
-                    "[_ignoredMember]": [gpf.$ClassProperty(true),
-                        gpf.$ClassNonSerialized()],
-                    _ignoredMember: "def"
-
-                },
-
-                public: {
-
-                    member3: false
-
-                }
-
-            });
-
-        it("save changed members to json", function () {
-            var a = new A(),
-                saved;
-            a.member2("ghi"); // Change value to ensure serialization
-            a.member3 = true;
-            a.ignoredMember("jkl");
-            saved = gpf.json.save(a);
-            assert(undefined === saved.member1); // unchanged is not serialized
-            assert("ghi" === saved.member2);
-            assert(true === saved.member3);
-            assert(undefined === saved.ignoredMember); // ignored
-        });
-
-        it("load members from json", function () {
-            var a = new A(),
-                saved = {
-                    member2: "ghi",
-                    member3: true,
-                    ignoredMember: "jkl"
-                };
-            a.member1("12");
-            a.ignoredMember("mno");
-            gpf.json.load(a, saved);
-            assert(34 === a.member1()); // restored to initial value
-            assert("ghi" === a.member2());
-            assert(true === a.member3);
-            assert("mno" === a.ignoredMember()); // ignored
-        });
 
     });
 
