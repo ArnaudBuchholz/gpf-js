58276f6a8a3684a193a87b86cfde077b105f4639
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-09-13 12:19:50 -0400

    Fixed test case

--- test/attributes.js
+++ test/attributes.js
@@ -15,7 +15,6 @@ describe("attributes", function () {
         },
 
         A = gpf.define("A", {
-
             protected: {
 
                 "[_a]": [ $Test1Value() ],
@@ -25,7 +24,6 @@ describe("attributes", function () {
                 _c: 0
 
             },
-
             public: {
 
                 constructor: function (value) {
@@ -37,13 +35,11 @@ describe("attributes", function () {
                 }
 
             }
-
         }),
 
         a = new A(),
 
         B = gpf.define("B", A, {
-
             protected: {
 
                 "[_b]": [$Test2Value()],
@@ -53,7 +49,6 @@ describe("attributes", function () {
                 _c: 0
 
             },
-
             public: {
 
                 constructor: function (value) {
@@ -65,7 +60,6 @@ describe("attributes", function () {
                     return this._b;
                 }
             }
-
         }),
 
         b = new B();
@@ -74,18 +68,18 @@ describe("attributes", function () {
 
         it("is available through gpf.attributes.Map", function () {
             var attributes = new gpf.attributes.Map(a);
-            assert(2 === attributes.count());
-            assert(1 === attributes.member("_a").getItemsCount());
-            assert(0 === attributes.member("_b").getItemsCount());
-            assert(1 === attributes.member("_c").getItemsCount());
+            assert(2 === attributes.getCount());
+            assert(1 === attributes.getMemberAttributes("_a").getItemsCount());
+            assert(0 === attributes.getMemberAttributes("_b").getItemsCount());
+            assert(1 === attributes.getMemberAttributes("_c").getItemsCount());
         });
 
         it("inherits from base class", function () {
             var attributes = new gpf.attributes.Map(b);
-            assert(4 === attributes.count());
-            assert(1 === attributes.member("_a").getItemsCount());
-            assert(1 === attributes.member("_b").getItemsCount());
-            assert(2 === attributes.member("_c").getItemsCount());
+            assert(4 === attributes.getCount());
+            assert(1 === attributes.getMemberAttributes("_a").getItemsCount());
+            assert(1 === attributes.getMemberAttributes("_b").getItemsCount());
+            assert(2 === attributes.getMemberAttributes("_c").getItemsCount());
         });
 
         it("signals any use on non-existing member", function () {
@@ -108,104 +102,95 @@ describe("attributes", function () {
 
         it("allows to test the presence of an attribute", function () {
             var attributes = new gpf.attributes.Map(a),
-                attributesForA = attributes.member("_a");
+                attributesForA = attributes.getMemberAttributes("_a");
             assert(1 === attributesForA.getItemsCount());
-            assert(null !== attributesForA.has(Test1ValueAttribute));
-            assert(null === attributesForA.has(Test2ValueAttribute));
+            assert(true === attributesForA.has(Test1ValueAttribute));
+            assert(false === attributesForA.has(Test2ValueAttribute));
         });
 
         it("filters on attribute type", function () {
             var attributes = new gpf.attributes.Map(b),
-                attributesForC = attributes.member("_c"),
-                test1ValueAttributesForC =
-                    attributesForC.filter(Test1ValueAttribute),
-                test2ValueAttributesForC =
-                    attributesForC.filter(Test2ValueAttribute);
+                attributesForC = attributes.getMemberAttributes("_c"),
+                test1ValueAttributesForC = attributesForC.filter(Test1ValueAttribute),
+                test2ValueAttributesForC = attributesForC.filter(Test2ValueAttribute);
             assert(2 === attributesForC.getItemsCount());
-            assert(null !== test1ValueAttributesForC.has(Test1ValueAttribute));
-            assert(null === test1ValueAttributesForC.has(Test2ValueAttribute));
-            assert(null === test2ValueAttributesForC.has(Test1ValueAttribute));
-            assert(null !== test2ValueAttributesForC.has(Test2ValueAttribute));
-        }); // filter
+            assert(true === test1ValueAttributesForC.has(Test1ValueAttribute));
+            assert(false === test1ValueAttributesForC.has(Test2ValueAttribute));
+            assert(false === test2ValueAttributesForC.has(Test1ValueAttribute));
+            assert(true === test2ValueAttributesForC.has(Test2ValueAttribute));
+        });
 
-        it("offers an enumeration function based on gpf.each", function () {
+        it("offers an enumeration function based on [].forEach", function () {
             var attributes = new gpf.attributes.Map(b),
-                attributesForC = attributes.member("_c"),
+                attributesForC = attributes.getMemberAttributes("_c"),
                 dictionaryOfAttributesForC = {},
-                result = attributesForC.each(function (idx, attribute, len) {
+                result = attributesForC.forEach(function (attribute, idx, array) {
                     assert("number" === typeof idx);
                     assert(attribute instanceof gpf.attributes.Attribute);
-                    assert("number" === typeof len);
-                    assert(2 === len);
-                    dictionaryOfAttributesForC
-                        [attribute.constructor.compatibleName()]
-                        = [attribute];
+                    assert(array instanceof Array);
+                    assert(2 === array.length);
+                    dictionaryOfAttributesForC[attribute.constructor.compatibleName()] = [attribute];
                 });
             assert(undefined === result);
             assert(1 === dictionaryOfAttributesForC.Test1ValueAttribute.length);
             assert(1 === dictionaryOfAttributesForC.Test2ValueAttribute.length);
         });
 
-        it("offers a stoppable enumeration function", function () {
-            var attributes = new gpf.attributes.Map(b),
-                attributesForC = attributes.member("_c"),
-                result = attributesForC.each(function (idx, attribute, len) {
-                    assert("number" === typeof idx);
-                    assert("number" === typeof len);
-                    assert(2 === len);
-                    if (attribute instanceof Test1ValueAttribute) {
-                        return attribute;
-                    }
-                });
-            assert(null !== result);
-            assert(result instanceof Test1ValueAttribute);
-        });
-
+        if (gpf.attributes.Array.prototype.every) {
+            it("offers a stoppable enumeration function", function () {
+                var attributes = new gpf.attributes.Map(b),
+                    attributesForC = attributes.getMemberAttributes("_c"),
+                    selectedAttribute,
+                    result = attributesForC.every(function (attribute, idx, array) {
+                        assert("number" === typeof idx);
+                        assert("number" === typeof len);
+                        assert(2 === array.length);
+                        if (attribute instanceof Test1ValueAttribute) {
+                            selectedAttribute = attribute;
+                            return false;
+                        }
+                        return true;
+                    });
+                assert(false === result);
+                assert(selectedAttribute instanceof Test1ValueAttribute);
+            });
+        } else {
+            it("offers a stoppable enumeration function");
+        }
     });
 
     describe("gpf.attributes.Map", function () {
 
         it("lists members that have attributes", function () {
             var attributes = new gpf.attributes.Map(a),
-                members = attributes.members();
+                members = attributes.getMembers();
             assert(2 === members.length);
+            // Order is not guaranteed
             assert(undefined !== gpf.test(members, "_a"));
             assert(undefined !== gpf.test(members, "_c"));
         });
 
         it("filters on attribute type", function () {
             var attributes = new gpf.attributes.Map(b),
-                attributesTest2Value =
-                    attributes.filter(Test2ValueAttribute);
-            assert(2 === attributesTest2Value.count());
-            assert(1 === attributesTest2Value.member("_c").getItemsCount());
+                attributesTest2Value = attributes.filter(Test2ValueAttribute);
+            assert(2 === attributesTest2Value.getCount());
+            assert(1 === attributesTest2Value.getMemberAttributes("_c").getItemsCount());
         });
 
-        it("offers an enumeration function based on gpf.each", function () {
+        it("offers an enumeration function based on [].forEach", function () {
             var attributes = new gpf.attributes.Map(a),
                 members = [],
-                result = attributes.each(function (member, attributeArray) {
+                result = attributes.forEach(function (attributeArray, member, dictionary) {
                     assert("string" === typeof member);
                     assert(attributeArray instanceof gpf.attributes.Array);
+                    assert(null !== dictionary);
                     members.push(member);
                 });
             assert(undefined === result);
             assert(2 === members.length);
+            // Order is not guaranteed
             assert(undefined !== gpf.test(members, "_a"));
             assert(undefined !== gpf.test(members, "_c"));
-        });
-
-        it("offers a stoppable enumeration function", function () {
-            var attributes = new gpf.attributes.Map(a),
-                result = attributes.each(function (member, attributeArray) {
-                    assert("string" === typeof member);
-                    assert(attributeArray instanceof gpf.attributes.Array);
-                    if ("_a" === member) {
-                        return attributeArray;
-                    }
-                });
-            assert(undefined !== result);
-            assert(result instanceof gpf.attributes.Array);
         });
 
         it("is modifiable without altering initial list", function () {
@@ -213,12 +198,12 @@ describe("attributes", function () {
                 attributesForA,
                 attributes2;
             attributes.add("_a", new Test2ValueAttribute());
-            assert(3 === attributes.count());
-            attributesForA = attributes.member("_a");
+            assert(3 === attributes.getCount());
+            attributesForA = attributes.getMemberAttributes("_a");
             assert(2 === attributesForA.getItemsCount());
             attributes2 = new gpf.attributes.Map(a);
-            assert(2 === attributes2.count());
-            attributesForA = attributes2.member("_a");
+            assert(2 === attributes2.getCount());
+            attributesForA = attributes2.getMemberAttributes("_a");
             assert(1 === attributesForA.getItemsCount());
         });
 
