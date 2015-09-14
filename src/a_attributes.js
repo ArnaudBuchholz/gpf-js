a16aafd91ac6941f3709af798b37eaf98d6aef1f
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-09-14 13:22:44 -0400

    Refactoring done

--- src/a_attributes.js
+++ src/a_attributes.js
@@ -36,12 +36,12 @@ var
             _alterPrototype: function (objPrototype) {
                 if (!(objPrototype instanceof _gpfAttribute)) {
                     throw gpf.Error.OnlyForAttributeClass({
-                        attributeName: _gpfGetClassDefinition(this.constructor).name()
+                        attributeName: _gpfGetClassDefinition(this.constructor)._name
                     });
                 }
                 if (this._member !== "Class") {
                     throw gpf.Error.OnlyOnClassForAttributeClass({
-                        attributeName: _gpfGetClassDefinition(this.constructor).name()
+                        attributeName: _gpfGetClassDefinition(this.constructor)._name
                     });
                 }
             }
@@ -56,14 +56,12 @@ var
      * @extends gpf.attributes.AttrClassOnlyAttribute
      */
     _gpfAttrConstraint = _gpfDefAttr("AttrConstraintAttribute", _GpfAttrOnly, {
-
         static: {
 
             // Name used to remember the original _alterPrototype handler
             originalAlterPrototype: "_alterPrototype:checked"
 
         },
-
         private: {
 
             /**
@@ -76,30 +74,22 @@ var
                 var statics = _gpfA.AttrConstraintAttribute,
                     originalAlterPrototype = statics.originalAlterPrototype,
                     targetAttribute = this,
-                    attributes;
-
+                    attributes = new _gpfA.Map(this);
                 // Get constraints set for THIS attribute
-                attributes = new _gpfA.Map(this);
-                attributes.filter(_gpfAttrConstraint).each(function (member, attributes) {
-                    _gpfIgnore(member);
-                    var itemsCount = attributes.getItemsCount(),
-                        idx;
-                    for (idx = 0; idx < itemsCount; ++idx) {
-                        attributes.getItem(idx)._check(targetAttribute, objPrototype);
-                    }
+                attributes.filter(_gpfAttrConstraint).forEach(function (attributes) {
+                    attributes.forEach(function(attribute) {
+                        attribute._check(targetAttribute, objPrototype);
+                    });
                 });
-
                 // OK, call _alterPrototype
                 targetAttribute[originalAlterPrototype].apply(targetAttribute, [objPrototype]);
             }
 
         },
-
         protected: {
 
             /**
-             * Throws an exception if target attribute can't be applied to
-             * objPrototype
+             * Throws an exception if target attribute can't be applied to objPrototype
              *
              * @param {gpf.attributes.Attribute} targetAttribute
              * @param {Object} objPrototype
@@ -109,7 +99,7 @@ var
                 throw gpf.Error.Abstract();
             },
 
-            // @closure
+            // @inheritdoc gpf.attributes.Attribute#_alterPrototype
             _alterPrototype: function (objPrototype) {
                 var statics = _gpfA.AttrConstraintAttribute,
                     originalAlterPrototype = statics.originalAlterPrototype;
@@ -118,8 +108,8 @@ var
                 this._super(objPrototype);
 
                 /**
-                 * Several constraint attributes might be defined, check if
-                 * the _alterPrototype has already been overridden
+                 * Several constraint attributes might be defined, check if the _alterPrototype has already been
+                 * overridden
                  */
                 if (undefined === objPrototype[originalAlterPrototype]) {
                     objPrototype[originalAlterPrototype] = objPrototype._alterPrototype;
@@ -128,7 +118,6 @@ var
             }
 
         }
-
     });
 
 /**
@@ -139,79 +128,73 @@ var
  * @alias gpf.$ClassAttribute
  */
 _gpfDefAttr("$ClassAttribute", _gpfAttrConstraint, {
-
     protected: {
 
+        // @inheritdoc gpf.attributes.AttrConstraintAttribute#_check
         _check: function (targetAttribute, objPrototype) {
             _gpfIgnore(objPrototype);
-            if (targetAttribute.member() !== "Class") {
-                var
-                    attributeClass = targetAttribute.constructor,
+            if (targetAttribute._member !== "Class") {
+                var attributeClass = targetAttribute.constructor,
                     attributeClassDef = _gpfGetClassDefinition(attributeClass);
                 throw gpf.Error.ClassOnlyAttribute({
-                    attributeName: attributeClassDef.name()
+                    attributeName: attributeClassDef._name
                 });
             }
         }
 
     }
-
 });
 
 /**
- * Used on attribute classes to mark them as member attribute (i.e. they can't
- * be used on Class)
+ * Used on attribute classes to mark them as member attribute (i.e. they can't be used on Class)
  *
  * @class gpf.attributes.MemberAttributeAttribute
  * @extends gpf.attributes.AttrConstraintAttribute
  * @alias gpf.$MemberAttribute
  */
 _gpfDefAttr("$MemberAttribute", _gpfAttrConstraint, {
-
     protected: {
 
-        _check: function (targetAttribute/*, objPrototype*/) {
-            if (targetAttribute.member() === "Class") {
-                var
-                    attributeClass = targetAttribute.constructor,
+        // @inheritdoc gpf.attributes.AttrConstraintAttribute#_check
+        _check: function (targetAttribute, objPrototype) {
+            _gpfIgnore(objPrototype);
+            if (targetAttribute._member === "Class") {
+                var attributeClass = targetAttribute.constructor,
                     attributeClassDef = _gpfGetClassDefinition(attributeClass);
                 throw gpf.Error.MemberOnlyAttribute({
-                    attributeName: attributeClassDef.name()
+                    attributeName: attributeClassDef._name
                 });
             }
         }
-
     }
-
 });
+
 /**
  * Used on attribute classes to mark them as unique through the class hierarchy or per member.
- * If one try to define it more than once, an error is raised.
+ * If one try to define it more than once, an error is raised
  *
  * @class gpf.attributes.UniqueAttributeAttribute
  * @extends gpf.attributes.AttrConstraintAttribute
  * @alias gpf.$UniqueAttribute
  */
 _gpfDefAttr("$UniqueAttribute", _gpfAttrConstraint, {
-
     private: {
 
         // The attribute is unique for the whole class when true or per member when false
         _classScope: true
 
     },
-
     protected: {
 
+        // @inheritdoc gpf.attributes.AttrConstraintAttribute#_check
         _check: function (targetAttribute, objPrototype) {
-            var
-                objectClass,
+            var objectClass,
                 objectClassDef,
                 objectClassAttributes,
                 attributeClass,
                 attributeClassDef,
                 attributesInObj,
-                member = targetAttribute.member();
+                member = targetAttribute._member;
 
             // Get object class definition & attributes
             objectClass = objPrototype.constructor;
@@ -226,30 +209,27 @@ _gpfDefAttr("$UniqueAttribute", _gpfAttrConstraint, {
 
             // Don't forget that targetAttribute is already added to the object
             if (this._classScope) {
-                if (1 < attributesInObj.count()) {
+                if (1 < attributesInObj.getCount()) {
                     throw gpf.Error.UniqueAttributeConstraint({
-                        attributeName: attributeClassDef.name(),
-                        className: objectClassDef.name()
+                        attributeName: attributeClassDef._name,
+                        className: objectClassDef._name
                     });
                 }
-            } else if (1 < attributesInObj.member(member).getItemsCount()) {
+            } else if (1 < attributesInObj.getMemberAttributes(member).getItemsCount()) {
                 throw gpf.Error.UniqueMemberAttributeConstraint({
-                    attributeName: attributeClassDef.name(),
-                    className: objectClassDef.name(),
+                    attributeName: attributeClassDef._name,
+                    className: objectClassDef._name,
                     memberName: member
                 });
             }
         }
 
     },
-
     public: {
 
         /**
-         * Restricts attribute multiplicity
-         *
-         * @param {Boolean} [classScope=true] classScope True to set limit to one instance per class
-         * (including hierarchy) or false to limit to one instance per member.
+         * @param {Boolean} [classScope=true] classScope True to set limit to one instance per class (including
+         * hierarchy) or false to limit to one instance per member.
          */
         constructor: function (classScope) {
             if (undefined !== classScope) {
@@ -258,5 +238,4 @@ _gpfDefAttr("$UniqueAttribute", _gpfAttrConstraint, {
         }
 
     }
-
 });
