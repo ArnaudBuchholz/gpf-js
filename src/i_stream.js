cf2cb40f6a417507d9314e914eeb6d126565d94d
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-09-15 10:48:03 -0400

    Refactoring

--- src/i_stream.js
+++ src/i_stream.js
@@ -10,14 +10,12 @@
  *
  * Each stream may have its own preferences (it's JavaScript after all).
  * I didn't want to make the interface too complex.
- * For write, the stream is allowed to reject the call if it does not receive
- * the expected format.
+ * For write, the stream is allowed to reject the call if it does not receive the expected format.
  */
 
 /**
- * The Readable stream interface is the abstraction for a source of data
- * that you are reading from. In other words, data comes out of a Readable
- * stream.
+ * The Readable stream interface is the abstraction for a source of data that you are reading from.
+ * In other words, data comes out of a Readable stream.
  *
  * @class gpf.interfaces.IReadableStream
  * @extends gpf.interfaces.Interface
@@ -35,8 +33,7 @@ _gpfDefIntrf("IReadableStream", {
      *     the stream can't be used after this.
      *   - EVENT_DATA: a buffer is provided, it can't be empty.
      *
-     * @param {Number} [size=0] size Number of bytes to read. Read
-     * as much as possible if 0
+     * @param {Number} [size=0] size Number of bytes to read. Read as much as possible if 0
      * @param {gpf.events.Handler} eventsHandler
      *
      * @event gpf.events.EVENT_DATA
@@ -54,15 +51,7 @@ _gpfDefIntrf("IReadableStream", {
 });
 
 /**
- * The Writable stream interface is an abstraction for a destination that
- * you are writing data to.
- * The expected behavior is:
- * - The callback is asynchronous
- * - One of the following callback must be called after a read
- *   - EVENT_ERROR: an error occurred.
- *     the stream can't be used after this.
- *   - EVENT_READY: the write operation succeeded, the provided buffer has
- *     been fully written (otherwise an error is thrown)
+ * The Writable stream interface is an abstraction for a destination that you are writing data to.
  *
  * @class gpf.interfaces.IReadableStream
  * @extends gpf.interfaces.Interface
@@ -70,14 +59,20 @@ _gpfDefIntrf("IReadableStream", {
 _gpfDefIntrf("IWritableStream", {
 
     /**
-     * Triggers the writing of data
+     * Triggers the writing of data.
+     * The expected behavior is:
+     * - The callback is asynchronous
+     * - One of the following callback must be called after a read
+     *   - EVENT_ERROR: an error occurred.
+     *     the stream can't be used after this.
+     *   - EVENT_READY: the write operation succeeded, the provided buffer has
+     *     been fully written (otherwise an error is thrown)
      *
      * @param {Number[]|String} buffer Unsigned Bytes/String buffer to write
      * @param {gpf.events.Handler} eventsHandler
      *
      * @event gpf.events.EVENT_READY
      * it is appropriate to begin writing more data to the stream
-     *
      */
     "[write]": [gpf.$ClassEventHandler()],
     write: function (buffer, eventsHandler) {
@@ -88,20 +83,19 @@ _gpfDefIntrf("IWritableStream", {
 
 /**
  * The stream combines both IReadableStream and IWritableStream
+ *
+ * @class gpf.interfaces.IStream
+ * @extends gpf.interfaces.Interface
  */
 _gpfDefIntrf("IStream", {
 
-    /**
-     * @inheritdoc gpf.interfaces.IReadableStream#read
-     */
+    // @inheritdoc gpf.interfaces.IReadableStream#read
     "[read]": [gpf.$ClassEventHandler()],
     read: function (size, eventsHandler) {
         _gpfIgnore(size, eventsHandler);
     },
 
-    /**
-     * @inheritdoc gpf.interfaces.IWritableStream#write
-     */
+    // @inheritdoc gpf.interfaces.IWritableStream#write
     "[write]": [gpf.$ClassEventHandler()],
     write: function (buffer, eventsHandler) {
         _gpfIgnore(buffer, eventsHandler);
