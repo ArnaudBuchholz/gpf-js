0eee5e35c4632f7aee04c3ea0af2a53107000474
Arnaud Buchholz <abuchholz@etelesolv.com>
2015-09-16 16:31:15 -0400

    Externalized version settings

--- make/node_make.js
+++ make/node_make.js
@@ -2,111 +2,94 @@
 /*global require, global, process*/
 
 var fs = require("fs"),
-    dependencyMissing = false;
-try {
-    global.esprima = require("esprima");
-} catch (e) {
-    console.error("Missing esprima, use npm install esprima");
-    dependencyMissing = true;
-}
-try {
-    global.escodegen = require("escodegen");
-} catch (e) {
-    console.error("Missing escodegen, use npm install escodegen");
-    dependencyMissing = true;
-}
-if (dependencyMissing) {
-    process.exit();
-}
+    esprima = require("esprima"),
+    escodegen = require("escodegen");
 
 global.gpfSourcesPath = "../src";
 require("../src/boot.js"); /*source version*/
 require("./make.js");  /*global make*/
 
-var
-    version,
-    sources = {
-        _list: gpf.sources()
-    },
-    idx,
-    output;
-
-// Handle the empty source tag
-idx = sources._list.indexOf("");
-if (-1 < idx) {
-    sources._list.splice(idx, sources._list.length - idx);
-}
-
-if (process.argv.length > 2) {
-    version = process.argv[2];
-} else {
-    version = "debug";
-}
+var version = process.argv[2] || "debug",
+    parameters,
+    sources = {},
+    temporary = {},
+    result;
 
 console.log("Generating version '" + version + "'");
-
-for (idx = 0; idx < sources._list.length; ++idx) {
-    sources[sources._list[idx]] =
-        fs.readFileSync("../src/" + sources._list[idx] + ".js").toString();
+try {
+    parameters = JSON.parse(fs.readFileSync(version + ".json").toString());
+} catch (e) {
+    console.error("Unknown or invalid version");
+    process.exit();
 }
+
+// Read sources
+gpf.sources().every(function (name) {
+    if (!name) {
+        // end marker
+        return false;
+    }
+    sources[name] = fs.readFileSync("../src/" + name + ".js").toString();
+});
 sources.UMD = fs.readFileSync("UMD.js").toString();
 sources.boot = fs.readFileSync("../src/boot.js").toString();
 
+fs.readFileSync("UMD.js").toString();
+
 try {
-    make(sources, version);
+    result = make(sources, parameters, temporary);
 } catch (e) {
     console.error(e.message);
 }
 
 // ---------- For debugging purposes
 
-function save (name, version) {
+function mkDir(path) {
+    if (!fs.existsSync(path)) {
+        var parentPath = path.split("/").spice(0, -1).join("/");
+        if (parentPath) {
+            mkDir(parentPath);
+        }
+        fs.mkdirSync(path);
+    }
+}
+
+function save (name) {
     var
         path = "../tmp/build/" + version + "/" + name,
-        parsed = sources[version],
+        parsed = temporary[version],
         json = parsed[name];
-    if (!fs.existsSync("../tmp")) {
-        fs.mkdirSync("../tmp");
-    }
-    if (!fs.existsSync("../tmp/build")) {
-        fs.mkdirSync("../tmp/build");
-    }
-    if (!fs.existsSync("../tmp/build/" + version)) {
-        fs.mkdirSync("../tmp/build/" + version);
-    }
     if (parsed[name + ".js"]) {
-        fs.writeFileSync(path + ".js",parsed[name + ".js"]);
+        fs.writeFileSync(path + ".js", temporary[name + ".js"]);
     }
     if (parsed[name + ".compact.js"]) {
-        fs.writeFileSync(path + ".compact.js",parsed[name + ".compact.js"]);
+        fs.writeFileSync(path + ".compact.js", temporary[name + ".compact.js"]);
     }
     fs.writeFileSync(path + ".json", JSON.stringify(json, true, 4));
 }
 
-for (idx = 0; idx < sources._list.length; ++idx) {
-    save(sources._list[idx], version);
-}
+mkDir("../tmp/build/" + version);
+gpf.sources().every(function (name) {
+    if (!name) {
+        // end marker
+        return false;
+    }
+    save(name);
+});
 save("UMD", version);
 save("boot", version);
 save("result", version);
 
-if (!fs.existsSync("../build")) {
-    fs.mkdirSync("../build");
-}
-output = ["../build/gpf-", version, ".js"];
-fs.writeFileSync(output.join(""), sources[version]["result.js"]);
+mkDir("../build");
+fs.writeFileSync("../build/gpf-" + version + ".js", result);
 
 // Use google compiler
 if ("release" === version) {
-    require("closure-compiler").compile(sources[version]["result.js"], {
-            //some: 'flag'
-            //, values: ['1', '2']
-        }, function (err, stdout/*, stderr*/) {
-            if (err) {
-                console.error(err);
-            } else {
-                fs.writeFileSync("../build/gpf.js", stdout);
-            }
+    require("closure-compiler").compile(result, {}, function (err, stdout/*, stderr*/) {
+        if (err) {
+            console.error(err);
+        } else {
+            fs.writeFileSync("../build/gpf.js", stdout);
         }
-    );
+    });
 }
