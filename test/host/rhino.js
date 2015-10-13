a641c81183b162342a9d7d0e4b000daa726e72c5
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-10-13 00:15:18 -0400

    Added test cases

--- test/host/rhino.js
+++ test/host/rhino.js
@@ -1,6 +1,25 @@
 /*jshint rhino:true*/
+/*global run*/
+
 print("Rhino showcase");
 /*exported gpfSourcesPath*/
 var gpfSourcesPath = "src/";
 load("src/boot.js");
-java.lang.System.exit(0);
+
+load("test/host/bdd.js");
+load("test/host/console.js");
+
+var sources = gpf.sources(),
+    len = sources.length,
+    idx,
+    src;
+for (idx = 0; idx < len; ++idx) {
+    src = sources[idx];
+    if (!src) {
+        break;
+    }
+    load("test/" + src + ".js");
+}
+
+run();
+gpf.handleTimeout();
