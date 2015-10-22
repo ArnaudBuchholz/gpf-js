f7ff59cc8e6a144cb47923edb4263714e51d998f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-10-22 16:33:52 -0400

    ESLint fixes

--- test/timeout.js
+++ test/timeout.js
@@ -17,35 +17,33 @@ describe("timeout", function () {
     });
 
     it("transmits scope", function (done) {
-        var scope = {},
-            /**
-             * Will be bound
-             *
-             * @this
-             */
-            callback = function () {
-                assert(this === scope);
-                done();
-            };
+        var scope = {};
+        /**
+         * @this bound scope
+         */
+        function callback () {
+            /*jshint validthis:true*/
+            assert(this === scope);
+            done();
+        }
         setTimeout(callback.bind(scope), 0);
     });
 
     it("transmits parameters", function (done) {
         var scope = {},
-            obj = {},
-            /**
-             * Will be bound
-             *
-             * @this
-             */
-            callback = function (a, b, c) {
-                assert(this === scope);
-                assert(arguments.length === 3);
-                assert(1 === a);
-                assert(undefined === b);
-                assert(obj === c);
-                done();
-            };
+            obj = {};
+        /**
+         * @this bound scope
+         */
+        function callback (a, b, c) {
+            /*jshint validthis:true*/
+            assert(this === scope);
+            assert(arguments.length === 3);
+            assert(1 === a);
+            assert(undefined === b);
+            assert(obj === c);
+            done();
+        }
         setTimeout(callback.bind(scope, 1, undefined, obj), 0);
     });
 
