244c2b87776f09eefa4569e258577d76bf4fc7a5
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-10-23 00:37:06 -0400

    Eslint fixes

--- test/interfaces.js
+++ test/interfaces.js
@@ -3,6 +3,8 @@
 /*eslint-env mocha*/
 /*global assert*/
 
+/*eslint-disable max-nested-callbacks*/
+
 describe("interfaces", function () {
 
     var gpfIEventDispatcher = gpf.interfaces.IEventDispatcher;
