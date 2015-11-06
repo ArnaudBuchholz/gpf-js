c5381f8ce94db62b04f637c08d343f96cbacc763
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-11-06 13:50:32 -0500

    Improved coverage

--- test/like.js
+++ test/like.js
@@ -39,7 +39,7 @@ describe("like", function () {
             assert(true === gpf.like(object, object));
         });
 
-        it("tolerates alike comparison", function () {
+        it("supports alike comparisons", function () {
             assert(true === gpf.like(1, new Number(1), true));
             assert(false === gpf.like("1", new Number(1), true));
             assert(true === gpf.like("abc", new String("abc"), true));
@@ -48,12 +48,59 @@ describe("like", function () {
             assert(false === gpf.like(0, new Boolean(false), true));
         });
 
-        it("tolerates deep comparison", function () {
-            assert(true === gpf.like(object, gpf.extend({}, object)));
-            assert(true === gpf.like(object2, gpf.extend({}, object2)));
-        });
+        describe("objects", function () {
 
-        it("supports complex deep comparison");
+            it("compares object members", function () {
+                assert(false === gpf.like(object, gpf.extend({rigtOnly: true}, object)));
+                assert(false === gpf.like(gpf.extend({leftOnly: true}, object)), object);
+            });
+
+            it("compares members' value", function () {
+                var left = gpf.extend({}, object),
+                    right = gpf.extend({}, object);
+                left.different = true;
+                right.different = false;
+                assert(false === gpf.like(left, right));
+            });
+
+            it("supports deep comparison", function () {
+                assert(true === gpf.like(object, gpf.extend({}, object)));
+                assert(true === gpf.like(object2, gpf.extend({}, object2)));
+            });
+
+            it("supports recursive comparison", function () {
+                var left = gpf.extend({}, object),
+                    right = gpf.extend({}, object);
+                left.test1 = left;
+                right.test1 = right;
+                left.test2 = right;
+                right.test2 = left;
+                assert(true === gpf.like(left, right));
+            });
+
+            describe("class comparison", function () {
+
+                function A () {
+                    this.value = 0;
+                }
+                function B () {
+                    this.value = 0;
+                }
+
+                var a = new A(),
+                    b = new B();
+
+                it("compares prototypes", function () {
+                    assert(false === gpf.like(a, b));
+                });
+
+                it("supports alike comparison", function () {
+                    assert(true === gpf.like(a, b, true));
+                });
+
+            });
+
+        });
 
     });
 
