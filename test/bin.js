bbe1ebc17065411871e3eca42c073bc95249fd72
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-11-07 00:07:37 -0500

    Improved coverage

--- test/bin.js
+++ test/bin.js
@@ -10,7 +10,8 @@ describe("bin", function () {
     var verifiedPows = [
         1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
         131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216,
-        33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296
+        33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296,
+        8589934592, 17179869184
     ];
 
     describe("gpf.bin.pow", function () {
@@ -36,6 +37,10 @@ describe("bin", function () {
     describe("gpf.bin.toAny", function () {
 
         describe("gpf.bin.toHexa", function () {
+
+            it("converts zero to zero", function () {
+                assert(gpf.bin.toHexa(0) === "0");
+            });
 
             it("converts a number to its hexadecimal value", function () {
                 assert(gpf.bin.toHexa(2882400152) === "ABCDEF98");
