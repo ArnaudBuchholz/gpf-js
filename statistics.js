485302be569e6577e8499f935edd8b040563fa12
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-11-17 22:18:56 -0500

    WIP

--- statistics.js
+++ statistics.js
@@ -1,11 +1,19 @@
-var fs = require("fs"),
-    coverageData = JSON.parse(fs.readFileSync("tmp/coverage/reports/coverage.json"));
+/*jshint node: true*/
+/*eslint-env node*/
+/*eslint-disable no-sync*/
+"use strict";
 
-var parts = {
-    statements: ["s", "statementMap"],
-    functions: ["f", "fnMap"],
-    branches: ["b", "branchMap"]
-}
+var fs = require("fs"),
+    coverageData = JSON.parse(fs.readFileSync("tmp/coverage/reports/coverage.json")),
+    coverageParts = {
+        statements: ["s", "statementMap"],
+        functions: ["f", "fnMap"],
+        branches: ["b", "branchMap"]
+    };
+
+// Use gpf library (source version)
+global.gpfSourcesPath = "src/";
+require("./src/boot.js");
 
 function computeCoverage (data)     {
     var result = {},
@@ -18,13 +26,13 @@ function computeCoverage (data)     {
         itemId,
         itemData,
         itemMap;
-    for (part in parts) {
-        if (parts.hasOwnProperty(part)) {
+    for (part in coverageParts) {
+        if (coverageParts.hasOwnProperty(part)) {
             count = 0;
             tested = 0;
             ignored = 0;
-            partData = data[parts[part][0]];
-            partMap = data[parts[part][1]];
+            partData = data[coverageParts[part][0]];
+            partMap = data[coverageParts[part][1]];
             for (itemId in partData) {
                 if (partData.hasOwnProperty(itemId)) {
                     ++count;
@@ -61,11 +69,9 @@ function computeCoverage (data)     {
     return result;
 }
 
-for (var file in coverageData) {
-    var fileCoverage = coverageData[file];
-    console.log(file);
+gpf.forEach(coverageData, function (fileCoverage, fileName) {
+    console.log(fileName);
     // console.log(fileCoverage);
     var coverage = computeCoverage(fileCoverage);
     console.log(coverage);
-    // process.exit(0);
-}
+});
