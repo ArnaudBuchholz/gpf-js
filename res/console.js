6ea5023e75dc070398f5328e96ffbe6e8de6dd19
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-11-20 12:50:53 -0500

    Create console.js

---
+++ res/console.js
@@ -0,0 +1,19 @@
+(function () {
+    "use strict";
+
+    var _console = window.console,
+        _wrapMethod = function (methodName) {
+            return function () {
+                _console[methodName].apply(_console, arguments);
+                var div = document.createElement("div");
+                div.appendChild(document.createTextNode(arguments[0].toString()));
+                document.body.appendChild(div).className = methodName;
+            };
+        };
+    window.console = {
+        log: _wrapMethod("log"),
+        warn: _wrapMethod("warn"),
+        error: _wrapMethod("error")
+    };
+
+}());
