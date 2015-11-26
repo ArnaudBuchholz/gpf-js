fb03dabc1f905862c7e5fdf22fa35c872adfb750
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-11-26 13:15:06 -0500

    Update boot.js with package version

---
+++ make/version.js
@@ -0,0 +1,20 @@
+/*jshint node: true*/
+/*eslint-env node*/
+/*eslint-disable no-sync*/
+"use strict";
+
+var fs = require("fs");
+
+var packageJson = JSON.parse(fs.readFileSync("package.json").toString()),
+    packageVersion = packageJson.version,
+    bootSrc = fs.readFileSync("src/boot.js").toString(),
+    bootVersionMatch = (/_gpfVersion += +\"(.*)\"/).exec(bootSrc),
+    bootVersion = bootVersionMatch[1];
+
+if (bootVersion !== packageVersion) {
+    console.log("Package version: " + packageJson.version);
+    console.log("Boot version   : " + bootVersion);
+    bootSrc = bootSrc.replace(bootVersionMatch[0], "_gpfVersion = \"" + packageVersion + "\"");
+    fs.writeFileSync("src/boot.js", bootSrc);
+    console.log("src/boot.js updated" + bootVersion);
+}
