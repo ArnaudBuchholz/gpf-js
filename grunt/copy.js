8c3cf1ffc2a85e00a77766ae7c7c365986fed18e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-02 15:14:16 -0500

    publishVersion

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,21 +1,36 @@
 "use strict";
 /*jshint node: true*/
 /*eslint-env node*/
+/*global configuration*/
 
 /*eslint-disable no-sync*/
 
-var IO_PATH = "../ArnaudBuchholz.github.io",
+var IO_PATH = "../ArnaudBuchholz.github.io/",
     fs = require("fs"),
     copy = {
-        getPlatoHistory: {}
+        getPlatoHistory: {},
+        setVersionPlato: {},
+        publishVersion: {}
     };
 
 if (fs.existsSync(IO_PATH)) {
     copy.getPlatoHistory = {
         expand: true,
         flatten: true,
-        src: IO_PATH + "/plato/gpf-js/report.history.*",
+        src: IO_PATH + "plato/gpf-js/report.history.*",
         dest: "./tmp/plato/"
+    };
+    copy.setVersionPlato = {
+        expand: true,
+        cwd: "./tmp/plato/",
+        src: "**/*.*",
+        dest: IO_PATH + "gpf/" + configuration.pkg.version + "/plato/"
+    };
+    copy.publishVersion = {
+        expand: true,
+        cwd: "./build/",
+        src: ["gpf.js", "gpf-debug.js"],
+        dest: IO_PATH + "gpf/" + configuration.pkg.version + "/"
     };
 }
 
