aa2be81b3d95e978252fd207fedd519cfe28d8e0
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-01 19:45:06 -0500

    Coverage/instruments

---
+++ grunt/instrument.js
@@ -0,0 +1,14 @@
+"use strict";
+/*jshint node: true*/
+/*eslint-env node*/
+
+module.exports = {
+    files: "src/*.js",
+    options: {
+        lazy: true,
+        debug: true,
+        noCompact: true,
+        noAutoWrap: true,
+        basePath: "tmp/coverage/instrument/"
+    }
+};
