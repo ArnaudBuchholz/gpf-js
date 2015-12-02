c105f9a8741e4e44d5da0bd2dcb045fa5706c5da
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-01 19:44:34 -0500

    Coverage instruments fixing

---
+++ grunt/tasks/fixInstrument.js
@@ -0,0 +1,27 @@
+"use strict";
+/*jshint node: true*/
+/*eslint-env node*/
+/*global configuration*/
+
+/*eslint-disable no-sync*/
+
+module.exports = function (grunt) {
+    grunt.registerTask("fixInstrument", function () {
+        // Because code generation uses templates that are instrumented, the __cov_XXX variables must be global
+        var fs = require("fs");
+        configuration.srcFiles.forEach(function (fileName) {
+            var srcPath = "tmp/coverage/instrument/" + fileName,
+                instrumentedLines = fs
+                    .readFileSync(srcPath)
+                    .toString()
+                    .split("\n"),
+            // Assuming the __cov_ variable is on the second line
+                secondLine = instrumentedLines[1];
+            if (0 === secondLine.indexOf("var ")) {
+                instrumentedLines[1] = "global." + secondLine.substr(4);
+                fs.writeFileSync(srcPath, instrumentedLines.join("\n"));
+                console.log(fileName + " updated");
+            }
+        });
+    });
+};
