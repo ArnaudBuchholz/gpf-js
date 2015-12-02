3d4db7e96be1fdc5cd7cf579cb0ab1902e487751
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-01 19:44:42 -0500

    Make task

---
+++ grunt/tasks/make.js
@@ -0,0 +1,21 @@
+"use strict";
+/*jshint node: true*/
+/*eslint-env node*/
+
+module.exports = function (grunt) {
+    grunt.registerTask("plato", [
+        "exec:version",
+        "default",
+        "mocha:source",
+        "exec:testWscript",
+        "exec:testRhino",
+        "exec:buildDebug",
+        "exec:buildRelease",
+        "mocha:debug",
+        "mochaTest:debug",
+        "exec:testWscriptDebug",
+        "mocha:release",
+        "mochaTest:release",
+        "exec:testWscriptRelease"
+    ]);
+};
