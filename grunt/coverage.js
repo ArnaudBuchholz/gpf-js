51a747c139985a2e48540dd22e1049d7bd8851a3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-01 19:43:34 -0500

    Coverage options

---
+++ grunt/coverage.js
@@ -0,0 +1,17 @@
+"use strict";
+/*jshint node: true*/
+/*eslint-env node*/
+
+module.exports = {
+    "default": {
+        options: {
+            thresholds: {
+                statements: 90,
+                branches: 90,
+                lines: 90,
+                functions: 90
+            },
+            dir: "tmp/coverage/reports"
+        }
+    }
+};
