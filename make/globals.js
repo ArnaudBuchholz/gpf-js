ffc96b1d852d0fb79b352b5b1b5881db262d1ea4
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-12-16 00:04:46 -0500

    Rewrote (WIP)

--- make/globals.js
+++ make/globals.js
@@ -10,71 +10,138 @@ require("../src/sources.js");
 var sources = gpf.sources();
 sources.unshift("boot"); // Also include boot
 
-var modules = {},
-    globalsOrigin = {};
+var rc = 0;
 
-function analyzeLine (module, line, lineIndex) {
-    var name,
-        description;
-    if (0 === lineIndex) {
-        // first line *must* be /*#ifndef(UMD)*/
+function Module (name) {
+    this.name = name;
+    Module.byName[name] = this;
+    this.lines = fs.readFileSync("./src/" + name + ".js").toString().split("\n");
+    this.imports = [];
+    this.exports = {};
+    this.filteredLines = [];
+}
+
+Module.prototype = {
+    // @property {String[]} Content lines
+    lines: [],
+
+    // @property {String[]} Imported symbols
+    imports: [],
+
+    // @property {Object} Dictionary of exported symbols mapping to their description
+    exports: {},
+
+    // @property {Number[]} Lines containing import/export definition
+    filteredLines: [],
+
+    // Log an error
+    error: function (text) {
+        console.error("[" + this.name + "] " + text);
+        --rc;
+    },
+
+    // Analyze the file content
+    analyze: function () {
+        this.lines.every(this._dispatchLine, this);
+    },
+
+    // Select method for the given line
+    _dispatchLine: function (line, lineIndex) {
+        if (0 === lineIndex) {
+            return this._checkLine0(line);
+        }
+        if (1 === lineIndex) {
+            return this._checkLine1(line);
+        }
+        if (line === "/*#endif*/") {
+            return false; // Stop
+        }
+        return this._analyzeLine(line, lineIndex);
+    },
+
+    // First line *must* be /*#ifndef(UMD)*/
+    _checkLine0: function (line) {
         if (line !== "/*#ifndef(UMD)*/") {
-            console.error("[" + module.name + "] missing /*#ifndef(UMD)*/");
+            module.error("missing /*#ifndef(UMD)*/");
             return false;
         }
         return true;
-    }
-    if (1 === lineIndex) {
-        // second line *must* be "use strict";
+    },
+
+    // Second line *must* be "use strict";
+    _checkLine1: function (line) {
         if (line !== "\"use strict\";") {
-            console.error("[" + module.name + "] missing \"use strict\";");
+            module.error("missing \"use strict\";");
             return false;
         }
         return true;
-    }
-    if (line === "/*#endif*/") {
-        return false;
-    }
-    if (0 === line.indexOf("/*global ")) {
-        name = line.split("*/")[0].substr(9).trim();
-        module.imports.push(name);
+    },
 
-    } else if (0 === line.indexOf("/*exported ")) {
-        name = line.split("*/")[0].substr(11).trim();
-        description = line.split("// ")[1] || "";
-        if (!description) {
-            console.error("[" + module.name + "] no description for " + name);
+    // Content line: select the proper handling
+    _analyzeLine: function (line, lineIndex) {
+        if (0 === line.indexOf("/*global ")) {
+            return this._processImport(line, lineIndex);
         }
-        module.exports[name] = description;
-        globalsOrigin[name] = module.name;
-
-    } else if (-1 < line.indexOf("eslint") || -1 < line.indexOf("jshint")) {
-        // ignore
-        return true;
-
-    } else {
-        console.error("[" + module.name + "] line " + (lineIndex + 1) + " not recognized");
+        if (0 === line.indexOf("/*exported ")) {
+            return this._processExport(line, lineIndex);
+        }
+        if (-1 < line.indexOf("eslint") || -1 < line.indexOf("jshint")) {
+            // ignore
+            return true;
+        }
+        this.error("line " + (lineIndex + 1) + " not recognized");
         return false;
-    }
-    module.filteredLines.push(lineIndex);
-    return true;
-}
+    },
 
-function analyze (name) {
-    var module = modules[name] = {};
-    module.name = name;
-    module.lines = fs.readFileSync("./src/" + name + ".js").toString().split("\n");
-    module.imports = [];
-    module.exports = {};
-    module.filteredLines = [];
-    module.lines.every(analyzeLine.bind(null, module));
-}
+    // global found
+    _processImport: function (line, lineIndex) {
+        var name = line.split("*/")[0].substr(9).trim();
+        this.imports.push(name);
+        this.filteredLines.push(lineIndex);
+        return true;
+    },
+
+    // exported found
+    _processExport: function (line, lineIndex) {
+        var name = line.split("*/")[0].substr(11).trim(),
+            description = line.split("// ")[1] || "";
+        if (!description) {
+            this.error("no description for " + name);
+        }
+        this.exports[name] = description;
+        Module.byExport[name] = this;
+        this.filteredLines.push(lineIndex);
+        return true;
+    },
+
+    // returns true if modified
+    rebuild: function () {
+        var before = this.lines.join("\n"),
+            lines = this.lines.map(function (line, lineIndex) {
+                return -1 === this.filteredLines.indexOf(lineIndex);
+            }, this),
+            headerLines = [];
+        this.imports.sort().forEach(function (name) {
+            var global = "/*global " + name + "*/ // " + Module.byExport[name].exports[name];
+            headerLines.push(global);
+        });
+        Object.keys(this.exports).sort().forEach(function (name) {
+            var exported = "/*exported " + name + "*/ // " + this.exports[name];
+            headerLines.push(exported);
+        });
+    }
+
+};
+
+Module.byName = {};
+Module.byExport = {};
 
 sources.every(function (source) {
     if (source === "") {
         return false;
     }
-    analyze(source);
+    var module = new Module(source);
+    module.analyze();
     return true;
 });
 
@@ -83,13 +150,12 @@ sources.every(function (source) {
     if (source === "") {
         return false;
     }
-    var module = modules[source],
+    var module = Module.byName[source],
         regionLines = [
             "//region " + source
         ],
         moduleExports = module.exports,
         names = Object.keys(moduleExports).sort(),
-        origin,
         description,
         global;
     names.forEach(function (name) {
@@ -102,7 +168,7 @@ sources.every(function (source) {
     });
     if (1 < regionLines.length) {
         regionLines.push("//endregion " + source);
-        // console.log(regionLines.join("\n"));
+        console.log(regionLines.join("\n"));
     }
     return true;
 });
