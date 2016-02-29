9b8116c5cb10d231ae02199c9a0a0dd51d36c322
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-02-28 23:23:43 -0500

    Use common loader

--- test/host/phantomjs.js
+++ test/host/phantomjs.js
@@ -1,10 +1,22 @@
 "use strict";
 /*jshint phantom: true*/
 /*eslint-env phantomjs*/
-var fs = require("fs"),
-    loadTests = require("./node_loader.js");
-require("./bdd.js"); /*global run*/
-loadTests(function (path) {
-    return fs.read(path);
+
+var fs = require("fs");
+require("./loader.js"); /*global loadGpfAndTests*/
+
+loadGpfAndTests({
+    global: global,
+    parameters: require("system").args.slice(1),
+    // https://groups.google.com/forum/#!msg/phantomjs/OswbWiKrLYI/ndoXvK13OrIJ
+    gpfPath: phantom.libraryPath.split("/").slice(0, -2).join(fs.separator),
+    pathSeparator: fs.separator,
+    log: function (text) {
+        console.log(text);
+    },
+    exit: phantom.exit,
+    require: require,
+    read: function (filePath) {
+        return fs.read(filePath);
+    }
 });
-run();
