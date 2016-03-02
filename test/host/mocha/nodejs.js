d96922f3e1116ab788bcc6652cde0b34290e2147
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-03-01 23:22:54 -0500

    Using common loader

--- test/host/mocha/nodejs.js
+++ test/host/mocha/nodejs.js
@@ -1,14 +1,33 @@
 "use strict";
+
 global.assert = require("assert");
 var Mocha = require("mocha"),
     mocha = new Mocha({ui: "bdd"}),
-    loadTests = require("../node_loader.js");
-loadTests(function (path) {
-    mocha.addFile(path);
-});
-// Now, you can run the tests.
-mocha.run(function (failures) {
-    process.on("exit", function () {
-        process.exit(failures);
-    });
+    path = require("path"),
+    fs = require("fs");
+
+require("../loader.js"); /*global loadGpfAndTests*/
+
+loadGpfAndTests({
+    parameters: process.argv.slice(2),
+    gpfPath: path.resolve(__dirname, "../../.."),
+    pathSeparator: path.sep,
+    log: console.log.bind(console),
+    exit: process.exit,
+    require: require,
+    read: function (filePath) {
+        return fs.readFileSync(filePath).toString();
+    },
+    useBDD: false,
+    loadTest: function (filePath) {
+        mocha.addFile(filePath);
+    },
+    done: function () {
+        // Now, you can run the tests.
+        mocha.run(function (failures) {
+            process.on("exit", function () {
+                process.exit(failures);
+            });
+        });
+    }
 });
