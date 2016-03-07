ba3a340020adf44d99ab902d1f32574724416d8f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-03-06 23:07:00 -0500

    Tested date conversion

--- test/base.js
+++ test/base.js
@@ -55,24 +55,7 @@ describe("base", function () {
             }
         },
         objectMembers = "number,string,null,object,function",
-        objectMembersNoNull = "number,string,object,function",
-        valuesTesting = [
-            /* value parameters,                        expected result, message */
-            [0, 1, undefined, 0,                        "nothing on numbers"],
-            ["0", 1, undefined, 0,                      "number from string"],
-            ["0", true, undefined, false,               "boolean from string (false)"],
-            ["yes", false, undefined, true,             "boolean from string (true)"],
-            [0, true, undefined, false,                 "boolean from number (false)"],
-            [1, false, undefined, true,                 "boolean from number (true)"],
-            [{}, false, undefined, false,               "boolean from anything else"],
-            [undefined, "empty", undefined, "empty",    "nothing and use default value"],
-            ["1.2", 1.1, undefined, 1.2,                "string to float"],
-            ["1.2", 1, "number", 1.2,                   "string to float as a number"],
-            [{}, 1, undefined, 1,                       "number from anything else"],
-            [1, "", undefined, "1",                     "string fron number"],
-            [1, object, undefined, object,              "object (only default)"]
-
-        ];
+        objectMembersNoNull = "number,string,object,function";
 
     describe("gpf.forEach", function () {
 
@@ -191,11 +174,37 @@ describe("base", function () {
 
         var date = new Date(2003, 0, 22, 23, 45, 0, 0);
 
-        valuesTesting.forEach(function (parameters) {
+        [
+            /* value parameters,                        expected result, message */
+            [0, 1, undefined, 0,                        "nothing on numbers"],
+            ["0", 1, undefined, 0,                      "number from string"],
+            ["0", true, undefined, false,               "boolean from string (false)"],
+            ["yes", false, undefined, true,             "boolean from string (true)"],
+            [0, true, undefined, false,                 "boolean from number (false)"],
+            [1, false, undefined, true,                 "boolean from number (true)"],
+            [{}, false, undefined, false,               "boolean from anything else"],
+            [undefined, "empty", undefined, "empty",    "nothing and use default value"],
+            ["1.2", 1.1, undefined, 1.2,                "string to float"],
+            ["1.2", 1, "number", 1.2,                   "string to float as a number"],
+            [{}, 1, undefined, 1,                       "number from anything else"],
+            [1, "", undefined, "1",                     "string fron number"],
+            [1, object, undefined, object,              "object (only default)"],
+            [new Date("2013-01-22"), "", undefined, "2013-01-22T00:00:00.000Z",
+                                                        "string from date"],
+            ["2013-01-22T00:00:00.000Z", new Date(), undefined, new Date("2013-01-22"),
+                                                        "date from string"]
+
+        ].forEach(function (parameters) {
 
             it("converts " + parameters[4], function () {
-                var result = gpf.value.apply(null, parameters.slice(0, 3));
-                assert(result === parameters[3]);
+                var result = gpf.value.apply(null, parameters.slice(0, 3)),
+                    expected = parameters[3];
+                if ("object" === typeof result && "object" === typeof expected) {
+                    // Compare string versions
+                    assert(result.toString() === parameters[3].toString());
+                } else {
+                    assert(result === parameters[3]);
+                }
             });
 
         });
