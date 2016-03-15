e622ad35ba07b965d53f809c169dc098212cf4ab
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-15 13:15:05 -0400

    Created aliases for common tasks

--- grunt/tasks/aliases.js
+++ grunt/tasks/aliases.js
@@ -2,4 +2,9 @@
 
 module.exports = function (grunt) {
     grunt.registerTask("plato", "exec:plato");
+    grunt.registerTask("wscript", "exec:testWscriptVerbose");
+    grunt.registerTask("rhino", "exec:testRhinoVerbose");
+    configuration.selenium.forEach(function (browser) {
+        grunt.registerTask(browser, "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose");
+    });
 };
