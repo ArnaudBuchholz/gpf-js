e89576e67231f306cb8a40b6b8b6dcd9186c1536
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-15 13:14:47 -0400

    Selenium configuration is dynamic

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -1,28 +1,36 @@
 "use strict";
 
 module.exports = function (grunt) {
-    grunt.registerTask("make", [
+    var testTasks = [
+        "exec:testWscript",
+        "exec:testRhino"
+    ];
+    configuration.selenium.forEach(function (browser) {
+        testTasks.push("exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1));
+    });
+    grunt.registerTask("make", [].concat([
         "exec:version",
         "default",
-        "mocha:source",
-        "exec:testWscript",
-        "exec:testRhino",
+        "mocha:source"
+    ], testTasks, [
         "exec:buildDebug",
         "exec:buildRelease",
         "uglify:buildRelease",
         "mocha:debug",
         "mochaTest:debug",
-        "restoreConsole",
-        "exec:testWscriptDebug",
-        "exec:testRhinoDebug",
+        "restoreConsole"
+    ], testTasks.map(function (name) {
+        return name + "Debug";
+    }), [
         "mocha:release",
         "mochaTest:release",
-        "restoreConsole",
-        "exec:testWscriptRelease",
-        "exec:testRhinoRelease",
+        "restoreConsole"
+    ], testTasks.map(function (name) {
+        return name + "Release";
+    }), [
         "uglify:buildTests",
         "copy:publishVersionPlato",
         "copy:publishVersion",
         "copy:publishTest"
-    ]);
+    ]));
 };
