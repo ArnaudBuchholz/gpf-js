97a2922d36b329d38101de34c0cee264b9896953
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-18 15:53:12 -0400

    Tests are now run in parallel

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -1,33 +1,15 @@
 "use strict";
 
 module.exports = function (grunt) {
-    var testTasks = [
-        "exec:testWscript",
-        "exec:testRhino"
-    ];
-    configuration.selenium.forEach(function (browser) {
-        testTasks.push("exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1));
-    });
     grunt.registerTask("make", [].concat([
         "exec:version",
         "default",
-        "mocha:source"
-    ], testTasks, [
+        "concurrent:source",
         "exec:buildDebug",
         "exec:buildRelease",
         "uglify:buildRelease",
-        "mocha:debug",
-        "mochaTest:debug",
-        "restoreConsole"
-    ], testTasks.map(function (name) {
-        return name + "Debug";
-    }), [
-        "mocha:release",
-        "mochaTest:release",
-        "restoreConsole"
-    ], testTasks.map(function (name) {
-        return name + "Release";
-    }), [
+        "concurrent:debug",
+        "concurrent:release",
         "uglify:buildTests",
         "copy:publishVersionPlato",
         "copy:publishVersion",
