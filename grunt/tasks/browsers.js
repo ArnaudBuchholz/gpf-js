8fd1945e91bd6faa0ced94d224186177e1435e53
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-18 16:18:07 -0400

    Aliases for browsers

--- grunt/tasks/browsers.js
+++ grunt/tasks/browsers.js
@@ -1,9 +1,6 @@
 "use strict";
 
 module.exports = function (grunt) {
-    grunt.registerTask("plato", "exec:plato");
-    grunt.registerTask("wscript", "exec:testWscriptVerbose");
-    grunt.registerTask("rhino", "exec:testRhinoVerbose");
     configuration.selenium.forEach(function (browser) {
         grunt.registerTask(browser, "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose");
     });
