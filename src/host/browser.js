9f0a3e2b9862da6f9b6957350ce6fb965b778ed9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-21 12:45:53 -0400

    Host specific (normalized)

---
+++ src/host/browser.js
@@ -0,0 +1,21 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST_BROWSER*/ // gpf.HOST_BROWSER
+/*global _gpfHost*/ // Host type
+/*global _gpfExit:true*/ // Exit function
+/*global _gpfMainContext:true*/ // Main context object
+/*global _gpfInBrowser:true*/ // The current host is a browser like
+/*#endif*/
+
+/*jshint browser: true*/
+/*eslint-env browser*/
+
+if (_GPF_HOST_BROWSER === _gpfHost) {
+
+    _gpfMainContext = window;
+    _gpfInBrowser = true;
+    _gpfExit = function (code) {
+        window.location = "https://arnaudbuchholz.github.io/gpf/exit.html?" + (code || 0);
+    };
+
+}
