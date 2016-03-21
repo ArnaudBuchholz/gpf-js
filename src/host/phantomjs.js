ecb0c2b4cf6f8699e9c50563721733ef74bd9fc6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-21 12:46:13 -0400

    Host specific (normalized)

---
+++ src/host/phantomjs.js
@@ -0,0 +1,22 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST_PHANTOMJS*/ // gpf.HOST_PHANTOMJS
+/*global _gpfHost*/ // Host type
+/*global _gpfExit:true*/ // Exit function
+/*global _gpfMainContext:true*/ // Main context object
+/*global _gpfInNode:true*/ // The current host is a nodeJS like
+/*global _gpfInBrowser:true*/ // The current host is a browser like
+/*#endif*/
+
+/*jshint phantom: true*/
+/*jshint browser: true*/
+/*eslint-env phantomjs, browser*/
+
+if (_GPF_HOST_PHANTOMJS === _gpfHost) {
+
+    _gpfMainContext = window;
+    _gpfInNode = true;
+    _gpfInBrowser = true;
+    _gpfExit = phantom.exit;
+
+}
