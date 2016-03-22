940acf6103888c594a4db86e55540ea65baeae7a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-22 13:09:33 -0400

    noConflict as a separate module

---
+++ src/noconflict.js
@@ -0,0 +1,36 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfMainContext*/ // Main context object
+/*#endif*/
+
+var /**
+     * To implement gpf.noConflict(), we need to keep the previous content of gpf.
+     * Makes sense only for the following hosts:
+     * - phantomjs
+     * - browser
+     * - unknown
+     *
+     * @type {undefined|Object}
+     */
+    _gpfConflictingSymbol;
+
+/*#ifdef(UMD)*/
+
+_gpfConflictingSymbol = _gpfMainContext.gpf;
+
+/* istanbul ignore next */ // web only
+/**
+ * Relinquish control of the gpf variable.
+ *
+ * @return {Object} current GPF instance
+ */
+gpf.noConflict = function () {
+    if (undefined === _gpfConflictingSymbol) {
+        delete _gpfMainContext.gpf;
+    } else {
+        _gpfMainContext.gpf = _gpfConflictingSymbol;
+    }
+    return gpf;
+};
+
+/*#endif*/
