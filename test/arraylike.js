207faa8191f1f51ed035941162b73ed88b55d7cc
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-03-28 10:36:41 -0400

    Test isArrayLike

---
+++ test/arraylike.js
@@ -0,0 +1,13 @@
+"use strict";
+
+describe("arraylike", function () {
+
+    describe("gpf.isArrayLike", function () {
+
+        it("checks if an object looks like an array", function () {
+            assert(true === gpf.isArrayLike([]));
+        });
+
+    });
+
+});
