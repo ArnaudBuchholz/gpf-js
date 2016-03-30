f0723ce126dd00f97957ed48d6c98b5579d66a28
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-03-30 13:14:22 -0400

    Connect is required for selenium tests

--- grunt/tasks/browsers.js
+++ grunt/tasks/browsers.js
@@ -2,6 +2,9 @@
 
 module.exports = function (grunt) {
     configuration.selenium.forEach(function (browser) {
-        grunt.registerTask(browser, "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose");
+        grunt.registerTask(browser, [
+            "connect:server",
+            "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose"
+        ]);
     });
 };
