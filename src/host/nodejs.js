004ca37422526bec0b5122d3784b47bd331fcf1b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-04-02 09:44:52 -0400

    Appears to be useless (done in boot)

--- src/host/nodejs.js
+++ src/host/nodejs.js
@@ -3,7 +3,6 @@
 /*global _GPF_HOST_NODEJS*/ // gpf.HOST_NODEJS
 /*global _gpfHost*/ // Host type
 /*global _gpfExit:true*/ // Exit function
-/*global _gpfMainContext:true*/ // Main context object
 /*global _gpfInNode:true*/ // The current host is a nodeJS like
 /*#endif*/
 
@@ -12,7 +11,6 @@
 
 if (_GPF_HOST_NODEJS === _gpfHost) {
 
-    _gpfMainContext = global;
     _gpfInNode = true;
     _gpfExit = process.exit;
 
