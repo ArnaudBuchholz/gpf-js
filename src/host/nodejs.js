6fdee93f3cf326b771ec14cb48da0401898dfa74
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-04-04 22:43:41 -0400

    Improves coverage

--- src/host/nodejs.js
+++ src/host/nodejs.js
@@ -1,14 +1,15 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST_NODEJS*/ // gpf.HOST_NODEJS
-/*global _gpfHost*/ // Host type
 /*global _gpfExit:true*/ // Exit function
+/*global _gpfHost*/ // Host type
 /*global _gpfInNode:true*/ // The current host is a nodeJS like
 /*#endif*/
 
 /*jshint node: true*/
 /*eslint-env node*/
 
+/* istanbul ignore else */ // Tested with NodeJS
 if (_GPF_HOST_NODEJS === _gpfHost) {
 
     _gpfInNode = true;
