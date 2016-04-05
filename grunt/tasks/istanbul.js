c3bbcc8fe53a039c43627fd0a4863da3e8544155
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-05 14:23:52 -0400

    merges with fixInstruments

--- grunt/tasks/istanbul.js
+++ grunt/tasks/istanbul.js
@@ -1,6 +1,26 @@
 "use strict";
 
 module.exports = function (grunt) {
+
+    grunt.registerTask("fixInstrument", function () {
+        // Because code generation uses templates that are instrumented, the __cov_XXX variables must be global
+        var fs = require("fs");
+        configuration.srcFiles.forEach(function (fileName) {
+            var srcPath = "tmp/coverage/instrument/" + fileName,
+                instrumentedLines = fs
+                    .readFileSync(srcPath)
+                    .toString()
+                    .split("\n"),
+            // Assuming the __cov_ variable is on the second line
+                secondLine = instrumentedLines[1];
+            if (0 === secondLine.indexOf("var ")) {
+                instrumentedLines[1] = "global." + secondLine.substr(4);
+                fs.writeFileSync(srcPath, instrumentedLines.join("\n"));
+                console.log(fileName + " updated");
+            }
+        });
+    });
+
     grunt.registerTask("istanbul", [
         "instrument",
         "fixInstrument",
@@ -10,4 +30,5 @@ module.exports = function (grunt) {
         "makeReport",
         "coverage"
     ]);
+
 };
