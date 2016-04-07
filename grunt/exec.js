e33abea806f2740939f80b97f566671f3fedfa01
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-04-06 22:55:14 -0400

    Rely on PATH for phantomjs

--- grunt/exec.js
+++ grunt/exec.js
@@ -70,8 +70,7 @@ function _buildTestConfig (name, command) {
 }
 
 _buildTestConfig("Node", "node test\\host\\nodejs.js");
-_buildTestConfig("Phantom", "node_modules\\grunt-mocha\\node_modules\\grunt-lib-phantomjs\\node_modules\\phantomjs\\"
-                            + "lib\\phantom\\phantomjs test\\host\\phantomjs.js");
+_buildTestConfig("Phantom", "phantomjs test\\host\\phantomjs.js");
 _buildTestConfig("Wscript", "cscript.exe /D /E:JScript test\\host\\cscript.js");
 _buildTestConfig("Rhino", "java -jar node_modules\\rhino-1_7r5-bin\\rhino1_7R5\\js.jar test\\host\\rhino.js");
 configuration.selenium.forEach(function (browser) {
