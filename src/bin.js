e1d9f458c8e26cc4712d1d3a3da90ac8ff91b105
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-12 17:25:38 -0400

    Improve maintainability

--- src/bin.js
+++ src/bin.js
@@ -7,24 +7,27 @@
 /*global _gpfMax32*/ // Max value on 32 bits
 /*#endif*/
 
-var
-    _gpfB64 = _gpfALPHA + _gpfAlpha + _gpfDigit + "+/",
+var _gpfB64 = _gpfALPHA + _gpfAlpha + _gpfDigit + "+/",
     _gpfB16 = "0123456789ABCDEF",
     _gpfBinZ = 987654321,
     _gpfBinW = (new Date()).getTime() & _gpfMax32;
+
+//region _gpfToBaseANY
+
+function _gpfGetBitLength (pow, length) {
+    if (undefined === length) {
+        return 32;
+    }
+    return length * pow;
+}
 
 function _gpfToBaseAnyEncodeUsingBitShifting (baseAndValue, pow, length) {
     var base = baseAndValue.base,
         value = baseAndValue.value,
         result = [],
-        bits,
+        bits = _gpfGetBitLength(pow, length),
         mask,
         digit;
-    if (undefined === length) {
-        bits = 32;
-    } else {
-        bits = length * pow;
-    }
     mask = (1 << bits - pow) - 1;
     bits = (1 << pow) - 1;
     while (0 !== value) {
@@ -49,13 +52,26 @@ function _gpfToBaseAnyEncodeUsingModulo (baseAndValue) {
     return result;
 }
 
+function _gpfIsBitShiftable (baseAndValue, pow, length) {
+    return -1 < pow && (undefined === length || length * pow <= 32);
+}
+
 function _gpfToBaseAnyEncode (baseAndValue, length) {
     var pow = gpf.bin.isPow2(baseAndValue.base.length);
-    if (-1 < pow && (undefined === length || length * pow <= 32)) {
+    if (_gpfIsBitShiftable(baseAndValue, pow, length)) {
         // Good conditions to use bits masking & shifting, will work with negative values and will be faster
         return _gpfToBaseAnyEncodeUsingBitShifting(baseAndValue, pow, length);
     }
     return _gpfToBaseAnyEncodeUsingModulo(baseAndValue);
+}
+
+function _gpfToBaseAnyPad (result, totalLength, pad) {
+    var padLength = pad.length,
+        resultLength = result.length;
+    while (resultLength < totalLength) {
+        result.unshift(pad.charAt(resultLength++ % padLength));
+    }
+    return result;
 }
 
 /**
@@ -72,23 +88,25 @@ function _gpfToBaseAnyEncode (baseAndValue, length) {
  */
 function _gpfToBaseANY (base, value, formattingOptions) {
     var length = formattingOptions.length,
-        pad = formattingOptions.pad,
-        result = _gpfToBaseAnyEncode({
-            base: base,
-            value: value
-        }, length);
-    // Padding
-    if (undefined !== length) {
-        if (undefined === pad) {
-            pad = base.charAt(0);
-        }
-        while (result.length < length) {
-            result.unshift(pad.charAt(result.length % pad.length));
-        }
-    } else if (0 === result.length) {
-        result = [base.charAt(0)]; // 0
+        paddingLength = length || 1,
+        paddingChars = formattingOptions.pad || base.charAt(0);
+    return _gpfToBaseAnyPad(_gpfToBaseAnyEncode({
+        base: base,
+        value: value
+    }, length), paddingLength, paddingChars).join("");
+}
+
+//endregion
+
+//region _gpfFromBaseANY
+
+// Skip initial padding characters
+function _gpfSkipPad (text, pad) {
+    var idx = 0;
+    while (idx < text.length && -1 === pad.indexOf(text.charAt(idx))) {
+        ++idx;
     }
-    return result.join("");
+    return idx;
 }
 
 /**
@@ -100,70 +118,93 @@ function _gpfToBaseANY (base, value, formattingOptions) {
  * @return {Number}
  */
 function _gpfFromBaseANY (base, text, pad) {
-    var
-        baseLength = base.length,
+    var baseLength = base.length,
         result = 0,
-        idx = 0;
-    if (undefined === pad) {
-        pad = base.charAt(0);
-    }
-    while (idx < text.length) {
-        if (-1 === pad.indexOf(text.charAt(idx))) {
-            break;
-        } else {
-            ++idx;
-        }
-    }
+        idx = _gpfSkipPad(text, pad || base.charAt(0));
     while (idx < text.length) {
         result = baseLength * result + base.indexOf(text.charAt(idx++));
     }
     return result;
 }
 
+//endregion
+
+//region _gpfPow2
+
+// Computes the power of 2, the slower way
+function _gpfSlowerComputePow2 (n) {
+    var result = _gpfMax32 + 1;
+    n -= 31;
+    while (--n) {
+        result *= 2;
+    }
+    return result;
+}
+
+// Computes the power of 2, try bit shifting
+function _gpfComputePow2 (n) {
+    if (31 > n) {
+        return 1 << n;
+    }
+    return _gpfSlowerComputePow2(n);
+}
+
+/**
+ * Gives the power of 2
+ *
+ * @param {Number} n the power to get
+ * @return {Number}
+ */
+function _gpfPow2 (n) {
+    var result = _gpfPow2[n];
+    if (result) {
+        return result;
+    }
+    result = _gpfComputePow2(n);
+    _gpfPow2[n] = result;
+    return result;
+}
+_gpfPow2[31] = _gpfMax31 + 1;
+_gpfPow2[32] = _gpfMax32 + 1;
+
+//endregion
+
+//region _gpfIsPow2
+
+// Computes the log2 (knowing value is a pow of 2)
+function _gpfComputeLog2 (value) {
+    var result = 0;
+    while (1 < value) {
+        ++result;
+        value /= 2;
+    }
+    return result;
+}
+
+/**
+ * Check if the given value is a power of 2
+ *
+ * @param {Number} value the value to check
+ * @return {Number} the power of 2 or -1
+ */
+function _gpfIsPow2 (value) {
+    // http://en.wikipedia.org/wiki/Power_of_two
+    if (0 < value && 0 === (value & value - 1)) {
+        return _gpfComputeLog2(value);
+    }
+    return -1;
+}
+
+//endregion
+
+
 gpf.bin = {
 
-    /**
-     * Computes the power of 2
-     *
-     * @param {Number} n the power to compute
-     * @return {Number}
-     */
-    pow2: function (n) {
-        if (31 === n) {
-            return _gpfMax31 + 1;
-        }
-        if (32 === n) {
-            return _gpfMax32 + 1;
-        }
-        if (31 > n) {
-            return 1 << n;
-        }
-        var result = _gpfMax32 + 1;
-        n -= 31;
-        while (--n) {
-            result *= 2;
-        }
-        return result;
-    },
+    // @inheritdoc _gpfPow2
+    pow2: _gpfPow2,
 
-    /**
-     * Check if the given value is a power of 2
-     *
-     * @param {Number} value the value to check
-     * @return {Number} the power of 2 or -1
-     */
-    isPow2: function (value) {
-        // http://en.wikipedia.org/wiki/Power_of_two
-        if (0 < value && 0 === (value & value - 1)) {
-            var result = 0;
-            while (1 < value) {
-                ++result;
-                value /= 2;
-            }
-            return result;
-        }
-        return -1;
-    },
+    // @inheritdoc _gpfIsPow2
+    isPow2: _gpfIsPow2,
 
     /**
      * Returns the hexadecimal encoding of value.
