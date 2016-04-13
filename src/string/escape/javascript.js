e28dbfbf31619bc15722707a64bdaab167cff470
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-13 14:20:01 -0400

    Javascript string escapes

---
+++ src/string/escape/javascript.js
@@ -0,0 +1,12 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfStringEscapes*/ // String replacement using dictionary map
+/*#endif*/
+
+_gpfStringEscapes.javascript = {
+    "\\": "\\\\",
+    "\"": "\\\"",
+    "\n": "\\n",
+    "\r": "\\r",
+    "\t": "\\t"
+};
