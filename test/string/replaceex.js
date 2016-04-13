b03e73519d03c7786598bc6210580dfeb352f6cc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-13 14:20:59 -0400

    Test case for _gpfStringReplaceEx

---
+++ test/string/replaceex.js
@@ -0,0 +1,22 @@
+"use strict";
+
+describe("string/replaceex", function () {
+
+    if (gpf.internals) {
+
+        describe("(internal) _gpfStringReplaceEx", function () {
+            var _gpfStringReplaceEx = gpf.internals._gpfStringReplaceEx;
+
+            it("replaces strings recursively", function () {
+                assert("add" === _gpfStringReplaceEx("abc", {
+                    "a": "abc",
+                    "b": "dc",
+                    "c": ""
+                }));
+            });
+
+        });
+
+    }
+
+});
