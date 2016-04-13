fbdb4f9f0c002c97b19cb5a8fda4ad08c348e43c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-13 14:18:50 -0400

    Isolate _gpfStringCapitalize

---
+++ src/string/capitalize.js
@@ -0,0 +1,20 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*exported _gpfStringCapitalize*/ // Capitalize the string
+/*#endif*/
+
+/**
+ * Capitalize the string
+ *
+ * @param {String} that
+ * @return {String}
+ */
+function _gpfStringCapitalize (that) {
+    return that.charAt(0).toUpperCase() + that.substr(1);
+}
+
+/*#ifndef(UMD)*/
+
+gpf.internals._gpfStringCapitalize = _gpfStringCapitalize;
+
+/*#endif*/
