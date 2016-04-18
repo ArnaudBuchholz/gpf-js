1bf82de7b729243b77d2b49feaf6a3ebfee7e486
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-17 22:39:30 -0400

    Improved maintainability

--- src/set.js
+++ src/set.js
@@ -4,33 +4,72 @@
 /*global _gpfExtend*/ // gpf.extend
 /*#endif*/
 
+//region test
+
+function _gpfArrayTest (array, value) {
+    var index = array.indexOf(value);
+    if (-1 < index) {
+        return index;
+    }
+}
+
+function _gpfObjectTest (object, value) {
+    var values = Object.values(object),
+        index = values.indexOf(value);
+    if (-1 < index) {
+        return Object.keys(object)[index];
+    }
+}
+
+function _gpfSetTest (set, value) {
+    if (set instanceof Array) {
+        return _gpfArrayTest(set, value);
+    }
+    return _gpfObjectTest(set, value);
+}
+
+//endregion
+
+//region clear
+
+function _gpfArrayClear (array, value) {
+    var index = array.indexOf(value);
+    if (-1 < index) {
+        array.splice(index, 1);
+    }
+}
+
+function _gpfObjectClear (object, value) {
+    var values = Object.values(object),
+        index = values.indexOf(value),
+        property;
+    if (-1 < index) {
+        property = Object.keys(object)[index];
+        delete object[property];
+    }
+}
+
+function _gpfSetClear (set, value) {
+    if (set instanceof Array) {
+        _gpfArrayClear(set, value);
+    } else {
+        _gpfObjectClear(set, value);
+    }
+    return set;
+}
+
+//endregion
+
 _gpfExtend(gpf, {
 
     /*
      * Find the first member of dictionary which value equals to value.
      *
-     * @param {Object/array} dictionary
+     * @param {Object/Array} set
      * @param {*} value
      * @return {String/number/undefined} undefined if not found
      */
-    test: function (dictionary, value) {
-        var idx;
-        if (dictionary instanceof Array) {
-            idx = dictionary.length;
-            while (idx > 0) {
-                if (dictionary[--idx] === value) {
-                    return idx;
-                }
-            }
-        } else {
-            for (idx in dictionary) {
-                if (dictionary.hasOwnProperty(idx) && dictionary[idx] === value) {
-                    return idx;
-                }
-            }
-        }
-        return undefined;
-    },
+    test: _gpfSetTest,
 
     /*
      * Inserts the value in the array if not already present.
@@ -42,13 +81,9 @@ _gpfExtend(gpf, {
      */
     set: function (array, value) {
         _gpfAssert(array instanceof Array, "gpf.set must be used with an Array");
-        var idx = array.length;
-        while (idx > 0) {
-            if (array[--idx] === value) {
-                return array; // Already set
-            }
+        if (undefined === _gpfArrayTest(array, value)) {
+            array.push(value);
         }
-        array.push(value);
         return array;
     },
 
@@ -61,24 +96,6 @@ _gpfExtend(gpf, {
      * @return {Object/array} dictionary
      * @chainable
      */
-    clear: function (dictionary, value) {
-        var idx;
-        if (dictionary instanceof Array) {
-            idx = dictionary.length;
-            while (idx > 0) {
-                if (dictionary[--idx] === value) {
-                    dictionary.splice(idx, 1);
-                    break;
-                }
-            }
-        } else {
-            for (idx in dictionary) {
-                if (dictionary.hasOwnProperty(idx) && dictionary[idx] === value) {
-                    delete dictionary[idx];
-                }
-            }
-        }
-        return dictionary;
-    }
+    clear: _gpfSetClear
 
 });
