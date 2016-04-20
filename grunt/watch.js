1194064cb6b619ca4f4a23581ac2bc79adc4eb59
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-20 12:59:02 -0400

    Set spawn to false to manipulate configuration

--- grunt/watch.js
+++ grunt/watch.js
@@ -2,9 +2,13 @@
 
 module.exports = {
     files: configuration.srcFiles.concat(configuration.testFiles),
-    // Take care of linting and maintainability
+    // Take care of linting and maintainability (don't use concurrent to allow configuration altering)
     tasks: [
+        "jshint",
         "eslint",
         "exec:plato"
-    ]
+    ],
+    options: {
+        spawn: false
+    }
 };
