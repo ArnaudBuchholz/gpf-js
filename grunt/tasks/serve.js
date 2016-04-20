34396853acee5c7ad9d68d698dce15526c164db1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-04-20 12:59:27 -0400

    Lint only the modified files

--- grunt/tasks/serve.js
+++ grunt/tasks/serve.js
@@ -1,7 +1,24 @@
 "use strict";
 
 module.exports = function (grunt) {
+
+    grunt.registerTask("pre-serve", function () {
+        var changedFiles = {};
+
+        var onChange = grunt.util._.debounce(function () {
+            grunt.config.set("jshint.files", Object.keys(changedFiles));
+            grunt.config.set("eslint.target", Object.keys(changedFiles));
+            changedFiles = {};
+        }, 200);
+
+        grunt.event.on("watch", function (action, filepath) {
+            changedFiles[filepath] = action;
+            onChange();
+        });
+    });
+
     grunt.registerTask("serve", [
+        "pre-serve",
         "connect:server",
         "watch"
     ]);
