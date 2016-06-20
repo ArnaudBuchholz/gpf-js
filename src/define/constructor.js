dfff24dfd7f70e70213d4e596275a8179bde7f59
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-06-19 23:35:56 -0400

    Externalizes constructor

---
+++ src/define/constructor.js
@@ -0,0 +1,49 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfErrorDeclare*/ // Declare new gpf.Error names
+/*global _GpfFunctionBuilder*/ // Function builder
+/*exported _getNewClassConstructor*/ // Allocate a named class constructor
+/*#endif*/
+
+_gpfErrorDeclare("define/constructor", {
+    "constructorFunction":
+        "This is a constructor function, use with new"
+});
+
+/**
+ * Template for new class constructor
+ * - Function can't be used without new
+ * - Uses closure to keep track of the class definition
+ * - Class name will be injected at the right place
+ *
+ * @param {_GpfClassDefinition} classDef
+ * @return {Function}
+ * @closure
+ */
+function _gpfNewClassConstructorTpl (classDef) {
+    return function () {
+        var me = this; //eslint-disable-line no-invalid-this
+        if (!(me instanceof classDef._Constructor)) {
+            throw gpf.Error.constructorFunction();
+        }
+        classDef._resolvedConstructor.apply(me, arguments);
+    };
+}
+
+/**
+ * Allocate a named class constructor for a class definition
+ *
+ * @param {_GpfClassDefinition} classDef Class definition
+ * @returns {Function} Constructor
+ */
+// Build a new constructor
+function _getNewClassConstructor (classDef) {
+    var builder = new _GpfFunctionBuilder(_gpfNewClassConstructorTpl),
+        constructorName = classDef.getName().split(".").pop();
+    builder.replaceInBody({
+        "function": "function " + constructorName
+    });
+    return builder.generate()(classDef);
+}
+
+/*#endif*/
