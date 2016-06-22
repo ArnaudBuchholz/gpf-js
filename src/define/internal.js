a895903f6c451eb28460b220d087067c8bf39b63
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-06-21 23:51:32 -0400

    Internal class definition processing

---
+++ src/define/internal.js
@@ -0,0 +1,33 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfObjectForEach*/ // Similar to [].forEach but for objects
+/*exported _gpfProcessInternalDefinition*/ // Process internal definition
+/*#endif*/
+
+var _gpfCleaningShortcuts = {
+    "-": "private",
+    "#": "protected",
+    "+": "public",
+    "~": "static"
+};
+
+// Replace the shortcut with the correct property name
+function _gpfCleanDefinition (name, shortcut) {
+    /*jshint validthis:true*/ // Bound to the definition below
+    var shortcutValue = this[shortcut];
+    if (undefined !== shortcutValue) {
+        this[name] = shortcutValue;
+        delete this[shortcut];
+    }
+}
+
+/**
+ * Process internal definition
+ *
+ * @param {Object} definition Internal definition with shortcuts
+ * @returns {Object} Definition modified to fit core definition requirements
+ */
+function _gpfProcessInternalDefinition (definition) {
+    _gpfObjectForEach(_gpfCleaningShortcuts, _gpfCleanDefinition, definition);
+    return definition;
+}
