435952929638fb6d16ff6cab561db9028a409452
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-06-30 00:43:57 -0400

    Class Member definition

---
+++ src/define/definition/member.js
@@ -0,0 +1,37 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GpfExtensibleObject*/ // Extensible
+/*exported _GpfClassDefMember*/ // GPF class member definition
+/*#endif*/
+
+/**
+ * Extensible information about a class member
+ *
+ * @class {_GpfClassDefMember}
+ * @param {_GpfClassDefinition} classDef Owning class definition
+ * @param {String} name Member name
+ * @param {String} type Member type
+ */
+function _GpfClassDefMember (classDef, name, type) {
+    /*jshint validthis:true*/ // constructor
+    this._classDef = classDef;
+    this._name = name;
+    if (type) {
+        this._type = type;
+    }
+}
+
+_GpfClassDefMember.prototype = {
+
+    // @property {_GpfClassDefinition} Owning class definition
+    _classDef: null,
+
+    // @property {String} Member name
+    _name: "",
+
+    // @property {String} Member type
+    _type: "undefined"
+
+};
+
+_GpfClassDefMember.extension = new _GpfExtensibleObject(_GpfClassDefMember.prototype);
