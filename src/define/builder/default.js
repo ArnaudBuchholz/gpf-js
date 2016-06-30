daf093af5d191009e7013b19fa798d9e943260c6
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-06-30 00:43:42 -0400

    Default class builder

---
+++ src/define/builder/default.js
@@ -0,0 +1,14 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*exported _gpfBuildDefaultClass*/
+/*#endif*/
+
+/**
+ * Build the constructor of the specified class definition
+ *
+ * @param {_GpfClassDefinition} classDef Class Definition
+ * @return {Function} Constructor of the class
+ */
+function _gpfBuildDefaultClass (classDef) {
+
+}
