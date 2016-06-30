3aeb25fc1daf4e9bc3b501c681f37402a15056eb
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-06-30 00:44:16 -0400

    Attribute handling

---
+++ src/define/processors/attribute.js
@@ -0,0 +1,8 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GpfClassDefinition*/ // GPF class definition
+/*global _GpfClassDefMember*/ // GPF class member definition
+/*#endif*/
+
+_GpfClassDefinition.extension.add("attributes", []);
+_GpfClassDefMember.extension.add("attributes", []);
