1d0c7971cddbd4792474b2fae01960858095a433
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-07-22 13:21:52 -0400

    htmllint

---
+++ grunt/htmllint.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+    src: {
+        options: {
+            force: false,
+            htmllintrc: true
+        },
+        src: [
+            "*.html"
+        ]
+    }
+};
