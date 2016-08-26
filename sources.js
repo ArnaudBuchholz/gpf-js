0b9bff17ab32b5191cc30bd1ff1cd9c78a05a8a0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-08-26 13:03:08 -0400

    WIP

--- sources.js
+++ sources.js
@@ -1,5 +1,5 @@
 "use strict";
-/*exported Source, SourceContainer*/
+/*exported Source, SourceArray*/
 
 var Source = gpf.define("Source", {
 
@@ -8,6 +8,10 @@ var Source = gpf.define("Source", {
         //@property {String} Source name
         "[_name]": [gpf.$ClassProperty()],
         _name: "",
+
+        //@property {String} Source description
+        "[_description]": [gpf.$ClassProperty(true)],
+        _description: "",
 
         //@property {Number} Source index
         "[_index]": [gpf.$ClassProperty(true)],
@@ -25,8 +29,8 @@ var Source = gpf.define("Source", {
         "[_doc]": [gpf.$ClassProperty(true)],
         _doc: false,
 
-        //@property {String[]} List of dependencies
-        "[_dependsOn]": [gpf.$ClassProperty()],
+        //@property {String[]} List of dependencies (boot is excluded)
+        "[_dependsOn]": [gpf.$ClassProperty(false, "dependencies")],
         _dependsOn: [],
 
         //@property {String[]} List of module dependencies
@@ -40,7 +44,7 @@ var Source = gpf.define("Source", {
          */
         _processDependencies: function (dependencies) {
             var myDependencies = dependencies[this._name] || [];
-            this._dependsOn = ["boot"].concat(myDependencies);
+            this._dependsOn = myDependencies;
             this._dependencyOf = [];
             Object.keys(dependencies).forEach(function (name) {
                 var nameDependencies = dependencies[name];
@@ -55,6 +59,7 @@ var Source = gpf.define("Source", {
 
         constructor: function (source, index, dependencies) {
             this._name = source.name;
+            this._description = source.description;
             this._index = index;
             if (source.load !== false) {
                 this._load = true;
@@ -66,13 +71,43 @@ var Source = gpf.define("Source", {
                 this._doc = true;
             }
             this._processDependencies(dependencies);
+        },
+
+        isReadOnly: function () {
+            return this._name === "boot";
+        },
+
+        testProperty: function (propertyName) {
+            if (!this._load) {
+                return false;
+            }
+            if ("test" === propertyName) {
+                return this._test;
+            } else if ("doc" === propertyName) {
+                return this._doc;
+            }
+            return true;
+        },
+
+        setProperty: function (propertyName, value) {
+            if ("load" === propertyName) {
+                this._load = value;
+            } else if ("test" === propertyName) {
+                this._test = value;
+            } else if ("doc" === propertyName) {
+                this._doc = value;
+            }
+        },
+
+        hasDependencies: function () {
+            return this._dependsOn.length > 0;
         }
 
     }
 
 });
 
-var SourceContainer = gpf.define({
+var SourceArray = gpf.define("SourceArray", {
 
     "private": {
 
@@ -86,8 +121,46 @@ var SourceContainer = gpf.define({
         constructor: function (sourcesJSON, dependenciesJSON) {
             var dependencies = JSON.parse(dependenciesJSON);
             JSON.parse(sourcesJSON).forEach(function (source, index) {
-                this._sources[source.name] = new Source(source, index, dependencies);
+                this._sources.push(new Source(source, index, dependencies));
             }, this);
+        },
+
+        /**
+         * Enumerate all sources
+         *
+         * @param {Function} callback
+         * @param {*} thisArg
+         */
+        forEach: function (callback, thisArg) {
+            this._sources.forEach(callback, thisArg);
+        },
+
+        /**
+         * Get the source by name
+         *
+         * @param {String} name
+         * @return {Source}
+         */
+        byName: function (name) {
+            var result;
+            this._sources.every(function (source) {
+                if (source.getName() === name) {
+                    result = source;
+                    return false;
+                }
+                return true;
+            });
+            return result;
+        },
+
+        /**
+         * Get the source by index
+         *
+         * @param {Number} index
+         * @return {Source}
+         */
+        byIndex: function (index) {
+            return this._sources[index];
         }
 
     }
