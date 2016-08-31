53af3abd9c41c5fc00aca2edd48f8a5c0f76ee02
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-08-31 13:12:01 -0400

    Adds dynamic middleware

--- grunt/connect.js
+++ grunt/connect.js
@@ -4,7 +4,19 @@ module.exports = {
     server: {
         options: {
             port: 8000,
-            hostname: "*"
+            hostname: "*",
+            open: true,
+            middleware: function (connect, options, middlewares) {
+
+               var MIDDLEWARE_FOLDER = "connect-middleware",
+                   fs = require("fs"),
+                   path = require("path");
+               fs.readdirSync(path.join(__dirname, MIDDLEWARE_FOLDER)).forEach(function (fileName) {
+                   middlewares.unshift(require("./" + MIDDLEWARE_FOLDER + "/" + fileName));
+               });
+
+                return middlewares;
+            }
         }
     }
 };
