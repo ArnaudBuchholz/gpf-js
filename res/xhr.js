dc0439ae593a306453a046cdea2b942aa2416e30
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-08-31 13:12:57 -0400

    Reworked to add POST

--- res/xhr.js
+++ res/xhr.js
@@ -4,10 +4,13 @@
 (function () {
     "use strict";
 
-    function _xhrGet (url) {
+    function _xhrSend (request, method, data) {
         return new Promise(function (resolve, reject) {
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", url);
+            xhr.open(method, request._url);
+            Object.keys(request._headers).forEach(function (headerName) {
+                xhr.setRequestHeader(headerName, request._headers[headerName]);
+            });
             xhr.onreadystatechange = function () {
                 if (4 === xhr.readyState) {
                     if (2 === Math.floor(xhr.status / 100)) {
@@ -17,10 +20,63 @@
                     }
                 }
             };
-            xhr.send();
+            xhr.send(data);
         });
     }
 
-    window.xhrGet = _xhrGet;
+    /**
+     * XHR request parameters handler
+     *
+     * @param {String} url
+     * @private
+     */
+    function _XhrRequest (url) {
+        /*jshint validthis:true*/
+        this._url = url;
+    }
+
+    _XhrRequest.prototype = {
+
+        // @property {String} URL of the request
+        _url: "",
+
+        // @property {Object} Request headers
+        _headers: {},
+
+        /**
+         * Set request headers
+         *
+         * @param {Object} headerDictionary
+         * @return {_XhrRequest} allows chaining
+         */
+        headers: function (headerDictionary) {
+            this._headers = headerDictionary;
+            return this;
+        },
+
+        /**
+         * GET
+         *
+         * @return {Promise} resolved with response text
+         */
+        get: function () {
+            return _xhrSend(this, "GET");
+        },
+
+        /**
+         * POST
+         *
+         * @param {*} request data
+         * @return {Promise} resolved with response text
+         */
+        post: function (data) {
+            return _xhrSend(this, "POST", data);
+        }
+
+    };
+
+    window.xhr = function (url) {
+        return new _XhrRequest(url);
+    };
 
 }());
