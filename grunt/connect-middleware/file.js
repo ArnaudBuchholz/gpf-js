3e9e29cc692ef33e4ab65952c026c27547ad0b14
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-09-02 13:10:44 -0400

    File handler simulating REST on files

--- grunt/connect-middleware/file.js
+++ grunt/connect-middleware/file.js
@@ -4,24 +4,41 @@ var BASE_URL = "/file://",
     fs = require("fs"),
     path = require("path");
 
+function _dump (request, response, filePath) {
+    var data = [];
+    request
+        .on("data", function (chunk) {
+            data.push(chunk);
+        })
+        .on("end", function () {
+            fs.writeFile(filePath, Buffer.concat(data), function (err) {
+                if (err) {
+                    response.statusCode = 500;
+                }
+                response.end();
+            });
+        });
+}
+
 module.exports = function (request, response, next) {
 
     if (0 !== request.url.indexOf(BASE_URL)) {
         return next();
     }
 
-    var filePath = path.join(request.url.substr(BASE_URL.length));
+    var filePath = path.join(__dirname, "../..", request.url.substr(BASE_URL.length));
 
-    if (request.method === "GET") {
-        // Read
-        return response.end(fs.readFileSync(filePath));
-
-    } else if (request.method === "PUT") {
-        // Update
-        request.on("data", function (data) {
-            fs.writeFileSync(filePath, data);
-            response.end();
-        });
-    }
+    fs.stat(filePath, function (err, stats) {
+        if ((err || !stats.isFile()) && ("GET" === request.method || "PUT" === request.method)) {
+            response.statusCode = 404;
+            response.end("File not found");
+            return;
+        }
+        if ("GET" === request.method) {
+            response.end(fs.readFileSync(filePath));
+        } else {
+            _dump(request, response, filePath);
+        }
+    });
 
 };
