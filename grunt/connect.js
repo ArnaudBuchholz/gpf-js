c034654a075902a66ecb1f07d5cb3cfb4e94d6c1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-09-06 16:37:20 -0400

    ESLint fixes

--- grunt/connect.js
+++ grunt/connect.js
@@ -8,12 +8,12 @@ module.exports = {
             open: true,
             middleware: function (connect, options, middlewares) {
 
-               var MIDDLEWARE_FOLDER = "connect-middleware",
-                   fs = require("fs"),
-                   path = require("path");
-               fs.readdirSync(path.join(__dirname, MIDDLEWARE_FOLDER)).forEach(function (fileName) {
-                   middlewares.unshift(require("./" + MIDDLEWARE_FOLDER + "/" + fileName));
-               });
+                var MIDDLEWARE_FOLDER = "connect-middleware",
+                    fs = require("fs"),
+                    path = require("path");
+                fs.readdirSync(path.join(__dirname, MIDDLEWARE_FOLDER)).forEach(function (fileName) {
+                    middlewares.unshift(require("./" + MIDDLEWARE_FOLDER + "/" + fileName));
+                });
 
                 return middlewares;
             }
