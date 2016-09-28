c861b112ca1f777a8f4d5ce7e90d18754b46f29d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-09-28 11:14:50 -0400

    Default set to serve

--- grunt/tasks/default.js
+++ grunt/tasks/default.js
@@ -2,9 +2,6 @@
 
 module.exports = function (grunt) {
     grunt.registerTask("default", [
-        "exec:globals",
-        "concurrent:linters",
-        "concurrent:quality",
-        "exec:metrics"
+        "serve"
     ]);
 };
