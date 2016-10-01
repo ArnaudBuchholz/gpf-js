569deb0812e556096a4f3a04973b7173952bf4d8
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-09-30 23:50:10 -0400

    Updates class definition (WIP)

--- src/define/definition/class.js
+++ src/define/definition/class.js
@@ -1,9 +1,17 @@
 /*#ifndef(UMD)*/
 "use strict";
+/*global _gpfAssert*/ // Assertion method
 /*global _GpfExtensibleObject*/ // Extensible
 /*global _GpfClassDefMember*/ // GPF class member definition
 /*exported _GpfClassDefinition*/ // GPF class definition
 /*#endif*/
+
+_gpfErrorDeclare("define/definition/class", {
+    "classMemberAlreadyExist":
+        "You can't add a member twice",
+    "classInvalidVisibility":
+        "Invalid visibility keyword"
+});
 
 /**
  * Extensible information about a class
@@ -13,11 +21,17 @@
  * @param {_GpfClassDefinition} [superClassDef=null] superClassDef Super class definition
  */
 function _GpfClassDefinition (qName, superClassDef) {
+    _gpfAssert(!superClassDef || superClassDef instanceof _GpfClassDefinition, "Expected a _GpfClassDefinition");
     /*jshint validthis:true*/ // constructor
+    var superMembers;
     this._qName = qName;
     if (superClassDef) {
         this._super = superClassDef;
+        superMembers = superClassDef._member;
+    } else {
+        superMembers = {};
     }
+    this._members = Object.create(superMembers);
 }
 
 _GpfClassDefMember.prototype = {
@@ -26,7 +40,29 @@ _GpfClassDefMember.prototype = {
     _qName: null,
 
     // @property {_GpfClassDefinition} Super class definition
-    _super: null
+    _super: null,
+
+    // @property {Object} Dictionary of members
+    _members: {},
+
+    /**
+     * Get a member by its name
+     *
+     * @param {String} name Member name
+     * @return {_GpfClassDefMember|undefined} Member
+     */
+    getMember: function (name) {
+        return this._members[name];
+    },
+
+    addMember: function (member) {
+        _gpfAssert(member instanceof _GpfClassDefMember, "Expected a _GpfClassDefMember");
+        var name = member.getName();
+        if (this._members.hasOwnProperty(name)) {
+            throw gpf.Error.classMemberAlreadyExist();
+        }
+        this._members[name] = member;
+    }
 
 };
 
