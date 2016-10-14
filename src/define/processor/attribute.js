e4405613f61793ab03e0d0d82ede30c26f764c77
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-10-14 13:02:42 -0400

    WIP

---
+++ src/define/processor/attribute.js
@@ -0,0 +1,29 @@
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GpfClassDefinition*/ // GPF class definition
+/*global _GpfClassDefMember*/ // GPF class member definition
+/*global _gpfErrorDeclare*/ // Declare new gpf.Error names
+/*#endif*/
+
+_gpfErrorDeclare("define/processor/attribute", {
+    "attributeOnNonExistingMember":
+        "Attribute {attributeName} must pair with a class member"
+});
+
+var attributeProcessor = {
+
+    matcher: /^\[(\w+)\]$/,
+
+    // TODO wait for all members to be processed
+    process: function (match, value, classDefinition) {
+        var attributeName = match[1],
+            member = classDefinition.getOwnMember(attributeName);
+        if (!member) {
+            throw gpf.Error.attributeOnNonExistingMember({
+                attributeName: attributeName
+            });
+        }
+        member.addAttributes(value);
+    }
+
+};
