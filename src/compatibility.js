3e4f9be96099d65ef452d8ec471e6b92be6e5054
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-01 12:37:30 -0400

    Documentation

--- src/compatibility.js
+++ src/compatibility.js
@@ -3,47 +3,62 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfEmptyFunc*/ // An empty function
-/*global _gpfExtend*/ // gpf.extend
+/*global _gpfObjectForEach*/ // Similar to [].forEach but for objects
 /*exported _gpfInstallCompatibility*/ // Define and install compatible methods
 /*#endif*/
+
+/**
+ * @typedef _GpfCompatibilityDescription
+ * @property {Function} on Constructor where compatible members should be added if missing
+ * @property {Object} methods Dictionary of named methods to add
+ * @property {Object} statics Dictionary of named statics to add
+ */
 
 /*#ifndef(UMD)*/
 
 /**
- * Polyfills for missing 'standard' methods
+ * Dictionary of compatibility descriptions, indexed by constructor name
  *
+ * @see {@link _GpfCompatibilityDescription}
  * @type {Object}
  */
 var _gpfCompatibility = {};
 
 /*#endif*/
 
+function _gpfInstallMissingMembers (on, members) {
+    _gpfObjectForEach(members, function (value, memberName) {
+        if (on[memberName] === undefined) {
+            on[memberName] = value;
+        }
+    });
+}
+
 function _gpfInstallCompatibleMethods (on, methods) {
-    _gpfExtend(on.prototype, methods || {}, _gpfEmptyFunc);
+    if (methods) {
+        _gpfInstallMissingMembers(on.prototype, methods);
+    }
 }
 
 function _gpfInstallCompatibleStatics (on, statics) {
-    _gpfExtend(on, statics || {}, _gpfEmptyFunc);
+    if (statics) {
+        _gpfInstallMissingMembers(on, statics);
+    }
 }
 
 /**
  * Define and install compatible methods
  *
- * @param {String} typeName
- * @param {Object} overrides Dictionary containing:
- * - {Function} on constructor to consider
- * - {Object} [methods=undefined] methods Dictionary of name to methods
- * - {Object} [statics=undefined] statics Dictionary of name to static functions
- * @private
+ * @param {String} typeName Type name ("Object", "String"...)
+ * @param {_GpfCompatibilityDescription} description Description of compatible methods
  */
-function _gpfInstallCompatibility (typeName, overrides) {
-    var on = overrides.on;
+function _gpfInstallCompatibility (typeName, description) {
+    var on = description.on;
 /*#ifndef(UMD)*/
-    _gpfCompatibility[typeName] = overrides;
+    _gpfCompatibility[typeName] = description;
 /*#endif*/
-    _gpfInstallCompatibleMethods(on, overrides.methods);
-    _gpfInstallCompatibleStatics(on, overrides.statics);
+    _gpfInstallCompatibleMethods(on, description.methods);
+    _gpfInstallCompatibleStatics(on, description.statics);
 }
 
 /*#ifndef(UMD)*/
