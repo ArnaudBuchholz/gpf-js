818e659b6cf485b1a6528a8965fae0164ff7f5cc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-01 13:11:03 -0400

    Documentation and simpler API

--- src/extend.js
+++ src/extend.js
@@ -3,69 +3,36 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfAssert*/ // Assertion method
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*global _gpfObjectForEach*/ // Similar to [].forEach but for objects
 /*exported _gpfExtend*/ // gpf.extend
 /*#endif*/
 
 /**
- * Callback function executed when a member must be overwritten.
+ * Used to assign member
  *
- * @callback gpfCallbackOverwriteMember
- *
- * @param {Object} object Destination object
- * @param {String} member Member name
- * @param {*} value Value to assign to member
- */
-
-/**
- * gpf.extend implementation of assign with no callback
- *
- * @param {*} value Value to assign to member
- * @param {String} member Member name
+ * @param {*} value Value to assign to object
+ * @param {String} memberName Member name
  * @this Object
  */
-function _gpfAssign (value, member) {
+function _gpfAssign (value, memberName) {
     /*jshint validthis:true*/
-    this[0][member] = value;
-}
-
-/**
- * gpf.extend implementation of assign with a callback
- *
- * @param {*} value Value to assign to member
- * @param {String} member Member name
- * @this Object
- */
-function _gpfAssignOrCall (value, member) {
-    /*jshint validthis:true*/
-    var dst = this[0],
-        overwriteCallback = this[2];
-    if (dst.hasOwnProperty(member)) {
-        overwriteCallback(dst, member, value);
-    } else {
-        dst[member] = value;
-    }
+    this[memberName] = value;
 }
 
 /**
  * Extends the destination object by copying own enumerable properties from the source object.
- * If a conflict has to be handled (i.e. member exists on both objects), the overwriteCallback has to handle it.
+ * If the member already exists, it is overwritten.
  *
  * @param {Object} destination Destination object
- * @param {Object} source Source object
- * @param {gpfCallbackOverwriteMember} [overwriteCallback] Overwrite callback
+ * @param {...Object} source Source objects
  * @return {Object} Destination object
  */
-function _gpfExtend (destination, source, overwriteCallback) {
-    var callbackToUse;
-    if (undefined === overwriteCallback) {
-        callbackToUse = _gpfAssign;
-    } else {
-        _gpfAssert("function" === typeof overwriteCallback, "Expected function");
-        callbackToUse = _gpfAssignOrCall;
-    }
-    _gpfObjectForEach(source, callbackToUse, arguments);
+function _gpfExtend (destination, source) {
+    _gpfIgnore(source);
+    [].slice.call(arguments, 1).forEach(function (nthSource) {
+        _gpfObjectForEach(nthSource, _gpfAssign, destination);
+    });
     return destination;
 }
 
