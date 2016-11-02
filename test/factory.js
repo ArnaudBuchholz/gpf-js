a4c577d7bbff2d0fe14770a49f378a039abe3969
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-02 13:20:54 -0400

    _gpfNewApplya

--- test/factory.js
+++ test/factory.js
@@ -4,21 +4,21 @@ describe("factory", function () {
 
     if (gpf.internals) {
 
-        describe("_gpfGenericFactory", function () {
+        describe("_gpfNewApply", function () {
 
-            var _gpfGenericFactory = gpf.internals._gpfGenericFactory;
+            var _gpfNewApply = gpf.internals._gpfNewApply;
 
             it("calls any constructor", function () {
                 function MyObject () {
                     this.value = 42;
                 }
-                var myInstance = _gpfGenericFactory.call(MyObject);
+                var myInstance = _gpfNewApply(MyObject, []);
                 assert(myInstance instanceof MyObject);
                 assert(42 === myInstance.value);
             });
 
             it("forwards parameters", function () {
-                var myArray = _gpfGenericFactory.call(Array, 42);
+                var myArray = _gpfNewApply(Array, [42]);
                 assert(myArray instanceof Array);
                 assert(42 === myArray.length);
             });
