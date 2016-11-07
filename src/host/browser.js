badec35053d47d96830d5a7bf47311b8a1f35b31
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-06 23:53:23 -0500

    Removes _gpfInBrowser

--- src/host/browser.js
+++ src/host/browser.js
@@ -6,7 +6,6 @@
 /*global _GPF_HOST*/
 /*global _gpfExit:true*/ // Exit function
 /*global _gpfHost*/ // Host type
-/*global _gpfInBrowser:true*/ // The current host is a browser like
 /*global _gpfWebDocument:true*/ // Browser document object
 /*global _gpfWebHead:true*/ // Browser head tag
 /*global _gpfWebWindow:true*/ // Browser window object
@@ -18,7 +17,6 @@
 /* istanbul ignore next */ // Tested with NodeJS
 if (_GPF_HOST.BROWSER === _gpfHost) {
 
-    _gpfInBrowser = true;
     _gpfExit = function (code) {
         window.location = "https://arnaudbuchholz.github.io/gpf/exit.html?" + (code || 0);
     };
