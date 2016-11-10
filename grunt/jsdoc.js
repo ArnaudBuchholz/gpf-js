b2967601cfcf9f01178cdfd6fe12aa42909518f3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-09 22:42:57 -0500

    Public and Private documentation

--- grunt/jsdoc.js
+++ grunt/jsdoc.js
@@ -1,13 +1,26 @@
 "use strict";
 
 module.exports = {
-    dist: {
+
+    "private": {
         src: configuration.docFiles.concat([
             "README.md"
         ]),
         options: {
             configure: "doc/jsdoc.conf.json",
-            destination: "tmp/docs"
+            access: "all",
+            destination: "tmp/doc/private"
+        }
+    },
+
+    "public": {
+        src: configuration.docFiles.concat([
+            "README.md"
+        ]),
+        options: {
+            configure: "doc/jsdoc.conf.json",
+            destination: "tmp/doc/public"
         }
     }
+
 };
