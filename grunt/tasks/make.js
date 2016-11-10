176782b0fe6c20cce25d141c3c19b4278d9fa17c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-09 22:51:09 -0500

    Includes documentation publishing

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
         "uglify:buildTests",
         "copy:publishVersionPlato",
         "copy:publishVersion",
+        "copy:publishVersionDoc",
         "copy:publishTest"
     ]));
 };
