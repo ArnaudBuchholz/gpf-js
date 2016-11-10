5a7549888775eef5aa57bfd52b4047ebe5662b56
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-09 23:59:29 -0500

    Detects connect to make it work with server and command line

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -1,20 +1,39 @@
 "use strict";
 
 module.exports = function (grunt) {
-    grunt.registerTask("make", [].concat([
-        "exec:version",
-        "check",
-        "connect:server",
-        "concurrent:source",
-        "exec:buildDebug",
-        "exec:buildRelease",
-        "uglify:buildRelease",
-        "concurrent:debug",
-        "concurrent:release",
-        "uglify:buildTests",
-        "copy:publishVersionPlato",
-        "copy:publishVersion",
-        "copy:publishVersionDoc",
-        "copy:publishTest"
-    ]));
+    grunt.registerTask("make", "Check and build GPF-JS", function () {
+        // Check if connect is already running
+        var http = require("http"),
+            done = this.async(); //eslint-disable-line no-invalid-this
+
+        function run (connectIsIdle) {
+            grunt.task.run([
+                "exec:version",
+                "check"
+            ]);
+            if (connectIsIdle) {
+                grunt.task.run("connect:server");
+            }
+            grunt.task.run([
+                "concurrent:source",
+                "exec:buildDebug",
+                "exec:buildRelease",
+                "uglify:buildRelease",
+                "concurrent:debug",
+                "concurrent:release",
+                "uglify:buildTests",
+                "copy:publishVersionPlato",
+                "copy:publishVersion",
+                "copy:publishVersionDoc",
+                "copy:publishTest"
+            ]);
+            done();
+        }
+
+        http.get("http://localhost:8000/package.json", function (res) {
+            run(200 !== res.statusCode);
+        }).on("error", function () {
+            run(true);
+        });
+    });
 };
