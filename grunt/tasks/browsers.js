f189aa9179146499941f5bee37cec0069019f81c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2016-11-10 13:02:48 -0500

    Use connectIf

--- grunt/tasks/browsers.js
+++ grunt/tasks/browsers.js
@@ -3,7 +3,7 @@
 module.exports = function (grunt) {
     configuration.selenium.forEach(function (browser) {
         grunt.registerTask(browser, [
-            "connect:server",
+            "connectIf",
             "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose"
         ]);
     });
