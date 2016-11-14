08a5b288bd31bd6e19a32ae147bf1153eed3354a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-11-14 15:34:55 -0500

    New exception handling

--- test/error.js
+++ test/error.js
@@ -5,37 +5,45 @@ describe("error", function () {
     describe("gpf.Error", function () {
 
         it("is used as an exception", function () {
+            var exceptionCaught;
             try {
-                throw gpf.Error.abstractMethod();
+                gpf.Error.abstractMethod();
             } catch (e) {
-                assert(e instanceof gpf.Error);
-                assert(e.constructor === gpf.Error);
+                exceptionCaught = e;
             }
+            assert(exceptionCaught instanceof Error); // Standard since JavaScript 1.1
+            assert(exceptionCaught instanceof gpf.Error);
+            assert(exceptionCaught instanceof gpf.Error.AbstractMethod);
+            assert(exceptionCaught.constructor === gpf.Error.AbstractMethod);
         });
 
         it("is documented", function () {
+            var exceptionCaught;
             try {
-                throw gpf.Error.abstractMethod();
+                gpf.Error.abstractMethod();
             } catch (e) {
-                assert(e.code === gpf.Error.CODE_ABSTRACTMETHOD);
-                assert(e.code === gpf.Error.abstractMethod.CODE);
-                assert(e.name === "abstractMethod");
-                assert(e.message === "Abstract method");
+                exceptionCaught = e;
             }
+            assert(exceptionCaught.code === gpf.Error.CODE_ABSTRACTMETHOD);
+            assert(exceptionCaught.code === gpf.Error.abstractMethod.CODE);
+            assert(exceptionCaught.name === "abstractMethod");
+            assert(exceptionCaught.message === "Abstract method");
         });
 
         it("can use substitution for message", function () {
+            var exceptionCaught;
             try {
-                throw gpf.Error.assertionFailed({
+                gpf.Error.assertionFailed({
                     message: "Test"
                 });
             } catch (e) {
-                assert(e instanceof gpf.Error);
-                assert(e.code === gpf.Error.CODE_ASSERTIONFAILED);
-                assert(e.code === gpf.Error.assertionFailed.CODE);
-                assert(e.name === "assertionFailed");
-                assert(e.message === "Assertion failed: Test");
+                exceptionCaught = e;
             }
+            assert(exceptionCaught instanceof gpf.Error.AssertionFailed);
+            assert(exceptionCaught.code === gpf.Error.CODE_ASSERTIONFAILED);
+            assert(exceptionCaught.code === gpf.Error.assertionFailed.CODE);
+            assert(exceptionCaught.name === "assertionFailed");
+            assert(exceptionCaught.message === "Assertion failed: Test");
         });
 
     });
