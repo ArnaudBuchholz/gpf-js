0a32c43e8e563b6142d37deea672381c2a3b40bb
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-11-21 12:46:07 -0500

    FIX #101

--- make/version.js
+++ make/version.js
@@ -17,75 +17,81 @@ if (verbose) {
 }
 
 if (bootVersion !== packageVersion) {
-    console.log("Package version: " + packageJson.version);
-    console.log("Boot version   : " + bootVersion);
     bootSrc = bootSrc.replace(bootVersionMatch[0], "_gpfVersion = \"" + packageVersion + "\"");
     fs.writeFileSync("src/boot.js", bootSrc);
     console.log("src/boot.js updated" + bootVersion);
 }
 
+function _injectSinceComment (source, jsdocComment, lastIndex) {
+    var newComment = jsdocComment.split("\n"),
+        pos,
+        len;
+    if (newComment.length === 1) {
+        // Need to find current indentation
+        pos = source.content.substr(0, lastIndex).lastIndexOf("\n") + 1;
+        len = lastIndex - jsdocComment.length - pos;
+        return [
+            "/**",
+            " * " + jsdocComment.substr(3, jsdocComment.length - 5).trim(),
+            " * @since " + version,
+            " */"
+        ].join("\n" + source.content.substr(pos, len));
+    }
+    newComment.splice(-1, 0, newComment[newComment.length - 1].replace("*/", "* @since " + version));
+    return newComment.join("\n");
+}
+
+function _removeSinceComment (jsdocComment) {
+    return jsdocComment
+        .split("\n")
+        .filter(function (line) {
+            return line.indexOf("@since") === 0;
+        })
+        .join("\n");
+}
+
+function _processCommentForSince (source, jsdocCommentMatch, lastIndex) {
+    var jsdocComment,
+        hasSince,
+        newComment;
+    jsdocComment = jsdocCommentMatch[0];
+    if (jsdocComment.indexOf("@lends") > -1) {
+        return false;
+    }
+    newComment = undefined;
+    hasSince = jsdocComment.indexOf("@since") > -1;
+    if (source.load !== false) {
+        if (!hasSince) {
+            newComment = _injectSinceComment(source, jsdocComment, lastIndex);
+        }
+    } else if (hasSince) {
+        newComment = _removeSinceComment(jsdocComment);
+    }
+    if (newComment) {
+        source.content = source.content.replace(jsdocComment, newComment);
+        return true;
+    }
+    return false;
+}
+
 var _reJsdocComment = /(?:\/\*\*(?:[^*]|\s|\*[^/])*\*\/)/g;
 
-sourcesJson.forEach(function (source) {
-    var sourceContent = fs.readFileSync("src/" + source.name + ".js").toString(),
-        jsdocCommentMatch,
-        jsdocComment,
+function _handleSinceInSource (source) {
+    var jsdocCommentMatch,
         modified = false,
-        hasSince,
-        newComment,
-        pos,
-        len,
-        indent,
         fileName;
+    source.content = fs.readFileSync("src/" + source.name + ".js").toString();
     _reJsdocComment.lastIndex = 0;
-    jsdocCommentMatch = _reJsdocComment.exec(sourceContent);
+    jsdocCommentMatch = _reJsdocComment.exec(source.content);
     while (jsdocCommentMatch) {
-        jsdocComment = jsdocCommentMatch[0];
-        if (jsdocComment.indexOf("@lends") > -1) {
-            jsdocCommentMatch = _reJsdocComment.exec(sourceContent);
-            continue;
-        }
-        newComment = undefined;
-        hasSince = jsdocComment.indexOf("@since") > -1;
-        if (source.load !== false) {
-            if (!hasSince) {
-                // Inject @since
-                newComment = jsdocComment.split("\n");
-                if (newComment.length === 1) {
-                    // Need to find current indentation
-                    pos = sourceContent.substr(0, _reJsdocComment.lastIndex).lastIndexOf("\n") + 1;
-                    len = _reJsdocComment.lastIndex - jsdocComment.length - pos;
-                    indent = sourceContent.substr(pos, len);
-                    newComment = indent + [
-                        "/**",
-                        " * " + jsdocComment.substr(3, jsdocComment.length - 5).trim(),
-                        " * @since " + version,
-                        " */"
-                    ].join("\n" + indent);
-                } else {
-                    newComment.splice(-1, 0, newComment[newComment.length -1].replace("*/", "* @since " + version));
-                    newComment = newComment.join("\n");
-                }
-            }
-        } else if (hasSince) {
-            // Remove @since
-            newComment = jsdocComment
-                .split("\n")
-                .filter(function (line) {
-                    return line.indexOf("@since") === 0;
-                })
-                .join("\n");
-        }
-        if (newComment) {
-            sourceContent = sourceContent.replace(jsdocComment, newComment);
-            // console.log(newComment);
-            modified = true;
-        }
-        jsdocCommentMatch = _reJsdocComment.exec(sourceContent);
+        modified = _processCommentForSince(source, jsdocCommentMatch, _reJsdocComment.lastIndex) || modified;
+        jsdocCommentMatch = _reJsdocComment.exec(source.content);
     }
     if (modified) {
         fileName = "src/" + source.name + ".js";
-        fs.writeFileSync(fileName, sourceContent);
+        fs.writeFileSync(fileName, source.content);
         console.log(fileName + " updated");
     }
-});
+}
+
+sourcesJson.forEach(_handleSinceInSource);
