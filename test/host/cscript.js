279e520c0df7221167a51f3115a127baf76e0d7d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-11-28 19:00:53 -0500

    Secure load

--- test/host/cscript.js
+++ test/host/cscript.js
@@ -24,6 +24,7 @@ function load (path) {
         /*jslint evil: false*/
     } catch (e) {
         WScript.Echo("An error occurred while evaluating: " + path + "\r\n" + e.message);
+        WScript.Quit(-1);
     }
 }
 
