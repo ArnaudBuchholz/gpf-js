6ba8594190655cb4093ba007edf958b2e2ce0477
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-12-03 11:30:11 -0500

    FIX #111

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -10,6 +10,7 @@ module.exports = function (grunt) {
         "exec:buildDebug",
         "exec:buildRelease",
         "uglify:buildRelease",
+        "exec:fixUglify",
         "concurrent:debug",
         "concurrent:release",
         "uglify:buildTests",
