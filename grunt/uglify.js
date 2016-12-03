883a0150d904ab3ee35dad8dba50db3c027f8664
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-12-03 10:51:29 -0500

    Increase portability by adding ascii_only

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -6,7 +6,8 @@
 module.exports = {
     options: {
         beautify: {
-            keep_quoted_props: true
+            keep_quoted_props: true,
+            ascii_only: true
         }
     },
     buildTests: {
