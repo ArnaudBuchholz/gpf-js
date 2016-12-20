f41b73646db1fb3e023e5c919edb1dc894d7ab6a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-12-20 18:05:42 -0500

    BDD no more part of tests

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     buildTests: {
         files: {
-            "build/tests.js": ["test/host/bdd.js", configuration.testFiles]
+            "build/tests.js": configuration.testFiles
         }
     },
     buildRelease: {
