7c8fcc33e62b4553c0284ff39a5e3a5dc3ffc815
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-12-19 23:19:00 -0500

    Also fix tests

--- make/fix_uglify.js
+++ make/fix_uglify.js
@@ -1,28 +1,37 @@
 "use strict";
 
 var EXCEPTION_VARIABLE = "e_",
-    fs = require("fs"),
-    srcFromUglify = fs.readFileSync("build/gpf.js").toString(),
-    result;
+    fs = require("fs");
 
 function getVariableNameMatcher (name) {
     return new RegExp("\\b" + name + "\\b", "g");
 }
 
-if (getVariableNameMatcher(EXCEPTION_VARIABLE).exec(srcFromUglify)) {
-    console.error("Exception variable '" + EXCEPTION_VARIABLE + "' already used");
-    process.exit(-1);
+function fix (name) {
+    var fileName =  "build/" + name + ".js",
+        before = fs.readFileSync(fileName).toString(),
+        after;
+
+    if (getVariableNameMatcher(EXCEPTION_VARIABLE).exec(before)) {
+        console.error("Exception variable '" + EXCEPTION_VARIABLE + "' already used in " + fileName);
+        return 0;
+    }
+
+    after = before.replace(new RegExp("catch\\((\\w+)\\)\\s*{([^}]*)}", "g"), function (match, varName, block) {
+        return [
+            "catch(",
+            EXCEPTION_VARIABLE,
+            "){",
+            block.replace(getVariableNameMatcher(varName), EXCEPTION_VARIABLE),
+            "}"
+        ].join("");
+    });
+
+    fs.writeFileSync(fileName, after);
+    console.log("Fixed uglify'ed version of " + fileName);
+    return 1;
 }
 
-result = srcFromUglify.replace(new RegExp("catch\\((\\w+)\\)\\s*{([^}]*)}", "g"), function (match, varName, block) {
-    return [
-        "catch(",
-        EXCEPTION_VARIABLE,
-        "){",
-        block.replace(getVariableNameMatcher(varName), EXCEPTION_VARIABLE),
-        "}"
-    ].join("");
-});
-
-fs.writeFileSync("build/gpf.js", result);
-console.log("Fixed uglify'ed version");
+if (2 !== fix("gpf") + fix("tests")) {
+    process.exit(-1);
+}
