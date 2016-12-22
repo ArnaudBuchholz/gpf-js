550433d0c5d782e388845194cf6ae8dc7e6aa8b8
Arnaud Buchholz <arnaud.buchholz@free.fr>
2016-12-22 14:16:42 -0500

    Also fixes .catch blocks

--- make/fix_uglify.js
+++ make/fix_uglify.js
@@ -1,37 +1,33 @@
 "use strict";
+/*jshint -W079*/ // Redefinition of Promise with bluebird
 
-var EXCEPTION_VARIABLE = "e_",
-    fs = require("fs");
+const
+    EXCEPTION_VARIABLE = "e_",
+    Promise = require("bluebird"),
+    fs = Promise.promisifyAll(require("fs")),
+    fileName = `build/${process.argv[2]}.js`,
+    getVariableNameMatcher = name => new RegExp("\\b" + name + "\\b", "g");
 
-function getVariableNameMatcher (name) {
-    return new RegExp("\\b" + name + "\\b", "g");
-}
-
-function fix (name) {
-    var fileName =  "build/" + name + ".js",
-        before = fs.readFileSync(fileName).toString(),
-        after;
-
-    if (getVariableNameMatcher(EXCEPTION_VARIABLE).exec(before)) {
-        console.error("Exception variable '" + EXCEPTION_VARIABLE + "' already used in " + fileName);
-        return false;
-    }
-
-    after = before.replace(new RegExp("catch\\((\\w+)\\)\\s*{([^}]*)}", "g"), function (match, varName, block) {
-        return [
-            "catch(",
-            EXCEPTION_VARIABLE,
-            "){",
-            block.replace(getVariableNameMatcher(varName), EXCEPTION_VARIABLE),
-            "}"
-        ].join("");
+fs.readFileAsync(fileName)
+    .then(buffer => buffer.toString())
+    // Processing try {} catch variable
+    .then(source => getVariableNameMatcher(EXCEPTION_VARIABLE).exec(source)
+        ? Promise.reject(new Error("Exception variable '" + EXCEPTION_VARIABLE + "' already used in " + fileName))
+        : source
+    )
+    .then(source => source.replace(/catch\((\w+)\)\s*{([^}]*)}/g, (match, varName, block) => [
+        "catch(",
+        EXCEPTION_VARIABLE,
+        "){",
+        block.replace(getVariableNameMatcher(varName), EXCEPTION_VARIABLE),
+        "}"
+    ].join("")))
+    // Processing <Promise>.catch syntax
+    .then(source => source.replace(/\.catch/g, "[\"catch\"]"))
+    // Serializing
+    .then(source => fs.writeFileAsync(fileName, source))
+    .then(() => console.log("Fixed uglify'ed version of " + fileName))
+    .catch(error => {
+        console.error("Error while fixing uglify'ed version of " + fileName + ": " + error.message);
+        process.exit(-1);
     });
-
-    fs.writeFileSync(fileName, after);
-    console.log("Fixed uglify'ed version of " + fileName);
-    return true;
-}
-
-if (!fix(process.argv[2])) {
-    process.exit(-1);
-}
