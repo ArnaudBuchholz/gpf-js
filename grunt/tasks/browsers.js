482343a3c41d8559d8d6ae76f29fe8d9490e99e9
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-10 10:41:08 -0500

    Configuration structure update

--- grunt/tasks/browsers.js
+++ grunt/tasks/browsers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function (grunt) {
-    configuration.selenium.forEach(function (browser) {
+    configuration.selenium.browsers.forEach(function (browser) {
         grunt.registerTask(browser, [
             "connectIf",
             "exec:test" + browser.charAt(0).toUpperCase() + browser.substr(1) + "Verbose"
