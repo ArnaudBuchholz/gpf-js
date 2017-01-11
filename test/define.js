6461dacc90d574db41f7cf89c4a20c10d2043a03
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-11 14:06:01 -0500

    WIP

--- test/define.js
+++ test/define.js
@@ -19,7 +19,28 @@ describe("define", function () {
                 }
             });
 
-            it("");
+            it("prevents constructor functions to be used without new", function () {
+                var exceptionCaught;
+                try {
+                    A(); // eslint-disable-line new-cap
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(exceptionCaught instanceof gpf.Error);
+                assert(exceptionCaught.code === gpf.Error.CODE_CONSTRUCTORFUNCTION);
+                assert(exceptionCaught.name === "constructorFunction");
+            });
+
+            var a = new A();
+
+            it("handles instanceof", function () {
+                assert(a instanceof A);
+            });
+
+            it("exposes methods", function () {
+                assert("function" === a.getMember);
+                assert("defaultValue" === a.getMember());
+            });
 
         });
 
