efbc3df967e8fb6af92a12f11b2edaf2891430dd
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-12 13:20:24 -0500

    WIP

--- src/define/class/checkDefinition.js
+++ src/define/class/checkDefinition.js
@@ -11,6 +11,17 @@ _gpfErrorDeclare("define/class/checkDefinition", {
     /**
      * ### Summary
      *
+     * The class name is invalid
+     *
+     * ### Description
+     *
+     * Only a valid JavaScript identifier (starting with an uppercase letter or _) is allowed
+     */
+    invalidClassName: "Invalid class name",
+
+    /**
+     * ### Summary
+     *
      * The class definition contains an invalid keyword
      *
      * ### Description
@@ -21,13 +32,7 @@ _gpfErrorDeclare("define/class/checkDefinition", {
 
 });
 
-/**
- * Check the class definition
- *
- * @param {Object} definition Class definition
- * @throws {gpf.Error.InvalidClassProperty}
- */
-function _gpfDefineCheckClassDefinition (definition) {
+function _gpfDefineCheckClassDefinitionFor$Propos (definition) {
     var properties = Object.keys(definition);
     if (properties.filter(function (name) {
         return name.charAt(0) === "$" && -1 === ["$class", "$extend"].indexOf(name);
@@ -36,3 +41,23 @@ function _gpfDefineCheckClassDefinition (definition) {
         gpf.Error.invalidClassProperty();
     }
 }
+
+function _gpfDefineCheckClassDefinitionForVisibility (definition) {
+    if ("super,public,private,protected,static,mixin".split(",").some(function (key) {
+        return undefined !== definition[key];
+    })) {
+        gpf.Error.invalidClassProperty();
+    }
+}
+
+/**
+ * Check the class definition
+ *
+ * @param {Object} definition Class definition
+ * @throws {gpf.Error.InvalidClassProperty}
+ */
+function _gpfDefineCheckClassDefinition (definition) {
+    _gpfDefineCheckClassDefinitionFor$Propos(definition);
+    _gpfDefineCheckClassDefinitionForVisibility(definition);
+}
+
