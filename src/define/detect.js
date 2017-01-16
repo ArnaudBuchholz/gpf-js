f12385c674fef2eab7644c3d7917e309ecf6790e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-16 14:31:23 -0500

    Detect entity type

--- src/define/detect.js
+++ src/define/detect.js
@@ -5,9 +5,10 @@
 "use strict";
 /*global _gpfErrorDeclare*/ // Declare new gpf.Error names
 /*global _gpfObjectForEach*/ // Similar to [].forEach but for objects
+/*exported _gpfDefineBuildTypedEntity*/ // Factory to create the correct entity type
 /*#endif*/
 
-_gpfErrorDeclare("define/checkDefinition", {
+_gpfErrorDeclare("define/detect", {
     /**
      * ### Summary
      *
@@ -16,29 +17,60 @@ _gpfErrorDeclare("define/checkDefinition", {
      * ### Description
      *
      * This error is thrown when the entity type is either missing or invalid
-     * @since 0.1.6
      */
     invalidEntityType: "Invalid entity type"
 
 });
 
 /**
- * Dictionary of typed definition
+ * Dictionary mapping type (class...) to the corresponding typed Entity constructor.
+ *
+ * This dictionary is filled by subsequent entity types.
  */
 var _gpfDefineTypedBuilders = {};
 
-function _gpfDefineBuildEndity (definition) {
-    var entityBuilder;
-    _gpfObjectForEach(definition, function (typeBuilder, type) {
+/**
+ * Search for type specific properties ($class...) and return associated builder function
+ *
+ * @param {Object} definition Entity definition literal object
+ * @return {Function|undefined} Entity builder or undefined
+ */
+function _gpfDefineRead$TypedProperties (definition) {
+    var ResultEntityBuilder;
+    _gpfObjectForEach(definition, function (TypedEntityBuilder, type) {
         if (definition["$" + type]) {
-            entityBuilder = typeBuilder;
+            ResultEntityBuilder = TypedEntityBuilder;
         }
     });
-    if (!entityBuilder) {
-        entityBuilder = _gpfDefineTypedBuilders[definition.$type];
-    }
-    if (!entityBuilder) {
+    return ResultEntityBuilder;
+}
+
+/**
+ * Check the $type property to return the associated builder function
+ *
+ * @param {Object} definition Entity definition literal object
+ * @return {Function} Entity builder
+ * @throws {gpf.Error.InvalidEntityType}
+ */
+function _gpfDefineCheck$TypeProperty (definition) {
+    var typedEntityBuilder = _gpfDefineTypedBuilders[definition.$type];
+    if (undefined === typedEntityBuilder) {
         gpf.Error.invalidEntityType();
     }
-    return new typeBuilder(definition);
+    return typedEntityBuilder;
+}
+
+/**
+ * Factory to create the correct entity type
+ *
+ * @param {Object} definition Entity definition literal object
+ * @return {_GpfEntityDefinition} Entity definition instance
+ * @throws {gpf.Error.InvalidEntityType}
+ */
+function _gpfDefineBuildTypedEntity (definition) {
+    var EntityBuilder = _gpfDefineRead$TypedProperties(definition);
+    if (!EntityBuilder) {
+        EntityBuilder = _gpfDefineCheck$TypeProperty(definition);
+    }
+    return new EntityBuilder(definition);
 }
