808615638a0032594e746fda6198b7c5cfc9d77a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-16 13:20:01 -0500

    Entity definition class

---
+++ src/define/entity.js
@@ -0,0 +1,37 @@
+/**
+ * @file Entity definition
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfAssert*/ // Assertion method
+/*exported _GpfEntityDefinition*/ // GPF class definition
+/*#endif*/
+
+/**
+ * Entity definition
+ * - Check the definition object for $ properties
+ * - Maintain a flat dictionary of members (using prototype inheritance)
+ *
+ * @param {Object} definition Entity definition
+ * @constructor
+ */
+function _GpfEntityDefinition (definition) {
+    _gpfAssert(definition && "object" === typeof definition, "Expected an entity definition");
+    /*jshint validthis:true*/ // constructor
+    /*eslint-disable no-invalid-this*/
+    this._initialDefinition = definition;
+    /*eslint-enable no-invalid-this*/
+}
+
+_GpfEntityDefinition.prototype = {
+
+    /** Definition provided upon construction */
+    _initialDefinition: {}
+
+};
+
+/*#ifndef(UMD)*/
+
+gpf.internals._GpfEntityDefinition = _GpfEntityDefinition;
+
+/*#endif*/
