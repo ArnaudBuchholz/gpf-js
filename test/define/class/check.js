4369d55fe7f83a5ca1b949e296091884c8997cc6
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-20 13:10:04 -0500

    Class definition specific checks

---
+++ test/define/class/check.js
@@ -0,0 +1,72 @@
+"use strict";
+
+describe("define/class/check", function () {
+
+    if (gpf.internals) {
+
+        describe("(internal) _GpfClassDefinition", function () {
+
+            var _GpfClassDefinition = gpf.internals._GpfClassDefinition,
+                _gpfDefineBuildTypedEntity = gpf.internals._gpfDefineBuildTypedEntity;
+
+            it("is used for class definition", function () {
+                var definition = _gpfDefineBuildTypedEntity({
+                    $class: "Test"
+                });
+                assert(definition instanceof _GpfClassDefinition);
+            });
+
+            it("accepts a valid class definition", function () {
+                _gpfDefineBuildTypedEntity({
+                    $class: "Test",
+                    member: function () {}
+                }).check();
+            });
+
+            [
+                "startingWithLowercaseLetterIsNotOK",
+                "The$signIsForbbidenIfNotAtTheBeginning",
+                "The_signIsForbbidenIfNotAtTheBeginning"
+            ].forEach(function (invalidClassName) {
+                it("rejects invalid class name (" + invalidClassName + ")", function () {
+                    var exceptionCaught;
+                    try {
+                        _gpfDefineBuildTypedEntity({
+                            $class: invalidClassName
+                        }).check();
+                    } catch (e) {
+                        exceptionCaught = e;
+                    }
+                    assert(exceptionCaught instanceof gpf.Error.InvalidClassName);
+                });
+
+            });
+
+            [
+                "StartingWithUppercaseLetterIsNotOK",
+                "the$signIsForbbidenIfNotAtTheBeginning",
+                "the_signIsForbbidenIfNotAtTheBeginning",
+                "class"
+            ].forEach(function (invalidPropertyName) {
+                it("rejects invalid property name (" + invalidPropertyName + ")", function () {
+                    var rawDefinition = {
+                            $class: "Test"
+                        },
+                        exceptionCaught;
+                    rawDefinition[invalidPropertyName] = true;
+                    try {
+
+                        _gpfDefineBuildTypedEntity(rawDefinition).check();
+                    } catch (e) {
+                        exceptionCaught = e;
+                    }
+                    assert(exceptionCaught instanceof gpf.Error.InvalidClassProperty);
+                });
+
+            });
+
+        });
+
+    }
+
+});
