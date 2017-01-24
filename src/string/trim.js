d70f179a698e2c137c46445df00144769ce43b83
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-24 16:09:06 -0500

    Isolates string trimming

---
+++ src/string/trim.js
@@ -0,0 +1,19 @@
+/**
+ * @file Helper to trim strings
+ * @since 0.1.6
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*exported _gpfStringTrim*/ // Trim the string
+/*#endif*/
+
+/**
+ * Trim the string
+ *
+ * @param {String} that String to trim
+ * @return {String} Trimmed string
+ * @since 0.1.6
+ */
+function _gpfStringTrim (that) {
+    return that.trim();
+}
