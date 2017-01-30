4a587efb7be6914f856e8af6f75158912b9d64e4
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-30 12:55:32 -0500

    WIP

--- src/define/class/build.js
+++ src/define/class/build.js
@@ -5,14 +5,14 @@
 "use strict";
 /*global _GpfClassDefinition*/ // Class definition
 /*global _gpfExtend*/ // gpf.extend
-/*global _gpfDefineGetSecuredNamedConstructor*/ // Allocate a secured named constructor
+/*global _gpfDefineGetClassSecuredConstructor*/ // Allocate a secured named constructor
 /*#endif*/
 
 _gpfExtend(_GpfClassDefinition.prototype, /** @lends _GpfClassDefinition.prototype */ {
 
     /** @inheritdoc */
-    build: function () {
-        var newClass = _gpfDefineGetSecuredNamedConstructor(this),
+    _build: function () {
+        var newClass = _gpfDefineGetClassSecuredConstructor(this),
             // Basic JavaScript inheritance mechanism: Defines the newClass prototype as an instance of the super class
             newPrototype = Object.create(this._extend.prototype);
 
@@ -22,7 +22,20 @@ _gpfExtend(_GpfClassDefinition.prototype, /** @lends _GpfClassDefinition.prototy
         // Enforce the constructor to be what we expect
         newPrototype.constructor = newClass;
 
+        this._resolveConstructor();
 
+        return newClass;
+    },
+
+    _resolveConstructor: function () {
+        /* jshint -W069*/
+        var ownConstructor = this._initialDefinition["constructor"]; //eslint-disable-line dot-notation
+        /* jshint +W069*/
+        if (ownConstructor) {
+            this._resolvedConstructor = ownConstructor;
+        } else {
+            this._resolvedConstructor = this._extend;
+        }
     }
 
 });
