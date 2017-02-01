88f0528225315ada8ee88d85637a4fc870bd9d59
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-01-31 23:55:03 -0500

    Preparing for super access

--- src/define/class/build.js
+++ src/define/class/build.js
@@ -33,12 +33,24 @@ _gpfExtend(_GpfClassDefinition.prototype, /** @lends _GpfClassDefinition.prototy
         return newClass;
     },
 
+    _addMethodToPrototype: function (newPrototype, memberName, value) {
+        newPrototype[memberName] = value;
+    },
+
+    _addMemberToPrototype: function (newPrototype, memberName, value) {
+        if ("function" === typeof value) {
+            this._addMethodToPrototype(newPrototype, memberName, value);
+        } else {
+            newPrototype[memberName] = value;
+        }
+    },
+
     _buildPrototype: function (newPrototype) {
         _gpfObjectForEach(this._initialDefinition, function (value, memberName) {
             if (memberName.charAt(0) !== "$" && memberName !== "constructor") {
-                newPrototype[memberName] = value;
+                this._addMemberToPrototype(newPrototype, memberName, value); //eslint-disable-line no-invalid-this
             }
-        });
+        }, this);
     },
 
     _resolveConstructor: function () {
