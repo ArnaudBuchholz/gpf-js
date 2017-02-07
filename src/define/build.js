2ee1cd882697393482ecb1cab0c1c2e6ad51b2c2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-02-07 13:21:04 -0500

    Object.assign

--- src/define/build.js
+++ src/define/build.js
@@ -7,10 +7,9 @@
 /*global _GpfEntityDefinition*/ // Entity definition
 /*global _gpfContext*/ // Resolve contextual string
 /*global _gpfEmptyFunc*/ // An empty function
-/*global _gpfExtend*/ // gpf.extend
 /*#endif*/
 
-_gpfExtend(_GpfEntityDefinition.prototype, /** @lends _GpfEntityDefinition.prototype */ {
+Object.assign(_GpfEntityDefinition.prototype, /** @lends _GpfEntityDefinition.prototype */ {
 
     /**
      * Instance builder function (a.k.a. public constructor)
