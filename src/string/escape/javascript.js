4d834d67c79b1bc8027e67c90f03e5534ddf22ff
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-02-07 13:26:20 -0500

    _gpfSyncReadSourceJSON

--- src/string/escape/javascript.js
+++ src/string/escape/javascript.js
@@ -5,12 +5,7 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfStringEscapes*/ // Dictionary of language to escapes
+/*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
 /*#endif*/
 
-_gpfStringEscapes.javascript = {
-    "\\": "\\\\",
-    "\"": "\\\"",
-    "\n": "\\n",
-    "\r": "\\r",
-    "\t": "\\t"
-};
+_gpfStringEscapes.javascript = _gpfSyncReadSourceJSON("string/escape/javascript.json");
