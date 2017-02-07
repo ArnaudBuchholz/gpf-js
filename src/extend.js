ba5f64f2c9050e0d3ed54b32436428b9fadd6dd3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-02-07 13:24:44 -0500

    FIX #130

--- src/extend.js
+++ src/extend.js
@@ -4,23 +4,7 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfIgnore*/ // Helper to remove unused parameter warning
-/*global _gpfObjectForEach*/ // Similar to [].forEach but for objects
-/*exported _gpfExtend*/ // gpf.extend
 /*#endif*/
-
-/**
- * Used to assign member
- *
- * @param {*} value Value to assign to object
- * @param {String} memberName Member name
- * @this Object
- * @since 0.1.5
- */
-function _gpfAssign (value, memberName) {
-    /*jshint validthis:true*/
-    this[memberName] = value;
-}
 
 /**
  * Extends the destination object by copying own enumerable properties from the source object.
@@ -30,17 +14,6 @@ function _gpfAssign (value, memberName) {
  * @param {...Object} source Source objects
  * @return {Object} Destination object
  * @since 0.1.5
+ * @deprecated since version 0.1.7, use Object.assign
  */
-function _gpfExtend (destination, source) {
-    _gpfIgnore(source);
-    [].slice.call(arguments, 1).forEach(function (nthSource) {
-        _gpfObjectForEach(nthSource, _gpfAssign, destination);
-    });
-    return destination;
-}
-
-/**
- * @gpf:sameas _gpfExtend
- * @since 0.1.5
- */
-gpf.extend = _gpfExtend;
+gpf.extend = Object.assign;
