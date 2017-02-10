7d40a87a220e12042d32ed6405c3d7fb7f708bbb
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-02-10 12:59:17 -0500

    Fix #125

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-    files: configuration.files.src.concat(configuration.files.test),
+    files: ["src/sources.json", "src/**/*.js", "test/**/*.js"],
     // Take care of linting and maintainability (don't use concurrent to allow configuration altering)
     tasks: [
         "jshint",
