85a29d5c8c6c674e19d45c7ef877ac54e73bd1c6
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-02-26 12:51:05 +0545

    Use common part

--- test/host/selenium.js
+++ test/host/selenium.js
@@ -1,44 +1,13 @@
 "use strict";
 
-var path = require("path"),
-    gpfPath = path.resolve(__dirname, "../.."),
-    useFileAccess = false,
-    httpPort = "8000",
-    args = process.argv.slice(2),
-    browser = args[0],
-    parameters = [],
+var config = require("./web_config.js")("selenium"),
     errorCount,
     buildWebDriverFor = require("./selenium/driverFactory.js"),
     By = require("selenium-webdriver").By,
     until = require("selenium-webdriver").until,
-    driver = buildWebDriverFor(browser);
+    driver = buildWebDriverFor(config.browserName);
 
-args.slice(1).forEach(function (arg) {
-    if ("-release" === arg) {
-        parameters.push("release");
-    } else if ("-debug" === arg) {
-        parameters.push("debug");
-    } else if ("-file" === arg) {
-        useFileAccess = true;
-    } else if (0 === arg.indexOf("-port:")) {
-        httpPort = arg.substr(6);
-    } else if (0 === arg.indexOf("legacy/")) {
-        parameters.push("version=" + arg.substr(7));
-    }
-});
-
-if (parameters.length) {
-    parameters = "?" + parameters.join("&");
-} else {
-    parameters = "";
-}
-
-if (useFileAccess) {
-    driver.get("file://" + gpfPath + "/test/host/web.html" + parameters);
-} else {
-    driver.get("http://localhost:" + httpPort + "/test/host/web.html" + parameters);
-}
-
+driver.get(config.url);
 driver.wait(until.titleIs("GPF Tests - done"), 10000);
 driver.findElements(By.id("status"))
     .then(function (elements) {
