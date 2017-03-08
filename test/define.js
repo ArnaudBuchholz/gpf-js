95f49cc8124987797347b6a695a804554076c4cc
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-08 16:02:47 -0500

    Preparing tests for #149

--- test/define.js
+++ test/define.js
@@ -13,6 +13,42 @@ describe("define", function () {
         delete gpf.context().test;
     });
 
+    function _generateNamesValidation (typeName, Exception, names) {
+
+        Object.keys(names).filter(function (name) {
+            return !names[name];
+        }).forEach(function (invalidName) {
+            it("rejects invalid " + typeName + " name (" + invalidName + ")", function () {
+                var exceptionCaught,
+                    definition = {};
+                definition["$" + typeName] = invalidName;
+                try {
+                    gpf.define(definition);
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(exceptionCaught instanceof Exception);
+            });
+        });
+
+        Object.keys(names).filter(function (name) {
+            return names[name];
+        }).forEach(function (validName) {
+            it("accepts valid " + typeName + " name (" + validName + ")", function () {
+                var exceptionCaught,
+                    definition = {};
+                definition["$" + typeName] = validName;
+                try {
+                    gpf.define(definition);
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(undefined === exceptionCaught);
+            });
+        });
+
+    }
+
     describe("gpf.define", function () {
 
         it("accepts only one parameter", function () {
@@ -20,55 +56,32 @@ describe("define", function () {
             assert(1 === gpf.define.length);
         });
 
-        describe("Simple class description", function () {
+        describe("Common validation", function () {
+
+            it("checks that he entity type is specified", function () {
+                var exceptionCaught;
+                try {
+                    gpf.define({});
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(exceptionCaught instanceof gpf.Error.InvalidEntityType);
+            });
+
+        });
+
+        describe("Class definition", function () {
 
             describe("Validation", function () {
 
-                it("checks that he entity type is specified", function () {
-                    var exceptionCaught;
-                    try {
-                        gpf.define({});
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidEntityType);
-                });
-
-                [
-                    "1NumberAtTheBeginningIsNotOK",
-                    "noUppercaseFirstLetterIsNotOK",
-                    "DollarSignAnywhereIsNotOK$"
-                ].forEach(function (invalidClassName) {
-                    it("rejects invalid class name (" + invalidClassName + ")", function () {
-                        var exceptionCaught;
-                        try {
-                            gpf.define({
-                                $class: invalidClassName
-                            });
-                        } catch (e) {
-                            exceptionCaught = e;
-                        }
-                        assert(exceptionCaught instanceof gpf.Error.InvalidClassName);
-                    });
-                });
-
-                [
-                    "A",
-                    "$B",
-                    "_C",
-                    "Test123"
-                ].forEach(function (validClassName) {
-                    it("validates class name (" + validClassName + ")", function () {
-                        var exceptionCaught;
-                        try {
-                            gpf.define({
-                                $class: validClassName
-                            });
-                        } catch (e) {
-                            exceptionCaught = e;
-                        }
-                        assert(undefined === exceptionCaught);
-                    });
+                _generateNamesValidation("class", gpf.Error.InvalidClassName, {
+                    "1NumberAtTheBeginningIsNotOK": false,
+                    "noUppercaseFirstLetterIsNotOK": false,
+                    "DollarSignAnywhereIsNotOK$": false,
+                    "A": true,
+                    "$B": true,
+                    "_C": true,
+                    "Test123": true
                 });
 
                 it("rejects invalid $ property names", function () {
@@ -112,197 +125,222 @@ describe("define", function () {
 
             });
 
-        });
+            describe("Implementation", function () {
 
-        describe("Definition", function () {
-
-            var A, a;
-
-            before(function () {
-                A = gpf.define({
-                    $class: "A",
-                    _member: "defaultValue",
-                    getMember: function () {
-                        return this._member;
-                    },
-                    "constructor": function (memberValue) {
-                        if (memberValue) {
-                            this._member = memberValue;
-                        }
-                        this._constructorOfA = true;
-                    },
-                    funcWith3params: function (p1, p2, p3) {
-                        return p1 + p2 + p3;
-                    },
-                    throwError: function () {
-                        throw new Error("error");
-                    }
-                });
-
-                a = new A();
-            });
-
-            it("prevents constructor functions to be used without new", function () {
-                var exceptionCaught;
-                try {
-                    A(); // eslint-disable-line new-cap
-                } catch (e) {
-                    exceptionCaught = e;
-                }
-                assert(exceptionCaught instanceof gpf.Error.ClassConstructorFunction);
-            });
-
-            it("creates a constructor with the same signature than the constructor property", function () {
-                assert(A.length === 1);
-            });
-
-            it("handles instanceof", function () {
-                assert(a instanceof A);
-            });
-
-            it("calls constructor property", function () {
-                assert(a._constructorOfA);
-            });
-
-            it("exposes methods", function () {
-                assert("function" === typeof a.getMember);
-                assert("defaultValue" === a.getMember());
-            });
-
-            describe("Subclassing", function () {
-
-                it("prevents invalid override", function () {
-                    var exceptionCaught;
-                    try {
-                        gpf.define({
-                            $class: "Test",
-                            $extend: A,
-                            getMember: false
-                        });
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidClassOverride);
-                });
-
-                var B, b;
+                var A, a;
 
                 before(function () {
-                    B = gpf.define({
-                        $class: "test.B",
-                        $extend: A,
-                        "constructor": function (initialValue) {
-                            this.$super("valueOfB");
-                            this._constructorOfB = initialValue;
-                        },
+                    A = gpf.define({
+                        $class: "A",
+                        _member: "defaultValue",
                         getMember: function () {
-                            return this.$super() + "-inB";
+                            return this._member;
                         },
-                        setMember: function (newValue) {
-                            this._member = newValue;
+                        "constructor": function (memberValue) {
+                            if (memberValue) {
+                                this._member = memberValue;
+                            }
+                            this._constructorOfA = true;
                         },
-                        baseTest: function () {
-                            return this.$super.getMember();
-                        },
-                        invalidSuperMember: function () {
-                            return this.$super.doesntExist();
-                        },
-                        noSuperMember: function () {
-                            return this.$super();
-                        },
-                        differentBinding: function () {
-                            return this.$super.getMember.call({
-                                _member: "different"
-                            });
-                        },
-                        checkSignature: function () {
-                            assert(this.$super.funcWith3params.length === 3);
+                        funcWith3params: function (p1, p2, p3) {
+                            return p1 + p2 + p3;
                         },
                         throwError: function () {
-                            this.$super();
+                            throw new Error("error");
                         }
                     });
 
-                    b = new B(3475);
+                    a = new A();
+                });
+
+                it("prevents constructor functions to be used without new", function () {
+                    var exceptionCaught;
+                    try {
+                        A(); // eslint-disable-line new-cap
+                    } catch (e) {
+                        exceptionCaught = e;
+                    }
+                    assert(exceptionCaught instanceof gpf.Error.ClassConstructorFunction);
+                });
+
+                it("creates a constructor with the same signature than the constructor property", function () {
+                    assert(A.length === 1);
                 });
 
                 it("handles instanceof", function () {
-                    assert(b instanceof A);
-                    assert(b instanceof B);
+                    assert(a instanceof A);
                 });
 
-                it("handles namespace", function () {
-                    assert(test.B === B);
+                it("calls constructor property", function () {
+                    assert(a._constructorOfA);
                 });
 
-                it("calls the constructor function", function () {
-                    assert(b._constructorOfB === 3475);
+                it("exposes methods", function () {
+                    assert("function" === typeof a.getMember);
+                    assert("defaultValue" === a.getMember());
                 });
 
-                it("calls the proper $super()", function () {
-                    assert(b._constructorOfA);
-                    assert(b.getMember() === "valueOfB-inB");
-                });
+                describe("Subclassing", function () {
 
-                it("offers a way to call named base method", function () {
-                    assert(b.baseTest() === "valueOfB");
-                });
+                    it("prevents invalid override", function () {
+                        var exceptionCaught;
+                        try {
+                            gpf.define({
+                                $class: "Test",
+                                $extend: A,
+                                getMember: false
+                            });
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidClassOverride);
+                    });
 
-                it("fails when accessing an unknown $super member", function () {
-                    var exceptionCaught;
-                    try {
-                        b.invalidSuperMember();
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidClassSuperMember);
-                });
+                    var B, b;
 
-                it("fails when accessing inexistent $super", function () {
-                    var exceptionCaught;
-                    try {
-                        b.noSuperMember();
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidClassSuper);
-                });
-
-                it("allows calling $super member with a different binding", function () {
-                    assert(b.differentBinding() === "different");
-                });
-
-                it("respects methods signature", function () {
-                    b.checkSignature();
-                });
-
-                it("does not leak $super", function () {
-                    assert(undefined === b.$super);
-                });
-
-                it("does not leak $super on exception", function () {
-                    try {
-                        b.throwError();
-                    } catch (e) {
-                        // ignore
-                    }
-                    assert(undefined === b.$super);
-                });
-
-                it("handles several versions of $super", function () {
-                    var C = gpf.define({
-                            $class: "C",
-                            $extend: B,
-                            testMethod: function () {
+                    before(function () {
+                        B = gpf.define({
+                            $class: "test.B",
+                            $extend: A,
+                            "constructor": function (initialValue) {
+                                this.$super("valueOfB");
+                                this._constructorOfB = initialValue;
+                            },
+                            getMember: function () {
+                                return this.$super() + "-inB";
+                            },
+                            setMember: function (newValue) {
+                                this._member = newValue;
+                            },
+                            baseTest: function () {
                                 return this.$super.getMember();
+                            },
+                            invalidSuperMember: function () {
+                                return this.$super.doesntExist();
+                            },
+                            noSuperMember: function () {
+                                return this.$super();
+                            },
+                            differentBinding: function () {
+                                return this.$super.getMember.call({
+                                    _member: "different"
+                                });
+                            },
+                            checkSignature: function () {
+                                assert(this.$super.funcWith3params.length === 3);
+                            },
+                            throwError: function () {
+                                this.$super();
                             }
-                        }),
-                        c = new C();
-                    assert("valueOfB-inB" === c.testMethod());
+                        });
+
+                        b = new B(3475);
+                    });
+
+                    it("handles instanceof", function () {
+                        assert(b instanceof A);
+                        assert(b instanceof B);
+                    });
+
+                    it("handles namespace", function () {
+                        assert(test.B === B);
+                    });
+
+                    it("calls the constructor function", function () {
+                        assert(b._constructorOfB === 3475);
+                    });
+
+                    it("calls the proper $super()", function () {
+                        assert(b._constructorOfA);
+                        assert(b.getMember() === "valueOfB-inB");
+                    });
+
+                    it("offers a way to call named base method", function () {
+                        assert(b.baseTest() === "valueOfB");
+                    });
+
+                    it("fails when accessing an unknown $super member", function () {
+                        var exceptionCaught;
+                        try {
+                            b.invalidSuperMember();
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidClassSuperMember);
+                    });
+
+                    it("fails when accessing inexistent $super", function () {
+                        var exceptionCaught;
+                        try {
+                            b.noSuperMember();
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidClassSuper);
+                    });
+
+                    it("allows calling $super member with a different binding", function () {
+                        assert(b.differentBinding() === "different");
+                    });
+
+                    it("respects methods signature", function () {
+                        b.checkSignature();
+                    });
+
+                    it("does not leak $super", function () {
+                        assert(undefined === b.$super);
+                    });
+
+                    it("does not leak $super on exception", function () {
+                        try {
+                            b.throwError();
+                        } catch (e) {
+                            // ignore
+                        }
+                        assert(undefined === b.$super);
+                    });
+
+                    it("handles several versions of $super", function () {
+                        var C = gpf.define({
+                                $class: "C",
+                                $extend: B,
+                                testMethod: function () {
+                                    return this.$super.getMember();
+                                }
+                            }),
+                            c = new C();
+                        assert("valueOfB-inB" === c.testMethod());
+                    });
                 });
+
             });
 
         });
+
+        if (gpf.interfaces) {
+
+            describe("Interface definition", function () {
+
+                describe("Validation", function () {
+
+                    _generateNamesValidation("interface", gpf.Error.InvalidInterfaceName, {
+                        "1NumberAtTheBeginningIsNotOK": false,
+                        "noUppercaseFirstLetterIsNotOK": false,
+                        "DollarSignAnywhereIsNotOK$": false,
+                        "NotStartingWithUppercaseI": false,
+                        "A": false,
+                        "$B": false,
+                        "_C": false,
+                        "Test123": false,
+                        "ISample": true,
+                        "IS": true
+                    });
+
+                });
+
+            });
+
+        }
 
     });
 
