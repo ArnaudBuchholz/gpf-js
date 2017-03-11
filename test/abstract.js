0e8da1f5954a4279fcc4918295cbea1621b597e7
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-10 23:28:20 -0500

    Test abstract

---
+++ test/abstract.js
@@ -0,0 +1,30 @@
+"use strict";
+
+describe("abstract", function () {
+
+    if (gpf.internals) {
+        var _gpfCreateAbstractFunction = gpf.internals._gpfCreateAbstractFunction;
+
+        describe("_gpfCreateAbstractFunction", function () {
+
+            it("generates a function with a given signature", function () {
+                var abstractFunc = _gpfCreateAbstractFunction(3);
+                assert(3 === abstractFunc.length);
+            });
+
+            it("generates a function that throws the abstractMethod exception", function () {
+                var abstractFunc = _gpfCreateAbstractFunction(0),
+                    exceptionCaught;
+                try {
+                    abstractFunc();
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(exceptionCaught instanceof gpf.Error.AbstractMethod);
+            });
+
+        });
+
+    }
+
+});
