7e78c965f04e73ac4263d300f77f0bd527714915
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-16 12:57:14 -0400

    Work on #150: gpf.interfaces.isImplementedBy

--- test/interfaces.js
+++ test/interfaces.js
@@ -21,5 +21,54 @@ describe("interfaces", function () {
 
     });
 
+    describe("gpf.interfaces.isImplementedBy", function () {
+
+        function TestRawClass () {}
+        TestRawClass.prototype = {
+            queryInterface: function (oneParameter) {
+                return oneParameter;
+            }
+        };
+
+        var TestGPFClass = gpf.define({
+            $class: "TestGPFClass",
+            queryInterface: function (oneParameter) {
+                return oneParameter;
+            }
+        });
+
+        it("validates literal objects (using interface constructor)", function () {
+            assert(true === gpf.interfaces.isImplementedBy(gpf.interfaces.IUnknown, {
+                queryInterface: function (oneParameter) {
+                    return oneParameter;
+                }
+            }));
+        });
+
+        it("validates instances from a raw JavaScript class (using interface name)", function () {
+            var obj = new TestRawClass();
+            assert(true === gpf.interfaces.isImplementedBy("gpf.interfaces.IUnknown", obj));
+        });
+
+        it("validates a raw JavaScript class", function () {
+            assert(true === gpf.interfaces.isImplementedBy("gpf.interfaces.IUnknown", TestRawClass));
+        });
+
+        it("validates instances from a GPF class", function () {
+            var obj = new TestGPFClass();
+            assert(true === gpf.interfaces.isImplementedBy(gpf.interfaces.IUnknown, obj));
+        });
+
+        it("validates a GPF class", function () {
+            assert(true === gpf.interfaces.isImplementedBy(gpf.interfaces.IUnknown, TestGPFClass));
+        });
+
+        it("rejects member if not a method", function () {
+            assert(false === gpf.interfaces.isImplementedBy("gpf.interfaces.IUnknown", {
+                queryInterface: "Not a method"
+            }));
+        });
+
+    });
 
 });
