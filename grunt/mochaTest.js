94fb7b0d8a69b3d1f7cd51ebc114f624f234769c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-17 13:25:20 -0400

    Fix #154

--- grunt/mochaTest.js
+++ grunt/mochaTest.js
@@ -9,108 +9,115 @@ function clearRequireCache () {
 }
 
 // Test automation inside NodeJS
-module.exports = {
-    source: {
-        options: {
-            reporter: "dot",
-            quiet: false,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpfSourcesPath = "src/";
-                },
-                "./src/boot.js",
-                function () {
-                    require("../test/host/console.js");
-                    global.assert = require("assert");
-                }
-            ]
+module.exports = (grunt) => {
+    return {
+        source: {
+            options: {
+                reporter: "dot",
+                quiet: false,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpfSourcesPath = "src/";
+                    },
+                    "./src/boot.js",
+                    () => {
+                        if (grunt.task.current.args[0] === "noConsole") {
+                            gpf.preventAssertWarnings(true);
+                        } else {
+                            require("../test/host/console.js");
+                        }
+                        global.assert = require("assert");
+                    }
+                ]
+            },
+            src: configuration.files.test
         },
-        src: configuration.files.test
-    },
-    verbose: {
-        options: {
-            reporter: "spec",
-            quiet: false,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpfSourcesPath = "src/";
-                },
-                "./src/boot.js",
-                function () {
-                    global.assert = require("assert");
-                }
-            ]
+        verbose: {
+            options: {
+                reporter: "spec",
+                quiet: false,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpfSourcesPath = "src/";
+                    },
+                    "./src/boot.js",
+                    () => {
+                        global.assert = require("assert");
+                    }
+                ]
+            },
+            src: configuration.files.test
         },
-        src: configuration.files.test
-    },
-    coverage: {
-        options: {
-            reporter: "json",
-            captureFile: "./tmp/coverage/mochaTest.json",
-            quiet: true,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpfSourcesPath = "tmp/coverage/instrument/src/";
-                },
-                "./tmp/coverage/instrument/src/boot.js",
-                function () {
-                    require("../test/host/console.js");
-                    global.assert = require("assert");
-                }
-            ]
+        coverage: {
+            options: {
+                reporter: "json",
+                captureFile: "./tmp/coverage/mochaTest.json",
+                quiet: true,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpfSourcesPath = "tmp/coverage/instrument/src/";
+                    },
+                    "./tmp/coverage/instrument/src/boot.js",
+                    () => {
+                        require("../test/host/console.js");
+                        global.assert = require("assert");
+                    }
+                ]
+            },
+            src: configuration.files.test
         },
-        src: configuration.files.test
-    },
-    debug: {
-        options: {
-            reporter: "dot",
-            quiet: false,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpf = require("../build/gpf-debug.js");
-                    require("../test/host/console.js");
-                    global.assert = require("assert");
-                }
-            ]
+        debug: {
+            options: {
+                reporter: "dot",
+                quiet: false,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpf = require("../build/gpf-debug.js");
+                        require("../test/host/console.js");
+                        global.assert = require("assert");
+                    }
+                ]
+            },
+            src: configuration.files.test
         },
-        src: configuration.files.test
-    },
-    release: {
-        options: {
-            reporter: "dot",
-            quiet: false,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpf = require("../build/gpf.js");
-                    require("../test/host/console.js");
-                    global.assert = require("assert");
-                }
-            ]
+        release: {
+            options: {
+                reporter: "dot",
+                quiet: false,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpf = require("../build/gpf.js");
+                        require("../test/host/console.js");
+                        global.assert = require("assert");
+                    }
+                ]
+            },
+            src: configuration.files.test
         },
-        src: configuration.files.test
-    },
-    legacy: {
-        options: {
-            reporter: "dot",
-            quiet: false,
-            require: [
-                function () {
-                    clearRequireCache();
-                    global.gpfSourcesPath = "src/";
-                },
-                "./src/boot.js",
-                function () {
-                    require("../test/host/console.js");
-                    global.assert = require("assert");
-                    delete gpf.internals;
-                }
-            ]
-        },
-        src: "test/legacy/<%= grunt.task.current.args[0] %>.js"
-    }
+        legacy: {
+            options: {
+                reporter: "dot",
+                quiet: false,
+                require: [
+                    () => {
+                        clearRequireCache();
+                        global.gpfSourcesPath = "src/";
+                    },
+                    "./src/boot.js",
+                    () => {
+                        require("../test/host/console.js");
+                        global.assert = require("assert");
+                        delete gpf.internals;
+                    }
+                ]
+            },
+            src: "test/legacy/<%= grunt.task.current.args[0] %>.js"
+        }
+    };
 };
+
