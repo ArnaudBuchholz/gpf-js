ea08e8f414de6b2cdd233b5915f25a39b938313a
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-20 11:28:45 -0400

    Simple performance evaluation

---
+++ test/perf/nodejs.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var testFile = process.argv[2],
+    testCases = require("./" + testFile + ".js"),
+    testResults = [];
+
+console.log("Please wait, evaluating test cases...");
+Object.keys(testCases).forEach(function (label) {
+    console.log("\t" + label);
+    var dtStart = new Date(),
+        count = 0,
+        testCase = testCases[label];
+    while (new Date() - dtStart < 1000) {
+        testCase();
+        ++count;
+    }
+    testResults.push({
+        label: label,
+        count: count
+    });
+});
+
+console.log("Results:");
+var lastCount = 0;
+testResults.sort(function (a, b) {
+    return a.count - b.count;
+
+}).forEach(function (result, index) {
+    var message = "\t[" + (index + 1) + "] " + result.label + ": " + result.count.toLocaleString("en-US");
+    if (0 < index) {
+        message += " +" + Math.floor(1000 * (result.count - lastCount) / lastCount) / 10 + "%";
+    }
+    console.log(message);
+    lastCount = result.count;
+});
