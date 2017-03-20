f3928fca67682659079d29e5eed0a5fb2f8ad108
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-20 08:27:01 -0400

    Code cleaning, ignore first round of test

--- test/host/loader.js
+++ test/host/loader.js
@@ -1,183 +1,186 @@
 /*eslint strict: [2, "function"]*/ // IIFE form
-/*jshint -W071*/
-(function () { //eslint-disable-line max-statements
+/*eslint-disable func-style*/ // So that everything is grouped in one var
+(function () {
     "use strict";
 
     /*global run*/ // From bdd.js
 
     var Func = Function,
-        sources;
-
-    var context = (function () {
-        /*global global*/ // NodeJS global
-        if ("object" === typeof global) {
-            return global;
-        }
-        /*global window*/ // Browser global
-        if ("undefined" !== typeof window) {
-            return window;
-        }
-        return this; //eslint-disable-line no-invalid-this
-    }());
-
-    function _resolvePath (configuration, relativePath) {
-        return configuration.gpfPath
-            .split(configuration.pathSeparator)
-            .concat(relativePath.split("/"))
-            .join(configuration.pathSeparator);
-    }
-
-    function _processParameters (configuration, options) {
-        var len = configuration.parameters.length,
-            idx,
-            value;
-        for (idx = 0; idx < len; ++idx) {
-            value = configuration.parameters[idx];
-            if (value.charAt(0) === "-") {
-                value = value.substr(1);
-                if (value in options && "boolean" === typeof options[value]) {
-                    options[value] = !options[value]; // Simple switch
-                }
-            } else {
-                options.tests.push(value);
+        sources,
+        context = (function () {
+            /*global global*/ // NodeJS global
+            if ("object" === typeof global) {
+                return global;
             }
-        }
-    }
+            /*global window*/ // Browser global
+            if ("undefined" !== typeof window) {
+                return window;
+            }
+            return this; //eslint-disable-line no-invalid-this
+        }()),
 
-    function _load (configuration, path) {
-        var content = configuration.read(path);
-        try {
-            /*jslint evil: true*/
-            eval(content); //eslint-disable-line no-eval
-            /*jslint evil: false*/
-        } catch (e) {
-            configuration.log("An error occurred while evaluating: " + path + "\r\n" + e.message);
-            configuration.exit(-1);
-        }
-    }
+        _resolvePath = function (configuration, relativePath) {
+            return configuration.gpfPath
+                .split(configuration.pathSeparator)
+                .concat(relativePath.split("/"))
+                .join(configuration.pathSeparator);
+        },
 
-    function _requireGpf (configuration, path) {
-        if (configuration.require) {
-            context.gpf = configuration.require(path);
-        } else {
-            _load(configuration, path);
-            if ("undefined" === typeof gpf) {
-                configuration.log("GPF was not loaded");
+        _processParameters = function (configuration, options) {
+            var len = configuration.parameters.length,
+                idx,
+                value;
+            for (idx = 0; idx < len; ++idx) {
+                value = configuration.parameters[idx];
+                if (value.charAt(0) === "-") {
+                    value = value.substr(1);
+                    if (value in options && "boolean" === typeof options[value]) {
+                        options[value] = !options[value]; // Simple switch
+                    }
+                } else {
+                    options.tests.push(value);
+                }
+            }
+        },
+
+        _load = function (configuration, path) {
+            var content = configuration.read(path);
+            try {
+                /*jslint evil: true*/
+                eval(content); //eslint-disable-line no-eval
+                /*jslint evil: false*/
+            } catch (e) {
+                configuration.log("An error occurred while evaluating: " + path + "\r\n" + e.message);
                 configuration.exit(-1);
             }
-        }
-    }
+        },
 
-    function _loadGpf (configuration, options, verbose) {
-        if (options.release) {
-            verbose("Using release version");
-            _requireGpf(configuration, _resolvePath(configuration, "build/gpf.js"));
-
-        } else if (options.debug) {
-            verbose("Using debug version");
-            _requireGpf(configuration, _resolvePath(configuration, "build/gpf-debug.js"));
-
-        } else {
-            verbose("Using source version");
-            // Set the source path
-            context.gpfSourcesPath = _resolvePath(configuration, "src/");
-            _load(configuration, _resolvePath(configuration, "src/boot.js"));
-        }
-        // Load the list of modules
-        var sourcesJson = configuration.read("src/sources.json");
-        sources = new Func("return " + sourcesJson + ";")();
-    }
-
-    function _loadBDD (configuration, verbose) {
-        if (false !== configuration.useBDD) {
-            verbose("Loading BDD");
-            _load(configuration, _resolvePath(configuration, "test/host/bdd.js"));
-        }
-    }
-
-    function _loadTest (configuration, path) {
-        if (configuration.loadTest) {
-            configuration.loadTest(path);
-        } else {
-            _load(configuration, path);
-        }
-    }
-
-    function loadTestsFromNames (configuration, names) {
-        var len = names.length,
-            sourceIdx,
-            source;
-        for (sourceIdx = 0; sourceIdx < len; ++sourceIdx) {
-            source = names[sourceIdx];
-            if (source.indexOf("legacy/") === 0) {
-                delete gpf.internals;
-            }
-            _loadTest(configuration, _resolvePath(configuration, "test/" + source + ".js"));
-        }
-    }
-
-    function loadTestsFromModules (configuration) {
-        var len = sources.length,
-            sourceIdx,
-            source;
-        for (sourceIdx = 0; sourceIdx < len; ++sourceIdx) {
-            source = sources[sourceIdx];
-            if (source.load !== false && source.test !== false) {
-                _loadTest(configuration, _resolvePath(configuration, "test/" + source.name + ".js"));
-            }
-        }
-    }
-
-    function _loadTests (configuration, options) {
-        if (options.tests.length) {
-            loadTestsFromNames(configuration, options.tests);
-        } else {
-            loadTestsFromModules(configuration);
-        }
-    }
-
-    function _runBDDPerf (configuration/*, options*/) {
-        var totalTimeSpent = 0,
-            loop = 1;
-        function callback (type, data) {
-            if (type === "results") {
-                configuration.log("Round " + loop + ": " + data.timeSpent + "ms");
-                totalTimeSpent += data.timeSpent;
-                if (9 < loop) {
-                    configuration.log("Mean time: " + Math.floor(totalTimeSpent / 10) + "ms");
-                } else {
-                    ++loop;
-                    run(callback);
+        _requireGpf = function (configuration, path) {
+            if (configuration.require) {
+                context.gpf = configuration.require(path);
+            } else {
+                _load(configuration, path);
+                if ("undefined" === typeof gpf) {
+                    configuration.log("GPF was not loaded");
+                    configuration.exit(-1);
                 }
             }
-        }
-        run(callback);
-    }
+        },
 
-    function _runBDD (configuration, options, verbose) {
-        if (false !== configuration.useBDD) {
-            verbose("Running BDD");
-            exit = configuration.exit; // used by BDD.js
-            if (options.perf) {
-                _runBDDPerf(configuration, options);
+        _loadGpf = function (configuration, options, verbose) {
+            if (options.release) {
+                verbose("Using release version");
+                _requireGpf(configuration, _resolvePath(configuration, "build/gpf.js"));
+
+            } else if (options.debug) {
+                verbose("Using debug version");
+                _requireGpf(configuration, _resolvePath(configuration, "build/gpf-debug.js"));
+
             } else {
-                run();
+                verbose("Using source version");
+                // Set the source path
+                context.gpfSourcesPath = _resolvePath(configuration, "src/");
+                _load(configuration, _resolvePath(configuration, "src/boot.js"));
             }
-        }
-    }
+            // Load the list of modules
+            var sourcesJson = configuration.read("src/sources.json");
+            sources = new Func("return " + sourcesJson + ";")();
+        },
 
-    function _safeRunBDD (configuration, options, verbose) {
-        try {
-            _runBDD(configuration, options, verbose);
-            if (configuration.done) {
-                configuration.done();
+        _loadBDD = function (configuration, verbose) {
+            if (false !== configuration.useBDD) {
+                verbose("Loading BDD");
+                _load(configuration, _resolvePath(configuration, "test/host/bdd.js"));
             }
-            gpf.handleTimeout();
-        } catch (e) {
-            configuration.log("An error occurred while running the tests:\r\n" + e.message);
-            configuration.exit(-1);
-        }
-    }
+        },
+
+        _loadTest = function (configuration, path) {
+            if (configuration.loadTest) {
+                configuration.loadTest(path);
+            } else {
+                _load(configuration, path);
+            }
+        },
+
+        _loadTestsFromNames = function (configuration, names) {
+            var len = names.length,
+                sourceIdx,
+                source;
+            for (sourceIdx = 0; sourceIdx < len; ++sourceIdx) {
+                source = names[sourceIdx];
+                if (source.indexOf("legacy/") === 0) {
+                    delete gpf.internals;
+                }
+                _loadTest(configuration, _resolvePath(configuration, "test/" + source + ".js"));
+            }
+        },
+
+        _loadTestsFromModules = function (configuration) {
+            var len = sources.length,
+                sourceIdx,
+                source;
+            for (sourceIdx = 0; sourceIdx < len; ++sourceIdx) {
+                source = sources[sourceIdx];
+                if (source.load !== false && source.test !== false) {
+                    _loadTest(configuration, _resolvePath(configuration, "test/" + source.name + ".js"));
+                }
+            }
+        },
+
+        _loadTests = function (configuration, options) {
+            if (options.tests.length) {
+                _loadTestsFromNames(configuration, options.tests);
+            } else {
+                _loadTestsFromModules(configuration);
+            }
+        },
+
+        _runBDDPerf = function (configuration/*, options*/) {
+            var totalTimeSpent = 0,
+                loop = 1;
+            function callback (type, data) {
+                if (type === "results") {
+                    if (1 === loop) {
+                        configuration.log("Round " + loop + ": " + data.timeSpent + "ms (ignored)");
+                    } else {
+                        configuration.log("Round " + loop + ": " + data.timeSpent + "ms");
+                        totalTimeSpent += data.timeSpent;
+                    }
+                    if (10 < loop) {
+                        configuration.log("Mean time: " + Math.floor(totalTimeSpent / 10) + "ms");
+                    } else {
+                        ++loop;
+                        run(callback);
+                    }
+                }
+            }
+            run(callback);
+        },
+
+        _runBDD = function (configuration, options, verbose) {
+            if (false !== configuration.useBDD) {
+                verbose("Running BDD");
+                exit = configuration.exit; // used by BDD.js
+                if (options.perf) {
+                    _runBDDPerf(configuration, options);
+                } else {
+                    run();
+                }
+            }
+        },
+
+        _safeRunBDD = function (configuration, options, verbose) {
+            try {
+                _runBDD(configuration, options, verbose);
+                if (configuration.done) {
+                    configuration.done();
+                }
+                gpf.handleTimeout();
+            } catch (e) {
+                configuration.log("An error occurred while running the tests:\r\n" + e.message);
+                configuration.exit(-1);
+            }
+        };
 
     /**
      * @param {Object} configuration
