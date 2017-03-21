2a2e71d10ecf61d63c5d74636cf0b19e786a2b07
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-21 09:38:37 -0400

    if/else vs ternary operator comparison

---
+++ test/perf/ifelse2ternary.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var count = 0;
+
+module.exports = {
+
+    "if/else": function () {
+        var result;
+        if (++count % 2) {
+            result = "1";
+        } else {
+            result = "2";
+        }
+        return result;
+    },
+
+    "?:": function () {
+        return (++count % 2) ? "1" : "2";
+    }
+
+};
