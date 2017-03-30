1077d00710e22e0eea49a48a3df310118e66b438
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-30 15:48:03 -0400

    Multiple sorting test

--- test/sort.js
+++ test/sort.js
@@ -2,11 +2,11 @@
 
 describe("sort", function () {
 
-    var array = [{id: 0, num: 5, str: "e"},
-                 {id: 1, num: 1, str: "b"},
-                 {id: 2, num: 4, str: "a"},
-                 {id: 3, num: 2, str: "d"},
-                 {id: 4, num: 3, str: "c"}];
+    var array = [{id: 0, num: 5, str: "e", group: 1},
+                 {id: 1, num: 1, str: "b", group: 0},
+                 {id: 2, num: 4, str: "a", group: 1},
+                 {id: 3, num: 2, str: "d", group: 0},
+                 {id: 4, num: 3, str: "c", group: 1}];
 
     function checkSortingResult (result, ids) {
         assert(result.every(function (item, index) {
@@ -18,17 +18,29 @@ describe("sort", function () {
 
         [{
             label: "sort on numeric values (ascending)",
-            sort: [{
-                num: true
-            }],
+            sort: {
+                property: "num"
+            },
             expected: [1, 3, 4, 2, 0]
         }, {
             label: "sort on string values (descending)",
             sort: [{
-                str: false
+                property: "str",
+                type: "string",
+                ascending: false
             }],
-            expected: [1, 3, 4, 2, 0]
-
+            expected: [0, 3, 4, 1, 2]
+        }, {
+            label: "sort on multiple values",
+            sort: [{
+                property: "group",
+                type: "number",
+                ascending: true
+            }, {
+                property: "num",
+                ascending: false
+            }],
+            expected: [3, 1, 0, 2, 4]
 
         }].forEach(function (testCase) {
 
