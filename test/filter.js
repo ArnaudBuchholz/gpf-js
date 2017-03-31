f70681b904fb485c8c6f4476fe1833300933f74c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-03-31 00:01:13 -0400

    Adding more tests and working on the syntax

--- test/filter.js
+++ test/filter.js
@@ -2,11 +2,11 @@
 
 describe("sort", function () {
 
-    var array = [{id: 0, num: 5, str: "e"},
-        {id: 1, num: 1, str: "b"},
-        {id: 2, num: 4, str: "a"},
-        {id: 3, num: 2, str: "d"},
-        {id: 4, num: 3, str: "c"}];
+    var array = [{id: 0, num: 5, str: "e", group: 1},
+                {id: 1, num: 1, str: "b", group: 0},
+                {id: 2, num: 4, str: "a", group: 1},
+                {id: 3, num: 2, str: "d", group: 0},
+                {id: 4, num: 3, str: "c", group: 1}];
 
     function checkFilteringResult (result, ids) {
         assert(result.length === ids.length);
@@ -18,16 +18,111 @@ describe("sort", function () {
     describe("gpf.createFilterFunction", function () {
 
         [{
-            label: "filter on numeric values",
+            label: "filters on numeric values",
+            filter: {eq: [{property: "num"}, 1]},
+            expected: [1]
+
+        }, {
+            label: "filters on string values",
             filter: {
-                num: {eq: 4}
+                eq: [{property: "str"}, "c"]
+            },
+            expected: [4]
+
+        }, {
+            label: "filters on string values (ne)",
+            filter: {
+                ne: [{property: "str"}, "c"]
+            },
+            expected: [0, 1, 2, 3]
+
+        }, {
+            label: "filters on string values (not eq)",
+            filter: {
+                not: {
+                    eq: [{property: "str"}, "c"]
+                }
+            },
+            expected: [0, 1, 2, 3]
+
+        }, {
+            label: "filters with or (no conditions)",
+            filter: {
+                or: []
             },
             expected: []
+
+        }, {
+            label: "filters with or (one condition)",
+            filter: {
+                or: [
+                    {eq: [{property: "num"}, 1]}
+                ]
+            },
+            expected: [1]
+
+        }, {
+            label: "filters with or (two conditions)",
+            filter: {
+                or: [
+                    {eq: [{property: "num"}, 1]},
+                    {eq: [{property: "str"}, "c"]}
+                ]
+            },
+            expected: [1, 4]
+
+        }, {
+            label: "filters with or (three conditions)",
+            filter: {
+                or: [
+                    {eq: [{property: "num"}, 1]},
+                    {eq: [{property: "str"}, "c"]},
+                    {eq: [{property: "group"}, 0]}
+                ]
+            },
+            expected: [1, 3, 4]
+
+        }, {
+            label: "filters with and (no conditions)",
+            filter: {
+                and: []
+            },
+            expected: [0, 1, 2, 3, 4]
+
+        }, {
+            label: "filters with and (one condition)",
+            filter: {
+                and: [
+                    {eq: [{property: "group"}, 1]}
+                ]
+            },
+            expected: [0, 2, 4]
+
+        }, {
+            label: "filters with and (two conditions)",
+            filter: {
+                and: [
+                    {eq: [{property: "group"}, 1]},
+                    {eq: [{property: "str"}, "c"]}
+                ]
+            },
+            expected: [4]
+
+        }, {
+            label: "filters with and (three conditions)",
+            filter: {
+                and: [
+                    {eq: [{property: "group"}, 1]},
+                    {eq: [{property: "str"}, "c"]},
+                    {eq: [{property: "num"}, 3]}
+                ]
+            },
+            expected: [4]
 
         }].forEach(function (testCase) {
 
             it(testCase.label, function () {
-                checkFilteringResult(array.sort(gpf.createFilterFunction(testCase.filter)), testCase.expected);
+                checkFilteringResult(array.filter(gpf.createFilterFunction(testCase.filter)), testCase.expected);
             });
 
         });
