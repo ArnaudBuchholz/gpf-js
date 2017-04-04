2b41013c852935157d38e1dfd1508f2a3a9b1200
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-04-04 11:04:33 -0400

    Split interface to increase maintainability

--- src/interfaces/enumerator.js
+++ src/interfaces/enumerator.js
@@ -3,8 +3,8 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfCreateAbstractFunction*/ // Build a function that throws the abstractMethod exception
-/*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfDefineInterface*/ // Internal interface definition helper
+/*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
 /*#endif*/
 
 /**
@@ -12,33 +12,28 @@
  *
  * @interface gpf.interfaces.IEnumerator
  */
-_gpfDefine({
-    $interface: "gpf.interfaces.IEnumerator",
 
-    /**
-     * Sets the enumerator to its initial position, which is *before* the first element in the collection.
-     *
-     * NOTE once reset has been called, you must call moveNext to access (or not) the first element
-     *
-     * @method gpf.interfaces.IEnumerator#reset
-     * @return {Promise} Resolved when the enumerator is ready to be used
-     */
-    reset: _gpfCreateAbstractFunction(0),
+/**
+ * Sets the enumerator to its initial position, which is *before* the first element in the collection.
+ *
+ * NOTE once reset has been called, you must call moveNext to access (or not) the first element
+ *
+ * @method gpf.interfaces.IEnumerator#reset
+ * @return {Promise} Resolved when the enumerator is ready to be used
+ */
 
-    /**
-     * Moves the enumerator to the next element of the enumeration.
-     *
-     * @method gpf.interfaces.IEnumerator#moveNext
-     * @return {Promise<*>} Resolved with the next element
-     */
-    moveNext: _gpfCreateAbstractFunction(0),
+/**
+ * Moves the enumerator to the next element of the enumeration.
+ *
+ * @method gpf.interfaces.IEnumerator#moveNext
+ * @return {Promise<*>} Resolved with the next element
+ */
 
-    /**
-     * Gets the current element in the collection.
-     *
-     * @method gpf.interfaces.IEnumerator#getCurrent
-     * @return {*} Current element in the collection (undefined if none)
-     */
-    getCurrent: _gpfCreateAbstractFunction(0)
+/**
+ * Gets the current element in the collection.
+ *
+ * @method gpf.interfaces.IEnumerator#getCurrent
+ * @return {*} Current element in the collection (undefined if none)
+ */
 
-});
+_gpfDefineInterface("Enumerator", _gpfSyncReadSourceJSON("interfaces/enumerator.json"));
