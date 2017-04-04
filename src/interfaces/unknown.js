1e7bc9c49c0451e14bc9fedd88b8dd6764f13ef9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-04-04 11:04:57 -0400

    Split interface to increase maintainability

--- src/interfaces/unknown.js
+++ src/interfaces/unknown.js
@@ -4,8 +4,8 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfCreateAbstractFunction*/ // Build a function that throws the abstractMethod exception
-/*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfDefineInterface*/ // Internal interface definition helper
+/*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
 /*#endif*/
 
 /**
@@ -14,16 +14,14 @@
  * @interface gpf.interfaces.IUnknown
  * @since 0.1.8
  */
-_gpfDefine({
-    $interface: "gpf.interfaces.IUnknown",
 
-    /**
-     * Retrieves an object supporting the requested interface (maybe the object itself)
-     *
-     * @method gpf.interfaces.IUnknown#queryInterface
-     * @param {Function} Interface specifier function
-     * @return {Object} The object implementing the interface or null
-     * @since 0.1.8
-     */
-    queryInterface: _gpfCreateAbstractFunction(1)
-});
+/**
+ * Retrieves an object supporting the requested interface (maybe the object itself)
+ *
+ * @method gpf.interfaces.IUnknown#queryInterface
+ * @param {Function} Interface specifier function
+ * @return {Object} The object implementing the interface or null
+ * @since 0.1.8
+ */
+
+_gpfDefineInterface("Unknown", _gpfSyncReadSourceJSON("interfaces/unknown.json"));
