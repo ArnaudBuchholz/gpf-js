f38359bb0409f67e0f8d596ec64fdcb942a9cb75
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-04-08 09:31:17 -0400

    getFileInfo

--- test/fs.js
+++ test/fs.js
@@ -4,15 +4,45 @@ describe("fs", function () {
 
     describe("gpf.fs.getFileStorage", function () {
 
-        it("is implemented only on specific hosts", function () {
-            var iFileStorage = gpf.fs.getFileStorage(),
-                host = gpf.host();
-            if (gpf.hosts.unknown === host) {
+        if (gpf.hosts.unknown === gpf.host()) {
+
+            it("returns a file storage interface", function () {
+                var iFileStorage = gpf.fs.getFileStorage();
                 assert(gpf.interfaces.isImplementedBy(gpf.interfaces.IFileStorage, iFileStorage));
-            } else {
+            });
+
+            it("can get file information", function (done) {
+                var iFileStorage = gpf.fs.getFileStorage();
+                iFileStorage.getInfo("test/data/file.bin")
+                    .then(function (info) {
+                        try {
+                            assert(info.type === gpf.fs.types.file);
+                            assert(info.fileName === "file.bin");
+                            assert(info.filefilePath.indexOf("test/data/file.bin") !== -1);
+                            assert(info.size === 256);
+                            assert(info.createdDateTime instanceof Date);
+                            assert(info.createdDateTime.getUTCFullYear() > 2010);
+                            assert(info.modifiedDateTime instanceof Date);
+                            assert(info.modifiedDateTime.getUTCFullYear() > 2010);
+
+                        } catch (e) {
+                            done(e);
+                        }
+                        done();
+
+                    }, function (error) {
+                        done(error);
+                    });
+            });
+
+        } else {
+
+            it("does not return a file storage interface", function () {
+                var iFileStorage = gpf.fs.getFileStorage();
                 assert(null === iFileStorage);
-            }
-        });
+            });
+
+        }
 
     });
 
