bd620e042647171329295dfb6b61dc09fe0b0708
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-04-10 16:03:16 -0400

    WIP

--- src/stream/node.js
+++ src/stream/node.js
@@ -3,10 +3,8 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfAssert*/ // Assertion method
 /*global _gpfDefine*/ // Shortcut for gpf.define
 /*global _gpfI*/ // gpf.interfaces
-/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*global _GPF_HOST*/
 /*global _gpfHost*/
 /*global _gpfCreateAbstractFunction*/ // Build a function that throws the abstractMethod exception
@@ -42,7 +40,7 @@ if (_GPF_HOST.NODEJS === _gpfHost) {
         /**
          * @property {Object} NodeJS stream object
          */
-        _stream: null,
+        _stream: null
 
     });
 
@@ -57,17 +55,6 @@ if (_GPF_HOST.NODEJS === _gpfHost) {
         $class: "gpf.node.ReadableStream",
         $extend: "gpf.node.BaseStream",
 
-        /**
-         * @param {Object} stream NodeJS stream object
-         * @constructor
-         */
-        constructor: function (stream) {
-            this.$super(stream);
-            stream.on("end", this._onEnd.bind(this));
-            stream.on("error", this._onError.bind(this));
-            stream.on("readable", this._onReadable.bind(this));
-        },
-
         /** @inheritdoc gpf.node.BaseStream#close */
         close: function () {
             return Promise.resolve();
@@ -76,144 +63,70 @@ if (_GPF_HOST.NODEJS === _gpfHost) {
         //region gpf.interfaces.IReadableStream
 
         /** @inheritdoc gpf.interfaces.IReadableStream#read */
-        read: _gpfStreamSecureRead(function (size) {
+        read: _gpfStreamSecureRead(function (output) {
+            var stream = this._stream; //eslint-disable-line no-invalid-this
             return new Promise(function (resolve, reject) {
-                this._resolve = resolve;
-                this._reject = reject;
-                if (this._ended) {
-                    return resolve([]);
-                }
-                _read(size);
-            }.bind(this));
-        },
-
-        _read: function (size) {
-            var chunk = this._stream.read(size);
-            if (this._failed) {
-                return reject(this._failed);
-            }
-            if (null === chunk) {
-                // Must wait for readable
-            }
-            resolve(chunk);
-
-            var chunk = this._stream.read(size);
-
-            if (null === chunk) {
-                //
-                return Promise.resolve([]);
-            }
-            return Promise.resolve(chunk);
-
-
-            this._setEventsHandler(eventsHandler, gpf.Error.readInProgress);
-            // If we received the "readable" event
-            if (this._readable) {
-                // We try to read a chunk
-                chunk = this._stream.read(size);
-                if (chunk) {
-                    this._onData(chunk);
-                    return;
-                }
-                // No chunk means we must wait for next "readable"
-                this._readable = false;
-            }
-            this._size = size;
-            this._stream.once("readable", this._boundOnReadable);
-        }),
+                stream
+                    .on("data", function (chunk) {
+                        stream.pause();
+                        output.write(chunk)
+                            .then(function () {
+                                stream.resume();
+                            }, function (e) {
+                                stream.close();
+                                reject(e);
+                            });
+                    })
+                    .on("end", resolve)
+                    .on("error", reject);
+            });
+        })
 
         //endregion
 
-            /**
-             * Handles "readable" stream event
-             * NOTE that it was registered with once
-             */
-            _onReadable: function () {
-                this._readable = true;
-                this._onData(this._stream.read(this._size));
-            },
-
-            // @property {Function} onReadable bound to this
-            _boundOnReadable: null,
-
-            /**
-             * Handles "data" stream event
-             *
-             * @param {Buffer} chunk
-             */
-            _onData: function (chunk) {
-                _gpfEventsFire.call(this, _GPF_EVENT_DATA, {buffer: gpf.node.buffer2JsArray(chunk)},
-                    this._getEventsHandler());
-            },
-
-            _onEnd: function () {
-                _gpfEventsFire.call(this, _GPF_EVENT_END_OF_DATA, {}, this._getEventsHandler());
-            },
-
-            _onError: function (error) {
-                _gpfEventsFire.call(this, _GPF_EVENT_ERROR, {error: error}, this._getEventsHandler());
-            }
-
-        }
     });
 
     /**
-     * Wraps a writable stream from NodeJS into a IReadableStream
+     * Wraps a writable stream from NodeJS into a IWritableStream
      *
-     * @class gpf.stream.NodeWritable
-     * @extends {gpf.node.Stream}
-     * @implements gpf.interfaces.IReadableStream
+     * @class {gpf.node.WritableStream}
+     * @extends {gpf.node.BaseStream}
+     * @implements {gpf.interfaces.IWritableStream}
      */
-    _gpfDefine("gpf.node.WritableStream", _GpfNodeStream, {
-        "[Class]": [gpf.$InterfaceImplement(_gpfI.IWritableStream)],
-        "+": {
+    _gpfDefine({
 
-            // @param {stream.Writable} stream
-            constructor: function (stream) {
-                this._super(stream);
-                this._boundOnWritten =  this._onWritten.bind(this);
-            },
-
-            // @inheritdoc gpf.node.Stream#close
-            close: function (eventsHandler) {
-                var me = this;
-                this._stream.end(function () {
-                    _gpfEventsFire.call(me, _GPF_EVENT_READY, {}, eventsHandler);
-                });
-            },
-
-            //region gpf.interfaces.IWritableStream
-
-            // @inheritdoc gpf.interfaces.IWritableStream#write
-            write: function (buffer, eventsHandler) {
-                this._setEventsHandler(eventsHandler, gpf.Error.writeInProgress);
-                this._stream.write(buffer, this._boundOnWritten);
-            }
-
-            //endregion
-
+        /** @inheritdoc gpf.node.BaseStream#close */
+        close: function () {
+            return Promise.resolve();
         },
-        "#": {
 
-            // @property {gpf.events.Handler} Last read eventsHandler also used as a critical section
-            _eventsHandler: null,
+        //region gpf.interfaces.IReadableStream
 
-            /**
-             * Handles the write callback
-             * NOTE used through the _boundOnWritten member
-             */
-            _onWritten: function (error) {
-                if (error) {
-                    _gpfEventsFire.call(this, _GPF_EVENT_ERROR, {error: error}, this._getEventsHandler());
-                } else {
-                    _gpfEventsFire.call(this, _GPF_EVENT_READY, {}, this._getEventsHandler());
-                }
-            },
+        /** @inheritdoc gpf.interfaces.IWritableStream#write */
+        read: _gpfStreamSecureWrite(function (buffer) {
+            var stream = this._stream; //eslint-disable-line no-invalid-this
+            return new Promise(function (resolve, reject) {
+                stream.write(buffer);
 
-            // @property {Function} onWritten bound to this
-            _boundOnWritten: null
+                stream
+                    .on("data", function (chunk) {
+                        stream.pause();
+                        output.write(chunk)
+                            .then(function () {
+                                stream.resume();
+                            }, function (e) {
+                                stream.close();
+                                reject(e);
+                            });
+                    })
+                    .on("end", resolve)
+                    .on("error", reject);
+            });
 
-        }
+        })
+
+        //endregion
+
     });
 
 }
