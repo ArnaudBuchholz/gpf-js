f47fab3a53a32a98a1b54746457d053721fa6408
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-04-13 15:43:21 -0400

    Test protection

--- test/stream/string.js
+++ test/stream/string.js
@@ -20,4 +20,35 @@ describe("stream/string", function () {
 
     });
 
+    describe("gpf.stream.WritableString", function () {
+
+        it("is protected against parallel calls", function () {
+            var exceptionCaught,
+                iWritableStream = new gpf.stream.WritableString();
+            try {
+                iWritableStream.write("1");
+                iWritableStream.write("2");
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.WriteInProgress);
+        });
+
+        it("supports multiple sequenced calls", function (done) {
+            var iWritableStream = new gpf.stream.WritableString();
+            iWritableStream.write("Hell")
+                .then(function () {
+                    return iWritableStream.write("o ");
+                })
+                .then(function () {
+                    return iWritableStream.write("World");
+                })
+                .then(function () {
+                    assert(iWritableStream.toString() === "Hello World");
+                    done();
+                });
+        });
+
+    });
+
 });
