564856ba2b8ffb7fffdbd5636e9c8d9409023169
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-04-13 22:36:20 -0400

    Documentation

--- src/stream/string.js
+++ src/stream/string.js
@@ -1,12 +1,13 @@
 /**
  * @file String related streams
+ * @since 0.1.9
  */
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfDefine*/ // Shortcut for gpf.define
-/*global _gpfStreamSecureInstallProgressFlag*/
-/*global _gpfStreamSecureRead*/
-/*global _gpfStreamSecureWrite*/
+/*global _gpfStreamSecureInstallProgressFlag*/ // Install the progress flag used by _gpfStreamSecureRead and Write
+/*global _gpfStreamSecureRead*/ // Generate a wrapper to secure multiple calls to stream#read
+/*global _gpfStreamSecureWrite*/ // Generates a wrapper to secure multiple calls to stream#write
 /*#endif*/
 
 var
@@ -15,6 +16,7 @@ var
      *
      * @class {gpf.node.ReadableStream}
      * @implements {gpf.interfaces.IReadableStream}
+     * @since 0.1.9
      */
     _gpfStreamReadableString = _gpfDefine({
         $class: "gpf.stream.ReadableString",
@@ -22,6 +24,7 @@ var
         /**
          * @param {String} buffer String buffer
          * @constructor
+         * @since 0.1.9
          */
         constructor: function (buffer) {
             this._buffer = buffer;
@@ -29,14 +32,20 @@ var
 
         //region gpf.interfaces.IReadableStream
 
-        /** @inheritdoc gpf.interfaces.IReadableStream#read */
+        /**
+         * @inheritdoc gpf.interfaces.IReadableStream#read
+         * @since 0.1.9
+         */
         read: _gpfStreamSecureRead(function (output) {
             return output.write(this._buffer); //eslint-disable-line no-invalid-this
         }),
 
         //endregion
 
-        /** Buffer */
+        /**
+         * Buffer
+         * @since 0.1.9
+         */
         _buffer: ""
 
     }),
@@ -46,18 +55,25 @@ var
      *
      * @class {gpf.node.WritableStream}
      * @implements {gpf.interfaces.IWritableStream}
+     * @since 0.1.9
      */
     _gpfStreamWritableString = _gpfDefine({
         $class: "gpf.stream.WritableString",
 
-        /** @constructor */
+        /**
+         * @constructor
+         * @since 0.1.9
+         */
         constructor: function () {
             this._buffer = [];
         },
 
         //region gpf.interfaces.IReadableStream
 
-        /** @inheritdoc gpf.interfaces.IWritableStream#write */
+        /**
+         * @inheritdoc gpf.interfaces.IWritableStream#write
+         * @since 0.1.9
+         */
         write: _gpfStreamSecureWrite(function (buffer) {
             this._buffer.push(buffer.toString()); //eslint-disable-line no-invalid-this
             return Promise.resolve();
@@ -73,6 +89,7 @@ var
          * Buffer
          *
          * @type {String[]}
+         * @since 0.1.9
          */
         _buffer: []
 
