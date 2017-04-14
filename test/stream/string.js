961a21615933a0343ca47c8696a1ce5ef286555b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-04-13 23:10:16 -0400

    Increase coverage

--- test/stream/string.js
+++ test/stream/string.js
@@ -4,9 +4,34 @@ describe("stream/string", function () {
 
     describe("gpf.stream.ReadableString", function () {
 
-        // it("is secured", function () {
-        //
-        // });
+        it("is accepts only IWritableStream", function () {
+            var iReadableStream = new gpf.stream.ReadableString("Hello World"),
+                exceptionCaught;
+            try {
+                iReadableStream.read({});
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.InterfaceExpected);
+        });
+
+        it("is protected against parallel calls", function () {
+            var iWritableStream = {
+                    write: function (buffer) {
+                        assert("string" === typeof buffer);
+                        return new Promise(function () {});
+                    }
+                },
+                iReadableStream = new gpf.stream.ReadableString("Hello World"),
+                exceptionCaught;
+            try {
+                iReadableStream.read(iWritableStream);
+                iReadableStream.read(iWritableStream);
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.ReadInProgress);
+        });
 
         it("writes to an IWritableStream", function (done) {
             var iWritableStream = new gpf.stream.WritableString(),
@@ -15,6 +40,26 @@ describe("stream/string", function () {
                 .then(function () {
                     assert(iWritableStream.toString() === "Hello World");
                     done();
+                });
+        });
+
+        it("forwards any error", function (done) {
+            var iWritableStream = {
+                    write: function (buffer) {
+                        assert("string" === typeof buffer);
+                        return Promise.reject(1);
+                    }
+                },
+                iReadableStream = new gpf.stream.ReadableString("Hello World");
+            iReadableStream.read(iWritableStream)
+                .then(function () {}, function (reason) {
+                    var exceptionCaught;
+                    try {
+                        assert(reason === 1);
+                    } catch (e) {
+                        exceptionCaught = e;
+                    }
+                    done(exceptionCaught);
                 });
         });
 
