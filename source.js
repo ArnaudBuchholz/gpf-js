019d4d209b8fbd7c1f24f7bd5be321367e872b06
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-03 14:49:46 -0400

    Implements require("gpf-js/source") by accessing parent gpf-js project if existing

--- source.js
+++ source.js
@@ -3,4 +3,36 @@
 "use strict";
 
 // allows require("gpf-js/source")
-module.exports = require("./src/boot.js");
+
+const
+    path = require("path"),
+    fs = require("fs"),
+    gpfSourcesPath = path.join(__dirname, "../../../gpf-js/src"),
+    backupOfGpfSourcePath = global.gpfSourcesPath,
+    backupOfGpf = global.gpf,
+
+    restoreGlobal = (name, value) => {
+        if (undefined === value) {
+            delete global[name];
+        } else {
+            global[name] = value;
+        }
+    };
+
+let
+    stats;
+
+try {
+    stats = fs.statSync(gpfSourcesPath);
+} catch (e) {}
+
+if (stats && stats.isDirectory()) {
+    global.gpfSourcesPath = gpfSourcesPath;
+    console.log(gpfSourcesPath);
+    require(gpfSourcesPath + "/boot.js");
+    restoreGlobal("gpfSourcesPath", backupOfGpfSourcePath);
+    module.exports = gpf;
+    restoreGlobal("gpf", backupOfGpf);
+} else {
+    module.exports = require("./build/gpf.js");
+}
