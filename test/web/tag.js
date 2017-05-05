3e7afa048e4e4e22e13884463ce5e0b92229c82c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-05 14:18:52 -0400

    Work on #167

--- test/web/tag.js
+++ test/web/tag.js
@@ -23,35 +23,39 @@ describe("web/tag", function () {
         it("supports tree building (parameters)", function () {
             var div = gpf.web.createTagFunction("div"),
                 span = gpf.web.createTagFunction("span"),
-                tree = div({className: "test1"}, "Hello ", span({className: "test2"}, "World"));
-            assert(tree.toString() === "<div class=\"test1\">Hello <span class=\"test2\">World</span></div>");
+                tree = div({className: "test1"}, "Hello ", span({className: "test2"}, "World!"));
+            assert(tree.toString() === "<div class=\"test1\">Hello <span class=\"test2\">World!</span></div>");
         });
 
         it("supports tree building (array parameter)", function () {
             var div = gpf.web.createTagFunction("div"),
                 span = gpf.web.createTagFunction("span"),
-                tree = div([span("Wor"), span("Wor")]);
-            assert(tree.toString() === "<div>Hello <span>Wor</span><span>ld</span></div>");
+                tree = div("Hello ", [span("Wor"), span("ld!")]);
+            assert(tree.toString() === "<div>Hello <span>Wor</span><span>ld!</span></div>");
         });
 
         it("allows DOM injection", function () {
-            var nodeProto = {
-                    _attributes: {},
-                    _children: [],
-                    appendChild: function (child) {
-                        this._children.push(child);
-                        return child;
-                    },
-                    setAttribute: function (name, value) {
-                        if (!this.hasOwnAttribute("_attributes")) {
-                            this._attributes = {};
-                        }
-                        this._attributes[name] = value;
+            function Node () {}
+            Node.prototype = {
+                _attributes: {},
+                _children: [],
+                appendChild: function (child) {
+                    if (!this.hasOwnProperty("_children")) {
+                        this._children = [];
                     }
+                    this._children.push(child);
+                    return child;
                 },
-                mockDocument = {
+                setAttribute: function (name, value) {
+                    if (!this.hasOwnProperty("_attributes")) {
+                        this._attributes = {};
+                    }
+                    this._attributes[name] = value;
+                }
+            };
+            var mockDocument = {
                     createElement: function (nodeName) {
-                        var node = Object.create(nodeProto);
+                        var node = new Node();
                         node.nodeName = nodeName;
                         node.ownerDocument = this;
                         return node;
@@ -65,13 +69,13 @@ describe("web/tag", function () {
                 span = gpf.web.createTagFunction("span"),
                 tree = div({className: "test"}, "Hello ", span("World!"));
             var result = tree.appendTo(mockNode);
-            assert(result instanceof nodeProto);
+            assert(result instanceof Node);
             assert(result.ownerDocument === mockDocument);
             assert(result.nodeName === "div");
-            assert(result._attributes.className === "test");
+            assert(result._attributes["class"] === "test");
             assert(result._children.length === 2);
             assert(result._children[0] === "Hello ");
-            assert(result._children[1] instanceof nodeProto);
+            assert(result._children[1] instanceof Node);
             assert(result._children[1]._children[0] === "World!");
         });
 
