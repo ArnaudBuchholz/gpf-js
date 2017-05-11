ebfe8748366c51d94797f73422bd3d704146ce35
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-11 18:09:52 -0400

    Adds wait time

--- grunt/connect-middleware/echo.js
+++ grunt/connect-middleware/echo.js
@@ -11,18 +11,22 @@ module.exports = (request, response, next) => {
     }
 
     let parsedUrl = url.parse(request.url, true),
-        statusCode;
+        statusCode = 200,
+        wait = 0;
 
     if (parsedUrl.query.status) {
         statusCode = parseInt(parsedUrl.query.status, 10);
-    } else {
-        statusCode = 200;
+    }
+    if (parsedUrl.query.wait) {
+        wait = parseInt(parsedUrl.query.wait, 10);
     }
 
-    response.statusCode = statusCode;
-    response.end(JSON.stringify({
-        method: request.method,
-        url: request.url,
-        content: parsedUrl.query.content
-    }));
+    setTimeout(() => {
+        response.statusCode = statusCode;
+        response.end(JSON.stringify({
+            method: request.method,
+            url: request.url,
+            content: parsedUrl.query.content
+        }));
+    }, wait);
 };
