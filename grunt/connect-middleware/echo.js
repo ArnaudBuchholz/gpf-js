b018529c75d8101cc746e6b9c359f3f38d883101
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-12 10:38:33 -0400

    Adds more instruments

--- grunt/connect-middleware/echo.js
+++ grunt/connect-middleware/echo.js
@@ -12,7 +12,9 @@ module.exports = (request, response, next) => {
 
     let parsedUrl = url.parse(request.url, true),
         statusCode = 200,
-        wait = 0;
+        wait = 0,
+        attributes,
+        content;
 
     if (parsedUrl.query.status) {
         statusCode = parseInt(parsedUrl.query.status, 10);
@@ -20,13 +22,25 @@ module.exports = (request, response, next) => {
     if (parsedUrl.query.wait) {
         wait = parseInt(parsedUrl.query.wait, 10);
     }
+    if (parsedUrl.query.attributes) {
+        attributes = JSON.parse(parsedUrl.query.attributes);
+    }
+    if (parsedUrl.query.content) {
+        content = parsedUrl.query.content;
+    } else {
+        content = JSON.stringify({
+            method: request.method,
+            url: request.url
+        });
+    }
 
     setTimeout(() => {
         response.statusCode = statusCode;
-        response.end(JSON.stringify({
-            method: request.method,
-            url: request.url,
-            content: parsedUrl.query.content
-        }));
+        if (attributes) {
+            Object.keys(attributes).forEach(name => {
+                response.setHeader(name, attributes[name]);
+            });
+        }
+        response.end(content);
     }, wait);
 };
