af155e6c19bc5b5ae412e46385a5cbd5d60e38ff
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-15 14:56:42 -0400

    Uses path separator to run phantomJS (or it fails)

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const
+    pathSep = require("path").sep,
+
     silent = {
         stdout: false,
         stderr: false
@@ -84,7 +86,8 @@ function _buildTestConfig (name, command) {
 }
 
 _buildTestConfig("Node", "node test/host/nodejs.js");
-_buildTestConfig("Phantom", "node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs test/host/phantomjs.js");
+_buildTestConfig("Phantom", `node_modules${pathSep}phantomjs-prebuilt${pathSep}lib${pathSep}phantom${pathSep}bin`
+                            + `${pathSep}phantomjs test/host/phantomjs.js`);
 _buildTestConfig("Wscript", "cscript.exe /D /E:JScript test/host/cscript.js");
 _buildTestConfig("Rhino", "java -jar node_modules/rhino-1_7r5-bin/rhino1_7R5/js.jar test/host/rhino.js");
 Object.keys(configuration.browsers).forEach(browserName =>{
