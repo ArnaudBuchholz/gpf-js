fac188f639a349a23a59a2391149dbc483317f5e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-15 12:39:46 -0400

    Use shared header parser

--- src/http/browser.js
+++ src/http/browser.js
@@ -5,26 +5,13 @@
 "use strict";
 /*global _GPF_HOST*/ // Host types
 /*global _gpfHost*/ // Host type
+/*global _gpfHttpParseHeaders*/ // Parse HTTP response headers
 /*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*global _gpfSetHttpRequestImpl*/ // Set the HTTP Request Implementation method
 /*#endif*/
 
 /*jshint browser: true*/
 /*eslint-env browser*/
-
-var _gpfHttpBrowserResponseHeadersParser = new RegExp("([a-zA-Z\\-]+)\\s*:(.*)", "g");
-
-function _gpfHttpBrowserGetResponseHeaders (xhr) {
-    _gpfHttpBrowserResponseHeadersParser.lastIndex = 0;
-    var responseHeaders = xhr.getAllResponseHeaders(),
-        match = _gpfHttpBrowserResponseHeadersParser.exec(responseHeaders),
-        result = {};
-    while (match) {
-        result[match[1]] = match[2];
-        match = _gpfHttpBrowserResponseHeadersParser.exec(responseHeaders);
-    }
-    return result;
-}
 
 /* istanbul ignore next */ // Because tested with NodeJS
 if (_GPF_HOST.BROWSER === _gpfHost) {
@@ -42,7 +29,7 @@ if (_GPF_HOST.BROWSER === _gpfHost) {
             if (4 === xhr.readyState) {
                 resolve({
                     status: xhr.status,
-                    headers: _gpfHttpBrowserGetResponseHeaders(xhr),
+                    headers: _gpfHttpParseHeaders(xhr.getAllResponseHeaders()),
                     responseText: xhr.responseText
                 });
             }
