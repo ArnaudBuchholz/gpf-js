21f4b0f144cfb687f91c3791e1ad9017ae1db7ef
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-16 23:52:01 -0400

    Improves maintainability

--- src/http/rhino.js
+++ src/http/rhino.js
@@ -12,39 +12,60 @@
 /*jshint rhino: true*/
 /*eslint-env rhino*/
 
+function _gpfHttpRhinoSetHeaders (httpConnection, headers) {
+    if (headers) {
+        Object.keys(headers).forEach(function (headerName) {
+            httpConnection.setRequestProperty(headerName, headers[headerName]);
+        });
+    }
+}
+
+function _gpfHttpRhinoSendData (httpConnection, data) {
+    if (data) {
+        httpConnection.setDoOutput(true);
+        var outputStreamWriter = new java.io.OutputStreamWriter(httpConnection.getOutputStream());
+        outputStreamWriter.write(data);
+        outputStreamWriter.flush();
+        outputStreamWriter.close();
+    }
+}
+
+function _gpfHttpRhinoGetResponse (httpConnection) {
+    try {
+        return httpConnection.getInputStream();
+    } catch (e) {
+        return httpConnection.getErrorStream();
+    }
+}
+
+function _gpfHttpRhinoGetResponseText (httpConnection) {
+    var response = _gpfHttpRhinoGetResponse(httpConnection),
+        scanner = new java.util.Scanner(response),
+        responseText = String(scanner.useDelimiter("\\A").next());
+    response.close();
+    return responseText;
+}
+
+function _gpfHttpRhinoGetHeaders (httpConnection) {
+    var headers = {},
+        headerFields = httpConnection.getHeaderFields(),
+        keys = headerFields.keySet().toArray();
+    keys.forEach(function (key) {
+        headers[String(key)] = String(headerFields.get(key).get(0));
+    });
+    return headers;
+}
+
 if (_GPF_HOST.RHINO === _gpfHost) {
 
     _gpfSetHttpRequestImpl(function (request, resolve, reject) {
         _gpfIgnore(reject);
         var httpConnection = new java.net.URL(request.url).openConnection();
         httpConnection.setRequestMethod(request.method);
-        if (request.headers) {
-            Object.keys(request.headers).forEach(function (headerName) {
-                httpConnection.setRequestProperty(headerName, request.headers[headerName]);
-            });
-        }
-        if (request.data) {
-            httpConnection.setDoOutput(true);
-            var outputStreamWriter = new java.io.OutputStreamWriter(httpConnection.getOutputStream());
-            outputStreamWriter.write(request.data);
-            outputStreamWriter.flush();
-            outputStreamWriter.close();
-        }
-        var response;
-        try {
-            response = httpConnection.getInputStream();
-        } catch (e) {
-            response = httpConnection.getErrorStream();
-        }
-        var scanner = new java.util.Scanner(response),
-            responseText = String(scanner.useDelimiter("\\A").next());
-        response.close();
-        var headers = {},
-            headerFields = httpConnection.getHeaderFields(),
-            keys = headerFields.keySet().toArray();
-        keys.forEach(function (key) {
-            headers[String(key)] = String(headerFields.get(key).get(0));
-        });
+        _gpfHttpRhinoSetHeaders(httpConnection, request.headers);
+        _gpfHttpRhinoSendData(httpConnection, request.data);
+        var responseText = _gpfHttpRhinoGetResponseText(httpConnection);
+        var headers = _gpfHttpRhinoGetHeaders(httpConnection);
         resolve({
             status: httpConnection.getResponseCode(),
             headers: headers,
