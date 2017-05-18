da12d6169e42e75c1b6845f922f1d80ca917f2ad
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-17 23:46:47 -0400

    Put in common some logic

---
+++ src/http/helpers.js
@@ -0,0 +1,63 @@
+/**
+ * @file HTTP helpers
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfRegExpForEach*/ // Executes the callback for each match of the regular expression
+/*exported _gpfHttpParseHeaders*/ // Parse HTTP response headers
+/*exported _gpfHttpGenSend*/ // Generates a function that implements the http send logic
+/*exported _gpfHttpGenSetHeaders*/ // Generates a function that transmit headers to the http object
+/*#endif*/
+
+var _gpfHttpHeadersParserRE = new RegExp("([^:\\s]+)\\s*: ?([^\\r]*)", "gm");
+
+/**
+ * Parse HTTP response headers
+ *
+ * @param {String} headers Response headers
+ * @return {Object} headers dictionary
+ */
+function _gpfHttpParseHeaders (headers) {
+    var result = {};
+    _gpfHttpHeadersParserRE.lastIndex = 0;
+    _gpfRegExpForEach(_gpfHttpHeadersParserRE, headers, function (match) {
+        result[match[1]] = match[2];
+    });
+    return result;
+}
+
+/**
+ * Generates a function that transmit headers to the http object
+ *
+ * @param {String} methodName Name of the method to call
+ * @return {Function} Method to set the headers
+ * @gpf:closure
+ */
+function _gpfHttpGenSetHeaders (methodName) {
+    return function (httpObj, headers) {
+        if (headers) {
+            var method = httpObj[methodName];
+            Object.keys(headers).forEach(function (headerName) {
+                method.call(httpObj, headerName, headers[headerName]);
+            });
+        }
+    };
+}
+
+/**
+ * Generates a function that implements the http send logic
+ *
+ * @param {String} methodName Name of the method to call
+ * @return {Function} Method to trigger the send
+ * @gpf:closure
+ */
+function _gpfHttpGenSend (methodName) {
+    return function (httpObj, data) {
+        var method = httpObj[methodName];
+        if (data) {
+            method.call(httpObj, data);
+        } else {
+            method.call(httpObj);
+        }
+    };
+}
