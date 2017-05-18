77df376fec4cda414cadef1a97dd2722f06723e4
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-18 14:22:04 -0400

    Improves maintainability

--- src/http/nodejs.js
+++ src/http/nodejs.js
@@ -6,7 +6,7 @@
 /*global _GPF_HOST*/ // Host types
 /*global _gpfHttpRequestImplByHost*/ // HTTP Request Implementation per host
 /*global _GpfNodeReadableStream*/ // gpf.node.ReadableStream
-/*global _GpfStreamWritableString*/ // gpf.stream.WritableString
+/*global _gpfStringFromStream*/ // Read the stream
 /*global _gpfNodeHttp*/ // Node require("http")
 /*global  _gpfNodeUrl*/ // Node require("url")
 /*#endif*/
@@ -16,15 +16,14 @@
 
 function _gpfHttpNodeProcessResponse (nodeResponse, resolve) {
     nodeResponse.setEncoding("utf8");
-    var iReadableStream = new _GpfNodeReadableStream(nodeResponse),
-        iWritableStream = new _GpfStreamWritableString();
-    iReadableStream
-        .read(iWritableStream)
-        .then(function () {
+    var iReadableStream = new _GpfNodeReadableStream(nodeResponse);
+    _gpfStringFromStream(iReadableStream)
+        .then(function (responseText) {
+            iReadableStream.close();
             resolve({
                 status: nodeResponse.statusCode,
                 headers: nodeResponse.headers,
-                responseText: iWritableStream.toString()
+                responseText: responseText
             });
         });
 }
