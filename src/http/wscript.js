790f09279157ba12ea0404053c05851081620706
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-17 23:47:32 -0400

    Improves logic

--- src/http/wscript.js
+++ src/http/wscript.js
@@ -4,44 +4,28 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfHost*/ // Host type
+/*global _gpfHttpGenSend*/ // Generates a function that implements the http send logic
+/*global _gpfHttpGenSetHeaders*/ // Generates a function that transmit headers to the http object
 /*global _gpfHttpParseHeaders*/ // Parse HTTP response headers
-/*global _gpfSetHttpRequestImpl*/ // Set the HTTP Request Implementation method
+/*global _gpfHttpRequestImplByHost*/ // HTTP Request Implementation per host
 /*#endif*/
 
 /*jshint wsh: true*/
 /*eslint-env wsh*/
 /*eslint-disable new-cap*/
 
-function _gpfHttpWScriptSetHeaders (winHttp, headers) {
-    if (headers) {
-        Object.keys(headers).forEach(function (headerName) {
-            winHttp.setRequestHeader(headerName, headers[headerName]);
-        });
-    }
-}
+var _gpfHttpWScriptSetHeaders = _gpfHttpGenSetHeaders("setRequestHeader"),
+    _gpfHttpWScriptSend = _gpfHttpGenSend("Send");
 
-function _gpfHttpWScriptSend (winHttp, data) {
-    if (data) {
-        winHttp.Send(data);
-    } else {
-        winHttp.Send();
-    }
-}
-
-if (_GPF_HOST.WSCRIPT === _gpfHost) {
-
-    _gpfSetHttpRequestImpl(function (request, resolve) {
-        var winHttp = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-        // winHttp.SetTimeouts(0, 60000, 30000, 30000);
-        winHttp.Open(request.method, request.url);
-        _gpfHttpWScriptSetHeaders(winHttp, request.headers);
-        _gpfHttpWScriptSend(winHttp, request.data);
-        resolve({
-            status: winHttp.Status,
-            headers: _gpfHttpParseHeaders(winHttp.GetAllResponseHeaders()),
-            responseText: winHttp.ResponseText
-        });
+_gpfHttpRequestImplByHost[_GPF_HOST.WSCRIPT] = function (request, resolve) {
+    var winHttp = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+    // winHttp.SetTimeouts(0, 60000, 30000, 30000);
+    winHttp.Open(request.method, request.url);
+    _gpfHttpWScriptSetHeaders(winHttp, request.headers);
+    _gpfHttpWScriptSend(winHttp, request.data);
+    resolve({
+        status: winHttp.Status,
+        headers: _gpfHttpParseHeaders(winHttp.GetAllResponseHeaders()),
+        responseText: winHttp.ResponseText
     });
-
-}
+};
