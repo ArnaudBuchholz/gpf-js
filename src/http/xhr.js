096c6b1d4537dc871653b4d058815a9e9288db07
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-17 23:47:43 -0400

    Improves logic

--- src/http/xhr.js
+++ src/http/xhr.js
@@ -4,34 +4,21 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfHost*/ // Host type
+/*global _gpfHttpGenSend*/ // Generates a function that implements the http send logic
+/*global _gpfHttpGenSetHeaders*/ // Generates a function that transmit headers to the http object
 /*global _gpfHttpParseHeaders*/ // Parse HTTP response headers
-/*global _gpfSetHttpRequestImpl*/ // Set the HTTP Request Implementation method
+/*global _gpfHttpRequestImplByHost*/ // HTTP Request Implementation per host
 /*#endif*/
 
 /*jshint browser: true*/
 /*eslint-env browser*/
 
-function _gpfHttpXhrSetHeaders (xhr, headers) {
-    if (headers) {
-        Object.keys(headers).forEach(function (headerName) {
-            xhr.setRequestHeader(headerName, headers[headerName]);
-        });
-    }
-}
+var _gpfHttpXhrSetHeaders = _gpfHttpGenSetHeaders("setRequestHeader"),
+    _gpfHttpXhrSend = _gpfHttpGenSend("send");
 
-function _gpfHttpXhrSend (xhr, data) {
-    if (data) {
-        xhr.send(data);
-    } else {
-        xhr.send();
-    }
-}
-
-/* istanbul ignore next */ // Because tested with NodeJS
-if (_GPF_HOST.BROWSER === _gpfHost || _GPF_HOST.PHANTOMJS === _gpfHost) {
-
-    _gpfSetHttpRequestImpl(function (request, resolve) {
+_gpfHttpRequestImplByHost[_GPF_HOST.BROWSER]
+    = _gpfHttpRequestImplByHost[_GPF_HOST.PHANTOMJS]
+    = function (request, resolve) {
         var xhr = new XMLHttpRequest();
         xhr.open(request.method, request.url);
         _gpfHttpXhrSetHeaders(xhr, request.headers);
@@ -45,6 +32,4 @@ if (_GPF_HOST.BROWSER === _gpfHost || _GPF_HOST.PHANTOMJS === _gpfHost) {
             }
         };
         _gpfHttpXhrSend(xhr, request.data);
-    });
-
-}
+    };
