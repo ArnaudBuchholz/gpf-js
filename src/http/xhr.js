11515cfba175c0d5f9fd8d1acc5e237e77e67c22
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-18 09:55:21 -0400

    Improves maintainability

--- src/http/xhr.js
+++ src/http/xhr.js
@@ -16,20 +16,31 @@
 var _gpfHttpXhrSetHeaders = _gpfHttpGenSetHeaders("setRequestHeader"),
     _gpfHttpXhrSend = _gpfHttpGenSend("send");
 
+function _gpfHttpXhrOpen (request) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(request.method, request.url);
+    return xhr;
+}
+
+function _gpfHttpXhrWaitForCompletion (xhr, callback) {
+    xhr.onreadystatechange = function () {
+        if (4 === xhr.readyState) {
+            callback();
+        }
+    };
+}
+
 _gpfHttpRequestImplByHost[_GPF_HOST.BROWSER]
     = _gpfHttpRequestImplByHost[_GPF_HOST.PHANTOMJS]
     = function (request, resolve) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(request.method, request.url);
+        var xhr = _gpfHttpXhrOpen(request);
         _gpfHttpXhrSetHeaders(xhr, request.headers);
-        xhr.onreadystatechange = function () {
-            if (4 === xhr.readyState) {
-                resolve({
-                    status: xhr.status,
-                    headers: _gpfHttpParseHeaders(xhr.getAllResponseHeaders()),
-                    responseText: xhr.responseText
-                });
-            }
-        };
         _gpfHttpXhrSend(xhr, request.data);
+        _gpfHttpXhrWaitForCompletion(xhr, function () {
+            resolve({
+                status: xhr.status,
+                headers: _gpfHttpParseHeaders(xhr.getAllResponseHeaders()),
+                responseText: xhr.responseText
+            });
+        });
     };
