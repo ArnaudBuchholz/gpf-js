023a52bc3010b40fccb46a69fc46583847e256e9
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-17 23:47:24 -0400

    Improves logic

--- src/http/rhino.js
+++ src/http/rhino.js
@@ -4,20 +4,14 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfHost*/ // Host type
-/*global _gpfSetHttpRequestImpl*/ // Set the HTTP Request Implementation method
+/*global _gpfHttpGenSetHeaders*/ // Generates a function that transmit headers to the http object
+/*global _gpfHttpRequestImplByHost*/ // HTTP Request Implementation per host
 /*#endif*/
 
 /*jshint rhino: true*/
 /*eslint-env rhino*/
 
-function _gpfHttpRhinoSetHeaders (httpConnection, headers) {
-    if (headers) {
-        Object.keys(headers).forEach(function (headerName) {
-            httpConnection.setRequestProperty(headerName, headers[headerName]);
-        });
-    }
-}
+var _gpfHttpRhinoSetHeaders = _gpfHttpGenSetHeaders("setRequestProperty");
 
 function _gpfHttpRhinoSendData (httpConnection, data) {
     if (data) {
@@ -55,20 +49,16 @@ function _gpfHttpRhinoGetHeaders (httpConnection) {
     return headers;
 }
 
-if (_GPF_HOST.RHINO === _gpfHost) {
-
-    _gpfSetHttpRequestImpl(function (request, resolve) {
-        var httpConnection = new java.net.URL(request.url).openConnection();
-        httpConnection.setRequestMethod(request.method);
-        _gpfHttpRhinoSetHeaders(httpConnection, request.headers);
-        _gpfHttpRhinoSendData(httpConnection, request.data);
-        var responseText = _gpfHttpRhinoGetResponseText(httpConnection);
-        var headers = _gpfHttpRhinoGetHeaders(httpConnection);
-        resolve({
-            status: httpConnection.getResponseCode(),
-            headers: headers,
-            responseText: responseText
-        });
+_gpfHttpRequestImplByHost[_GPF_HOST.RHINO] = function (request, resolve) {
+    var httpConnection = new java.net.URL(request.url).openConnection();
+    httpConnection.setRequestMethod(request.method);
+    _gpfHttpRhinoSetHeaders(httpConnection, request.headers);
+    _gpfHttpRhinoSendData(httpConnection, request.data);
+    var responseText = _gpfHttpRhinoGetResponseText(httpConnection);
+    var headers = _gpfHttpRhinoGetHeaders(httpConnection);
+    resolve({
+        status: httpConnection.getResponseCode(),
+        headers: headers,
+        responseText: responseText
     });
-
-}
+};
