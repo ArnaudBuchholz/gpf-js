c1e9c24c7ebc2e0b5fc36adb63e646c11ec646b0
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-18 10:26:25 -0400

    Getting the requirements from the beginning

--- src/host/nodejs.js
+++ src/host/nodejs.js
@@ -7,13 +7,35 @@
 /*global _GPF_HOST*/ // Host types
 /*global _gpfExit:true*/ // Exit function
 /*global _gpfHost*/ // Host type
+/*global _gpfNodeFs:true*/ // Node require("fs")
+/*exported _gpfNodeHttp*/ // Node require("http")
+/*exported _gpfNodeUrl*/ // Node require("url")
 /*#endif*/
 
 /*jshint node: true*/
 /*eslint-env node*/
 
+var
+    /**
+     * require("http")
+     *
+     * @type {Object}
+     */
+    _gpfNodeHttp,
+
+    /**
+     * require("url")
+     *
+     * @type {Object}
+     */
+    _gpfNodeUrl;
+
 /* istanbul ignore else */ // Tested with NodeJS
 if (_GPF_HOST.NODEJS === _gpfHost) {
+
+    _gpfNodeFs = require("fs");
+    _gpfNodeHttp = require("http");
+    _gpfNodeUrl = require("url");
 
     /* istanbul ignore next */ // Not testable
     _gpfExit = function (code) {
