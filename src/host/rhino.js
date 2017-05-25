0dd87411ebfa50dbd82332ff53d864ef2fec9f0f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-25 12:53:14 -0400

    Improves coverage / simplify

--- src/host/rhino.js
+++ src/host/rhino.js
@@ -5,41 +5,29 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
+/*global _gpfBootImplByHost*/ // Boot host specific implementation per host
+/*global _gpfConsoleGenerate*/ // Generate an object that can be used to simulate console methods
 /*global _gpfExit:true*/ // Exit function
-/*global _gpfHost*/ // Host type
 /*global _gpfMainContext*/ // Main context object
 /*#endif*/
 
 /*jshint rhino: true*/
 /*eslint-env rhino*/
 
-/* istanbul ignore next */ // Tested with NodeJS
-if (_GPF_HOST.RHINO === _gpfHost) {
+/**
+ * @namespace gpf.rhino
+ * @description Root namespace for Rhino specifics
+ */
+gpf.rhino = {};
+
+_gpfBootImplByHost[_GPF_HOST.RHINO] = function () {
 
     // Define console APIs
-    _gpfMainContext.console = {
-        log: function (t) {
-            print("    " + t);
-        },
-        info: function (t) {
-            print("[?] " + t);
-        },
-        warn: function (t) {
-            print("/!\\ " + t);
-        },
-        error: function (t) {
-            print("(X) " + t);
-        }
-    };
+    _gpfMainContext.console = _gpfConsoleGenerate(print);
 
+    /* istanbul ignore next */ // Not testable
     _gpfExit = function (code) {
         java.lang.System.exit(code);
     };
 
-    /**
-     * @namespace gpf.rhino
-     * @description Root namespace for Rhino specifics
-     */
-    gpf.rhino = {};
-
-}
+};
