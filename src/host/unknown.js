2fcb84e19beefefc6fdca610142e11c8e2d35514
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-25 12:39:41 -0400

    Unknown host adapter

---
+++ src/host/unknown.js
@@ -0,0 +1,11 @@
+/**
+ * @file Unknown host adapter
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST*/ // Host types
+/*global _gpfBootImplByHost*/ // Boot host specific implementation per host
+/*global _gpfEmptyFunc*/ // An empty function
+/*#endif*/
+
+_gpfBootImplByHost[_GPF_HOST.UNKNOWN] = _gpfEmptyFunc;
