bbc5db37edc05dbf3bc414910738d67f2116b761
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-30 13:18:04 -0400

    Improves coverage (and improves _gpfIsArrayLike)

--- src/arraylike.js
+++ src/arraylike.js
@@ -4,9 +4,6 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _GPF_HOST*/ // Host types
-/*global _gpfHost*/ // Host type
-/*global _gpfWebWindow*/ // Browser window object
 /*exported _gpfIsArray*/ // Return true if the parameter is an array
 /*exported _gpfIsArrayLike*/ // Return true if the parameter looks like an array
 /*#endif*/
@@ -24,24 +21,12 @@ var _gpfIsArray = Array.isArray;
  * Return true if the parameter looks like an array, meaning a property length is available and members can be
  * accessed through the [] operator. The length property does not have to be writable.
  *
- * **NOTE**: when running in a browser, this includes
- * [HTMLCollection](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection)
- * and [NodeList](https://developer.mozilla.org/en-US/docs/Web/API/NodeList) types
- *
  * @param {Object} obj Object to test
  * @return {Boolean} True if array-like
  * @since 0.1.5
  */
-var _gpfIsArrayLike  = function (obj) { //eslint-disable-line func-style
-    return _gpfIsArray(obj);
-};
-
-if (_GPF_HOST.BROWSER === _gpfHost && (_gpfWebWindow.HTMLCollection || _gpfWebWindow.NodeList)) {
-    _gpfIsArrayLike = function (obj) {
-        return _gpfIsArray(obj)
-            || obj instanceof _gpfWebWindow.HTMLCollection
-            || obj instanceof _gpfWebWindow.NodeList;
-    };
+function _gpfIsArrayLike (obj) {
+    return _gpfIsArray(obj) || Math.floor(obj.length) === obj.length;
 }
 
 /**
