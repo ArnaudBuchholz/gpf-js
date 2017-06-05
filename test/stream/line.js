f3dc9e18452b7856c96716d584af34cd1fa5f909
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-06-05 00:29:42 -0400

    Testing the sequences

--- test/stream/line.js
+++ test/stream/line.js
@@ -2,29 +2,8 @@
 
 describe("stream/line", function () {
 
-    it("generates lines out of any stream", function (done) {
-        var lineIndex = 0,
-            expectedLines = [
-                "abcdef",
-                "ghijkl",
-                "mnopqr",
-                "stuvwx",
-                "yz"
-            ],
-            lineStream = new gpf.stream.LineAdapter(),
-            outStream = {
-                write: function (buffer) {
-                    if (buffer !== expectedLines[lineIndex]) {
-                        return Promise.reject("No match");
-                    }
-                    ++lineIndex;
-                    return Promise.resolve("No match");
-                }
-            };
-        lineStream.read(outStream)
-            .then(function () {
-                return lineStream.write("abc");
-            })
+    function _part1 (lineStream) {
+        return lineStream.write("abc")
             .then(function () {
                 return lineStream.write("def\n");
             })
@@ -33,20 +12,88 @@ describe("stream/line", function () {
             })
             .then(function () {
                 return lineStream.write("ijkl\r");
-            })
-            .then(function () {
-                return lineStream.write("\nmnopqr\n");
-            })
+            });
+    }
+
+    function _part2 (lineStream) {
+        return lineStream.write("\nmnopqr\n")
             .then(function () {
                 return lineStream.write("stuvwx\nyz");
+            });
+    }
+
+    function _getOutput () {
+        var lineIndex = 0,
+            expectedLines = [
+                "abcdef",
+                "ghijkl",
+                "mnopqr",
+                "stuvwx",
+                "yz"
+            ];
+        return {
+            write: function (buffer) {
+                if (buffer !== expectedLines[lineIndex]) {
+                    return Promise.reject("No match");
+                }
+                ++lineIndex;
+                return Promise.resolve();
+            }
+        };
+    }
+
+    it("generates lines out of any stream (read first)", function (done) {
+        var lineStream = new gpf.stream.LineAdapter();
+        lineStream.read(_getOutput())
+            .then(function () {
+                return _part1(lineStream);
+            })
+            .then(function () {
+                return _part2(lineStream);
+            })
+            .then(function () {
+                return lineStream.endOfStream();
+            })
+            .then(function () {
+                done();
+            })["catch"](done);
+    });
+
+    it("generates lines out of any stream (write first)", function (done) {
+        var lineStream = new gpf.stream.LineAdapter();
+        _part1(lineStream)
+            .then(function () {
+                return _part2(lineStream);
             })
             .then(function () {
                 return lineStream.write("\n");
             })
             .then(function () {
+                return lineStream.endOfStream();
+            })
+            .then(function () {
+                return lineStream.read(_getOutput());
+            })
+            .then(function () {
                 done();
             })["catch"](done);
+    });
 
+    it("generates lines out of any stream (mixed)", function (done) {
+        var lineStream = new gpf.stream.LineAdapter();
+        _part1(lineStream)
+            .then(function () {
+                return lineStream.read(_getOutput());
+            })
+            .then(function () {
+                return _part2(lineStream);
+            })
+            .then(function () {
+                return lineStream.endOfStream();
+            })
+            .then(function () {
+                done();
+            })["catch"](done);
     });
 
 });
