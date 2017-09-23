6ef8644869b22dfcc6cfa47d8d0a8707ae91b3f3
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-23 18:05:36 -0400

    IThenable interface

---
+++ src/interfaces/thenable.js
@@ -0,0 +1,34 @@
+/**
+ * @file IThenable interface
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfDefineInterface*/ // Internal interface definition helper
+/*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
+/*exported _gpfIThenable*/ // gpf.interfaces.IThenable
+/*#endif*/
+
+/**
+ * The Thenable interface helps identifying Promise object through the
+ * [then](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then) method
+ *
+ * @interface gpf.interfaces.IThenable
+ */
+
+/**
+ * The then() method returns a Promise. It takes up to two arguments: callback functions for the success and failure
+ * cases of the Promise
+ *
+ * @method gpf.interfaces.IThenable#then
+ * @param {Function} onFulfilled called if the Promise is fulfilled
+ * @param {Function} [onRejected] called if the Promise is rejected
+ * @return {Promise} Promise
+ */
+
+/**
+ * IThenable interface specifier
+ *
+ * @type {gpf.interfaces.IThenable}
+ */
+var _gpfIThenable = _gpfDefineInterface("Thenable",
+    _gpfSyncReadSourceJSON("interfaces/thenable.json"));
