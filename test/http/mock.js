40e46c104f36ce9864580b80413651538504cdc1
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-26 12:50:31 -0400

    More tests

--- test/http/mock.js
+++ test/http/mock.js
@@ -5,14 +5,14 @@ describe("gpf.http.mock", function () {
     var baseUrl;
 
     before(function () {
-        baseUrl = "http://localhost:" + config.httpPort + "/echo/?";
+        baseUrl = "http://localhost:" + config.httpPort + "/echo/";
     });
 
     it("exposes a method to mock requests", function () {
         assert("function" === typeof gpf.http.mock);
     });
 
-    it("exposes a method to cancel one specific mocking", function () {
+    it("exposes a method to remove one specific mocking", function () {
         assert("function" === typeof gpf.http.mock.remove);
     });
 
@@ -20,17 +20,20 @@ describe("gpf.http.mock", function () {
         assert("function" === typeof gpf.http.mock.reset);
     });
 
-    describe("first mocking example", function () {
+    describe("Simple mocking example", function () {
 
         var mockId;
 
-        before(function () {
+        beforeEach(function () {
             mockId = gpf.http.mock({
                 method: gpf.http.methods.get,
                 url: /echo\/\?status=([0-9]+)/,
                 response: function (request, status) {
+                    if (status === "400") {
+                        return; // Don't mock
+                    }
                     return Promise.resolve({
-                        status: String(parseInt(status, 10) + 1),
+                        status: parseInt(status, 10) + 1,
                         headers: {
                             "x-mock": true
                         },
@@ -40,15 +43,144 @@ describe("gpf.http.mock", function () {
             });
         });
 
-        after(function () {
-            gpf.http.mock.remove(mockId);
+        afterEach(function () {
+            if (mockId) {
+                gpf.http.mock.remove(mockId);
+            }
+
         });
 
         it("matches request URL and method", function (done) {
-            gpf.http.get(baseUrl + "status=200").then(function (response) {
-                assert(response.status === "201");
+            gpf.http.get(baseUrl + "?status=200").then(function (response) {
+                assert(response.status === 201);
                 assert(response.headers["x-mock"] === true);
                 assert(response.responseText === "It works");
+                done();
+            })["catch"](done);
+        });
+
+        it("matches request URL and method (non matching URL)", function (done) {
+            gpf.http.get(baseUrl).then(function (response) {
+                assert(response.status === 200);
+                assert(response.headers["x-mock"] === undefined);
+                done();
+            })["catch"](done);
+        });
+
+        it("matches request URL and method (non matching method)", function (done) {
+            gpf.http.put(baseUrl, "?status=200").then(function (response) {
+                assert(response.status === 200);
+                assert(response.headers["x-mock"] === undefined);
+                done();
+            })["catch"](done);
+        });
+
+        it("may decide to not mock", function (done) {
+            gpf.http.get(baseUrl + "?status=400").then(function (response) {
+                assert(response.status === 400);
+                assert(response.headers["x-mock"] === undefined);
+                done();
+            })["catch"](done);
+        });
+
+        it("does not mock once removed", function (done) {
+            gpf.http.mock.remove(mockId);
+            mockId = undefined;
+            gpf.http.get(baseUrl + "?status=200").then(function (response) {
+                assert(response.status === 200);
+                assert(response.headers["x-mock"] === undefined);
+                done();
+            })["catch"](done);
+        });
+
+        it("does not mock once removed (reset)", function (done) {
+            gpf.http.mock.reset();
+            mockId = undefined;
+            gpf.http.get(baseUrl + "?status=200").then(function (response) {
+                assert(response.status === 200);
+                assert(response.headers["x-mock"] === undefined);
+                done();
+            })["catch"](done);
+        });
+
+        it("does not fail if removed twice", function () {
+            gpf.http.mock.reset();
+            gpf.http.mock.remove(mockId);
+            mockId = undefined;
+        });
+
+    });
+
+    describe("Advanced mocking example", function () {
+
+        var mockIdForResponse3;
+
+        beforeEach(function () {
+
+            function getResponse (index) {
+                return function () {
+                    return Promise.resolve({
+                        status: 200,
+                        headers: {},
+                        responseText: "answer-" + index
+                    });
+                };
+            }
+
+            gpf.http.mock({
+                method: gpf.http.methods.get,
+                url: /echo\/lifo/,
+                response: getResponse(1)
+            });
+            gpf.http.mock({
+                method: gpf.http.methods.get,
+                url: /echo\/lifo/,
+                response: getResponse(2)
+            });
+            mockIdForResponse3 = gpf.http.mock({
+                method: gpf.http.methods.get,
+                url: /echo\/lifo/,
+                response: getResponse(3)
+            });
+            gpf.http.mock({
+                method: gpf.http.methods.put,
+                url: /echo\/lifo/,
+                response: getResponse(4)
+            });
+            gpf.http.mock({
+                method: gpf.http.methods.get,
+                url: /echo\/redirect/,
+                response: function (request) {
+                    return gpf.http.get(request.url.replace("/redirect", "/lifo"));
+                }
+            });
+        });
+
+        afterEach(function () {
+            gpf.http.mock.reset();
+        });
+
+        it("uses LIFO priority", function (done) {
+            gpf.http.get(baseUrl + "lifo").then(function (response) {
+                assert(response.status === 200);
+                assert(response.responseText === "answer-3");
+                done();
+            })["catch"](done);
+        });
+
+        it("preserves the order when removing a mocked request", function (done) {
+            gpf.http.mock.remove(mockIdForResponse3);
+            gpf.http.get(baseUrl + "lifo").then(function (response) {
+                assert(response.status === 200);
+                assert(response.responseText === "answer-2");
+                done();
+            })["catch"](done);
+        });
+
+        it("allows redirection", function (done) {
+            gpf.http.get(baseUrl + "redirect").then(function (response) {
+                assert(response.status === 200);
+                assert(response.responseText === "answer-3");
                 done();
             })["catch"](done);
         });
