f4de4eaecae5852c8154b177c6f12af7f6bd4f70
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-04 16:43:49 -0400

    Full fledge AMD example (#168)

---
+++ test/require/amd.js
@@ -0,0 +1,10 @@
+/*global define*/
+/*eslint strict: [2, "function"]*/
+define("amd", ["data.json"], function (data) {
+    "use strict";
+
+    return {
+        type: "amd",
+        data: data
+    };
+});
