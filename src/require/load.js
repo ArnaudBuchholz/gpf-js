28379e3a03cfdd2c50963b7fbd5d3f0f5808717c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-06 15:39:24 -0400

    Isolates _gpfRequireLoad (#168)

--- src/require/load.js
+++ src/require/load.js
@@ -1,6 +1,48 @@
 /**
- * @file Require loading implementation
+ * @file Require resource loading implementation
  */
 /*#ifndef(UMD)*/
 "use strict";
+/*global _GPF_FS_OPENFOR*/ // File system stream opening mode
+/*global _GPF_HOST*/ // Host types
+/*global _GPF_HTTP_METHODS*/ // HTTP Methods
+/*global _GpfStreamWritableString*/ // gpf.stream.WritableString
+/*global _gpfFileStorageByHost*/ // gpf.interfaces.IFileStorage per host
+/*global _gpfHost*/ // Host type
+/*global _gpfHttpRequest*/ // HTTP request common implementation
+/*exported _gpfRequireLoad*/ // Load the resource
 /*#endif*/
+
+/**
+ * Load the resource
+ * @param {String} name Resource name
+ * @return {Promise} Resolved with the resource
+ */
+var _gpfRequireLoad;
+
+function _gpfRequireLoadHTTP (name) {
+    return _gpfHttpRequest({
+        method: _GPF_HTTP_METHODS.GET,
+        url: name
+    }).then(function (response) {
+        return response.responseText;
+    });
+}
+
+function _gpfRequireLoadFS (name) {
+    var fs = _gpfFileStorageByHost[_gpfHost],
+        iWritableStream = new _GpfStreamWritableString();
+    return fs.fs.openTextStream(name, _GPF_FS_OPENFOR.READING)
+        .then(function (iReadStream) {
+            return iReadStream.read(iWritableStream);
+        })
+        .then(function () {
+            return iWritableStream.toString();
+        });
+}
+
+if (_gpfHost === _GPF_HOST.BROWSER || _gpfHost === _GPF_HOST.PHANTOM_JS) {
+    _gpfRequireLoad = _gpfRequireLoadHTTP;
+} else {
+    _gpfRequireLoad = _gpfRequireLoadFS;
+}
