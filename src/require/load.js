da652bc29a5667302b05a7dead46945f90d1de65
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-12 15:23:13 -0400

    Documentation (#192)

--- src/require/load.js
+++ src/require/load.js
@@ -1,5 +1,6 @@
 /**
  * @file Require resource loading implementation
+ * @since 0.2.2
  */
 /*#ifndef(UMD)*/
 "use strict";
@@ -10,7 +11,7 @@
 /*global _gpfFileStorageByHost*/ // gpf.interfaces.IFileStorage per host
 /*global _gpfHost*/ // Host type
 /*global _gpfHttpRequest*/ // HTTP request common implementation
-/*global _gpfPathExtension*/
+/*global _gpfPathExtension*/ // Get the extension of the last name of a path (including dot)
 /*exported _gpfRequireLoad*/ // Load the resource
 /*exported _gpfRequireProcessor*/ // Mapping of resource extension to processor function
 /*#endif*/
@@ -56,6 +57,7 @@ if (_gpfHost === _GPF_HOST.BROWSER || _gpfHost === _GPF_HOST.PHANTOM_JS) {
  * Mapping of resource extension to processor function
  *
  * @type {Object}
+ * @since 0.2.2
  */
 var _gpfRequireProcessor = {};
 
@@ -64,6 +66,7 @@ var _gpfRequireProcessor = {};
  *
  * @param {String} name Resource name
  * @return {Promise<*>} Resolved with the resource result
+ * @since 0.2.2
  */
 function _gpfRequireLoad (name) {
     var me = this;
