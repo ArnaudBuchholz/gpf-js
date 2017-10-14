73405671f4c02e3bb4cbd7e8a5b5972795332e2d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-14 10:14:47 -0400

    Introduces gpf.fs.read for all hosts (#168)

---
+++ src/fs/phantomjs.js
@@ -0,0 +1,16 @@
+/**
+ * @file PhantomJS File system implementation
+ * @since 0.2.2
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST*/ // Host types
+/*global _gpfFsReadImplByHost*/ // gpf.fs.read per host
+/*global _gpfNodeFs*/ // Node/PhantomJS require("fs")
+/*#endif*/
+
+_gpfFsReadImplByHost[_GPF_HOST.PHANTOMJS] = function (path) {
+    return new Promise(function (resolve) {
+        resolve(_gpfNodeFs.read(path));
+    });
+};
