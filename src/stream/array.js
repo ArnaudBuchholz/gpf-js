34c1e9cae906e9e1dde7df8f81415ba57345c16f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-20 13:54:44 -0400

    gpf.stream.WritableArray (#196)

--- src/stream/array.js
+++ src/stream/array.js
@@ -1,126 +1,93 @@
 /**
- * @file Array helpers
+ * @file Array related streams
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _GPF_EVENT_DATA*/ // gpf.events.EVENT_DATA
-/*global _GPF_EVENT_END_OF_DATA*/ // gpf.events.EVENT_END_OF_DATA
-/*global _GPF_EVENT_READY*/ // gpf.events.EVENT_READY
-/*global _gpfAssert*/ // Assertion method
 /*global _gpfDefine*/ // Shortcut for gpf.define
-/*global _gpfEventsFire*/ // gpf.events.fire (internal, parameters must match)
-/*global _gpfI*/ // gpf.interfaces
-/*global _gpfStreamPipe*/ // gpf.stream.pipe
-/*exported _gpfArrayStream*/ // IReadableStream & IWritableStream for array
+/*global _gpfStreamSecureInstallProgressFlag*/ // Install the progress flag used by _gpfStreamSecureRead and Write
+/*global _gpfStreamSecureRead*/ // Generate a wrapper to secure multiple calls to stream#read
+/*global _gpfStreamSecureWrite*/ // Generates a wrapper to secure multiple calls to stream#write
+/*exported _GpfStreamReadableArray*/ // gpf.stream.ReadableArray
+/*exported _GpfStreamWritableArray*/ // gpf.stream.WritableArray
 /*#endif*/
 
 var
-    /**
-     * Implements IReadableStream & IWritableStream on top of an array (FIFO read / write)
-     * NOTE content is ignored, only the array logic is used
-     *
-     * @class ArrayStream
-     * @extends Object
-     * @implements gpf.interfaces.IReadableStream, gpf.interfaces.IWritableStream
-     */
-    _GpfArrayStream = _gpfDefine("ArrayStream", {
-        "[Class]": [gpf.$InterfaceImplement(_gpfI.IReadableStream), gpf.$InterfaceImplement(_gpfI.IWritableStream)],
-        "+": {
+    _GpfStreamReadableArray = _gpfDefine(/** @lends gpf.stream.ReadableArray.prototype */ {
+        $class: "gpf.stream.ReadableArray",
 
-            /**
-             * @param {Array} [array=undefined] array Cloned
-             * @constructor
-             */
-            constructor: function (array) {
-                if (array instanceof Array) {
-                    this._buffer = [].concat(array);
-                } else {
-                    this._buffer = [];
-                }
-            },
-
-            //region gpf.interfaces.IReadableStream
-
-            // @inheritdoc gpf.interfaces.IReadableStream#read
-            read: function (count, eventsHandler) {
-                var result;
-                if (0 === this._buffer.length) {
-                    _gpfEventsFire.call(this, _GPF_EVENT_END_OF_DATA, {}, eventsHandler);
-                } else {
-                    result = this._buffer.splice(0, count);
-                    _gpfEventsFire.call(this, _GPF_EVENT_DATA, {buffer: result}, eventsHandler);
-                }
-            },
-
-            //endregion
-
-            //region gpf.interfaces.IReadableStream
-
-            // @inheritdoc gpf.interfaces.IWritableStream#read
-            write: function (buffer, eventsHandler) {
-                _gpfAssert(buffer instanceof Array, "Array expected");
-                this._buffer = this._buffer.concat(buffer);
-                _gpfEventsFire.call(this, _GPF_EVENT_READY, {}, eventsHandler);
-            },
-
-            //endregion
-
-            /**
-             * Consolidate the result array
-             * @return {Array}
-             */
-            toArray: function () {
-                return [].concat(this._buffer);
-            }
-
+        /**
+         * Wraps an array inside a readable stream.
+         * Each array item is written separately to the output
+         *
+         * @constructor gpf.stream.ReadableArray
+         * @implements {gpf.interfaces.IReadableStream}
+         * @param {Array} buffer Array buffer
+         */
+        constructor: function (buffer) {
+            this._buffer = buffer;
         },
-        "-": {
 
-            // @property {Array} buffer
-            _buffer: []
+        //region gpf.interfaces.IReadableStream
 
-        }
+        /**
+         * @gpf:sameas gpf.interfaces.IReadableStream#read
+         */
+        read: _gpfStreamSecureRead(function (output) {
+            var buffer = this._buffer, //eslint-disable-line no-invalid-this
+                step = 0;
+            function write () {
+                if (buffer.length === step) {
+                    return Promise.resolve();
+                }
+                return output.write(buffer[step++]);
+            }
+            return write();
+        }),
+
+        //endregion
+
+        /**
+         * Buffer
+         */
+        _buffer: []
+
+    }),
+
+    _GpfStreamWritableArray = _gpfDefine(/** @lends gpf.stream.WritableArray.prototype */ {
+        $class: "gpf.stream.WritableArray",
+
+        /**
+         * Creates a writable stream that pushes all writes into an array
+         *
+         * @constructor gpf.stream.WritableArray
+         * @implements {gpf.interfaces.IWritableStream}
+         */
+        constructor: function () {
+            this._buffer = [];
+        },
+
+        //region gpf.interfaces.IReadableStream
+
+        /**
+         * @gpf:sameas gpf.interfaces.IWritableStream#write
+         */
+        write: _gpfStreamSecureWrite(function (buffer) {
+            this._buffer.push(buffer); //eslint-disable-line no-invalid-this
+            return Promise.resolve();
+        }),
+
+        //endregion
+
+        toArray: function () {
+            return this._buffer;
+        },
+
+        /**
+         * Buffer
+         */
+        _buffer: []
+
     });
 
-Object.assign(gpf, {
-
-    /**
-     * Converts the string into a stream
-     *
-     * @param {Array} that
-     * @return {Object} Implementing gpf.interfaces.IReadableStream & gpf.interfaces.IWritableStream
-     */
-    arrayToStream: function (that) {
-        return new _GpfArrayStream(that);
-    },
-
-    /**
-     * Converts the stream into an array
-     *
-     * @param {gpf.interfaces.IReadableStream} stream
-     * @param {gpf.events.Handler} eventsHandler
-     *
-     * @event gpf.events.EVENT_READY
-     * finished reading the stream, the buffer is provided
-     *
-     * @eventParam {Array} buffer
-     */
-    arrayFromStream: function (stream, eventsHandler) {
-        if (stream instanceof _GpfArrayStream) {
-            _gpfEventsFire(_GPF_EVENT_READY, {buffer: stream.toArray()}, eventsHandler);
-        } else {
-            var arrayStream = new _GpfArrayStream();
-            _gpfStreamPipe({
-                readable: stream,
-                writable: arrayStream
-            }, function (event) {
-                if (_GPF_EVENT_READY === event.type) {
-                    _gpfEventsFire(_GPF_EVENT_READY, {buffer: arrayStream.toArray()}, eventsHandler);
-                } else {
-                    _gpfEventsFire(event, {}, eventsHandler);
-                }
-            });
-        }
-    }
-
-});
+_gpfStreamSecureInstallProgressFlag(_GpfStreamReadableArray);
+_gpfStreamSecureInstallProgressFlag(_GpfStreamWritableArray);
