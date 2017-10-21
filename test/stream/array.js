57e03760f9d70df407e7a16918949b95d8800e6e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-10-21 06:56:49 -0400

    gpf.stream.ReadableArray (#196)

--- test/stream/array.js
+++ test/stream/array.js
@@ -2,26 +2,48 @@
 
 describe("stream/array", function () {
 
+    var array = ["Hell", "o ", "World", null, {}];
+
+    function compare (result, done) {
+        try {
+            assert(array.length === result.length);
+            array.forEach(function (value, index) {
+                assert(value === result[index]);
+            });
+            done();
+        } catch (e) {
+            done(e);
+        }
+    }
+
+    describe("gpf.stream.ReadableArray", function () {
+
+        it("writes each array item sequentially", function (done) {
+            var iReadableStream = new gpf.stream.ReadableArray(array),
+                result = [];
+            iReadableStream.read({
+                write: function (item) {
+                    result.push(item);
+                    return Promise.resolve();
+                }
+            })
+                .then(function () {
+                    compare(result, done);
+                }, done);
+        });
+
+    });
+
     describe("gpf.stream.WritableArray", function () {
 
-        it("supports multiple sequenced calls", function (done) {
+        it("stores each call as an array item", function (done) {
             var iWritableStream = new gpf.stream.WritableArray(),
-                source = ["Hell", "o ", "World", null, {}],
                 step = 0;
             function write () {
-                if (source.length === step) {
-                    try {
-                        var result = iWritableStream.toArray();
-                        assert(source.length === result.length);
-                        source.forEach(function (value, index) {
-                            assert(value === result[index]);
-                        });
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
+                if (array.length === step) {
+                    compare(iWritableStream.toArray(), done);
                 } else {
-                    iWritableStream.write(source[step++]).then(write, done);
+                    iWritableStream.write(array[step++]).then(write, done);
                 }
             }
             write();
