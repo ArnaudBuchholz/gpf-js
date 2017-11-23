a01e9e65fd0bcda672a01b6e23e60de8f823735f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-11-22 23:41:58 -0500

    WIP (#202)

--- test/stream/pipe.js
+++ test/stream/pipe.js
@@ -2,4 +2,13 @@
 
 describe("stream/pipe", function () {
 
+    it("chains an IReadableStream with a IWritableStream", function (done) {
+        var iWritableStream = new gpf.stream.WritableString();
+        gpf.stream.pipe(new gpf.stream.ReadableString("Hello World"), iWritableStream)
+            .then(function () {
+                assert(iWritableStream.toString() === "Hello World");
+                done();
+            });
+    });
+
 });
