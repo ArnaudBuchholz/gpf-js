18ee97a5d33b09092aca4290fbec6d40e3487829
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-11-23 00:19:21 -0500

    WIP (#202)

--- test/stream/pipe.js
+++ test/stream/pipe.js
@@ -2,6 +2,34 @@
 
 describe("stream/pipe", function () {
 
+    describe("parameters validation", function () {
+
+        it("fails if the first parameter is not an IReadableStream", function () {
+            var exceptionCaught;
+            try {
+                gpf.stream.pipe({});
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.InterfaceExpected);
+        });
+
+        it("fails if no destination", function () {
+            var exceptionCaught;
+            try {
+                gpf.stream.pipe({
+                    read: function (iWritableStream) {
+                        return Promise.resolve(iWritableStream);
+                    }
+                });
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.InterfaceExpected);
+        });
+
+    });
+
     it("chains an IReadableStream with a IWritableStream", function (done) {
         var iWritableStream = new gpf.stream.WritableString();
         gpf.stream.pipe(new gpf.stream.ReadableString("Hello World"), iWritableStream)
