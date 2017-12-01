0678b343583ba8974fa7a70d2a725882deffa73e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-11-30 19:51:52 -0500

    Disable HTTP2 warning with nodejs 8.8.0 (#202)

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,9 @@
 /*jshint node: true*/
 /*eslint-env node*/
 
+// https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V8.md#2017-10-24-version-880-current-mylesborins
+process.env.NODE_NO_HTTP2 = 1; //eslint-disable-line no-process-env
+
 module.exports = function (grunt) {
 
     require("time-grunt")(grunt);
