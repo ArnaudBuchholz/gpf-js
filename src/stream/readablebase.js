098b91874a7ada49fab44adc186c630c969acc9e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-12-05 23:29:48 -0500

    First draft (#198)

--- src/stream/readablebase.js
+++ src/stream/readablebase.js
@@ -15,13 +15,18 @@ var
         $class: "gpf.stream.ReadableBase",
 
         /**
-         * Readable stream Base
+         * Readable stream Base.
+         *
+         * Handles the read part by offering protected output methods:
+         * - {@see gpf.stream.ReadableBase.prototype._outputData}
+         * - {@see gpf.stream.ReadableBase.prototype._outputFlush}
+         * - {@see gpf.stream.ReadableBase.prototype._outputError}
          *
          * @constructor gpf.stream.ReadableBase
          * @implements {gpf.interfaces.IReadableStream}
          */
         constructor: function () {
-            this._writes = [];
+            this._outputBuffer = [];
         },
 
         /**
@@ -29,14 +34,14 @@ var
          *
          * @type {gpf.interfaces.IWritableStream}
          */
-        _output: null,
+        _outputStream: null,
 
         /**
-         * Records to write
+         * Data to write
          *
          * @type {Array}
          */
-        _writes: [],
+        _outputBuffer: [],
 
         /**
          * Read resolve function
@@ -56,8 +61,8 @@ var
 
         _writeToOutput: function () {
             var me = this;
-            if (me._writes.length) {
-                return me._output.write(me._writes.shift())
+            if (me._outputBuffer.length) {
+                return me._outputStream.write(me._outputBuffer.shift())
                     .then(function () {
                         return me._writeToOutput();
                     });
@@ -66,7 +71,7 @@ var
         },
 
         _writeIfOutput: function () {
-            if (this._output) {
+            if (this._outputStream) {
                 return this._writeToOutput();
             }
             return Promise.resolve();
@@ -78,21 +83,25 @@ var
          * @return {Promise} Resolved when written
          * @protected
          */
-        _write: function (data) {
+        _outputWrite: function (data) {
             _gpfIgnore(data);
             if (arguments.length) {
-                this._writes = this._writes.concat([].slice.call(arguments));
+                this._outputBuffer = this._outputBuffer.concat([].slice.call(arguments));
             }
             return this._writeIfOutput();
         },
 
-        _flush: function () {
+        _outputFlush: function () {
             var me = this;
             me._writeIfOutput()
                 .then(function () {
-                    me._output = null;
+                    me._outputStream = null;
                     me._readResolve();
                 });
+        },
+
+        _outputError: function (reason) {
+            this._readReject(reason);
         },
 
         //endregion
@@ -104,9 +113,9 @@ var
          */
         read: _gpfStreamSecureRead(function (output) {
             var me = this; //eslint-disable-line no-invalid-this
-            me._output = output;
-            if (me._writes.length) {
-                return me._write();
+            me._outputStream = output;
+            if (me._outputBuffer.length) {
+                me._outputData();
             }
             return new Promise(function (resolve, reject) {
                 me._readResolve = resolve;
