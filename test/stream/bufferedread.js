c36a4d078d4ffe87a1bd0ee7c94a7cafa82d159f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-12-07 19:16:07 -0500

    Organize test cases (#198)

--- test/stream/bufferedread.js
+++ test/stream/bufferedread.js
@@ -48,99 +48,107 @@ describe("stream/bufferedread", function () {
                 })["catch"](done);
         }
 
-        it("outputs data before reading", function (done) {
-            var myReadable = new TestReadable();
-            myReadable
-                .appendToReadBuffer("Hello", "World!")
-                .completeReadBuffer();
-            _checkResult(myReadable, done);
+        describe("data output", function () {
+
+            it("outputs data before reading", function (done) {
+                var myReadable = new TestReadable();
+                myReadable
+                    .appendToReadBuffer("Hello", "World!")
+                    .completeReadBuffer();
+                _checkResult(myReadable, done);
+            });
+
+            it("outputs data while reading", function (done) {
+                var myReadable = new TestReadable();
+                myReadable.appendToReadBuffer("Hello");
+                _checkResult(myReadable, done);
+                myReadable
+                    .appendToReadBuffer("World!")
+                    .completeReadBuffer();
+            });
+
+            it("outputs data after reading", function (done) {
+                var myReadable = new TestReadable();
+                _checkResult(myReadable, done);
+                myReadable
+                    .appendToReadBuffer("Hello", "World!")
+                    .completeReadBuffer();
+            });
+
         });
 
-        it("outputs data while reading", function (done) {
-            var myReadable = new TestReadable();
-            myReadable.appendToReadBuffer("Hello");
-            _checkResult(myReadable, done);
-            myReadable
-                .appendToReadBuffer("World!")
-                .completeReadBuffer();
-        });
+        describe("error management", function () {
 
-        it("outputs data after reading", function (done) {
-            var myReadable = new TestReadable();
-            _checkResult(myReadable, done);
-            myReadable
-                .appendToReadBuffer("Hello", "World!")
-                .completeReadBuffer();
-        });
+            function _ignore (data) {
+                return data;
+            }
 
-        function _ignore (data) {
-            return data;
-        }
+            function _checkError (iReadableStream, done) {
+                _pipe(iReadableStream)
+                    .then(function () {
+                        throw new Error("Should fail");
+                    }, function (reason) {
+                        assert(reason.message === "FAIL");
+                        done();
+                    })["catch"](done);
+            }
 
-        function _checkError (iReadableStream, done) {
-            _pipe(iReadableStream)
-                .then(function () {
-                    throw new Error("Should fail");
-                }, function (reason) {
-                    assert(reason.message === "FAIL");
-                    done();
-                })["catch"](done);
-        }
+            it("generates errors before reading", function (done) {
+                var myReadable = new TestReadable();
+                myReadable
+                    .appendToReadBuffer("Hello", "World!")
+                    .setReadError(new Error("FAIL"));
+                _checkError(myReadable, done);
+            });
 
-        it("generates errors before reading", function (done) {
-            var myReadable = new TestReadable();
-            myReadable
-                .appendToReadBuffer("Hello", "World!")
-                .setReadError(new Error("FAIL"));
-            _checkError(myReadable, done);
-        });
+            it("generates errors while reading", function (done) {
+                var myReadable = new TestReadable();
+                myReadable.appendToReadBuffer("Hello");
+                _checkError(myReadable, done);
+                myReadable.appendToReadBuffer("World!")
+                    .setReadError(new Error("FAIL"));
+            });
 
-        it("generates errors while reading", function (done) {
-            var myReadable = new TestReadable();
-            myReadable.appendToReadBuffer("Hello");
-            _checkError(myReadable, done);
-            myReadable.appendToReadBuffer("World!")
-                .setReadError(new Error("FAIL"));
-        });
+            it("generates errors after reading", function (done) {
+                var myReadable = new TestReadable();
+                _checkError(myReadable, done);
+                myReadable
+                    .appendToReadBuffer("Hello", "World!")
+                    .setReadError(new Error("FAIL"));
+            });
 
-        it("generates errors after reading", function (done) {
-            var myReadable = new TestReadable();
-            _checkError(myReadable, done);
-            myReadable
-                .appendToReadBuffer("Hello", "World!")
-                .setReadError(new Error("FAIL"));
-        });
+            function _checkWriteError (iWritableStream, done) {
+                var myReadable = new TestReadable();
+                myReadable
+                    .appendToReadBuffer("Hello", "World!")
+                    .completeReadBuffer();
+                gpf.stream.pipe(myReadable, iWritableStream)
+                    .then(function () {
+                        throw new Error("Should fail");
+                    }, function (reason) {
+                        assert(reason.message === "FAIL");
+                        done();
+                    })["catch"](done);
+            }
 
-        function _checkWriteError (iWritableStream, done) {
-            var myReadable = new TestReadable();
-            myReadable
-                .appendToReadBuffer("Hello", "World!")
-                .completeReadBuffer();
-            gpf.stream.pipe(myReadable, iWritableStream)
-                .then(function () {
-                    throw new Error("Should fail");
-                }, function (reason) {
-                    assert(reason.message === "FAIL");
-                    done();
-                })["catch"](done);
-        }
+            it("forwards errors (exception)", function (done) {
+                _checkWriteError({
+                    write: function (data) {
+                        _ignore(data);
+                        throw new Error("FAIL");
+                    }
+                }, done);
+            });
 
-        it("forwards errors (exception)", function (done) {
-            _checkWriteError({
-                write: function (data) {
-                    _ignore(data);
-                    throw new Error("FAIL");
-                }
-            }, done);
-        });
+            it("forwards errors (reject)", function (done) {
+                _checkWriteError({
+                    write: function (data) {
+                        _ignore(data);
+                        return Promise.reject(new Error("FAIL"));
+                    }
+                }, done);
+            });
 
-        it("forwards errors (reject)", function (done) {
-            _checkWriteError({
-                write: function (data) {
-                    _ignore(data);
-                    return Promise.reject(new Error("FAIL"));
-                }
-            }, done);
         });
 
     });
