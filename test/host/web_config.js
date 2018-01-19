d5de3e8a2045d4b35ccfc3f282c913a033ef8db0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-01-19 16:46:00 -0500

    Reverts 9d228cd (#218)

--- test/host/web_config.js
+++ test/host/web_config.js
@@ -54,7 +54,6 @@ module.exports = function (browserType, urlParameters) {
             browserName = arg;
         }
     });
-    verbose(result); // FORCE DEBUG MODE
 
     result.browserName = browserName;
     result.browser = configFile.content.browsers[browserName];
