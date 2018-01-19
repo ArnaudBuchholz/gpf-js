f9e07a9ac27fd318f9b1b2751f1025385f45d0dc
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-01-18 19:57:58 -0500

    Travis configuration (#218)

---
+++ grunt/config/travis.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = function (grunt) {
+    return function () {
+        const done = this.async(); //eslint-disable-line no-invalid-this
+        grunt.util.spawn({
+            cmd: "node",
+            args: ["make/config", "-quiet"],
+            opts: {
+                stdio: "inherit"
+            }
+        }, error => error
+            ? done()
+            : grunt.util.spawn({
+                grunt: true,
+                args: [
+                    "exec:globals",
+                    "concurrent:linters",
+                    "concurrent:quality",
+                    "exec:metrics:safeCoverage"
+                ],
+                opts: {
+                    stdio: "inherit"
+                }
+            }, done)
+        );
+    };
+};
