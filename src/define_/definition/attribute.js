dcb11abd21d656dcebe3879ee1d5aec02c48c84d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-01-29 13:30:25 -0500

    Removes @lends that is automated (#212)

--- src/define_/definition/attribute.js
+++ src/define_/definition/attribute.js
@@ -21,7 +21,7 @@ function _gpfToAttributeArray (attributes) {
 
 //region Extension of _GpfClassDefinition
 
-Object.assign(_GpfClassDefinition.prototype, /** @lends _GpfClassDefinition.prototype */ {
+Object.assign(_GpfClassDefinition.prototype, {
 
     /**
      * Class attributes
@@ -105,7 +105,7 @@ Object.assign(_GpfClassDefinition.prototype, /** @lends _GpfClassDefinition.prot
 
 //region Extension of _GpfClassDefMember
 
-Object.assign(_GpfClassDefMember.prototype, /** @lends _GpfClassDefMember.prototype */ {
+Object.assign(_GpfClassDefMember.prototype, {
 
     /**
      * Member attributes
