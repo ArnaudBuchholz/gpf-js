e8769ed77d28b8fd5edd228f4136c362cf199f89
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-01-29 23:20:39 -0500

    New test case (#219)

---
+++ test/require/indep_commonjs.js
@@ -0,0 +1,13 @@
+/*
+ * The scope is 'private'.
+ * No imports
+ * Exports are made through the assignment to module.exports.
+ */
+"use strict";
+
+module.exports = {
+    type: "indep_commonjs",
+    data: {
+        "member": "value"
+    }
+};
