62d118c0aaf97e534e695a8df06f16ff52dfc868
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-02-05 14:16:24 -0500

    Improves detection (#220)

--- make/config.js
+++ make/config.js
@@ -7,15 +7,25 @@ const
     fs = require("fs"),
     path = require("path"),
     inquirer = require("inquirer"),
+    colors = require("colors"),
     ConfigFile = require("./configFile.js"),
     config = new ConfigFile(),
     isWindows = (/^win/).test(process.platform),
 
     spawnProcess = (command, params) => new Promise(function (resolve, reject) {
         let process = require("child_process").spawn(command, params),
-            output = [];
-        process.stdout.on("data", buffer => output.push(buffer.toString()));
-        process.stderr.on("data", buffer => output.push(buffer.toString()));
+            output = [],
+            log = buffer => console.log(buffer.grey),
+            err = buffer => console.error(buffer.red),
+            out = method => {
+                return rawBuffer => {
+                    const buffer = rawBuffer.toString();
+                    buffer.split("\n").forEach(method);
+                    output.push(buffer);
+                }
+            };
+        process.stdout.on("data", out(log));
+        process.stderr.on("data", out(err));
         process.on("error", reject);
         process.on("close", () => resolve(output.join("")));
     }),
@@ -31,10 +41,26 @@ const
             choices: ["Autodetect", "Yes", "No"]
         }]),
 
-    detectWScript = () => spawnProcess("cscript.exe", ["/Nologo", "/E:JScript", "build/gpf.js"])
-            .then(output => output.indexOf("Can't find script engine") === -1, () => false),
+    detectWScript = () => spawnProcess("cscript.exe", ["/E:JScript", "build/gpf.js"])
+        .then(output => {
+            if (output.indexOf("Can't find script engine") === -1) {
+                console.log("\tWScript detected".cyan);
+            } else {
+                console.log("\tWScript detected *but* JScript engine missing".magenta);
+            }
+        }, () => {
+            console.log("\tWScript *not* detected".magenta);
+            return false;
+        }),
 
-    detectJava = () => spawnProcess("java", ["-version"]).then(() => true, () => false),
+    detectJava = () => spawnProcess("java", ["-version"])
+        .then(() => {
+            console.log("\tJAVA detected".cyan);
+            return true;
+        }, () => {
+            console.log("\tJAVA *not* detected".magenta);
+            return false;
+        }),
 
     askForQualityMetrics = () => inquirer.prompt([{
         type: "confirm",
@@ -83,7 +109,7 @@ const
         let confirmation;
         if (quietMode || 0 === Object.keys(config.content.browsers).length) {
             confirmation = Promise.resolve({confirmed: true});
-            console.log("Detecting browsers compatible with selenium");
+            console.log("Detecting browsers compatible with selenium".gray);
         } else {
             confirmation = inquirer.prompt([{
                 type: "confirm",
@@ -99,7 +125,7 @@ const
 
     checkCmdLineSafari = () => {
         if (!config.content.browsers.safari && isWindows) {
-            console.log("Checking safari for windows...");
+            console.log("Checking safari for windows...".gray);
             let safariBin = path.join(process.env.ProgramFiles, "safari\\safari.exe");
             if (!fs.existsSync(safariBin)) {
                 safariBin = path.join(process.env["ProgramFiles(x86)"], "safari\\safari.exe");
@@ -108,7 +134,7 @@ const
                 safariBin = undefined;
             }
             if (safariBin) {
-                console.log(`Safari: ${safariBin}`);
+                console.log(`\tSafari: ${safariBin}`.cyan);
                 config.content.browsers.safari = {
                     type: "spawn",
                     bin: safariBin
@@ -127,10 +153,10 @@ const
         if (config.content.browsers.chrome || isWindows) {
             promise = Promise.resolve();
         } else {
-            console.log("Checking chrome for non windows...");
+            console.log("Checking chrome for non windows...".gray);
             promise = spawnProcess("google-chrome-stable", ["--product-version"])
                 .then(function () {
-                    console.log("Chrome: google-chrome-stable");
+                    console.log("\tChrome: google-chrome-stable".cyan);
                     config.content.browsers.chrome = {
                         type: "spawn",
                         bin: "google-chrome-stable",
@@ -146,7 +172,7 @@ const
         let confirmation;
         if (quietMode || 0 === Object.keys(config.content.browsers).length) {
             confirmation = Promise.resolve({confirmed: true});
-            console.log("Detecting browsers compatible with command line use");
+            console.log("Detecting browsers compatible with command line use".gray);
         } else {
             confirmation = inquirer.prompt([{
                 type: "confirm",
@@ -164,7 +190,7 @@ const
 Promise.resolve()
 
     .then(()=> config.isNew()
-        ? (console.log("No configuration file found"), config)
+        ? console.log("No configuration file found")
         : inquirer.prompt([{
             type: "confirm",
             name: "confirmed",
@@ -174,7 +200,6 @@ Promise.resolve()
                 if (!answers.confirmed) {
                     throw new Error("aborted");
                 }
-                return config;
             })
     )
     .then(() => quietMode
@@ -198,10 +223,17 @@ Promise.resolve()
     .then(answers => "Autodetect" === answers.choice ? detectJava() : "Yes" === answers.choice)
     .then(javaInstalled => {
         config.content.host.java = javaInstalled;
-        if (javaInstalled && process.env.JAVA_HOME) {
-            const jjsPath = path.join(process.env.JAVA_HOME, `bin${path.sep}jjs` + (isWindows ? ".exe" : ""));
-            if (fs.existsSync(jjsPath)) {
-                config.content.host.nashorn = jjsPath;
+        if (javaInstalled) {
+            if (process.env.JAVA_HOME) {
+                const jjsPath = path.join(process.env.JAVA_HOME, `bin${path.sep}jjs` + (isWindows ? ".exe" : ""));
+                if (fs.existsSync(jjsPath)) {
+                    console.log(`\tNashorn: ${jjsPath}`.cyan);
+                    config.content.host.nashorn = jjsPath;
+                } else {
+                    console.log("\tNashorn *not* detected".magenta);
+                }
+            } else {
+                console.log("\tMissing JAVA_HOME, Nashorn can't be configured".magenta);
             }
         }
         if (!quietMode) {
