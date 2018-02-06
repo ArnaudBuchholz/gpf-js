0296372b258c737e4165194b0d4ea6e42b6eddf1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-02-06 17:12:16 -0500

    gpf.rhino is replaced with gpf.java (#220)

--- src/host/rhino.js
+++ src/host/rhino.js
@@ -6,29 +6,15 @@
 "use strict";
 /*global _GPF_HOST*/ // Host types
 /*global _gpfBootImplByHost*/ // Boot host specific implementation per host
-/*global _gpfConsoleGenerate*/ // Generate an object that can be used to simulate console methods
-/*global _gpfExit:true*/ // Exit function
-/*global _gpfMainContext*/ // Main context object
+/*global _gpfJavaHostImpl*/ // Common implementation for Java hosts
 /*#endif*/
-
-/*jshint rhino: true*/
-/*eslint-env rhino*/
 
 /**
  * @namespace gpf.rhino
  * @description Root namespace for Rhino specifics
  * @since 0.2.1
+ * @deprecated since version 0.2.4, use gpf.java instead
  */
-gpf.rhino = {};
+gpf.rhino = gpf.java;
 
-_gpfBootImplByHost[_GPF_HOST.RHINO] = function () {
-
-    // Define console APIs
-    _gpfMainContext.console = _gpfConsoleGenerate(print);
-
-    /* istanbul ignore next */ // exit.1
-    _gpfExit = function (code) {
-        java.lang.System.exit(code);
-    };
-
-};
+_gpfBootImplByHost[_GPF_HOST.RHINO] = _gpfJavaHostImpl;
