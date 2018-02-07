f6aa119ae98e0f735ae6fc6d440b0564d6280bff
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-02-06 19:59:57 -0500

    Support Nashorn (#220)

--- make/UMD.js
+++ make/UMD.js
@@ -14,6 +14,7 @@
      *
      * 2014-12-04 ABZ Extended for PhantomJS
      * 2015-05-29 ABZ Modified to catch former value of gpf
+     * 2018-02-06 ABZ Modified to get global context if missing
      */
     if (typeof define === "function" && define.amd) {
         define(["exports"], factory);
