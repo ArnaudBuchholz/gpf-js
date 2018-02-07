1c85410be1dcee03b8c749a48d8fa33999b9877f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-02-06 19:33:33 -0500

    Improves maintainability (#220)

--- src/fs/nashorn.js
+++ src/fs/nashorn.js
@@ -1,12 +1,13 @@
 /**
  * @file Nashorn File system implementation
+ * @since 0.2.4
  */
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfFsReadImplByHost*/ // gpf.fs.read per host
 /*global _GpfStreamJavaReadable*/ // gpf.java.ReadableStream
 /*global _GpfStreamWritableString*/ // gpf.stream.WritableString
+/*global _gpfFsReadImplByHost*/ // gpf.fs.read per host
 /*#endif*/
 
 /*jshint rhino: true*/
@@ -15,17 +16,15 @@
 _gpfFsReadImplByHost[_GPF_HOST.NASHORN] = function (path) {
     var javaPath = java.nio.file.Paths.get(path);
     if (java.nio.file.Files.exists(javaPath)) {
-        try {
+        return new Promise(function (resolve, reject) {
             var javaInputStream = java.nio.file.Files.newInputStream(javaPath),
                 iStreamReader = new _GpfStreamJavaReadable(javaInputStream),
                 iWritableString = new _GpfStreamWritableString();
             return iStreamReader.read(iWritableString)
                 .then(function () {
-                    return iWritableString.toString();
-                });
-        } catch (e) {
-            return Promise.reject(e);
-        }
+                    resolve(iWritableString.toString());
+                })["catch"](reject);
+        });
     }
     return Promise.reject(new Error("File not found")); // To be improved
 };
