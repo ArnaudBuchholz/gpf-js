69ac13443f881a9c89929e91c9cb667e594b80eb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-02-09 16:08:22 -0500

    Preparing WScript virtual environment (#226)

--- test/host/wscript/setup.js
+++ test/host/wscript/setup.js
@@ -2,3 +2,9 @@
 
 global.WScript = require("./WScript");
 global.ActiveXObject = require("./ActiveXOBject");
+
+const _console = console;
+Object.defineProperty(global, "console", {
+    get: () => _console,
+    set: value => 0 // ignore
+});
