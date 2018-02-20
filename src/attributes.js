3b4b0698a2da6d254060d39bd1f945c30ce65d5c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-02-19 23:38:06 -0500

    WIP (#230)

--- src/attributes.js
+++ src/attributes.js
@@ -4,7 +4,16 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfAttribute*/ // Shortcut for gpf.attributes.Attribute
+/*global _gpfDefineGetEntityFromBuilder*/ // Retrieves entity definition from instance instance builder
 /*#endif*/
+
+function _gpfAttributesGetFromClass (classConstructor, baseAttributeClass) {
+    var entityDefinition = _gpfDefineGetEntityFromBuilder(classConstructor);
+    if (!entityDefinition) {
+        return {};
+    }
+    return entityDefinition._attributes;
+}
 
 /**
  * Get attributes defined for the object / $class
@@ -14,7 +23,13 @@
  * @return {Object} Dictionary of attributes grouped per members
  */
 function _gpfAttributesGet (objectOrClass, baseAttributeClass) {
-    
+    if ("function" !== typeof objectOrClass) {
+        objectOrClass = objectOrClass.constructor;
+    }
+    if (undefined === baseAttributeClass) {
+        baseAttributeClass = _gpfAttribute;
+    }
+    return _gpfAttributesGetFromClass(objectOrClass, baseAttributeClass);
 }
 
 /** @gpf:sameas _gpfAttributesGet */
