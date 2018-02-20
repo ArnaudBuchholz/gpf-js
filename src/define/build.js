26aab12396276131f905c89f64461616d7b5a051
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-02-19 23:37:57 -0500

    Basic implementation of entity definition retrieve (#230)

--- src/define/build.js
+++ src/define/build.js
@@ -7,7 +7,31 @@
 /*global _GpfEntityDefinition*/ // Entity definition
 /*global _gpfContext*/ // Resolve contextual string
 /*global _gpfEmptyFunc*/ // An empty function
+/*global _gpfArrayForEachFalsy*/ // _gpfArrayForEach that returns first truthy value computed by the callback
+/*exported _gpfDefineGetEntityFromBuilder*/ // Retrieves entity definition from instance instance builder
 /*#endif*/
+
+/**
+ * Array of defined entities.
+ * @type {_GpfEntityDefinition[]}
+ */
+var _gpfDefinedEntities = [];
+
+/**
+ * Retrieves entity definition from instance instance builder.
+ * NOTE: This is an internal solution that has the advantage of not exposing the entity definitions.
+ *       For performance reasons, this may change in the future.
+ *
+ * @param {Function} instanceBuilder Instance builder
+ * @return {_GpfEntityDefinition|undefined} Entity definition (if found)
+ */
+function _gpfDefineGetEntityFromBuilder (instanceBuilder) {
+    return _gpfArrayForEachFalsy(_gpfDefinedEntities, function (entityDefinition) {
+        if (entityDefinition.getInstanceBuilder() === instanceBuilder) {
+            return entityDefinition;
+        }
+    });
+}
 
 Object.assign(_GpfEntityDefinition.prototype, {
 
@@ -40,6 +64,7 @@ Object.assign(_GpfEntityDefinition.prototype, {
             _gpfContext(this._namespace.split("."), true)[this._name] = value;
         }
         this._instanceBuilder = value;
+        _gpfDefinedEntities.push(this);
     },
 
     /**
