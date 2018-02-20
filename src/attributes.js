2cd6ce9d588ceddf7110b1a7386c9a47657205d2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-02-20 14:52:54 -0500

    Link class definition to parent definition when extend is used (#230)

--- src/attributes.js
+++ src/attributes.js
@@ -1,18 +1,18 @@
 /**
  * @file gpf.attributes.get
+ * @since 0.2.4
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfAttribute*/ // Shortcut for gpf.attributes.Attribute
 /*global _gpfDefineGetEntityFromBuilder*/ // Retrieves entity definition from instance instance builder
 /*#endif*/
 
 function _gpfAttributesGetFromClass (classConstructor, baseAttributeClass) {
     var entityDefinition = _gpfDefineGetEntityFromBuilder(classConstructor);
-    if (!entityDefinition) {
-        return {};
+    if (entityDefinition) {
+        return entityDefinition.getAttributes(baseAttributeClass);
     }
-    return entityDefinition._attributes;
+    return {};
 }
 
 /**
@@ -21,16 +21,17 @@ function _gpfAttributesGetFromClass (classConstructor, baseAttributeClass) {
  * @param {Object|Function} objectOrClass Object instance or class constructor
  * @param {gpf.attributes.Attribute} [baseAttributeClass] Base attribute class used to filter results
  * @return {Object} Dictionary of attributes grouped per members
+ * @since 0.2.4
  */
 function _gpfAttributesGet (objectOrClass, baseAttributeClass) {
     if ("function" !== typeof objectOrClass) {
         objectOrClass = objectOrClass.constructor;
     }
-    if (undefined === baseAttributeClass) {
-        baseAttributeClass = _gpfAttribute;
-    }
     return _gpfAttributesGetFromClass(objectOrClass, baseAttributeClass);
 }
 
-/** @gpf:sameas _gpfAttributesGet */
+/**
+ * @gpf:sameas _gpfAttributesGet
+ * @since 0.2.4
+ */
 gpf.attributes.get = _gpfAttributesGet;
