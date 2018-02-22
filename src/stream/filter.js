1693164974fa1885881ce17f86fec7bd7f66695b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-02-22 14:50:53 -0500

    Filter stream (#221)

--- src/stream/filter.js
+++ src/stream/filter.js
@@ -4,6 +4,7 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfEmptyFunc*/ // An empty function
 /*global _gpfStreamSecureInstallProgressFlag*/ // Install the progress flag used by _gpfStreamSecureRead and Write
 /*global _gpfStreamSecureRead*/ // Generate a wrapper to secure multiple calls to stream#read
 /*global _gpfStreamSecureWrite*/
@@ -26,6 +27,81 @@ var
             this._filter = filter;
         },
 
+        //region internal handling
+
+        /**
+         * Promise used to wait for data
+         * @type {Promise}
+         */
+        _dataInPromise: undefined,
+
+        /**
+         * Resolve function of _dataInPromise
+         * @type {Function}
+         */
+        _dataInResolve: _gpfEmptyFunc,
+
+        /**
+         * Resolve function of _writeData's Promise
+         * @type {Function}
+         */
+        _dataOutResolve: _gpfEmptyFunc,
+
+        /**
+         * Reject function of _writeData's Promise
+         * @type {Function}
+         */
+        _dataOutReject: _gpfEmptyFunc,
+
+        /**
+         * Wait until data was written to this stream
+         *
+         * @return {Promise} Resolved when a data as been written to this stream
+         */
+        _waitForData: function () {
+            var me = this;
+            if (!me._dataInPromise) {
+                me._dataInPromise = new Promise(function (resolve) {
+                    me._dataInResolve = resolve;
+                }).then(function (value) {
+                    delete me._dataInPromise;
+                    delete me._dataInResolve;
+                    return value;
+                }, function (reason) {
+                    delete me._dataInPromise;
+                    delete me._dataInResolve;
+                    return Promise.reject(reason);
+                });
+            }
+            return me._dataInPromise;
+        },
+
+        /**
+         * Waits for the read API to write it out
+         *
+         * @param {*} data Data to write
+         * @return {Promise} Resolved when write operation has been done on output
+         */
+        _writeData: function (data) {
+            var me = this;
+            me._waitForData();
+            me._dataInResolve(data);
+            return new Promise(function (resolve, reject) {
+                me._dataOutResolve = resolve;
+                me._dataOutReject = reject;
+            }).then(function (value) {
+                delete me._dataOutResolve;
+                delete me._dataOutReject;
+                return value;
+            }, function (reason) {
+                delete me._dataOutResolve;
+                delete me._dataOutReject;
+                return Promise.reject(reason);
+            });
+        },
+
+        //endregion
+
         //region gpf.interfaces.IReadableStream
 
         /**
@@ -33,19 +109,16 @@ var
          */
         read: _gpfStreamSecureRead(function (output) {
             var me = this; //eslint-disable-line no-invalid-this
-            me._readWriteToStream = output;
-            me._readCheckIfData();
-            return new Promise(function (resolve, reject) {
-                me._readResolve = resolve;
-                me._readReject = reject;
-            });
+            return me._waitForData()
+                .then(function (data) {
+                    if (undefined !== data) {
+                        return output.write(data).then(me._dataOutResolve, me._dataOutReject);
+                    }
+                    return Promise.resolve(); // Nothing to write
+                });
         }),
 
         //endregion
-
-        _waitForRead: function () {
-
-        },
 
         //region gpf.interfaces.IWritableStream
 
@@ -55,8 +128,7 @@ var
         write: _gpfStreamSecureWrite(function (data) {
             var me = this; //eslint-disable-line no-invalid-this
             if (me._filter(data)) {
-                me._data = data;
-                return me._waitForRead();
+                return me._writeData(data);
             }
             return Promise.resolve();
         })
