7b85dca0f8af8008405b1cb6c7a82deacd9b8415
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-07 13:38:03 -0500

    Ensure loading of debug & release versions in the simulator (#228)

--- test/host/node_cscript.js
+++ test/host/node_cscript.js
@@ -1,4 +1,12 @@
 "use strict";
 
+// trick UMD loader to load debug & release versions through a fake AMD define
+function define (any, factory) {
+    global.gpf = {};
+    factory(global.gpf);
+}
+define.amd = true;
+global.define = define;
+
 require("./wscript/setup.js");
 require("./cscript.js");
