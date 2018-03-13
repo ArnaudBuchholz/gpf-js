b7f3c887b8033f6f7d2e91edb036a631c4d45c62
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-13 16:26:36 -0400

    Plato tile (#183)

---
+++ res/tiles/plato.js
@@ -0,0 +1,48 @@
+gpf.require.define({
+    Tile: "tile.js",
+    dom: "../dom.js",
+    config: "../../tmp/config.json"
+
+}, function (require) {
+    "use strict";
+
+    var dom = require.dom,
+        metrics = require.config.metrics;
+
+    return gpf.define({
+        $class: "Plato",
+        $extend: require.Tile,
+
+        constructor: function () {
+            this.$super("plato", "Plato report");
+            this._setGruntCommand("plato", "Execute plato");
+        },
+
+        getDynamicContent: function () {
+            return gpf.http.get("/tmp/plato/report.json")
+                .then(function (response) {
+                    if (404 === response.status) {
+                        return [];
+                    }
+                    return JSON.parse(response.responseText);
+                })
+                .then(function (reportData) {
+                    var maintainability = reportData.summary.average.maintainability,
+                        statusClassName;
+                    if (maintainability < metrics.maintainability) {
+                        statusClassName = "ko";
+                    } else {
+                        statusClassName = "ok";
+                    }
+                    return [
+                        dom.div({
+                            className: "percentage " + statusClassName,
+                            link: "tmp/plato/index.html",
+                        }, maintainability)
+                    ];
+                });
+        }
+
+    });
+
+});
