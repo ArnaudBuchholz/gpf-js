29ec6ede53d3169902132304c03b1ada2f2b1603
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-03-12 23:44:35 -0400

    Loading of tiles (#183)

--- res/tiles/main.js
+++ res/tiles/main.js
@@ -1,7 +1,32 @@
 gpf.require.define({
+    dom: "../dom.js",
     Sources: "sources.js"
 
 }, function (require) {
     "use strict";
+
+    var dom = require.dom,
+        tiles = [
+            require.Sources
+        ].map(function (TileClass) {
+            return new TileClass();
+        });
+
+    function refresh(tile) {
+        return tile.getDynamicContent()
+            .then(function (content) {
+                var dynamicRoot = document.getElementById(tile.getId()).querySelector(".dynamic");
+                dom.clear(dynamicRoot);
+                content.forEach(function (tag) {
+                    tag.appendTo(dynamicRoot);
+                });
+            });
+    }
+
+    var tilesRoot = document.getElementById("tiles");
+    tiles.forEach(function (tile) {
+      tile.render().appendTo(tilesRoot);
+      refresh(tile);
+    });
 
 });
