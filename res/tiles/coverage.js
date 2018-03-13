a57370a3c0831322cc2aa3f9fbe3fd2ba7857531
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-13 16:26:30 -0400

    Coverage tile (#183)

---
+++ res/tiles/coverage.js
@@ -0,0 +1,61 @@
+gpf.require.define({
+    Tile: "tile.js",
+    dom: "../dom.js",
+    config: "../../tmp/config.json",
+    CoverageReport: "../../make/coverage.js"
+
+}, function (require) {
+    "use strict";
+
+    var dom = require.dom,
+        metrics = require.config.metrics;
+
+    return gpf.define({
+        $class: "Coverage",
+        $extend: require.Tile,
+
+        constructor: function () {
+            this.$super("coverage", "Code coverage");
+        },
+
+        getDynamicContent: function () {
+            return gpf.http.get("/tmp/coverage/reports/coverage.json")
+                .then(function (response) {
+                    if (404 === response.status) {
+                        return [];
+                    }
+                    return JSON.parse(response.responseText);
+                })
+                .then(function (coverageData) {
+                    var report = new require.CoverageReport(coverageData),
+                        global = report.getGlobal();
+
+                    function coverage (type) {
+                        var ratio = global[type].getCoverageRatio(),
+                            statusClassName;
+                        if (ratio < metrics.coverage[type]) {
+                            statusClassName = "ko";
+                        } else {
+                            statusClassName = "ok";
+                        }
+                        if (100 === ratio) {
+                            ratio = "100.0"
+                        }
+                        return dom.div({
+                            className: "coverage " + type + " " + statusClassName,
+                            link: "tmp/coverage/reports/lcov-report/index.html"
+                        }, ratio + "%");
+                    }
+
+                    return [
+                        coverage("statements"),
+                        coverage("functions"),
+                        coverage("branches")
+                    ];
+
+                });
+        }
+
+    });
+
+});
