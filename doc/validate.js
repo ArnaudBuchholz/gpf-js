15102c212ae22e9b3c03fecbb09b1e2d42e5008c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-20 13:32:08 -0400

    Validate links inside documentation (#236)

--- doc/validate.js
+++ doc/validate.js
@@ -1,25 +1,65 @@
 "use strict";
 
-const
-    gpf = require("../build/gpf.js"),
-    processed = {},
-    stack = [process.argv[2]],
+require("colors");
 
-    check = (url) => {
+let
+    errors = 0;
+
+/* Need source version because of https support */
+global.gpfSourcesPath = "src/";
+require("../src/boot.js");
+
+const
+    CACHE_FILE = "tmp/doc.cache",
+    fs = require("fs"),
+    gpfFs = gpf.fs.getFileStorage(),
+    processed = {},
+    cache = [],
+    stack = [{
+        method: "GET",
+        url: process.argv[2]
+    }],
+    verbose = process.argv.some(arg => arg === "-verbose"),
+    log = verbose ? console.log.bind(console) : () => {},
+
+    enqueue = ({url, method}) => {
+        if (!processed.hasOwnProperty(url)) {
+            stack.push({url, method});
+        }
+    },
+
+    check = ({url, method}) => {
         if (processed.hasOwnProperty(url)) {
             return Promise.resolve();
         }
-        return gpf.http.get(url).then(response => {
-            console.log(url, response.status);
-            if (response.status !== 200) {
-                processed[url] = false;
+        return gpf.http[method.toLowerCase()](url).then(response => {
+            processed[url] = response;
+            if (-1 === [200, 301, 302].indexOf(response.status)) {
+                console.error(method.magenta, url.magenta, response.status.toString().red);
                 return;
+            } else {
+                if (method === "HEAD") {
+                    cache.push(url);
+                }
+                log(method.grey, url.grey, response.status.toString().green);
             }
-            processed[url] = true;
             const baseUrl = !gpf.path.extension(url) ? url : gpf.path.parent(url);
             response.responseText.replace(/href="([^"]+)"/g, function (text, subUrl) {
-                if (subUrl.indexOf("http") !== 0 && gpf.path.extension(subUrl) === ".html") {
-                    stack.push(gpf.path.join(baseUrl, subUrl));
+                subUrl = subUrl.replace(/\r|\n/g, "");
+                if (subUrl.indexOf("http") !== 0) {
+                    // Relative
+                    if (gpf.path.extension(subUrl) === ".html") {
+                        enqueue({
+                            method: "GET",
+                            url: gpf.path.join(baseUrl, subUrl)
+                        });
+                    }
+                } else {
+                    // Absolute
+                    enqueue({
+                        method: "HEAD",
+                        url: subUrl
+                    });
                 }
             });
         })
@@ -29,7 +69,23 @@ const
         ? check(stack.shift()).then(next)
         : 0;
 
-console.log(`Checking ${stack[0]}`);
-next().then(() => {
-    console.log(`Done...`);
-});
+log("Loading cache...");
+gpf.fs.read(CACHE_FILE)
+    .then(cached => cached.split("\n").forEach(url => {processed[url] = true}), () => {})
+    .then(() => {
+        log(`Checking ${stack[0].url}...`);
+        return next();
+    })
+    .then(() => {}, () => {}) // Absorb errors
+    .then(() => {
+        log("Done.");
+        if (cache.length) {
+            log("Saving cache...");
+            return gpfFs.openTextStream(CACHE_FILE, gpf.fs.openFor.appending)
+                .then(iWritableStream => iWritableStream.write(cache.join("\n"))
+                    .then(() => gpfFs.close(iWritableStream)));
+        }
+    })
+    .then(() => {
+        process.exit(errors);
+    });
