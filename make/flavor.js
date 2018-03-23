c2250af7aecba96a99176c6bb4a21d3fca90438d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-23 15:23:29 -0400

    Flavor processing (WIP) (#248)

---
+++ make/flavor.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = function (sources, dependencies, modifiers) {
+    var initial = sources.filter(function (source) {
+        var tags = (source.tags || "").split(" ");
+        if (tags.indexOf("core") !== -1) {
+            return true;
+        }
+    });
+
+    return initial;
+};
