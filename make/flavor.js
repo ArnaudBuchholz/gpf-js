ec1893a5204a694c841e0c0a20ce6a68d2900aac
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-23 15:27:19 -0400

    Linter fix (#248)

--- make/flavor.js
+++ make/flavor.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = function (sources, dependencies, modifiers) {
+module.exports = function (sources/*, dependencies, modifiers*/) {
     var initial = sources.filter(function (source) {
         var tags = (source.tags || "").split(" ");
         if (tags.indexOf("core") !== -1) {
