4b982a39f6055475f1816fc8b7937b6e011b1332
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-03-24 09:55:29 -0400

    Initial filtering (#248)

--- make/flavor.js
+++ make/flavor.js
@@ -1,12 +1,51 @@
 "use strict";
 
-module.exports = function (sources/*, dependencies, modifiers*/) {
-    var initial = sources.filter(function (source) {
-        var tags = (source.tags || "").split(" ");
-        if (tags.indexOf("core") !== -1) {
-            return true;
-        }
-    });
+function categorize (tags) {
+    var array = tags.split(" ");
+    return {
+        hosts: array.filter(function (tag) {
+            return 0 === tag.indexOf("host:");
+        }).map(function (hostTag) {
+            return hostTag.substr(5);
+        }),
+        features: array.filter(function (tag) {
+            return tag && -1 === tag.indexOf(":");
+        })
+    };
+}
 
-    return initial;
+function intersect (array1, array2) {
+    return array1.some(function (value1) {
+        return array2.indexOf(value1) !== -1;
+    });
+}
+
+function checkCore (tags) {
+    return -1 !== tags.features.indexOf("core");
+}
+
+function checkFeature (tags, requested) {
+    if (tags.features.length && requested.features.length) {
+        return intersect(tags.features, requested.features);
+    }
+}
+
+function checkHost (tags, requested) {
+    if (tags.hosts.length && requested.hosts.length) {
+        return intersect(tags.hosts, requested.hosts);
+    }
+}
+
+module.exports = function (sources, dependencies, request) {
+    var requested =  categorize(request),
+        // Initial is based on tags
+        allowed = sources.map(function (source) {
+            var tags =  categorize(source.tags || "");
+            return checkCore(tags) // core sources are always kept
+                || checkFeature(tags, requested)
+                || checkHost(tags, requested)
+                || false;
+        });
+
+    return allowed;
 };
