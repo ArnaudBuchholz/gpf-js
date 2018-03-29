325067436255d7e6a14be8e7778829a03c9e26df
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-03-29 13:41:47 -0400

    WIP (#248)

--- make/flavor.js
+++ make/flavor.js
@@ -20,20 +20,35 @@ function intersect (array1, array2) {
     });
 }
 
-function checkCore (tags) {
+function includeCore (tags) {
     return -1 !== tags.features.indexOf("core");
 }
 
-function checkFeature (tags, requested) {
+function includeFeature (tags, requested) {
     if (tags.features.length && requested.features.length) {
         return intersect(tags.features, requested.features);
     }
 }
 
-function checkHost (tags, requested) {
+function includeHost (tags, requested) {
     if (tags.hosts.length && requested.hosts.length) {
         return intersect(tags.hosts, requested.hosts);
     }
+}
+
+function includeRequestedSources (sources, requested) {
+    return sources.map(function (source) {
+        var tags =  categorize(source.tags || ""),
+            shouldIncludeFeature;
+        if (includeCore(tags)) {
+            return true;
+        }
+        shouldIncludeFeature = includeFeature(tags, requested);
+        if (undefined !== shouldIncludeFeature) {
+            return shouldIncludeFeature;
+        }
+        return includeHost(tags, requested);
+    });
 }
 
 function getSourceIndex (sources, name) {
@@ -48,23 +63,23 @@ function getSourceIndex (sources, name) {
     return result;
 }
 
-module.exports = function (sources, dependencies, request) {
+/*
+function merge () {
+    var result = [];
+    [].slice.call(arguments, 0).forEach(function (allowed) {
+        allowed.forEach(function (value, index) {
+            result[index] = result[index] || value;
+        });
+    });
+    return result;
+}
+*/
+
+function getFlavor (sources, dependencies, request) {
     var requested =  categorize(request),
         // Initial is based on tags
-        allowed = sources.map(function (source) {
-            var tags =  categorize(source.tags || ""),
-                feature;
-            if (checkCore(tags)) {
-                return true;
-            }
-            feature = checkFeature(tags, requested);
-            if (undefined !== feature) {
-                return feature;
-            }
-            return checkHost(tags, requested) || false;
-        }),
-        index = sources.length,
-        source;
+        allowed = includeRequestedSources(sources, requested),
+        index = sources.length;
     function allow (dependency) {
         allowed[getSourceIndex(sources, dependency)] = true;
     }
@@ -72,8 +87,9 @@ module.exports = function (sources, dependencies, request) {
         if (!allowed[index]) {
             continue;
         }
-        source = sources[index];
-        (dependencies[source.name] || []).forEach(allow);
+        (dependencies[sources[index].name] || []).forEach(allow);
     }
     return allowed;
-};
+}
+
+module.exports = getFlavor;
