a39c85c6df1bb610c112ca750884254bba6d109b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-02 00:34:16 -0400

    Cleaning of publication folder (#237)

--- grunt/clean.js
+++ grunt/clean.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     plato: ["tmp/plato"],
-    tmp: ["tmp/*", "!tmp/.eslintrc", "!tmp/.gitignore", "!tmp/.jshintrc", "!tmp/README.md", "!tmp/import_plato.js"]
+    tmp: ["tmp/*", "!tmp/.eslintrc", "!tmp/.gitignore", "!tmp/.jshintrc", "!tmp/README.md", "!tmp/import_plato.js"],
+    publish: ["tmp/publish/*", "tmp/publish/.*"]
 };
