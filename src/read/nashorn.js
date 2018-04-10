f87f01830c65735652373d6c08e2012eebdedd73
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-10 17:44:23 -0400

    Nahsorn specific read impl (#251)

---
+++ src/read/nashorn.js
@@ -0,0 +1,30 @@
+/**
+ * @file Nashorn read implementation
+ * @since 0.2.4
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST*/ // Host types
+/*global _GpfStreamJavaReadable*/ // gpf.java.ReadableStream
+/*global _GpfStreamWritableString*/ // gpf.stream.WritableString
+/*global _gpfReadImplByHost*/ // gpf.read per host
+/*#endif*/
+
+/*jshint rhino: true*/
+/*eslint-env rhino*/
+
+_gpfReadImplByHost[_GPF_HOST.NASHORN] = function (path) {
+    var javaPath = java.nio.file.Paths.get(path);
+    if (java.nio.file.Files.exists(javaPath)) {
+        return new Promise(function (resolve, reject) {
+            var javaInputStream = java.nio.file.Files.newInputStream(javaPath),
+                iStreamReader = new _GpfStreamJavaReadable(javaInputStream),
+                iWritableString = new _GpfStreamWritableString();
+            return iStreamReader.read(iWritableString)
+                .then(function () {
+                    resolve(iWritableString.toString());
+                })["catch"](reject);
+        });
+    }
+    return Promise.reject(new Error("File not found")); // To be improved
+};
