90e614776c98de4446b64107aedc7c49278922e3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-10 18:11:51 -0400

    Test case (#251)

---
+++ test/read.js
@@ -0,0 +1,38 @@
+"use strict";
+
+describe("read", function () {
+
+    var data = "test/data";
+
+    describe("gpf.read", function () {
+
+        it("gives a generic read access to the file system", function (done) {
+            gpf.read(gpf.path.join(data, "folder/hello world.txt"))
+                .then(function (content) {
+                    try {
+                        assert(content === "hello world\n");
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                }, done);
+        });
+
+        it("fails if the file does not exist", function (done) {
+            gpf.read(gpf.path.join(data, "nope"))
+                .then(function () {
+                    done(new Error("Should not happen"));
+
+                }, function (reason) {
+                    try {
+                        assert(reason instanceof Error);
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+        });
+
+    });
+
+});
