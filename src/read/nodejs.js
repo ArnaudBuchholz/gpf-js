bd25952f12ae97102d47c0d478960946d03da928
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-10 17:44:47 -0400

    NodeJS specific read impl (#251)

---
+++ src/read/nodejs.js
@@ -0,0 +1,21 @@
+/**
+ * @file NodeJS read implementation
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST*/ // Host types
+/*global _gpfNodeFs*/ // Node/PhantomJS require("fs")
+/*global _gpfReadImplByHost*/ // gpf.read per host
+/*#endif*/
+
+_gpfReadImplByHost[_GPF_HOST.NODEJS] = function (path) {
+    return new Promise(function (resolve, reject) {
+        _gpfNodeFs.readFile(path, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data.toString());
+            }
+        });
+    });
+};
