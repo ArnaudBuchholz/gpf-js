cf25854d13755fe5dbe43089cb6b3281d54b6178
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-11 12:54:12 -0400

    Documentation (#251)

--- src/read/wscript.js
+++ src/read/wscript.js
@@ -1,17 +1,22 @@
 /**
  * @file WScript specific File System implementation
+ * @since 0.2.6
  */
- /*#ifndef(UMD)*/
- "use strict";
- /*global _GPF_HOST*/ // Host types
- /*global _gpfReadImplByHost*/ // gpf.read per host
- /*global _gpfMsFSO*/ // Scripting.FileSystemObject activeX
- /*#endif*/
+/*#ifndef(UMD)*/
+"use strict";
+/*global _GPF_HOST*/ // Host types
+/*global _gpfMsFSO*/ // Scripting.FileSystemObject activeX
+/*global _gpfReadImplByHost*/ // gpf.read per host
+/*#endif*/
 
- _gpfReadImplByHost[_GPF_HOST.WSCRIPT] = function (path) {
-     return new Promise(function (resolve) {
-         var file = _gpfMsFSO.OpenTextFile(path, 1, false);
-         resolve(file.ReadAll());
-         file.Close();
-     });
- };
+/*jshint wsh:true*/
+/*eslint-env wsh*/
+/*eslint-disable new-cap*/ // FileSystem object APIs are uppercased
+
+_gpfReadImplByHost[_GPF_HOST.WSCRIPT] = function (path) {
+    return new Promise(function (resolve) {
+        var file = _gpfMsFSO.OpenTextFile(path, 1, false);
+        resolve(file.ReadAll());
+        file.Close();
+    });
+};
