fb7248a1ff6fb8e5eaf654784370e36940d471b3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-11 12:54:38 -0400

    Isolates read (#251)

--- test/fs.js
+++ test/fs.js
@@ -287,39 +287,4 @@ describe("fs", function () {
 
     });
 
-    if (gpf.host() !== gpf.hosts.browser) {
-
-        describe("gpf.fs.read", function () {
-
-            it("gives a generic read access to the file system", function (done) {
-                gpf.fs.read(gpf.path.join(data, "folder/hello world.txt"))
-                    .then(function (content) {
-                        try {
-                            assert(content === "hello world\n");
-                            done();
-                        } catch (e) {
-                            done(e);
-                        }
-                    }, done);
-            });
-
-            it("fails if the file does not exist", function (done) {
-                gpf.fs.read(gpf.path.join(data, "nope"))
-                    .then(function () {
-                        done(new Error("Should not happen"));
-
-                    }, function (reason) {
-                        try {
-                            assert(reason instanceof Error);
-                            done();
-                        } catch (e) {
-                            done(e);
-                        }
-                    });
-            });
-
-        });
-
-    }
-
 });
