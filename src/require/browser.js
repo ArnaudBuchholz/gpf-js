28e4ad07b66f52a87bc24bf45cda8abfff0aff60
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-11 22:37:28 -0400

    Simpler source mapping (#244)

--- src/require/browser.js
+++ src/require/browser.js
@@ -5,30 +5,13 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _GPF_HTTP_METHODS*/ // HTTP Methods
-/*global _gpfHttpRequest*/ // HTTP request common implementation
-/*global _gpfRequireLoadByHost*/ // Host specific loading procedure
+/*global _gpfPathJoin*/ // Join all arguments together and normalize the resulting path
 /*global _gpfRequireSourceMapByHost*/ // Host specific source mapping procedure
 /*#endif*/
 
 /*jshint browser: true*/
 /*eslint-env browser*/
 
-_gpfRequireLoadByHost[_GPF_HOST.BROWSER] = function _gpfRequireLoadHTTP (name) {
-    return _gpfHttpRequest({
-        method: _GPF_HTTP_METHODS.GET,
-        url: name
-    }).then(function (response) {
-        return response.responseText;
-    });
-};
-
 _gpfRequireSourceMapByHost[_GPF_HOST.BROWSER] = function _gpfRequireSourceMapBrowswer (name, content) {
-    var baseUrl;
-    if ("/" === name.charAt(0)) {
-        baseUrl = location.origin;
-    } else {
-        baseUrl = location.toString();
-    }
-    return "//# sourceURL=" + baseUrl + name + "?gpf.require\n" + content;
+    return "//# sourceURL=" + location.origin + _gpfPathJoin(location.pathname, name) + "?gpf.require\n" + content;
 };
