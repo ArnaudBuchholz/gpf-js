0bb78fe1923029497e9065a4e93ae8f166396305
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-12 19:47:58 -0400

    Removes _gpfBootImplByHost

--- src/host.js
+++ src/host.js
@@ -5,11 +5,8 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfBootImplByHost*/ // Boot host specific implementation per host
 /*global _gpfHost*/ // Host type
 /*#endif*/
-
-_gpfBootImplByHost[_gpfHost]();
 
 /**
  * Host type enumeration
