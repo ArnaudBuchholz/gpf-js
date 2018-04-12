249c7e783d41be85889d613621db4872e6e7540e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-12 13:12:11 -0400

    Computes _gpfDosPath (and includes _gpfNodePath)

--- src/host/nodejs.js
+++ src/host/nodejs.js
@@ -6,10 +6,12 @@
 "use strict";
 /*global _GPF_HOST*/ // Host types
 /*global _gpfBootImplByHost*/ // Boot host specific implementation per host
+/*global _gpfDosPath:true*/ // DOS-like path
 /*global _gpfExit:true*/ // Exit function
 /*global _gpfNodeFs:true*/ // Node/PhantomJS require("fs")
 /*exported _gpfNodeHttp*/ // Node require("http")
 /*exported _gpfNodeHttps*/ // Node require("https")
+/*exported _gpfNodePath*/ // Node require("path")
 /*exported _gpfNodeUrl*/ // Node require("url")
 /*#endif*/
 
@@ -34,6 +36,14 @@ var
     _gpfNodeHttps,
 
     /**
+     * require("path")
+     *
+     * @type {Object}
+     * @since 0.1.5
+     */
+    _gpfNodePath,
+
+    /**
      * require("url")
      *
      * @type {Object}
@@ -50,10 +60,13 @@ gpf.node = {};
 
 _gpfBootImplByHost[_GPF_HOST.NODEJS] = function () {
 
+    _gpfNodePath = require("path");
     _gpfNodeFs = require("fs");
     _gpfNodeHttp = require("http");
     _gpfNodeHttps = require("https");
     _gpfNodeUrl = require("url");
+
+    _gpfDosPath = _gpfNodePath.sep === "\\";
 
     /* istanbul ignore next */ // exit.1
     _gpfExit = function (code) {
