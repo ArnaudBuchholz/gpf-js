0ab81e4a5f632683f2fd175291729dbc22874c5d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-11 22:38:00 -0400

    No source mapping (#244)

--- src/require/others.js
+++ src/require/others.js
@@ -5,21 +5,8 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GPF_HOST*/ // Host types
-/*global _gpfRead*/ // Generic read method
-/*global _gpfRequireLoadByHost*/ // Host specific loading procedure
 /*global _gpfRequireSourceMapByHost*/ // Host specific source mapping procedure
 /*#endif*/
-
-function _gpfRequireLoadFS (name) {
-    // Must be relative to the current execution path
-    var path;
-    if (name.charAt(0) === "/") {
-        path = "." + name;
-    } else {
-        path = "./" + name;
-    }
-    return _gpfRead(path);
-}
 
 function _gpfRequireSourceMapNone (name, content) {
     return content;
@@ -30,7 +17,5 @@ Object.keys(_GPF_HOST)
         return hostKey !== "BROWSER";
     })
     .forEach(function (hostKey) {
-        var host = _GPF_HOST[hostKey];
-        _gpfRequireLoadByHost[host] = _gpfRequireLoadFS;
-        _gpfRequireSourceMapByHost[host] = _gpfRequireSourceMapNone;
+        _gpfRequireSourceMapByHost[_GPF_HOST[hostKey]] = _gpfRequireSourceMapNone;
     });
