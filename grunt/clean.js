eba1537667e471d899785f467b1346119e9e5f96
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-19 09:26:33 -0400

    Keep config.json & doc.cache (#258)

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,7 +1,21 @@
 "use strict";
 
 module.exports = {
-    plato: ["tmp/plato"],
-    tmp: ["tmp/*", "!tmp/.eslintrc", "!tmp/.gitignore", "!tmp/.jshintrc", "!tmp/README.md", "!tmp/import_plato.js"],
-    publish: ["tmp/publish/*", "tmp/publish/.*"]
+    plato: [
+        "tmp/plato"
+    ],
+    tmp: [
+        "tmp/*",
+        "!tmp/.eslintrc",
+        "!tmp/.gitignore",
+        "!tmp/.jshintrc",
+        "!tmp/README.md",
+        "!tmp/import_plato.js",
+        "!tmp/config.json",
+        "!tmp/doc.cache"
+    ],
+    publish: [
+        "tmp/publish/*",
+        "tmp/publish/.*"
+    ]
 };
