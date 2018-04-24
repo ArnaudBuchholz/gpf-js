270b6d351e6a8e558a4a41fd782a31892374e3f0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-24 13:14:18 -0400

    Flavor version generation (#252)

--- make/make.js
+++ make/make.js
@@ -3,13 +3,20 @@
 const
     fs = require("fs"),
     build = require("./build.js"),
+    flavor = require("./flavor.js"),
+    sources = JSON.parse(fs.readFileSync("../src/sources.json")),
+    dependencies = JSON.parse(fs.readFileSync("../build/dependencies.json")),
     sourcesDict = {};
 let
     debug = () => {},
     version = "debug",
+    flavorSettings,
+    flavorFilter,
+    outputName,
+    parametersType,
     parameters,
     debugParameters,
-    sources,
+    sourcesToProcess,
     result;
 
 // Cheap parameter parsing
@@ -23,14 +30,24 @@ process.argv.slice(2).forEach(value => {
     }
 });
 
+if (version && 0 === version.indexOf("flavor/")) {
+    flavorSettings = JSON.parse(fs.readFileSync(version + ".json").toString());
+    outputName = flavorSettings.output;
+    flavorFilter = flavor(sources, dependencies, flavorSettings.flavor);
+    parametersType = "release";
+} else {
+    outputName = version;
+    parametersType = version;
+}
+
 console.log(`Generating version '${version}'`);
-debug("\tReading parameters...");
+debug("\tReading generation parameters...");
 try {
     debugParameters = JSON.parse(fs.readFileSync("debug.json").toString());
     if ("debug" === version) {
         parameters = debugParameters;
     } else {
-        parameters = JSON.parse(fs.readFileSync(version + ".json").toString());
+        parameters = JSON.parse(fs.readFileSync(parametersType + ".json").toString());
     }
     parameters.debugRewriteOptions = debugParameters.rewriteOptions;
 } catch (e) {
@@ -39,13 +56,14 @@ try {
 }
 
 // Get the list of sources
-debug("\tGetting the list of sources...");
-sources = JSON.parse(fs.readFileSync("../src/sources.json"))
+debug("\tGetting the list of sources to process...");
+sourcesToProcess = sources
+    .filter((source, index) => !flavorFilter || flavorFilter[index])
     .filter(source => source.load !== false)
     .map(source => source.name);
 
 // Read sources
-sources.forEach(name => {
+sourcesToProcess.forEach(name => {
     debug(`\tReading ${name}...`);
     sourcesDict[name] = fs.readFileSync(`../src/${name}.js`).toString();
 });
@@ -72,7 +90,7 @@ parameters.temporaryPath = `../tmp/build/${version}`;
 mkDir(parameters.temporaryPath);
 
 //Go over sources to create other temporary folders
-sources.forEach(name => {
+sourcesToProcess.forEach(name => {
     if (name.indexOf("/")) {
         // remove file name
         name = name.split("/");
@@ -96,4 +114,4 @@ try {
 
 debug("\tCreating output folder...");
 mkDir("../build");
-fs.writeFileSync(`../build/gpf-${version}.js`, result);
+fs.writeFileSync(`../build/gpf-${outputName}.js`, result);
