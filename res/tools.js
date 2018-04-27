9190023401eb654b7bc26db9af4893505c1884e9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-27 13:24:34 -0400

    Putting in common some code (#252)

---
+++ res/tools.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    /*global module*/
+
+    module.exports = {
+
+        capitalize: function (string) {
+            return string.charAt(0).toUpperCase() + string.substr(1);
+        }
+
+    };
+
+}());
