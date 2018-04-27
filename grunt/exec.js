fec60e11e31db3690e007e3b29b4168f31aaece7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-27 13:58:35 -0400

    Refactoring (#252)

--- grunt/exec.js
+++ grunt/exec.js
@@ -2,8 +2,25 @@
 
 const
     fs = require("fs"),
-    sep = require("path").sep,
+    tools = require("../res/tools.js"),
+    osPathSeparator = require("path").sep,
     isWindows = (/^win/).test(process.platform),
+    toArray = arrayLike => [].slice.call(arrayLike),
+    sep = path => osPathSeparator === "/" ? path : path.replace(/\//g, osPathSeparator),
+
+    // Node supports / on any host
+    platoCmd = "node node_modules/plato/bin/plato -l .jshintrc -t GPF-JS -d tmp/plato",
+    jsdocCmd = "node node_modules/jsdoc/jsdoc -d tmp/jsdoc --verbose -a all -c doc/private.json",
+    checkdocCmd = `node doc/validate http://localhost:${configuration.serve.httpPort}/tmp/doc/public/index.html`,
+
+    config = (cmd, ...options) => {
+        if ("string" === typeof cmd || "function" === typeof cmd) {
+            cmd = {
+                cmd: cmd
+            };
+        }
+        return Object.assign.apply(Object, [cmd].concat(options));
+    },
 
     silent = {
         stdout: false,
@@ -19,104 +36,73 @@ const
     },
     failIfNot0 = {
         exitCode: 0
+    },
+
+    testConfig = (name, command, parameters) => {
+        if (isWindows && command.indexOf(" ") !== -1) {
+            command = `"${command}"`;
+        }
+        command += ` ${parameters}`;
+        const cmd = suffix => function () {
+            let parameter;
+            if (arguments.length) {
+                parameter = toArray(arguments).join(" ") + " ";
+            } else {
+                parameter = "";
+            }
+            return command + " " + parameter + suffix;
+        };
+        module.exports[`test${name}`] = config(cmd(""), silent, failIfNot0);
+        module.exports[`test${name}Verbose`] = config(cmd("-debugger"), verbose, failIfNot0);
+        module.exports[`test${name}Coverage`] = config(cmd("-coverage"), silent, failIfNot0);
+        module.exports[`test${name}Debug`] = config(cmd("-debug"), silent, failIfNot0);
+        module.exports[`test${name}Release`] = config(cmd("-release"), silent, failIfNot0);
+        module.exports[`test${name}Legacy`] = config(version => `${command} legacy/${version}`, silent, failIfNot0);
     };
 
-// Custom command lines
-module.exports = {
-    buildDebug: Object.assign({
-        cmd: "node make.js debug",
-        cwd: "make"
-    }, silent, failIfNot0),
-    buildRelease: Object.assign({
-        cmd: "node make.js release",
-        cwd: "make"
-    }, silent, failIfNot0),
-    plato: Object.assign({
-        cmd: () => "node node_modules/plato/bin/plato -l .jshintrc -t GPF-JS -d tmp/plato "
-                    + configuration.files.src.join(" ")
-    }, verbose),
-    metrics: Object.assign({
-        cmd: function () {
-            return "node make/metrics.js " + [].slice.call(arguments).join(" ");
-        }
-    }, showErrors, failIfNot0),
-    globals: Object.assign({
-        cmd: "node make/globals.js"
-    }, showErrors, failIfNot0),
-    version: Object.assign({
-        cmd: "node make/version.js"
-    }, silent, failIfNot0),
-    detectSelenium: Object.assign({
-        cmd: "node test/host/selenium/detect.js"
-    }, verbose, failIfNot0),
-    jsdoc: Object.assign({
-        cmd: function () {
-            return "node node_modules/jsdoc/jsdoc -d tmp/jsdoc --verbose -a all "
-                + "-c doc/private.json " + [].slice.call(arguments).join(" ");
-        }
-    }, verbose, failIfNot0),
-    fixUglify: Object.assign({
-        cmd: name => `node make/fix_uglify.js ${name}`
-    }, verbose, failIfNot0),
-    checkDoc: Object.assign({
-        cmd: `node doc/validate http://localhost:${configuration.serve.httpPort}/tmp/doc/public/index.html`
-    }, verbose, failIfNot0)
-};
-
-function _buildTestConfig (name, command, parameters) {
-    if (isWindows && command.indexOf(" ") !== -1) {
-        command = `"${command}"`;
-    }
-    command += ` ${parameters}`;
-    function buildCommand (suffix) {
-        return function () {
-            let parameter;
-            if (0 === arguments.length) {
-                parameter = "";
-            } else {
-                parameter = " " + [].slice.call(arguments, 0).join(" ");
-            }
-            return command + parameter + suffix;
-        };
-    }
-    module.exports["test" + name] = Object.assign({
-        cmd: buildCommand("")
-    }, silent, failIfNot0);
-    module.exports["test" + name + "Verbose"] = Object.assign({
-        cmd: buildCommand(" -debugger")
-    }, verbose, failIfNot0);
-    module.exports["test" + name + "Coverage"] = Object.assign({
-        cmd: buildCommand(" -coverage")
-    }, silent, failIfNot0);
-    module.exports["test" + name + "Debug"] = Object.assign({
-        cmd: buildCommand(" -debug")
-    }, silent, failIfNot0);
-    module.exports["test" + name + "Release"] = Object.assign({
-        cmd: buildCommand(" -release")
-    }, silent, failIfNot0);
-    module.exports["test" + name + "Legacy"] = Object.assign({
-        cmd: version => `${command} legacy/${version}`
-    }, silent, failIfNot0);
-}
-
-_buildTestConfig("Node", "node", "test/host/nodejs.js");
 let phantomJsBin = "/usr/local/phantomjs/bin/phantomjs"; // If already installed
 if (!fs.existsSync(phantomJsBin)) {
     // Assume it is installed with the phantomjs-prebuilt package
-    phantomJsBin = `node_modules${sep}phantomjs-prebuilt${sep}lib${sep}phantom${sep}bin${sep}phantomjs`;
+    phantomJsBin = sep("node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs");
 }
-_buildTestConfig("Phantom", phantomJsBin, `--web-security=false test${sep}host${sep}phantomjs.js`);
-_buildTestConfig("Wscript", "cscript.exe", `/D /E:JScript test${sep}host${sep}cscript.js`);
-_buildTestConfig("Nodewscript", "node", "test/host/node_cscript.js");
-_buildTestConfig("Rhino", "java", `-jar node_modules${sep}rhino-1_7r5-bin${sep}rhino1_7R5${sep}js.jar`
-    + ` test${sep}host${sep}java.js`);
-_buildTestConfig("Nashorn", configuration.host.nashorn, `test${sep}host${sep}java.js  --`);
+
+// Custom command lines
+module.exports = {
+    buildDebug: config({cmd: "node make.js debug", cwd: "make"}, silent, failIfNot0),
+    buildRelease: config({cmd: "node make.js release", cwd: "make"}, silent, failIfNot0),
+    plato: config(() => `${platoCmd} ${configuration.files.src.join(" ")}`, verbose),
+    metrics: config((...args) => `node make/metrics.js ${args.join(" ")}`, showErrors, failIfNot0),
+    globals: config("node make/globals.js", showErrors, failIfNot0),
+    version: config("node make/version.js", silent, failIfNot0),
+    detectSelenium: config("node test/host/selenium/detect.js", verbose, failIfNot0),
+    jsdoc: config((...args) => `${jsdocCmd} ${args.join(" ")}`, verbose, failIfNot0),
+    fixUglify: config(name => `node make/fix_uglify.js ${name}`, verbose, failIfNot0),
+    checkDoc: config(checkdocCmd, verbose, failIfNot0)
+};
+
+// Flavor builds
+Object.keys(configuration.files.flavors).forEach(flavor => {
+    module.exports[`build${tools.capitalize(flavor)}`] = config({
+        cmd: `node make.js flavor/${flavor}`,
+        cwd: "make"
+    }, silent, failIfNot0);
+});
+
+// Test configurations
+testConfig("Node", "node", "test/host/nodejs.js");
+testConfig("Phantom", phantomJsBin, sep("--web-security=false test/host/phantomjs.js"));
+testConfig("Wscript", "cscript.exe", `/D /E:JScript ${sep("test/host/cscript.js")}`);
+testConfig("Nodewscript", "node", "test/host/node_cscript.js");
+testConfig("Rhino", "java", sep("-jar node_modules/rhino-1_7r5-bin/rhino1_7R5/js.jar test/host/java.js"));
+testConfig("Nashorn", configuration.host.nashorn, sep("test/host/java.js  --"));
+
 Object.keys(configuration.browsers).forEach(browserName =>{
-    let browserConfig = configuration.browsers[browserName],
-        capitalizedBrowserName = browserName.charAt(0).toUpperCase() + browserName.substr(1);
+    const
+        browserConfig = configuration.browsers[browserName],
+        capitalizedBrowserName = tools.capitalize(browserName);
     if ("selenium" === browserConfig.type) {
-        _buildTestConfig(capitalizedBrowserName, "node", `test/host/selenium.js ${browserName}`);
+        testConfig(capitalizedBrowserName, "node", `test/host/selenium.js ${browserName}`);
     } else if ("spawn" === browserConfig.type) {
-        _buildTestConfig(capitalizedBrowserName, "node", `test/host/browser.js ${browserName}`);
+        testConfig(capitalizedBrowserName, "node", `test/host/browser.js ${browserName}`);
     }
 });
