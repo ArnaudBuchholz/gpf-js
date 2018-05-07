68228563b3011f4d022129d52b39ad380d772990
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-05-07 09:07:23 -0400

    Flavor testing (#252)

--- grunt/concurrent.js
+++ grunt/concurrent.js
@@ -62,6 +62,19 @@ module.exports = {
 
 };
 
+Object.keys(configuration.files.flavors).forEach(flavor => {
+    const
+        definition = configuration.files.flavors[flavor],
+        flavorHosts = definition.flavor.split(" ")
+            .filter(token => 0 === token.indexOf("host:"))
+            .map(token => token.substr(5)),
+        tasks = Object.keys(hosts)
+            .filter(name => 0 === flavorHosts.length || -1 !== flavorHosts.indexOf(name))
+            .reduce((list, name) => list.concat(hosts[name]), [])
+            .map(name => `exec:test${name}`);
+    module.exports[`flavor@${flavor}`] = tasks.map(name => `${name}Flavor:${flavor}`)
+});
+
 configuration.files.legacyTest.forEach(versionFile => {
     let version = versionFile.substr(0, versionFile.lastIndexOf("."));
     module.exports[`legacy${version}`] = testTasks.map(task => `${task}Legacy:${version}`);
