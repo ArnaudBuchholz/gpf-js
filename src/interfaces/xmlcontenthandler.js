3d8473b882f8bf3ff041e17e7b477aa51da725f9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-03 12:56:18 -0400

    Simplification and documentation of the interface (#264)

--- src/interfaces/xmlcontenthandler.js
+++ src/interfaces/xmlcontenthandler.js
@@ -14,7 +14,68 @@
  * @interface gpf.interfaces.IXmlContentHandler
  */
 
+/**
+ * Signal character data
+ *
+ * @method gpf.interfaces.IXmlContentHandler#characters
+ * @param {String} buffer characters
+ */
 
+/**
+ * Signal the end of the document
+ *
+ * @method gpf.interfaces.IXmlContentHandler#endDocument
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ * Signal the end of an element
+ *
+ * @method gpf.interfaces.IXmlContentHandler#endElement
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ *  Signal the scope end of a prefix-URI namespace mapping
+ *
+ * @method gpf.interfaces.IXmlContentHandler#endPrefixMapping
+ * @param {String} prefix Prefix of the prefix-URI mapping
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ * Signal a processing instruction
+ *
+ * @method gpf.interfaces.IXmlContentHandler#processingInstruction
+ * @param {String} target Target of processing instruction
+ * @param {String} data Data of processing instruction
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ * Signal the beginning of the document
+ *
+ * @method gpf.interfaces.IXmlContentHandler#startDocument
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ * Signal the beginning of an element
+ *
+ * @method gpf.interfaces.IXmlContentHandler#startElement
+ * @param {String} qName Qualified name, [prefix:]localName
+ * @param {String} uri Namespace URI associated with the name
+ * @param {Object} attributes attribute dictionary (string/string)
+ * @return {Promise} Resolved when ready
+ */
+
+/**
+ * Signal the scope start a prefix-URI namespace mapping
+ *
+ * @param {String} prefix Prefix of the prefix-URI mapping
+ * @param {String} uri Namespace URI associated with the prefix
+ * @return {Promise} Resolved when ready
+ */
 
 /**
  * IXmlContentHandler interface specifier
