aa27047bf342096e299c2485c1f5dd16c95d4cf7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-06 16:33:27 -0400

    Starting the writer (#265 & #264)

--- src/xml/writer.js
+++ src/xml/writer.js
@@ -3,5 +3,34 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfIXmlContentHandler*/
+/*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _GpfStreamBufferedRead*/ // gpf.stream.BufferedRead
+/*exported _GpfXmlWriter*/ // gpf.xml.Writer
 /*#endif*/
+
+var
+    /**
+     * XML writer
+     *
+     * @constructor gpf.xml.Writer
+     * @implements {gpf.interfaces.IReadableStream}
+     * @implements {gpf.interfaces.IXmlContentHandler}
+     * @extends gpf.stream.BufferedRead
+     */
+    _GpfXmlWriter = _gpfDefine({
+        $class: "gpf.xml.Writer",
+        $extend: _GpfStreamBufferedRead,
+
+        // region gpf.interfaces.IXmlContentHandler
+
+        /**
+         * @gpf:sameas gpf.interfaces.IXmlContentHandler#characters
+         */
+        characters: function (buffer) {
+            this._appendToReadBuffer(buffer);
+            return Promise.resolve();
+        }
+
+        //endregion
+
+    });
