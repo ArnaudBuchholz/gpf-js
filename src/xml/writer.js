b4113455e11743bfd2ddd60f362e93b184041873
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-09 12:55:27 -0400

    First implementation (#265)

--- src/xml/writer.js
+++ src/xml/writer.js
@@ -5,6 +5,8 @@
 "use strict";
 /*global _gpfDefine*/ // Shortcut for gpf.define
 /*global _gpfErrorDeclare*/ // Declare new gpf.Error names
+/*global  _gpfStringEscapeFor*/ // Make the string content compatible with lang
+/*global _gpfObjectForEach*/
 /*global _gpfIgnore*/
 /*global _GpfStreamBufferedRead*/ // gpf.stream.BufferedRead
 /*exported _GpfXmlWriter*/ // gpf.xml.Writer
@@ -51,23 +53,32 @@ var
             }
         },
 
-        _checkIfElementsExist: function (hasElements) {
-            if (hasElements !== (this._elements.length !== 0)) {
+        _checkIfElementsExist: function (mustHaveElements) {
+            if (mustHaveElements && this._elements.length === 0) {
                 gpf.Error.invalidXmlWriterState();
             }
         },
 
-        _checkState: function (started, hasElements) {
+        _checkState: function (started, mustHaveElements) {
             this._checkIfStarted(started);
-            this._checkIfElementsExist(hasElements);
+            this._checkIfElementsExist(mustHaveElements);
         },
 
         // region gpf.interfaces.IXmlContentHandler
 
+        _addContentToLastElement: function () {
+            var element = this._elements[0];
+            if (element && !element.content) {
+                this._appendToReadBuffer(">");
+                element.content = true;
+            }
+        },
+
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#characters */
         characters: function (buffer) {
             this._checkState(true, true);
-            this._appendToReadBuffer(buffer);
+            this._addContentToLastElement();
+            this._appendToReadBuffer(_gpfStringEscapeFor(buffer.toString(), "xml"));
             return Promise.resolve();
         },
 
@@ -81,8 +92,12 @@ var
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#endElement */
         endElement: function () {
             this._checkState(true, true);
-            var qName = this._elements.shift();
-            this._appendToReadBuffer("</" + qName + ">");
+            var element = this._elements.shift();
+            if (element.content) {
+                this._appendToReadBuffer("</" + element.qName + ">");
+            } else {
+                this._appendToReadBuffer("/>");
+            }
             return Promise.resolve();
         },
 
@@ -96,7 +111,7 @@ var
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#processingInstruction */
         processingInstruction: function (target, data) {
             this._checkState(true, false);
-            this._appendToReadBuffer("<?" + target + " " + data + ">");
+            this._appendToReadBuffer("<?" + target + " " + data + "?>\n");
             return Promise.resolve();
         },
 
@@ -107,10 +122,25 @@ var
             return Promise.resolve();
         },
 
+        _processAttributes: function (attributes) {
+            _gpfObjectForEach(attributes, function (value, name) {
+                this._appendToReadBuffer(" " + name + "=\"");
+                this._appendToReadBuffer(_gpfStringEscapeFor(value.toString(), "xml"));
+                this._appendToReadBuffer("\"");
+            }, this);
+        },
+
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#startElement */
         startElement: function (qName, attributes) {
             this._checkState(true, false);
-
+            this._addContentToLastElement();
+            this._appendToReadBuffer("<" + qName);
+            if (attributes) {
+                this._processAttributes(attributes);
+            }
+            this._elements.unshift({
+                qName: qName
+            });
         },
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#startPrefixMapping */
