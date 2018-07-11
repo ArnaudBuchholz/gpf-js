0eceed87d409b1affcba26cba8462dfa2229b1c8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-11 15:37:32 -0400

    Namespace support (#265)

--- test/xml/writer.js
+++ test/xml/writer.js
@@ -43,10 +43,10 @@ describe("xml/writer", function () {
         assert(false);
     }
 
-    function assessInvalidXmlWriterState (done) {
+    function assessError (done, ErrorClass) {
         return function (reason) {
             try {
-                assert(reason instanceof gpf.Error.InvalidXmlWriterState);
+                assert(reason instanceof ErrorClass);
                 done();
             } catch (error) {
                 done(error);
@@ -54,107 +54,166 @@ describe("xml/writer", function () {
         };
     }
 
-    it("implements gpf.interfaces.IXmlContentHandler", function () {
-        assert(gpf.interfaces.isImplementedBy(gpf.interfaces.IXmlContentHandler, gpf.xml.Writer));
-    });
+    function assessInvalidXmlWriterState (done) {
+        return assessError(done, gpf.Error.InvalidXmlWriterState);
+    }
 
-    it("can be piped", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document/>");
-        })
-            .startDocument()
-            .startElement("document")
-            .endElement()
-            .endDocument()["catch"](done);
-    });
+    function assessInvalidXmlName (done) {
+        return assessError(done, gpf.Error.InvalidXmlName);
+    }
 
-    it("allows processing instructions", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<?test test-data?>\n<document/>");
-        })
-            .startDocument()
-            .processingInstruction("test", "test-data")
-            .startElement("document")
-            .endElement()
-            .endDocument()["catch"](done);
-    });
+    describe("Interface specification", function () {
 
-    it("supports attributes (string value)", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document test=\"value\"/>");
-        })
-            .startDocument()
-            .startElement("document", {
-                "test": "value"
+        it("implements gpf.interfaces.IXmlContentHandler", function () {
+            assert(gpf.interfaces.isImplementedBy(gpf.interfaces.IXmlContentHandler, gpf.xml.Writer));
+        });
+
+        it("implements gpf.interfaces.IReadableStream", function () {
+            assert(gpf.interfaces.isImplementedBy(gpf.interfaces.IReadableStream, gpf.xml.Writer));
+        });
+
+        it("can be piped", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document/>");
             })
-            .endElement()
-            .endDocument()["catch"](done);
+                .startDocument()
+                .startElement("document")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
     });
 
-    it("supports attributes (number value)", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document test=\"2\"/>");
-        })
-            .startDocument()
-            .startElement("document", {
-                "test": 2
+    describe("Basic manipulations", function () {
+
+        it("allows processing instructions", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<?test test-data?>\n<document/>");
             })
-            .endElement()
-            .endDocument()["catch"](done);
-    });
+                .startDocument()
+                .processingInstruction("test", "test-data")
+                .startElement("document")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
 
-    it("supports multiple attributes", function (done) {
-        allocateWriter(done, function (output) {
-            // Order of attributes is *not* significant
-            assert(output === "<document test1=\"value1\" test2=\"value2\"/>"
-                || output === "<document test2=\"value2\" test1=\"value1\"/>");
-        })
-            .startDocument()
-            .startElement("document", {
-                "test1": "value1",
-                "test2": "value2"
+        it("supports attributes (string value)", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document test=\"value\"/>");
             })
-            .endElement()
-            .endDocument()["catch"](done);
-    });
+                .startDocument()
+                .startElement("document", {
+                    "test": "value"
+                })
+                .endElement()
+                .endDocument()["catch"](done);
+        });
 
-    it("supports nodes hierarchy", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document><a><b/></a><c/></document>");
-        })
-            .startDocument()
-            .startElement("document")
-            .startElement("a")
-            .startElement("b")
-            .endElement()
-            .endElement()
-            .startElement("c")
-            .endElement()
-            .endElement()
-            .endDocument()["catch"](done);
-    });
-
-    it("escapes attributes value", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document test=\"&lt;&amp;&gt;\"/>");
-        })
-            .startDocument()
-            .startElement("document", {
-                test: "<&>"
+        it("supports attributes (number value)", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document test=\"2\"/>");
             })
-            .endElement()
-            .endDocument()["catch"](done);
+                .startDocument()
+                .startElement("document", {
+                    "test": 2
+                })
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("supports multiple attributes", function (done) {
+            allocateWriter(done, function (output) {
+                // Order of attributes is *not* significant
+                assert(output === "<document test1=\"value1\" test2=\"value2\"/>"
+                    || output === "<document test2=\"value2\" test1=\"value1\"/>");
+            })
+                .startDocument()
+                .startElement("document", {
+                    "test1": "value1",
+                    "test2": "value2"
+                })
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("supports nodes hierarchy", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document><a><b/></a><c/></document>");
+            })
+                .startDocument()
+                .startElement("document")
+                .startElement("a")
+                .startElement("b")
+                .endElement()
+                .endElement()
+                .startElement("c")
+                .endElement()
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("escapes attributes value", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document test=\"&lt;&amp;&gt;\"/>");
+            })
+                .startDocument()
+                .startElement("document", {
+                    test: "<&>"
+                })
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("escapes text value", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document>&lt;&amp;&gt;</document>");
+            })
+                .startDocument()
+                .startElement("document")
+                .characters("<&>")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
     });
 
-    it("escapes text value", function (done) {
-        allocateWriter(done, function (output) {
-            assert(output === "<document>&lt;&amp;&gt;</document>");
-        })
-            .startDocument()
-            .startElement("document")
-            .characters("<&>")
-            .endElement()
-            .endDocument()["catch"](done);
+    describe("Namespacing", function () {
+
+        it("qualifies elements (using prefix)", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<test:document xmlns:test=\"namespace-uri\"/>");
+            })
+                .startDocument()
+                .startPrefixMapping("test", "namespace-uri")
+                .startElement("test:document")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("qualifies elements (default namespace)", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document xmlns=\"namespace-uri\"/>");
+            })
+                .startDocument()
+                .startPrefixMapping("", "namespace-uri")
+                .startElement("document")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
+        it("allows xml namespace prefix", function (done) {
+            allocateWriter(done, function (output) {
+                assert(output === "<document xml:space=\"preserve\"> </document>");
+            })
+                .startDocument()
+                .startElement("document", {
+                    "xml:space": "preserve"
+                })
+                .characters(" ")
+                .endElement()
+                .endDocument()["catch"](done);
+        });
+
     });
 
     describe("Invalid XML state", function () {
@@ -184,6 +243,38 @@ describe("xml/writer", function () {
                 .startDocument()
                 .startElement("document")
                 .endDocument()["catch"](assessInvalidXmlWriterState(done));
+        });
+
+        it("prevents overloading a namespace prefix at the same level", function (done) {
+            allocateWriter(done, shouldNotSucceed)
+                .startPrefixMapping("test", "namespace-uri")
+                .startPrefixMapping("test", "namespace-uri2")
+                .startDocument()
+                .endDocument()["catch"](assessInvalidXmlWriterState(done));
+        });
+
+        it("prevents overloading of predefined namespace prefix (xml)", function (done) {
+            allocateWriter(done, shouldNotSucceed)
+                .startPrefixMapping("xml", "namespace-uri")["catch"](assessInvalidXmlWriterState(done));
+        });
+
+        it("prevents overloading of predefined namespace prefix (xmlns)", function (done) {
+            allocateWriter(done, shouldNotSucceed)
+                .startPrefixMapping("xmlns", "namespace-uri")["catch"](assessInvalidXmlWriterState(done));
+        });
+
+        it("validates element name", function (done) {
+            allocateWriter(done, shouldNotSucceed)
+                .startDocument()
+                .startElement("docu/ment")["catch"](assessInvalidXmlName(done));
+        });
+
+        it("validates attribute name", function (done) {
+            allocateWriter(done, shouldNotSucceed)
+                .startDocument()
+                .startElement("document", {
+                    "te/st": "value"
+                })["catch"](assessInvalidXmlName(done));
         });
 
     });
