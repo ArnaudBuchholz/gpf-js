f4cdbb4977220511ebf536b4a67b6597cf8405e2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-18 11:24:14 -0400

    Put in common OS detection (#261)

--- res/tools.js
+++ res/tools.js
@@ -2,11 +2,18 @@
     "use strict";
     /*global module*/
 
+    const
+        isWindows = (/^win/).test(process.platform),
+        isMacOS = process.platform === "darwin";
+
     module.exports = {
 
         capitalize: function (string) {
             return string.charAt(0).toUpperCase() + string.substr(1);
-        }
+        },
+
+        isWindows: isWindows,
+        isMacOS: isMacOS
 
     };
 
