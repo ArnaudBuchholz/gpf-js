996d93d162091560865fa80402a54739e7e5ba10
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-07-22 22:34:46 -0400

    WIP (#266)

--- src/xml/writer.js
+++ src/xml/writer.js
@@ -4,6 +4,7 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfEmptyFunc*/
 /*global _gpfErrorDeclare*/ // Declare new gpf.Error names
 /*global  _gpfStringEscapeFor*/ // Make the string content compatible with lang
 /*global _gpfObjectForEach*/
@@ -24,48 +25,11 @@ _gpfErrorDeclare("xml/writer", {
      *
      * This error is used when a method can not be called due to the current XML writer state
      */
-    invalidXmlWriterState: "Invalid XML Writer state",
-
-    /**
-     * ### Summary
-     *
-     * Invalid XML name
-     *
-     * ### Description
-     *
-     * Invalid XML Element and attribute name
-     */
-    invalidXmlName: "Invalid XML name",
-
-    /**
-     * ### Summary
-     *
-     * Invalid XML namespace prefix
-     *
-     * ### Description
-     *
-     * Invalid XML namespace prefix
-     */
-    invalidXmlNamespacePrefix: "Invalid XML namespace prefix",
-
-    /**
-     * ### Summary
-     *
-     * Unknown XML namespace prefix
-     *
-     * ### Description
-     *
-     * This error is triggered when an element or an attribute is prefixed with an unknown namespace prefix
-     */
-    unknownXmlNamespacePrefix: "Unknown XML namespace prefix"
+    invalidXmlWriterState: "Invalid XML Writer state"
 
 });
 
 var
-    _gpfXmlWriterNameValidation = new RegExp("^[a-z_][a-zA-Z0-9_\\-]*$"),
-    _gpfXmlWriterNamespacePrefixValidation = new RegExp("^(|[a-z_][a-zA-Z0-9_]*)$"),
-    _gpfXmlWriterPredefinedNamespacePrefixes = ["xml", "xmlns"],
-
     _GpfXmlWriter = _gpfDefine({
         $class: "gpf.xml.Writer",
         $extend: _GpfStreamBufferedRead,
@@ -81,17 +45,11 @@ var
         constructor: function () {
             this._elements = [];
             this._nextNamespaces = {};
+            this._checkIfStarted = gpf.Error.invalidXmlWriterState;
         },
 
         _elements: [],
         _nextNamespaces: {},
-        _started: false,
-
-        _checkIfStarted: function (started) {
-            if (this._started !== started) {
-                gpf.Error.invalidXmlWriterState();
-            }
-        },
 
         _checkIfElementsExist: function (hasElements) {
             if (hasElements !== (this._elements.length !== 0)) {
@@ -99,8 +57,8 @@ var
             }
         },
 
-        _checkState: function (started, hasElements) {
-            this._checkIfStarted(started);
+        _checkState: function (hasElements) {
+            this._checkIfStarted();
             if (undefined !== hasElements) {
                 this._checkIfElementsExist(hasElements);
             }
@@ -120,53 +78,26 @@ var
             }
         },
 
-        _checkValidName: function (name) {
-            if (!name.match(_gpfXmlWriterNameValidation)) {
-                gpf.Error.invalidXmlName();
-            }
-        },
-
-        _getNamespacePrefixURI: function (prefix) {
-            return _gpfArrayForEachFalsy(this._elements, function (element) {
-                return element.namespaces[prefix];
-            });
-        },
-
-        _checkNamespacePrefixWasDefined: function (prefix) {
-            if (!this._getNamespacePrefixURI(prefix)) {
-                gpf.Error.unknownXmlNamespacePrefix();
-            }
-        },
-
-        _checkNamespacePrefix: function (prefix) {
-            if (prefix !== "xml") {
-                this._checkNamespacePrefixWasDefined(prefix);
-            }
-        },
-
-        _checkQualifiedName: function (qName) {
-            var columnPos = qName.indexOf(":");
-            if (-1 === columnPos) {
-                this._checkValidName(qName);
-            } else {
-                this._checkNamespacePrefix(qName.substr(0, columnPos));
-                this._checkValidName(qName.substr(columnPos + 1));
-            }
-        },
-
         _writeAttribute: function (qName, value) {
             this._appendToReadBuffer(" " + qName + "=\"");
             this._appendToReadBuffer(_gpfStringEscapeFor(value.toString(), "xml"));
             this._appendToReadBuffer("\"");
         },
 
-        _validateAndwriteAttribute: function (value, qName) {
-            this._checkQualifiedName(qName);
-            this._writeAttribute(qName, value);
+        _getNamespacePrefixes: function () {
+
+            return _gpfArrayForEachFalsy(this._elements, function (element) {
+                return element.namespaces[prefix];
+            });
         },
 
         _processAttributes: function (attributes) {
-            _gpfObjectForEach(attributes, this._validateAndwriteAttribute, this);
+            _gpfObjectForEach(attributes,function (value, qName){
+                /*jshint validthis:true*/
+                var me = this; //eslint-disable-line no-invalid-this
+                _gpfXmlCheckQualifiedAttributeName(qName, me._getNamespacePrefixes());
+                me._writeAttribute(qName, value);
+            }, this);
         },
 
         _processNamespaces: function (namespaces) {
@@ -181,24 +112,11 @@ var
             }, this);
         },
 
-        _checkNotAPredefinedNamespacePrefix: function (prefix) {
-            if (_gpfXmlWriterPredefinedNamespacePrefixes.indexOf(prefix) !== -1) {
-                gpf.Error.invalidXmlNamespacePrefix();
-            }
-        },
-
-        _checkValidNamespacePrefix: function (prefix) {
-            if (!prefix.match(_gpfXmlWriterNamespacePrefixValidation)) {
-                gpf.Error.invalidXmlNamespacePrefix();
-            }
-            this._checkNotAPredefinedNamespacePrefix(prefix);
-        },
-
         // region gpf.interfaces.IXmlContentHandler
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#characters */
         characters: function (buffer) {
-            this._checkState(true, true);
+            this._checkState(true);
             this._addContentToLastElement();
             this._appendToReadBuffer(_gpfStringEscapeFor(buffer.toString(), "xml"));
             return Promise.resolve();
@@ -206,14 +124,15 @@ var
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#endDocument */
         endDocument: function () {
-            this._checkState(true, false);
+            this._checkState(false);
+            this._checkIfStarted = gpf.Error.invalidXmlWriterState;
             this._completeReadBuffer();
             return Promise.resolve();
         },
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#endElement */
         endElement: function () {
-            this._checkState(true, true);
+            this._checkState(true);
             var element = this._elements.shift();
             if (element.content) {
                 this._appendToReadBuffer("</" + element.qName + ">");
@@ -235,14 +154,16 @@ var
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#processingInstruction */
         processingInstruction: function (target, data) {
-            this._checkState(true, false);
+            this._checkState(false);
             this._appendToReadBuffer("<?" + target + " " + data + "?>\n");
             return Promise.resolve();
         },
 
         /** @gpf:sameas gpf.interfaces.IXmlContentHandler#startDocument */
         startDocument: function () {
-            this._checkState(false, false);
+            this._checkIfStarted = _gpfEmptyFunc;
+            this.startDocument = gpf.Error.invalidXmlWriterState;
+            this._checkState(false);
             this._started = true;
             return Promise.resolve();
         },
