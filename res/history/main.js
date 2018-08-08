603a3e9476b634953786db7cb2162e9c4b9476e6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-08 12:34:34 -0400

    WIP (#274)

---
+++ res/history/main.js
@@ -0,0 +1,22 @@
+gpf.require.define({
+    releases: "../../build/releases.json",
+    dom: "../dom.js"
+
+}, function (require) {
+    "use strict";
+
+    var dom = require.dom,
+        lastRelease = require.releases[require.releases.length - 1];
+
+    var version = dom.div({
+        className: "version"
+    }, [
+        dom.div({
+            className: "title",
+        }, lastRelease.label)
+    ]);
+
+    debugger;
+    version.appendTo(document.body);
+
+});
