139cda5321ad268194f49a723d2003bfb963cef0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-23 21:35:48 -0400

    Default value and adds required (#281)

--- test/serial/property.js
+++ test/serial/property.js
@@ -18,46 +18,64 @@ describe("serial/property", function () {
 
         describe("_gpfSerialPropertyCheck", function () {
 
-            it("validates name", function () {
-                var exceptionCaught;
-                try {
-                    _gpfSerialPropertyCheck({
-                        name: "OK"
+            it("validates name and default properties", function () {
+                var property = _gpfSerialPropertyCheck({
+                    name: "OK"
+                });
+                assert(property.type === gpf.serial.types.string);
+                assert(property.required === false);
+            });
+
+            values
+                .concat(undefined)
+                .forEach(function (invalidName) {
+                    it("rejects invalid 'name' (" + JSON.stringify(invalidName) + ")", function () {
+                        var exceptionCaught;
+                        try {
+                            _gpfSerialPropertyCheck({
+                                name: invalidName
+                            });
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidSerialName);
                     });
-                } catch (e) {
-                    exceptionCaught = e;
-                }
-                assert(!exceptionCaught);
-            });
-
-            [undefined].concat(values).forEach(function (invalidName) {
-                it("rejects invalid names (" + JSON.stringify(invalidName) + ")", function () {
-                    var exceptionCaught;
-                    try {
-                        _gpfSerialPropertyCheck({
-                            name: invalidName
-                        });
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidSerialName);
                 });
-            });
 
-            values.forEach(function (invalidType) {
-                it("rejects invalid types (" + JSON.stringify(invalidType) + ")", function () {
-                    var exceptionCaught;
-                    try {
-                        _gpfSerialPropertyCheck({
-                            name: "OK",
-                            type: invalidType
-                        });
-                    } catch (e) {
-                        exceptionCaught = e;
-                    }
-                    assert(exceptionCaught instanceof gpf.Error.InvalidSerialType);
+            values
+                .forEach(function (invalidType) {
+                    it("rejects invalid 'type' (" + JSON.stringify(invalidType) + ")", function () {
+                        var exceptionCaught;
+                        try {
+                            _gpfSerialPropertyCheck({
+                                name: "OK",
+                                type: invalidType
+                            });
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidSerialType);
+                    });
                 });
-            });
+
+            values
+                .filter(function (value) {
+                    return "boolean" !== typeof value;
+                })
+                .forEach(function (invalidRequired) {
+                    it("rejects invalid 'required' (" + JSON.stringify(invalidRequired) + ")", function () {
+                        var exceptionCaught;
+                        try {
+                            _gpfSerialPropertyCheck({
+                                name: "OK",
+                                required: invalidRequired
+                            });
+                        } catch (e) {
+                            exceptionCaught = e;
+                        }
+                        assert(exceptionCaught instanceof gpf.Error.InvalidSerialRequired);
+                    });
+                });
 
         });
 
