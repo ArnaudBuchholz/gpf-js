354b7b500385642d90c51babfc699e4398246a83
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-23 21:35:51 -0400

    Default value and adds required (#281)

--- src/serial/property.js
+++ src/serial/property.js
@@ -29,7 +29,19 @@ _gpfErrorDeclare("serial/property", {
      *
      * Type should be one of the enumeration {@see gpf.serial.types}
      */
-    "invalidSerialType": "Invalid serial type"
+    "invalidSerialType": "Invalid serial type",
+
+    /**
+     * ### Summary
+     *
+     * Required of serializable property is invalid
+     *
+     * ### Description
+     *
+     * Required can either be true or false
+     */
+    "invalidSerialRequired": "Invalid serial required"
+
 });
 
 /**
@@ -44,6 +56,7 @@ gpf.serial = {};
  * @typedef gpf.typedef.serializableProperty
  * @property {String} name Name of the property
  * @property {gpf.serial.types} [type=gpf.serial.types.string] Type of the property
+ * @property {Boolean} [required=false] Property must have a value
  * @see gpf.attributes.Serializable
  */
 
@@ -82,18 +95,40 @@ function _gpfSerialPropertyCheckNameRegExp (name) {
     }
 }
 
-function _gpfSerialPropertyCheckName (name) {
-    _gpfSerialPropertyCheckNameType(name);
-    _gpfSerialPropertyCheckNameRegExp(name);
+function _gpfSerialPropertyCheckName (property) {
+    _gpfSerialPropertyCheckNameType(property.name);
+    _gpfSerialPropertyCheckNameRegExp(property.name);
 }
 
 var _gpfSerialPropertyTypes = Object.keys(_GPF_SERIAL_TYPE).map(function (name) {
     return _GPF_SERIAL_TYPE[name];
 });
 
-function _gpfSerialPropertyCheckType (type) {
+function _gpfSerialPropertyCheckTypeExists (type) {
     if (_gpfSerialPropertyTypes.indexOf(type) === -1) {
         gpf.Error.invalidSerialType();
+    }
+}
+
+function _gpfSerialPropertyCheckType (property) {
+    if (undefined === property.type) {
+        property.type = _GPF_SERIAL_TYPE.STRING;
+    } else {
+        _gpfSerialPropertyCheckTypeExists(property.type);
+    }
+}
+
+function _gpfSerialPropertyCheckRequiredType (required) {
+    if (typeof required !== "boolean") {
+        gpf.Error.invalidSerialRequired();
+    }
+}
+
+function _gpfSerialPropertyCheckRequired (property) {
+    if (undefined === property.required) {
+        property.required = false;
+    } else {
+        _gpfSerialPropertyCheckRequiredType(property.required);
     }
 }
 
@@ -101,14 +136,16 @@ function _gpfSerialPropertyCheckType (type) {
  * Check that the serializable property definition is valid
  *
  * @param {gpf.typedef.serializableProperty} property Property definition to validate
+ * @return {gpf.typedef.serializableProperty} Completed property definition
  * @throws {gpf.Error.InvalidSerialName}
  * @throws {gpf.Error.InvalidSerialType}
  */
 function _gpfSerialPropertyCheck (property) {
-    _gpfSerialPropertyCheckName(property.name);
-    if (property.type !== undefined) {
-        _gpfSerialPropertyCheckType(property.type);
-    }
+    property = Object.assign(property); // Clone
+    _gpfSerialPropertyCheckName(property);
+    _gpfSerialPropertyCheckType(property);
+    _gpfSerialPropertyCheckRequired(property);
+    return property;
 }
 
 /*#ifndef(UMD)*/
