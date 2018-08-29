5b3bd685d8d04b8b5fbb2b4ec115b31091646abb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-29 15:34:35 -0400

    Member attribute tests (#282)

---
+++ test/attributes/memberattribute.js
@@ -0,0 +1,80 @@
+"use strict";
+
+describe("attributes/memberattribute", function () {
+
+    describe("gpf.attributes.MemberAttribute", function () {
+
+        it("can't be used on a non-attribute class", function () {
+            var exceptionCaught;
+            try {
+                gpf.define({
+                    $class: "B",
+                    $attributes: [new gpf.attributes.MemberAttribute()]
+                });
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.RestrictedBaseClassAttribute);
+        });
+
+        it("can't be used on a member of an attribute class", function () {
+            var exceptionCaught;
+            try {
+                gpf.define({
+                    $class: "Attribute",
+                    $extend: gpf.attributes.Attribute,
+
+                    "[test]": [new gpf.attributes.MemberAttribute()],
+                    test: function () {}
+                });
+            } catch (e) {
+                exceptionCaught = e;
+            }
+            assert(exceptionCaught instanceof gpf.Error.ClassAttributeOnly);
+        });
+
+        describe("When used properly", function () {
+
+            var Attribute,
+                attribute;
+
+            before(function () {
+                Attribute = gpf.define({
+                    $class: "Attribute",
+                    $extend: gpf.attributes.Attribute,
+                    $attributes: [new gpf.attributes.MemberAttribute()]
+                });
+                attribute = new Attribute();
+            });
+
+            it("fails definition if used at class level", function () {
+                var exceptionCaught;
+                try {
+                    gpf.define({
+                        $class: "Test",
+                        $attributes: [attribute]
+                    });
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(exceptionCaught instanceof gpf.Error.MemberAttributeOnly);
+            });
+
+            it("accepts definition if used at member level", function () {
+                var exceptionCaught;
+                try {
+                    gpf.define({
+                        $class: "Test",
+                        "[test]": [attribute],
+                        test: function () {}
+                    });
+                } catch (e) {
+                    exceptionCaught = e;
+                }
+                assert(!exceptionCaught);
+            });
+
+        });
+    });
+
+});
