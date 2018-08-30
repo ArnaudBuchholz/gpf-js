4f6dff37df8bed748d90a49d7502f84c6c2d628c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-30 16:41:09 -0400

    Documentation (#282)

--- src/attributes/memberattribute.js
+++ src/attributes/memberattribute.js
@@ -1,19 +1,21 @@
 /**
  * @file gpf.attributes.MemberAttribute attribute
+ * @since 0.2.8
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfAttributesAttributeAttribute*/
+/*global _gpfAttributesAttributeAttribute*/ // Shortcut for gpf.attributes.AttributeAttribute
 /*global _gpfAttributesCheckMemberOnly*/ // Ensures attribute is used only at member level
 /*global _gpfDefine*/ // Shortcut for gpf.define
-/*global _gpfIgnore*/
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*exported _gpfAttributesMemberAttribute*/ // Shortcut for gpf.attributes.MemberAttribute
 /*#endif*/
 
 /**
 * Attribute to restrict the use of an attribute to the member level
  *
- * @class gpf.attributes.AttributeForInstanceOf
+ * @class gpf.attributes.MemberAttribute
+ * @since 0.2.8
  */
 var _gpfAttributesMemberAttribute = _gpfDefine({
     $class: "gpf.attributes.MemberAttribute",
