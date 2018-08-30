c143c87a7a88c873d4096bc9b817ef5b8c37fb8d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-30 16:41:03 -0400

    Documentation (#282)

--- src/attributes/classattribute.js
+++ src/attributes/classattribute.js
@@ -1,19 +1,21 @@
 /**
  * @file gpf.attributes.ClassAttribute attribute
+ * @since 0.2.8
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfAttributesAttributeAttribute*/
+/*global _gpfAttributesAttributeAttribute*/ // Shortcut for gpf.attributes.AttributeAttribute
 /*global _gpfAttributesCheckClassOnly*/ // Ensures attribute is used only at class level
 /*global _gpfDefine*/ // Shortcut for gpf.define
-/*global _gpfIgnore*/
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*exported _gpfAttributesClassAttribute*/ // Shortcut for gpf.attributes.ClassAttribute
 /*#endif*/
 
 /**
  * Attribute to restrict the use of an attribute to the class level
  *
- * @class gpf.attributes.AttributeForInstanceOf
+ * @class gpf.attributes.ClassAttribute
+ * @since 0.2.8
  */
 var _gpfAttributesClassAttribute = _gpfDefine({
     $class: "gpf.attributes.ClassAttribute",
