f70b3feb2ea7d393368b0cdd3ce80881fb3209ab
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-30 16:40:40 -0400

    Documentation (#282)

--- src/attributes/attribute.js
+++ src/attributes/attribute.js
@@ -5,7 +5,7 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfDefine*/ // Shortcut for gpf.define
-/*global _gpfIgnore*/
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*exported _gpfAttribute*/ // Shortcut for gpf.attributes.Attribute
 /*#endif*/
 
@@ -33,6 +33,7 @@ var _gpfAttribute = _gpfDefine({
      * @param {String} member Member name or empty if global to the class
      * @param {_GpfClassDefinition} classDefinition Class definition
      * @private
+     * @since 0.2.8
      */
     _check: function (member, classDefinition) {
         _gpfIgnore(member, classDefinition);
@@ -46,6 +47,7 @@ var _gpfAttribute = _gpfDefine({
      * @param {_GpfClassDefinition} classDefinition Class definition
      * @param {Object} classPrototype Class prototype being built
      * @private
+     * @since 0.2.8
      */
     _build: function (member, classDefinition, classPrototype) {
         _gpfIgnore(member, classDefinition, classPrototype);
