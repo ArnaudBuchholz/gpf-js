d0ef5b3d07c48989c25da8fba65028eb23c0b083
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-01 00:04:20 -0400

    Tests (#274)

--- res/history/main.js
+++ res/history/main.js
@@ -15,7 +15,63 @@ gpf.require.define({
             className: "title"
         }, lastRelease.label)
     ]);
-
     version.appendTo(document.body);
+
+    dom.div({
+        className: "coverage",
+        style: "width: 640px; height: 400px;"
+    }).appendTo(document.body);
+
+    new Chartist.Line('.coverage', {
+        series: [{
+            name: "statements",
+            data: require.releases.map(function (release) {
+                return {
+                    x: new Date(release.date),
+                    y: release.metrics.coverage.statements.total
+                };
+            })
+        }, {
+            name: "branches",
+            data: require.releases.map(function (release) {
+                return {
+                    x: new Date(release.date),
+                    y: release.metrics.coverage.branches.total
+                };
+            })
+        }, {
+            name: "functions",
+            data: require.releases.map(function (release) {
+                return {
+                    x: new Date(release.date),
+                    y: release.metrics.coverage.functions.total
+                };
+            })
+        }, {
+            name: "maintainability",
+            data: require.releases.map(function (release) {
+                return {
+                    x: new Date(release.date),
+                    y: release.metrics.maintainability
+                };
+            })
+        }]
+    }, {
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        },
+        axisX: {
+            type: Chartist.FixedScaleAxis,
+            divisor: 5,
+            labelInterpolationFnc: function(dateValue) {
+                var date = new Date(dateValue);
+                return date.getFullYear() + "-" + date.getMonth();
+            }
+        },
+        axisY: {
+            onlyInteger: true
+        }
+    });
 
 });
