bcd576cc3bab3137ef41bbd5abdb8a9132900c57
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-06 16:18:22 -0400

    Unique attribute (#282)

---
+++ src/attributes/uniqueattribute.js
@@ -0,0 +1,29 @@
+/**
+ * @file gpf.attributes.UniqueAttribute attribute
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*global _gpfAttributesAttributeAttribute*/ // Shortcut for gpf.attributes.AttributeAttribute
+/*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfAttributesCheckAppliedOnlyOnce*/
+/*exported _gpfAttributesMemberAttribute*/ // Shortcut for gpf.attributes.MemberAttribute
+/*#endif*/
+
+/**
+ * Attribute to restrict the use of an attribute to only once
+ * It throws {@link gpf.Error.UniqueAttributeUsedTwice} if the target attribute is used more than once at any level.
+ *
+ * @class gpf.attributes.UniqueAttribute
+ * @extends gpf.attributes.Attribute
+ */
+var _gpfAttributesUniqueAttribute = _gpfDefine({
+    $class: "gpf.attributes.UniqueAttribute",
+    $extend: _gpfAttributesAttributeAttribute,
+
+    _targetCheck: function (member, classDefinition) {
+        _gpfAttributesCheckAppliedOnlyOnce(member, classDefinition);
+    }
+
+});
+
+gpf.attributes.UniqueAttribute = _gpfAttributesUniqueAttribute;
