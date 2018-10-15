8b5ba24b5c307caf1c0e51fef79fe476981b74d2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-15 15:11:58 -0400

    WIP (#291)

--- test/attributes/attribute.js
+++ test/attributes/attribute.js
@@ -13,4 +13,38 @@ describe("attributes/attribute", function () {
         assert(exceptionCaught instanceof gpf.Error.AbstractClass);
     });
 
+    describe("basic usage", function () {
+
+        var MyAttribute,
+            MyClassWithAttributes,
+            classAttribute,
+            memberAttribute;
+
+        before(function () {
+            MyAttribute = gpf.define({
+                $class: "MyAttribute",
+                $extend: gpf.attributes.Attribute
+            });
+            classAttribute = new MyAttribute();
+            memberAttribute = new MyAttribute();
+            MyClassWithAttributes = gpf.define({
+                $class: "MyClassWithAttributes",
+                $attributes: [classAttribute],
+                "[member]": [memberAttribute],
+                member: "Hello World"
+            });
+        });
+
+        it("provides class constructor information", function () {
+            assert(classAttribute.getClassConstructor() === MyClassWithAttributes);
+            assert(memberAttribute.getClassConstructor() === MyClassWithAttributes);
+        });
+
+        it("provides member name information", function () {
+            assert(classAttribute.getMemberName() === undefined);
+            assert(memberAttribute.getMemberName() === "member");
+        });
+
+    });
+
 });
