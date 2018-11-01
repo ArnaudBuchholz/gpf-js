98a902ba6419e8a3a882c4dcb9e90f1372e7cc8e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-01 13:18:24 -0400

    !yoda style (#280)

--- test/arraylike.js
+++ test/arraylike.js
@@ -5,36 +5,36 @@ describe("arraylike", function () {
     describe("gpf.isArrayLike", function () {
 
         it("checks if an object looks like an array", function () {
-            assert(true === gpf.isArrayLike([]));
+            assert(gpf.isArrayLike([]) === true);
         });
 
         it("recognizes a simulated array", function () {
-            assert(true === gpf.isArrayLike({
+            assert(gpf.isArrayLike({
                 length: 2,
                 "0": 0,
                 "1": 1
-            }));
+            }) === true);
         });
 
         it("recognizes a simulated empty array", function () {
-            assert(true === gpf.isArrayLike({length: 0}));
+            assert(gpf.isArrayLike({length: 0}) === true);
         });
 
         it("recognizes a simulated incomplete array", function () {
-            assert(true === gpf.isArrayLike({
+            assert(gpf.isArrayLike({
                 length: 2,
                 "1": 1
-            }));
+            }) === true);
         });
 
         it("recognizes a simulated non empty array with no items", function () {
-            assert(true === gpf.isArrayLike({
+            assert(gpf.isArrayLike({
                 length: 2
-            }));
+            }) === true);
         });
 
         it("accepts a string as an array like", function () {
-            assert(true === gpf.isArrayLike("abc"));
+            assert(gpf.isArrayLike("abc") === true);
         });
 
         [
@@ -48,16 +48,16 @@ describe("arraylike", function () {
             new Date()
         ].forEach(function (value) {
             it("rejects any non fitting value (" + JSON.stringify(value) + ")", function () {
-                assert(false === gpf.isArrayLike(value));
+                assert(gpf.isArrayLike(value) === false);
             });
         });
 
         it("rejects a simulated array with an invalid length property", function () {
-            assert(false === gpf.isArrayLike({
+            assert(gpf.isArrayLike({
                 length: 2.5,
                 "0": 0,
                 "1": 1
-            }));
+            }) === false);
         });
 
     });
