2d8c3ae2d3e4c1f16a25d4179b86b628ff381940
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-05 23:42:35 -0500

    no-magic-numbers (#280)

--- src/read/browser.js
+++ src/read/browser.js
@@ -10,12 +10,15 @@
 /*global _gpfReadSetImplIf*/ // Set the read implementation if the host matches
 /*#endif*/
 
+var _GPF_READ_HTTP_STATUS_CLASS = 100,
+    _GPF_READ_HTTP_STATUS_CLASS_OK = 2;
+
 function _gpfReadHttp (path) {
     return _gpfHttpRequest({
         method: _GPF_HTTP_METHODS.GET,
         url: path
     }).then(function (response) {
-        if (Math.floor(response.status / 100) !== 2) {
+        if (Math.floor(response.status / _GPF_READ_HTTP_STATUS_CLASS) !== _GPF_READ_HTTP_STATUS_CLASS_OK) {
             throw new Error(response.responseText);
         }
         return response.responseText;
