d594a538c6363bbe22c6401d40ae8ba32ebf33e4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-13 19:04:54 -0500

    no-magic-numbers (#280)

--- grunt/tasks/make.js
+++ grunt/tasks/make.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         "connectIf",
         "concurrent:source"
     ].concat(configuration.files.legacyTest
-        .map(version => `concurrent:legacy${version.substr(0, version.lastIndexOf("."))}`)
+        .map(version => `concurrent:legacy${version}`)
     ).concat([
         "exec:buildDebug",
         "exec:buildRelease",
