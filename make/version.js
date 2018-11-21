c663e428e9c37da168992ed97d181c82fee49622
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-21 15:28:29 -0500

    no-magic-numbers + simplification (#280)

--- make/version.js
+++ make/version.js
@@ -3,115 +3,86 @@
 const
     FIRST_CMDLINE_PARAM = 2,
     GENERIC_ERROR = -1,
-    fs = require("fs"),
+    {readFileSync, writeFileSync} = require("fs"),
     verbose = process.argv[FIRST_CMDLINE_PARAM] === "-verbose",
-    packageJson = JSON.parse(fs.readFileSync("package.json").toString()),
-    sourcesJson = JSON.parse(fs.readFileSync("src/sources.json").toString()),
-    packageVersion = packageJson.releaseVersion,
-    releaseVersion = (/\d+\.\d+\.\d+/).exec(packageVersion).toString();
-
-let bootSrc = fs.readFileSync("src/boot.js").toString();
-
-const
+    packageJson = JSON.parse(readFileSync("package.json").toString()),
+    sourcesJson = JSON.parse(readFileSync("src/sources.json").toString()),
+    packageVersion = packageJson.version,
+    releaseVersion = (/\d+\.\d+\.\d+/).exec(packageVersion).toString(),
+    bootSrc = readFileSync("src/boot.js").toString(),
     VERSION_CAPTURING_GROUP = 1,
     bootVersionMatch = (/_gpfVersion += +"(.*)"/).exec(bootSrc),
     bootVersion = bootVersionMatch[VERSION_CAPTURING_GROUP];
 
 if (verbose) {
-    console.log("Release version: " + releaseVersion);
-    console.log("Package version: " + packageVersion);
-    console.log("Boot    version: " + bootVersion);
+    console.log(`Release version : ${releaseVersion}
+Package version : ${packageVersion}
+Boot version    : ${bootVersion}`);
 }
 
 if (bootVersion !== packageVersion) {
-    bootSrc = bootSrc.replace(bootVersionMatch.toString(), `_gpfVersion = "${packageVersion}"`);
-    fs.writeFileSync("src/boot.js", bootSrc);
-    console.log("src/boot.js updated" + bootVersion);
+    writeFileSync("src/boot.js", bootSrc.replace(bootVersionMatch.toString(), `_gpfVersion = "${packageVersion}"`));
+    console.log(`Version in src/boot.js updated to ${bootVersion}`);
 }
 
 const
     JSDOC_START = "/**",
     JSDOC_END = "*/",
 
-    _injectSinceComment = (source, jsdocComment, lastIndex) => {
+    _injectSince = (jsdocComment) => {
         const
             ONE_LINE_ONLY = 1,
             BEFORE_LAST = -1,
-            KEEP = 0;
-        let newCommentLines = jsdocComment.split("\n");
-        if (newCommentLines.length === ONE_LINE_ONLY) {
-            // Need to find current indentation
+            START = 0,
+            KEEP = 0,
+            lines = jsdocComment.split("\n"),
+            lastLine = [...lines].pop(),
+            firstLineIsEmpty = jsdocComment.startsWith("\n");
+        if (firstLineIsEmpty) {
+            lines.shift();
+        }
+        if (lines.length === ONE_LINE_ONLY) {
             const
-                pos = source.content.substr(0, lastIndex).lastIndexOf("\n") + 1,
-                len = lastIndex - jsdocComment.length - pos;
-            return [
-                JSDOC_START,
-                ` * ${jsdocComment.substr(JSDOC_START.length,
-                    jsdocComment.length - (JSDOC_START + JSDOC_END).length).trim()}`,
-                ` * @since ${releaseVersion}`,
-                " " + JSDOC_END
-            ].join(`\n${source.content.substr(pos, len)}`);
+                jsdocStart = lastLine.indexOf(JSDOC_START),
+                jsdocAfterStart = jsdocStart + JSDOC_START.length,
+                jsdocEnd = lastLine.indexOf(JSDOC_END),
+                indent = lastLine.substring(START, jsdocStart);
+            lines.splice(START, lines.length,
+                `${indent}${JSDOC_START}`,
+                `${indent} * ${lastLine.substring(jsdocAfterStart, jsdocEnd).trim()}`,
+                `${indent} * @since ${releaseVersion}`,
+                `${indent} ${JSDOC_END}`
+            );
+        } else {
+            // Multi line comment
+            lines.splice(BEFORE_LAST, KEEP, lastLine.replace(JSDOC_END, `* @since ${releaseVersion}`));
         }
-        // Multi line comment
-        newCommentLines.splice(BEFORE_LAST, KEEP,
-            newCommentLines[newCommentLines.length - 1].replace(JSDOC_END, `* @since ${releaseVersion}`));
-        return newCommentLines.join("\n");
+        if (firstLineIsEmpty) {
+            lines.unshift("");
+        }
+        return lines.join("\n");
     },
 
-    _removeSinceComment = (jsdocComment) => jsdocComment
-        .split("\n")
-        .filter(line => !line.includes("@since"))
-        .join("\n"),
-
-    _processCommentForSince = (source, jsdocCommentMatch, lastIndex) => {
-        let jsdocComment,
-            hasSince,
-            newComment;
-        jsdocComment = jsdocCommentMatch[0];
-        if (jsdocComment.includes("@lends")) {
-            return false;
+    _processJsdocComment = (jsdocComment) => {
+        if (jsdocComment.includes("@lends") || jsdocComment.includes("@since")) {
+            return jsdocComment; // ignore
         }
-        newComment = undefined;
-        hasSince = jsdocComment.includes("@since");
-        if (source.load !== false) {
-            if (!hasSince) {
-                newComment = _injectSinceComment(source, jsdocComment, lastIndex);
-            }
-        } else if (hasSince) {
-            newComment = _removeSinceComment(jsdocComment);
-        }
-        if (newComment) {
-            source.content = source.content.replace(jsdocComment, newComment);
-            return true;
-        }
-        return false;
+        return _injectSince(jsdocComment);
     },
-
-    _reJsdocComment = /(?:\/\*\*(?:[^*]|\s|\*[^/])*\*\/)/g,
 
     _handleSinceInSource = (source) => {
-        let jsdocCommentMatch,
-            modified = false,
-            fileName = `src/${source.name}.js`;
+        const fileName = `src/${source.name}.js`;
         try {
-            source.content = fs.readFileSync(fileName).toString();
+            source.content = readFileSync(fileName).toString();
         } catch (e) {
-            if (source.load !== false) {
-                console.error(e);
-                process.exit(GENERIC_ERROR);
-            }
+            console.error(e);
+            process.exit(GENERIC_ERROR);
         }
-        _reJsdocComment.lastIndex = 0;
-        jsdocCommentMatch = _reJsdocComment.exec(source.content);
-        while (jsdocCommentMatch) {
-            modified = _processCommentForSince(source, jsdocCommentMatch, _reJsdocComment.lastIndex) || modified;
-            jsdocCommentMatch = _reJsdocComment.exec(source.content);
-        }
-        if (modified) {
-            fs.writeFileSync(fileName, source.content);
+        const modified = source.content.replace(/(^|\n)( |\t)*\/\*\*([^*]|\s|\*[^/])*\*\//g, _processJsdocComment);
+        if (modified !== source.content) {
+            writeFileSync(fileName, modified);
             console.log(fileName + " updated");
         }
     };
 
-sourcesJson[0].load = true; // boot must be altered too
 sourcesJson.forEach(_handleSinceInSource);
