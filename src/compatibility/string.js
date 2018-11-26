7e2518e7c7994a8655b41398c26a0ef9be27ab5a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-26 13:03:47 -0500

    Implements padStart and padEnd to remove magic numbers (#280)

--- src/compatibility/string.js
+++ src/compatibility/string.js
@@ -11,6 +11,23 @@
 
 var _GPF_COMPATIBILITY_STRING_OPTIONAL_PARAM_INDEX = 1;
 
+function _gpfStringGetOptionalParam (args, defaultValue) {
+    var value = args[_GPF_COMPATIBILITY_STRING_OPTIONAL_PARAM_INDEX];
+    if (!value) {
+        return defaultValue;
+    }
+    return value;
+}
+
+var _GPF_COMPATIBILITY_STRING_ARRAY_LENGTH_OFFSET = 1;
+
+function _gpfStringBuildPaddingString (length, targetLength, padString) {
+    if (length < targetLength) {
+        return new Array(targetLength - length + _GPF_COMPATIBILITY_STRING_ARRAY_LENGTH_OFFSET).join(padString);
+    }
+    return "";
+}
+
 _gpfCompatibilityInstallMethods("String", {
     on: String,
 
@@ -18,19 +35,31 @@ _gpfCompatibilityInstallMethods("String", {
 
         // Introduced with ECMAScript 2015
         endsWith: function (search) {
-            var len = Math.min(arguments[_GPF_COMPATIBILITY_STRING_OPTIONAL_PARAM_INDEX] || this.length, this.length);
+            var len = Math.min(_gpfStringGetOptionalParam(arguments, this.length), this.length);
             return this.substring(len - search.length, len) === search;
         },
 
         // Introduced with ECMAScript 2015
         includes: function (search) {
-            var position = arguments[_GPF_COMPATIBILITY_STRING_OPTIONAL_PARAM_INDEX] || _GPF_START;
+            var position = _gpfStringGetOptionalParam(arguments, _GPF_START);
             return this.indexOf(search, position) !== _GPF_NOT_FOUND;
+        },
+
+        padEnd: function (targetLength) {
+            var padString = _gpfStringGetOptionalParam(arguments, " "),
+                padding = _gpfStringBuildPaddingString(this.length, targetLength, padString);
+            return this + padding;
+        },
+
+        padStart: function (targetLength) {
+            var padString = _gpfStringGetOptionalParam(arguments, " "),
+                padding = _gpfStringBuildPaddingString(this.length, targetLength, padString);
+            return padding + this;
         },
 
         // Introduced with ECMAScript 2015
         startsWith: function (search) {
-            var position = arguments[_GPF_COMPATIBILITY_STRING_OPTIONAL_PARAM_INDEX] || _GPF_START;
+            var position = _gpfStringGetOptionalParam(arguments, _GPF_START);
             return this.substr(position, search.length) === search;
         },
 
