c2e875c553d1072a5a6390a015040b0d1abbabd5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-12-06 13:17:58 -0500

    Preparing tests #296

---
+++ test/define/import.es6.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class MyAbstractClass {
+    $attributes: [],
+    $abstract: true,
+
+    _name: ""
+
+}
+
+class MyClass extends MyAbstractClass {
+
+    constructor () {
+
+    }
+
+}
