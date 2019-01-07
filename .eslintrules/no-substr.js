8a371820a0c3cbbe5659678baa8865530f8265cb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-07 13:02:07 -0500

    Add fake documentation URL (may come later) (#293)

--- .eslintrules/no-substr.js
+++ .eslintrules/no-substr.js
@@ -5,6 +5,7 @@
 module.exports = {
     meta: {
         docs: {
+            url: "https://github.com/ArnaudBuchholz/gpf-js/blob/master/.eslintrules/no-substr.js",
             description: "Forbid the use of String.prototype.substr API",
             category: "Custom rules",
             recommended: false
