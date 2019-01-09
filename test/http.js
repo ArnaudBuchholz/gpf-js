8dd5d37770efba48573bd6ce14a5a1544c3610b0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-09 16:01:42 -0500

    linter warnings (#298)

--- test/http.js
+++ test/http.js
@@ -21,150 +21,156 @@ describe("http", function () {
             baseUrl = "http://localhost:" + config.httpPort + "/echo/?";
         });
 
-        it("allows the GET operation", function (done) {
-            gpf.http.request({
-                method: gpf.http.methods.get,
-                url: baseUrl + "status=200"
-
-            }).then(function (response) {
-                assert(response.status === 200);
-                var echoed = JSON.parse(response.responseText);
-                assert(echoed.method === "GET");
-                assert(echoed.url === "/echo/?status=200");
-                done();
-            })["catch"](done);
-        });
-
-        it("offers the GET shortcut (using string parameter)", function (done) {
-            gpf.http.get(baseUrl + "status=200").then(function (response) {
-                assert(response.status === 200);
-                var echoed = JSON.parse(response.responseText);
-                assert(echoed.method === "GET");
-                assert(echoed.url === "/echo/?status=200");
-                done();
-            })["catch"](done);
-        });
-
-        it("offers the GET shortcut (using object parameter)", function (done) {
-            gpf.http.get({
-                url: baseUrl + "status=200&response=Hello%20World"
-            }).then(function (response) {
-                assert(response.status === 200);
-                assert(response.responseText === "Hello World");
-                done();
-            })["catch"](done);
-        });
-
-        it("succeeds when the HTTP request fails", function (done) {
-            gpf.http.get(baseUrl + "status=500").then(function (response) {
-                assert(response.status === 500);
-                done();
-            })["catch"](done);
-        });
-
-        it("succeeds when the HTTP response is empty", function (done) {
-            gpf.http.get(baseUrl + "response=").then(function (response) {
-                assert(response.responseText === "");
-                done();
-            })["catch"](done);
-        });
-
-        it("forwards request headers", function (done) {
-            gpf.http.request({
-                method: gpf.http.methods.get,
-                url: baseUrl + "status=200",
-                headers: {
-                    "x-test": "Value"
-                }
-
-            }).then(function (response) {
-                assert(response.status === 200);
-                assert(response.headers["x-test"] === "Value");
-                done();
-
-            })["catch"](done);
-        });
-
-        it("gives access to response headers", function (done) {
-            gpf.http.request({
-                method: gpf.http.methods.get,
-                url: baseUrl + "status=200&headers={\"x-test-1\":\"OK\",\"x-test-2\":123}"
-
-            }).then(function (response) {
-                assert(response.status === 200);
-                assert(response.headers["x-test-1"] === "OK");
-                assert(response.headers["x-test-2"] === "123");
-                done();
-            })["catch"](done);
-        });
-
-        [
-            "post",
-            "put",
-            "options",
-            "delete",
-            "head"
-
-        ].forEach(function (method, index) {
-
-            var uppercasedMethod = method.toUpperCase(),
-                body,
-                checkResponse;
-
-            if (index < 2) { // post & put
-                body = "Hello World";
-            }
-
-            if (method === "head") {
-                checkResponse = function () {}; // No response expected
-            } else {
-                checkResponse = function (responseText) {
-                    var echoed = JSON.parse(responseText);
-                    assert(echoed.method === uppercasedMethod);
-                    assert(echoed.url === "/echo/?status=200");
-                    if (body) {
-                        assert(echoed.body === body);
-                    } else {
-                        assert(!echoed.body);
-                    }
-                };
-            }
-
-            it("allows the " + uppercasedMethod + " operation", function (done) {
+        describe("basics", function () {
+            it("allows the GET operation", function (done) {
                 gpf.http.request({
-                    method: gpf.http.methods[method],
-                    url: baseUrl + "status=200",
-                    data: body
+                    method: gpf.http.methods.get,
+                    url: baseUrl + "status=200"
 
                 }).then(function (response) {
                     assert(response.status === 200);
-                    checkResponse(response.responseText);
+                    var echoed = JSON.parse(response.responseText);
+                    assert(echoed.method === "GET");
+                    assert(echoed.url === "/echo/?status=200");
                     done();
                 })["catch"](done);
             });
 
-            it("offers the " + uppercasedMethod + " shortcut", function (done) {
-                gpf.http[method](baseUrl + "status=200", body).then(function (response) {
+            it("offers the GET shortcut (using string parameter)", function (done) {
+                gpf.http.get(baseUrl + "status=200").then(function (response) {
                     assert(response.status === 200);
-                    checkResponse(response.responseText);
+                    var echoed = JSON.parse(response.responseText);
+                    assert(echoed.method === "GET");
+                    assert(echoed.url === "/echo/?status=200");
                     done();
                 })["catch"](done);
             });
 
+            it("offers the GET shortcut (using object parameter)", function (done) {
+                gpf.http.get({
+                    url: baseUrl + "status=200&response=Hello%20World"
+                }).then(function (response) {
+                    assert(response.status === 200);
+                    assert(response.responseText === "Hello World");
+                    done();
+                })["catch"](done);
+            });
+
+            it("succeeds when the HTTP request fails", function (done) {
+                gpf.http.get(baseUrl + "status=500").then(function (response) {
+                    assert(response.status === 500);
+                    done();
+                })["catch"](done);
+            });
+
+            it("succeeds when the HTTP response is empty", function (done) {
+                gpf.http.get(baseUrl + "response=").then(function (response) {
+                    assert(response.responseText === "");
+                    done();
+                })["catch"](done);
+            });
         });
 
-        it("allows any verb (MERGE)", function (done) {
-            gpf.http.request({
-                method: "merge",
-                url: baseUrl + "status=200"
+        describe("headers", function () {
+            it("forwards request headers", function (done) {
+                gpf.http.request({
+                    method: gpf.http.methods.get,
+                    url: baseUrl + "status=200",
+                    headers: {
+                        "x-test": "Value"
+                    }
 
-            }).then(function (response) {
-                assert(response.status === 200);
-                var echoed = JSON.parse(response.responseText);
-                assert(echoed.method === "MERGE");
-                assert(echoed.url === "/echo/?status=200");
-                done();
-            })["catch"](done);
+                }).then(function (response) {
+                    assert(response.status === 200);
+                    assert(response.headers["x-test"] === "Value");
+                    done();
+
+                })["catch"](done);
+            });
+
+            it("gives access to response headers", function (done) {
+                gpf.http.request({
+                    method: gpf.http.methods.get,
+                    url: baseUrl + "status=200&headers={\"x-test-1\":\"OK\",\"x-test-2\":123}"
+
+                }).then(function (response) {
+                    assert(response.status === 200);
+                    assert(response.headers["x-test-1"] === "OK");
+                    assert(response.headers["x-test-2"] === "123");
+                    done();
+                })["catch"](done);
+            });
+        });
+
+        describe("methods", function () {
+            [
+                "post",
+                "put",
+                "options",
+                "delete",
+                "head"
+
+            ].forEach(function (method, index) {
+
+                var uppercasedMethod = method.toUpperCase(),
+                    body,
+                    checkResponse;
+
+                if (index < 2) { // post & put
+                    body = "Hello World";
+                }
+
+                if (method === "head") {
+                    checkResponse = function () {}; // No response expected
+                } else {
+                    checkResponse = function (responseText) {
+                        var echoed = JSON.parse(responseText);
+                        assert(echoed.method === uppercasedMethod);
+                        assert(echoed.url === "/echo/?status=200");
+                        if (body) {
+                            assert(echoed.body === body);
+                        } else {
+                            assert(!echoed.body);
+                        }
+                    };
+                }
+
+                it("allows the " + uppercasedMethod + " operation", function (done) {
+                    gpf.http.request({
+                        method: gpf.http.methods[method],
+                        url: baseUrl + "status=200",
+                        data: body
+
+                    }).then(function (response) {
+                        assert(response.status === 200);
+                        checkResponse(response.responseText);
+                        done();
+                    })["catch"](done);
+                });
+
+                it("offers the " + uppercasedMethod + " shortcut", function (done) {
+                    gpf.http[method](baseUrl + "status=200", body).then(function (response) {
+                        assert(response.status === 200);
+                        checkResponse(response.responseText);
+                        done();
+                    })["catch"](done);
+                });
+
+            });
+
+            it("allows any verb (MERGE)", function (done) {
+                gpf.http.request({
+                    method: "merge",
+                    url: baseUrl + "status=200"
+
+                }).then(function (response) {
+                    assert(response.status === 200);
+                    var echoed = JSON.parse(response.responseText);
+                    assert(echoed.method === "MERGE");
+                    assert(echoed.url === "/echo/?status=200");
+                    done();
+                })["catch"](done);
+            });
         });
 
         if (gpf.hosts.browser !== gpf.host()) {
