4a364dec0204e8607db2ee634b82829fd1a755c2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-26 11:01:13 -0500

    ES6 specific tests (#304)

---
+++ test/define.es6.js
@@ -0,0 +1,82 @@
+"use strict";
+
+/*jshint esversion: 6 */
+/*eslint-env es6*/
+
+describe("define.es6", function () {
+
+    describe("gpf.define", function () {
+
+        describe("Class definition", function () {
+
+            describe("Implementation", function () {
+
+                describe("Subclassing an ES6 class", function () {
+
+                    class A {
+                        constructor (value) {
+                            this._a = "A";
+                            this._value = value;
+                        }
+                        a () {
+                            return this._a;
+                        }
+                        value () {
+                            return this._value;
+                        }
+                    }
+
+                    class B extends A {
+                        constructor (value) {
+                            super(value);
+                            this._b = "A";
+                        }
+                        b () {
+                            return this._b;
+                        }
+                    }
+
+                    let C, c;
+
+                    before(function () {
+                        C = gpf.define({
+                            $class: "C",
+                            $extend: B,
+                            "constructor": function (value) {
+                                this.$super(value);
+                            },
+                            c: function () {
+                                return this.$super.a() + this.$super.b();
+                            }
+                        });
+
+                        c = new C(3475);
+                    });
+
+                    it("handles instanceof", function () {
+                        assert(c instanceof A);
+                        assert(c instanceof B);
+                        assert(c instanceof C);
+                    });
+
+                    it("calls the constructor function", function () {
+                        assert(c.value() === 3475);
+                    });
+
+                    it("offers a way to call named base method", function () {
+                        assert(c.c() === "AB");
+                    });
+
+                    it("does not leak $super", function () {
+                        assert(undefined === c.$super);
+                    });
+
+                });
+
+            });
+
+        });
+
+    });
+
+});
