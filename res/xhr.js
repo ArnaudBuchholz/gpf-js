e13ab1b248f2d66629686ad16e8d4268a29ac78e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-27 23:37:30 -0500

    Adds synchronous requests (#304)

--- res/xhr.js
+++ res/xhr.js
@@ -8,24 +8,45 @@
         HTTP_STATUS_CLASS_FACTOR = 100,
         HTTP_STATUS_CLASS_OK = 2;
 
-    function xhrSend (request, method, data) {
-        var result = new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.open(method, request._url);
-            Object.keys(request._headers).forEach(function (headerName) {
-                xhr.setRequestHeader(headerName, request._headers[headerName]);
-            });
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === READYSTATE_DONE) {
-                    if (Math.floor(xhr.status / HTTP_STATUS_CLASS_FACTOR) === HTTP_STATUS_CLASS_OK) {
-                        resolve(xhr.responseText);
-                    } else {
-                        reject(xhr.statusText);
-                    }
-                }
-            };
-            xhr.send(data);
+    function noop () {}
+
+    function xhrSucceeded (xhr) {
+        return Math.floor(xhr.status / HTTP_STATUS_CLASS_FACTOR) === HTTP_STATUS_CLASS_OK;
+    }
+
+    function xhrSend (request, resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(request.method, request._url, !request._synchronous);
+        Object.keys(request._headers).forEach(function (headerName) {
+            xhr.setRequestHeader(headerName, request._headers[headerName]);
         });
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === READYSTATE_DONE) {
+                if (xhrSucceeded(xhr)) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject(xhr.statusText);
+                }
+            }
+        };
+        xhr.send(request.data);
+        return xhr;
+    }
+
+    function xhrExecute (request, method, data) {
+        request.method = method;
+        request.data = data;
+        var result;
+        if (request._synchronous) {
+            result = xhrSend(request, noop, noop);
+            result.then = function (callback) {
+                callback(result.responseText);
+            };
+        } else {
+            result = new Promise(function (resolve, reject) {
+                xhrSend(request, resolve, reject);
+            });
+        }
         result.asJson = function () {
             return result.then(function (responseText) {
                 return JSON.parse(responseText);
@@ -38,17 +59,24 @@
      * XHR request parameters handler
      *
      * @param {String} url URL to request
+     * @param {Boolean} [synchronous=false] Synchronous request
      * @constructor
      * @private
      */
-    function XhrRequest (url) {
+    function XhrRequest (url, synchronous) {
         this._url = url;
+        if (synchronous === true) {
+            this._synchronous = true;
+        }
     }
 
     XhrRequest.prototype = {
 
         /** URL of the request */
         _url: "",
+
+        /** Makes a synchronous request */
+        _synchronous: false,
 
         /** Request headers */
         _headers: {},
@@ -79,17 +107,17 @@
             httpVerb = methodName.toUpperCase();
         if (isDataExpected) {
             XhrRequest.prototype[methodName] = function (data) {
-                return xhrSend(this, httpVerb, data);
+                return xhrExecute(this, httpVerb, data);
             };
         } else {
             XhrRequest.prototype[methodName] = function () {
-                return xhrSend(this, httpVerb);
+                return xhrExecute(this, httpVerb);
             };
         }
     });
 
-    window.xhr = function (url) {
-        return new XhrRequest(url);
+    window.xhr = function (url, synchronous) {
+        return new XhrRequest(url, synchronous);
     };
 
 }());
