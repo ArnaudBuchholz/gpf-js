157131a8b62cb262a80789e0f503249a5ee51bce
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-27 23:37:54 -0500

    Adds include and features (#304)

--- test/host/web_loader.js
+++ test/host/web_loader.js
@@ -1,4 +1,5 @@
 /*eslint strict: [2, "function"]*/ // IIFE form
+/*eslint-disable func-style*/ // So that everything is grouped in one var
 /*global xhr*/
 (function () {
     "use strict";
@@ -38,208 +39,223 @@
                 }
             }
         },
-        _tests;
+        _tests,
 
-    function _waitForTestCases (testFiles, callback) {
-        var testFileURL,
-            sourceURL;
-        function _testCaseLoaded (testCaseSource) {
+        _evalTestCase = function (testFileURL, testCaseSource) {
+            var sourceURL = window.location.toString().split("/test/")[0] + "/test/" + testFileURL;
             /*jshint -W061*/
             eval("//# sourceURL=" + sourceURL + "\n" + testCaseSource); //eslint-disable-line no-eval
             /*jshint +W061*/
-            _waitForTestCases(testFiles, callback);
-        }
-        if (testFiles.length) {
-            testFileURL = testFiles.shift();
-            sourceURL = window.location.toString().split("/test/")[0] + "/test/" + testFileURL;
-            xhr(window.gpfTestsPath + testFileURL).get()
-                .then(_testCaseLoaded, _error);
-            return;
-        }
-        // Everything is loaded
-        callback();
-    }
+        },
 
-    function _detectLegacy () {
-        var legacyVersion = (/version=([0-9.]+)/).exec(window.location.search);
-        if (legacyVersion) {
-            return legacyVersion[1];
-        }
-    }
-
-    function _patchLegacy (version) {
-        // gpf is loaded
-        return xhr("/test/legacy/legacy.json").get().asJson()
-            .then(function (legacy) {
-                return legacy.filter(function (item) {
-                    return item.before > version;
-                });
-            })
-            .then(function (legacy) {
-                var
-                    _describes = [],
-                    _describe = window.describe,
-                    _it = window.it;
-                window.describe = function (label) {
-                    var issue;
-                    legacy.every(function (item) {
-                        if (item.ignore.some(function (ignore) {
-                            return ignore.describe === label && !ignore.it;
-
-                        })) {
-                            issue = item.issue;
-                            return false;
-                        }
-                        return true;
-                    });
-                    if (issue) {
-                        _log("describe(\"" + label + "\", ...) ignored, see issue #" + issue);
-                    } else {
-                        _describes.unshift(label);
-                        _describe.apply(this, arguments);
-                        _describes.shift();
-                    }
-                };
-                window.it = function (label) {
-                    var issue;
-                    legacy.every(function (item) {
-                        if (item.ignore.some(function (ignore) {
-                            return ignore.describe === _describes[0] && ignore.it === label;
-
-                        })) {
-                            issue = item.issue;
-                            return false;
-                        }
-                        return true;
-                    });
-                    if (issue) {
-                        _log("it(\"" + label + "\", ...) ignored, see issue #" + issue);
-                    } else {
-                        _it.apply(this, arguments);
-                    }
-                };
-            });
-    }
-
-    // Actively wait for GPF to be loaded
-    function _waitForLoad (callback) {
-        // Check if the GPF library is loaded
-        if (typeof gpf === "undefined") {
-            if (--_MAX_WAIT) {
-                setTimeout(function () {
-                    _waitForLoad(callback);
-                }, 100);
-            } else {
-                _error("Unable to load GPF");
+        _waitForTestCases = function (testFiles, callback) {
+            var testFileURL;
+            function _testCaseLoaded (testCaseSource) {
+                _evalTestCase(testFileURL, testCaseSource);
+                _waitForTestCases(testFiles, callback);
             }
-            return;
-        }
-        // Legacy test case?
-        var legacy = _detectLegacy();
-        if (legacy) {
-            delete gpf.internals;
-            _patchLegacy(legacy)
-                .then(function () {
-                    _waitForTestCases(["legacy/" + legacy + ".js"], callback);
-                });
-            return;
-        }
-        // GPF Library being loaded, Promises are existing (either natively or through compatiblity)
-        var getTestFiles;
-        if (_tests) {
-            getTestFiles = Promise.resolve(_tests);
-        } else {
-            // Test files from sources.json
-            getTestFiles = xhr(gpfSourcesPath + "sources.json").get().asJson()
-                .then(function (sources) {
-                    return sources
-                        .filter(function (source) {
-                            return source.load !== false && source.test !== false;
-                        })
-                        .map(function (source) {
-                            return source.name;
+            if (testFiles.length) {
+                testFileURL = testFiles.shift();
+                xhr(window.gpfTestsPath + testFileURL).get()
+                    .then(_testCaseLoaded, _error);
+                return;
+            }
+            // Everything is loaded
+            callback();
+        },
+
+        _detectLegacy = function () {
+            var legacyVersion = (/version=([0-9.]+)/).exec(window.location.search);
+            if (legacyVersion) {
+                return legacyVersion[1];
+            }
+        },
+
+        _patchLegacy = function (version) {
+            // gpf is loaded
+            return xhr("/test/legacy/legacy.json").get().asJson()
+                .then(function (legacy) {
+                    return legacy.filter(function (item) {
+                        return item.before > version;
+                    });
+                })
+                .then(function (legacy) {
+                    var
+                        _describes = [],
+                        _describe = window.describe,
+                        _it = window.it;
+                    window.describe = function (label) {
+                        var issue;
+                        legacy.every(function (item) {
+                            if (item.ignore.some(function (ignore) {
+                                return ignore.describe === label && !ignore.it;
+
+                            })) {
+                                issue = item.issue;
+                                return false;
+                            }
+                            return true;
                         });
+                        if (issue) {
+                            _log("describe(\"" + label + "\", ...) ignored, see issue #" + issue);
+                        } else {
+                            _describes.unshift(label);
+                            _describe.apply(this, arguments);
+                            _describes.shift();
+                        }
+                    };
+                    window.it = function (label) {
+                        var issue;
+                        legacy.every(function (item) {
+                            if (item.ignore.some(function (ignore) {
+                                return ignore.describe === _describes[0] && ignore.it === label;
+
+                            })) {
+                                issue = item.issue;
+                                return false;
+                            }
+                            return true;
+                        });
+                        if (issue) {
+                            _log("it(\"" + label + "\", ...) ignored, see issue #" + issue);
+                        } else {
+                            _it.apply(this, arguments);
+                        }
+                    };
                 });
-        }
-        getTestFiles.then(function (files) {
-            _waitForTestCases(files
-                .map(function (name) {
-                    return name + ".js";
-                }), callback);
-        });
-    }
+        },
 
-    function buildFlavorSetup (flavor) {
-        return function () {
-            _tests = gpfFlavors[flavor].tests;
+        // Actively wait for GPF to be loaded
+        _waitForLoad = function (callback) {
+            // Check if the GPF library is loaded
+            if (typeof gpf === "undefined") {
+                if (--_MAX_WAIT) {
+                    setTimeout(function () {
+                        _waitForLoad(callback);
+                    }, 100);
+                } else {
+                    _error("Unable to load GPF");
+                }
+                return;
+            }
+            // Legacy test case?
+            var legacy = _detectLegacy();
+            if (legacy) {
+                delete gpf.internals;
+                _patchLegacy(legacy)
+                    .then(function () {
+                        _waitForTestCases(["legacy/" + legacy + ".js"], callback);
+                    });
+                return;
+            }
+            // GPF Library being loaded, Promises are existing (either natively or through compatiblity)
+            var getTestFiles;
+            if (_tests) {
+                getTestFiles = Promise.resolve(_tests);
+            } else {
+                // Test files from sources.json
+                getTestFiles = xhr(gpfSourcesPath + "sources.json").get().asJson()
+                    .then(function (sources) {
+                        return sources
+                            .filter(function (source) {
+                                return source.load !== false && source.test !== false;
+                            })
+                            .map(function (source) {
+                                return source.name;
+                            });
+                    });
+            }
+            getTestFiles.then(function (files) {
+                _waitForTestCases(files
+                    .map(function (name) {
+                        return name + ".js";
+                    }), callback);
+            });
+        },
+
+        buildFlavorSetup = function (flavor) {
+            return function () {
+                _tests = gpfFlavors[flavor].tests;
+            };
+        },
+
+        _addFlavors = function () {
+            var flavor;
+            for (flavor in gpfFlavors) {
+                if (gpfFlavors.hasOwnProperty(flavor)) {
+                    _versions["flavor:" + flavor] = {
+                        label: "\"" + flavor + "\" flavor",
+                        path: "../build/gpf-" + flavor + ".js",
+                        setup: buildFlavorSetup(flavor)
+                    };
+                }
+            }
+        },
+
+        _getVersionParameters = function () {
+            var locationSearch = location.search,
+                version;
+            if (typeof gpfFlavors !== "undefined") {
+                _addFlavors();
+            }
+            for (version in _versions) {
+                if (_versions.hasOwnProperty(version) && locationSearch.indexOf(version) !== -1) {
+                    return _versions[version];
+                }
+            }
+        },
+
+        _detectVersion = function () {
+            var versionParameters = _getVersionParameters(),
+                version,
+                path = "boot.js?map";
+            if (versionParameters) {
+                version = versionParameters.label;
+                path = versionParameters.path || "boot.js";
+                versionParameters.setup();
+            } else {
+                version = "sources";
+            }
+            _log("Using " + version + " version");
+            return path;
+        },
+
+        _loadVersion = function (callback) {
+            var head = document.getElementsByTagName("head")[0],
+                script = document.createElement("script"),
+                version = _detectVersion();
+            script.src = gpfSourcesPath + version;
+            script.language = "javascript";
+            head.insertBefore(script, head.firstChild);
+            _waitForLoad(callback);
+        },
+
+        _setupInclude = function () {
+            window.include = function (source) {
+                var path = window.gpfTestsPath + source + ".js";
+                xhr(path, true).get()
+                    .then(function (responseText) {
+                        _evalTestCase(path, responseText);
+                    });
+            };
+        },
+
+        _setupConfig = function () {
+            window.config = {};
+            config.httpPort = parseInt(location.port, 10);
+            var msie = (/(msie|trident)/i).test(navigator.userAgent);
+            if (msie) {
+                config.timerResolution = 5; // Found out that this is the only way to safely run tests
+            }
+            include("host/features");
         };
-    }
-
-    function _addFlavors () {
-        var flavor;
-        for (flavor in gpfFlavors) {
-            if (gpfFlavors.hasOwnProperty(flavor)) {
-                _versions["flavor:" + flavor] = {
-                    label: "\"" + flavor + "\" flavor",
-                    path: "../build/gpf-" + flavor + ".js",
-                    setup: buildFlavorSetup(flavor)
-                };
-            }
-        }
-    }
-
-    function _getVersionParameters () {
-        var locationSearch = location.search,
-            version;
-        if (typeof gpfFlavors !== "undefined") {
-            _addFlavors();
-        }
-        for (version in _versions) {
-            if (_versions.hasOwnProperty(version) && locationSearch.indexOf(version) !== -1) {
-                return _versions[version];
-            }
-        }
-    }
-
-    function _detectVersion () {
-        var versionParameters = _getVersionParameters(),
-            version,
-            path = "boot.js?map";
-        if (versionParameters) {
-            version = versionParameters.label;
-            path = versionParameters.path || "boot.js";
-            versionParameters.setup();
-        } else {
-            version = "sources";
-        }
-        _log("Using " + version + " version");
-        return path;
-    }
-
-    function _loadVersion (callback) {
-        var head = document.getElementsByTagName("head")[0],
-            script = document.createElement("script"),
-            version = _detectVersion();
-        script.src = gpfSourcesPath + version;
-        script.language = "javascript";
-        head.insertBefore(script, head.firstChild);
-        _waitForLoad(callback);
-    }
-
-    function _setupConfig () {
-        window.config = {};
-        config.httpPort = parseInt(location.port, 10);
-        var msie = (/(msie|trident)/i).test(navigator.userAgent);
-        if (msie) {
-            config.timerResolution = 5; // Found out that this is the only way to safely run tests
-        }
-    }
 
     /*
      * Load the GPF framework and test cases.
      * When done, execute the callback.
      */
     window.load = function (callback) {
+        _setupInclude();
         _setupConfig();
         _loadVersion(callback);
     };
