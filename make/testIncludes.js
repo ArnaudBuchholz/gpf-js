70a921c58bc521e8542ae74db368af909ddb7a95
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-28 23:03:10 -0500

    Consolidate included test files (#304)

--- make/testIncludes.js
+++ make/testIncludes.js
@@ -5,7 +5,28 @@ const
     path = require("path"),
     ConfigFile = require("./configFile.js"),
     configFile = new ConfigFile(true),
-    outPath = path.join(__dirname, "../tmp/build/testIncludes.js");
+    uglify = require("uglify-es"),
+    options = {
+        parse: {
+            ecma: 8
+        },
+        mangle: {
+          keep_classnames: true
+        },
+        output: {
+          ecma             : 8,
+          ie8              : false,
+          inline_script    : true,
+          keep_quoted_props: true
+        }
+    },
+    outPath = path.join(__dirname, "../tmp/build/testIncludes.js"),
+    includes = {};
+
+configFile.content.files.testInclude.forEach(source => {
+    includes[source.match(/^test\/(.*)\.js$/)[1]] =
+      uglify.minify(fs.readFileSync(path.join(__dirname, `../${source}`)).toString(), options).code;
+});
 
 let
     testIncludes = `(function () {
@@ -13,8 +34,11 @@ let
 
     var safeFunc = Function,
         context = safeFunc("return this;")(),
-        ;
+        _includes = ${JSON.stringify(includes, undefined, 8)};
 
+    context.include = function (source) {
+        eval(_includes[source]);
+    };
 
 }());`;
 
