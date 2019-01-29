a6b544a821011ad43fba18447c45a9aee5b5f3cd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-29 15:55:39 -0500

    Ensures the features can be enforced to true or false (#304)

--- test/host/features.js
+++ test/host/features.js
@@ -1,19 +1,29 @@
 "use strict";
 (function () {
-
     /*global config*/
-    var Func = Function;
 
-    var es6class;
-    try {
-        new Func("class A {}")();
-        es6class = true;
-    } catch (e) {
-        es6class = false;
+    var Func = Function,
+        context = new Func("return this;")(),
+        features = {
+            es6class: "class A {}"
+        };
+
+    for (var feature in features) {
+        if (features.hasOwnProperty(feature)) {
+            var detection = features[feature],
+                detected = context["features:" + feature];
+            if (detected === undefined) {
+                try {
+                    new Func(detection)();
+                    detected = true;
+                } catch (e) {
+                    detected = false;
+                }
+            }
+            features[feature] = detected;
+        }
     }
 
-    config.features = {
-        es6class: es6class
-    };
+    config.features = features;
 
 }());
