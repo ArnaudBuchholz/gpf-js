7cfed30db8824d06720876bb892e0b1d250ad76a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-02 16:08:23 -0500

    Improves quality (#240)

--- src/compatibility/base64/btoa.js
+++ src/compatibility/base64/btoa.js
@@ -1,37 +1,52 @@
 /**
  * @file btoa polyfill
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfBase64*/
+/*global _GPF_12_BITS*/ // 12
+/*global _GPF_18_BITS*/ // 18
+/*global _GPF_1_BYTE*/ // 1
+/*global _GPF_2_BYTES*/ // 2
+/*global _GPF_6_BITS*/ // 6
+/*global _GPF_6_BITS_MASK*/ // 63
+/*global _GPF_START*/ // 0
+/*global _gpfBase64*/ // Base64 encoding string
+/*global _gpfMaxUnsignedByte*/ // 255
 /*exported _gpfBtoa*/ // btoa polyfill
 /*#endif*/
 
-// https://github.com/MaxArt2501/base64-js/blob/master/base64.js
+var _GPF_BTOA_MAX_PADDING = 3;
 
 function _gpfBtoaCheck (stringToEncode, index) {
     var value = stringToEncode.charCodeAt(index);
-    if (value > 255) {
+    if (value > _gpfMaxUnsignedByte) {
         throw new TypeError("The string to be encoded contains characters outside of the Latin1 range.");
     }
     return value;
 }
 
 function _gpfBtoa (stringToEncode) {
-    var bitmap, a, b, c,
-        result = "", i = 0,
-        rest = stringToEncode.length % 3; // To determine the final padding
-    for (; i < stringToEncode.length;) {
-        a = _gpfBtoaCheck(stringToEncode, i++);
-        b = _gpfBtoaCheck(stringToEncode, i++);
-        c = _gpfBtoaCheck(stringToEncode, i++);
-        bitmap = a << 16 | b << 8 | c;
-        result += _gpfBase64.charAt(bitmap >> 18 & 63) + _gpfBase64.charAt(bitmap >> 12 & 63)
-                + _gpfBase64.charAt(bitmap >> 6 & 63) + _gpfBase64.charAt(bitmap & 63);
+    var bitmap,
+        a,
+        b,
+        c,
+        result = "",
+        index = 0,
+        rest = stringToEncode.length % _GPF_BTOA_MAX_PADDING; // To determine the final padding
+    for (; index < stringToEncode.length;) {
+        a = _gpfBtoaCheck(stringToEncode, index++);
+        b = _gpfBtoaCheck(stringToEncode, index++);
+        c = _gpfBtoaCheck(stringToEncode, index++);
+        bitmap = a << _GPF_2_BYTES | b << _GPF_1_BYTE | c;
+        result += _gpfBase64.charAt(bitmap >> _GPF_18_BITS & _GPF_6_BITS_MASK)
+                + _gpfBase64.charAt(bitmap >> _GPF_12_BITS & _GPF_6_BITS_MASK)
+                + _gpfBase64.charAt(bitmap >> _GPF_6_BITS & _GPF_6_BITS_MASK)
+                + _gpfBase64.charAt(bitmap & _GPF_6_BITS_MASK);
     }
     // If there's need of padding, replace the last 'A's with equal signs
     if (rest) {
-        return result.slice(0, rest - 3) + "===".substring(rest);
+        return result.slice(_GPF_START, rest - _GPF_BTOA_MAX_PADDING) + "===".substring(rest);
     }
     return result;
 }
