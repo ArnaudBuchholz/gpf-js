c49ae01b1e08f3d6a464c45184dc3e28e0186090
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-02 16:07:58 -0500

    Documentation (#240)

--- src/compatibility/base64.js
+++ src/compatibility/base64.js
@@ -1,11 +1,12 @@
 /**
  * @file base64 polyfill
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfCompatibilityInstallGlobal*/ // Install compatible global if missing
 /*global _gpfAtob*/ // atob polyfill
 /*global _gpfBtoa*/ // btoa polyfill
+/*global _gpfCompatibilityInstallGlobal*/ // Install compatible global if missing
 /*#endif*/
 
 _gpfCompatibilityInstallGlobal("atob", _gpfAtob);
