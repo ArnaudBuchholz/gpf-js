3fd2095264a4de2e33b37cf8c36717dcbf6768ab
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-04 13:38:28 -0500

    no-empty-modules (#240)

---
+++ .eslintrules/no-empty-module.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/*jshint esversion: 6*/
+
+const
+    path = require("path"),
+    tools = require("../res/tools");
+
+module.exports = {
+    meta: {
+        docs: {
+            url: "https://github.com/ArnaudBuchholz/gpf-js/blob/master/.eslintrules/empty-module.js",
+            description: "Assess that modules have at least one function",
+            category: "Custom rules",
+            recommended: false
+        },
+        fixable: "code",
+        schema: [],
+        messages: {
+            noEmptyModule: "Module has no function"
+        }
+    },
+    create (context) {
+        let hasFunction = false;
+        return {
+            "Program:exit" (node) {
+                if (!hasFunction) {
+                    context.report({
+                        node,
+                        messageId: "noEmptyModule",
+                        fix: fixer => {
+                            const
+                                fileNameParts = context.getFilename().split(path.sep),
+                                fileName = fileNameParts
+                                    .splice(fileNameParts.indexOf("src") + 1)
+                                    .map(tools.capitalize)
+                                    .join("")
+                                    .replace(".js", "");
+                            return fixer.insertTextAfter(node, `
+
+/*#ifndef(UMD)*/
+
+// Generates an empty function to reflect the null complexity of this module
+function _gpf${fileName} () {}
+/*exported _gpf${fileName}*/
+
+/*#endif*/
+`);
+                        }
+                    });
+                }
+            },
+            FunctionDeclaration (node) {
+                hasFunction = true;
+            },
+            FunctionExpression (node) {
+                hasFunction = true;
+            },
+            ArrowFunctionExpression (node) {
+                hasFunction = true;
+            }
+        };
+    }
+};
