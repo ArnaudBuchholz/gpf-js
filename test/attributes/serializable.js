2c888becee0732d5f6e707c57f5dd1d8ae42dc76
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-07 13:11:55 -0500

    name is not required (#300)

--- test/attributes/serializable.js
+++ test/attributes/serializable.js
@@ -9,16 +9,13 @@ describe("attributes/serializable", function () {
             $class: "A",
 
             "[_member1]": [new gpf.attributes.Serializable({
-                name: "Member1",
                 type: gpf.serial.types.string,
                 required: true
             })],
             _member1: "0",
 
-            "[_member2]": [new gpf.attributes.Serializable({
-                name: "Member2"
-            })],
-            _member2: ""
+            "[member2]": [new gpf.attributes.Serializable()],
+            member2: ""
         });
     });
 
@@ -27,11 +24,11 @@ describe("attributes/serializable", function () {
         it("provides serialization information on members", function () {
             var attributes = gpf.attributes.get(A, gpf.attributes.Serializable),
                 member1 = attributes._member1[0].getProperty(),
-                member2 = attributes._member2[0].getProperty();
-            assert(member1.name === "Member1");
+                member2 = attributes.member2[0].getProperty();
+            assert(member1.name === "member1");
             assert(member1.type === gpf.serial.types.string);
             assert(member1.required);
-            assert(member2.name === "Member2");
+            assert(member2.name === "member2");
             assert(member2.type === gpf.serial.types.string);
             assert(!member2.required);
         });
