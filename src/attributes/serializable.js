a1a4412084acf8ed0bb217d9907cb13e7755a174
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-07 13:12:03 -0500

    Implementation (#300)

--- src/attributes/serializable.js
+++ src/attributes/serializable.js
@@ -8,9 +8,19 @@
 /*global _gpfAttributesMemberAttribute*/ // Shortcut for gpf.attributes.MemberAttribute
 /*global _gpfAttributesUniqueAttribute*/ // Shortcut for gpf.attributes.UniqueAttribute
 /*global _gpfDefine*/ // Shortcut for gpf.define
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*global _gpfSerialPropertyCheck*/ // Check that the serializable property definition is valid
 /*exported _gpfAttributesSerializable*/ // Shortcut for gpf.attributes.Serializable
 /*#endif*/
+
+var _GPF_ATTRIBUTES_SERIALIZABLE_SKIP_ = 1;
+
+function _gpfAttributesSerializableExtractName (name) {
+    if (name.startsWith("_")) {
+        return name.substring(_GPF_ATTRIBUTES_SERIALIZABLE_SKIP_);
+    }
+    return name;
+}
 
 var _gpfAttributesSerializable = _gpfDefine({
     $class: "gpf.attributes.Serializable",
@@ -38,7 +48,23 @@ var _gpfAttributesSerializable = _gpfDefine({
      * @since 0.2.8
      */
     constructor: function (property) {
-        this._property = _gpfSerialPropertyCheck(property);
+        if (property) {
+            this._property = Object.assign({}, property);
+        } else {
+            this._property = {};
+        }
+    },
+
+    /**
+     * @inheritdoc
+     * @since 0.2.9
+     */
+    _check: function (member, classDefinition) {
+        _gpfIgnore(classDefinition);
+        if (!this._property.name) {
+            this._property.name = _gpfAttributesSerializableExtractName(member);
+        }
+        this._property = _gpfSerialPropertyCheck(this._property);
     },
 
     /**
