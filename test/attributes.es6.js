df20476d4c07c0bd18a2cd1a13ae4b2969773df2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-15 14:19:46 -0500

    Test case (#296)

--- test/attributes.es6.js
+++ test/attributes.es6.js
@@ -2,44 +2,54 @@
 
 describe("attributes.es6", function () {
 
-	var basePath;
+    var basePath;
 
-	before(function () {
-		if (gpf.host() === gpf.hosts.browser) {
-			if (config.httpPort === 0) {
-				// published version
-				basePath = "/gpf/test-resources/require";
-			} else {
-				// local version
-				basePath = "/test/require";
-			}
-		} else {
-			// Execution path is always the root folder of the project
-			basePath = "test/require";
-		}
-		gpf.require.configure({
-			base: basePath
-		});
-	});
+    before(function () {
+        if (gpf.host() === gpf.hosts.browser) {
+            if (config.httpPort === 0) {
+                // published version
+                basePath = "/gpf/test-resources/require";
+            } else {
+                // local version
+                basePath = "/test/require";
+            }
+        } else {
+            // Execution path is always the root folder of the project
+            basePath = "test/require";
+        }
+        gpf.require.configure({
+            base: basePath
+        });
+    });
 
-	describe("Defining attributes on an ES6 class through gpf.require.define", function () {
+    describe("Defining attributes on an ES6 class through gpf.require.define", function () {
 
-		beforeEach(function () {
-			gpf.require.configure({
-				clearCache: true
-			});
-		});
+        beforeEach(function () {
+            gpf.require.configure({
+                clearCache: true
+            });
+        });
 
-		it("loads js file defining a class with attributes", function (done) {
-			gpf.require.define({
-				Test: "es6/class_with_attributes.js"
-			}, function (require) {
+        it("loads js file defining a class with attributes", function (done) {
+            gpf.require.define({
+                classes: "es6/class_with_attributes.js"
+            }, function (require) {
 
-				var Test = require.Test;
+                var
+                    Attribute = require.classes.Attribute,
+                    Test = require.classes.Test;
 
-			}).then(undefined, done);
-		});
+                var attributes = gpf.attributes.get(Test);
+                assert(Object.keys(attributes).length === 1);
+                assert(attributes.id.length === 1);
+                assert(attributes.id[0] instanceof Attribute);
+                assert(attributes.id[0].value === 1);
 
-	});
+                done();
+
+            }).then(undefined, done);
+        });
+
+    });
 
 });
