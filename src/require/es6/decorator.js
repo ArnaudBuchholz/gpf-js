5c06715cce5ea5d62c89b979d5184b7d0a0964aa
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-15 14:21:09 -0500

    ES6 decorator preprocessor (#296)

---
+++ src/require/es6/decorator.js
@@ -0,0 +1,11 @@
+/**
+ * @file ES6 decorators implementation
+ */
+/*#ifndef(UMD)*/
+"use strict";
+/*exported _gpfRequireEs6DecoratorPreprocess*/
+/*#endif*/
+
+function _gpfRequireEs6DecoratorPreprocess (source) {
+    return source;
+}
