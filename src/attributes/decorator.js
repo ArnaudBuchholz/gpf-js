bcf3e15d1803ee66f99db60e66dae0acdd045828
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-20 13:19:15 -0500

    Importing class definition if not existing (#296)

--- src/attributes/decorator.js
+++ src/attributes/decorator.js
@@ -1,10 +1,13 @@
 /**
  * @file ES6 decorator implementation
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfArraySlice*/
-/*global _gpfIgnore*/
+/*global _gpfArraySlice*/ // [].slice.call
+/*global _gpfAssert*/ // Assertion method
+/*global _gpfDefineClassImport*/ // Retrieves or import entity definition from instance builder
+/*global _gpfIgnore*/ // Helper to remove unused parameter warning
 /*#endif*/
 
 /**
@@ -13,10 +16,13 @@
  * @param {...gpf.attributes.Attribute} attribute Attributes to add
  * @return {Function} decorator function
  * @throws {gpf.Error.InvalidParameter}
+ * @since 0.2.9
  */
 function _gpfAttributesDecorator () {
     var attributes = _gpfArraySlice(arguments);
     return function (ClassConstructor, member/*, descriptor*/) {
+        var entityDefinition = _gpfDefineClassImport(ClassConstructor);
+        _gpfAssert(entityDefinition !== null, "Can't access the entity definition");
         attributes.forEach(function (attribute) {
             // Add attribute
             _gpfIgnore(ClassConstructor, member, attribute);
@@ -26,5 +32,6 @@ function _gpfAttributesDecorator () {
 
 /**
  * @gpf:sameas _gpfAttributesDecorator
+ * @since 0.2.9
  */
 gpf.attributes.decorator = _gpfAttributesDecorator;
