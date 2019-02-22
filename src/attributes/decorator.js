31b5acf141330c9a6ac7828a8a0992a5f82aa732
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-21 20:23:23 -0500

    Handle class decorator (#296)

--- src/attributes/decorator.js
+++ src/attributes/decorator.js
@@ -5,14 +5,34 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _gpfArraySlice*/ // [].slice.call
-/*global _gpfAssert*/ // Assertion method
 /*global _gpfDefineClassImport*/ // Retrieves or import entity definition from instance builder
 /*global _gpfDefClassAttrCheck*/ // Check attribute
 /*global _gpfDefClassAttrBuild*/ // _gpfDefClassAttrBuild
+/*global _GPF_DEFINE_CLASS_ATTRIBUTES_NAME*/ // $attributes
 /*#endif*/
 
+function _gpfAttributesDecoratorGetAttributesKeyFromMember (member) {
+    if (!member) {
+        return _GPF_DEFINE_CLASS_ATTRIBUTES_NAME;
+    }
+    return member;
+}
+
+function _gpfAttributesDecoratorProcessEachAttribute (entityDefinition, member, attributes) {
+    attributes.forEach(function (attribute) {
+        _gpfDefClassAttrCheck.call(entityDefinition, member, attribute);
+        _gpfDefClassAttrBuild(member, attribute, entityDefinition.getInstanceBuilder().prototype);
+    });
+}
+
+function _gpfAttributesDecoratorAddAttributes (entityDefinition, member, attributes) {
+    var key = _gpfAttributesDecoratorGetAttributesKeyFromMember(member);
+    entityDefinition._attributes[key] = (entityDefinition._attributes[key] || []).concat(attributes);
+    _gpfAttributesDecoratorProcessEachAttribute(entityDefinition, member, attributes);
+}
+
 /**
- * Get attributes defined for the object / class
+ * Bridge ES6 decorators with attributes
  *
  * @param {...gpf.attributes.Attribute} attribute Attributes to add
  * @return {Function} decorator function
@@ -22,16 +42,7 @@
 function _gpfAttributesDecorator () {
     var attributes = _gpfArraySlice(arguments);
     return function (ClassConstructor, member/*, descriptor*/) {
-        var entityDefinition = _gpfDefineClassImport(ClassConstructor);
-        _gpfAssert(entityDefinition !== null, "Can't access the entity definition");
-        if (!entityDefinition._attributes[member]) {
-            entityDefinition._attributes[member] = [];
-        }
-        attributes.forEach(function (attribute) {
-            entityDefinition._attributes[member].push(attribute);
-            _gpfDefClassAttrCheck.call(entityDefinition, member, attribute);
-            _gpfDefClassAttrBuild(member, attribute, ClassConstructor.prototype);
-        });
+        _gpfAttributesDecoratorAddAttributes(_gpfDefineClassImport(ClassConstructor), member, attributes);
     };
 }
 
