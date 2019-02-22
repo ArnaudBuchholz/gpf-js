bac55007c6368aad7fdf14511cf85820bb8e74ec
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-21 20:22:45 -0500

    Improved test case (#296)

--- test/require/es6/class_with_attributes.js
+++ test/require/es6/class_with_attributes.js
@@ -1,16 +1,28 @@
 const attribute = gpf.attributes.decorator;
 
+// @attribute(new gpf.attributes.MemberAttribute())
 class Attribute extends gpf.attributes.Attribute {
+
     get value () {
-        return 1;
+        return this._value;
+    }
+
+    constructor (value) {
+        super();
+        this._value = value;
     }
 }
 
 class Test {
 
-    // @attribute(new Attribute())
+    // @attribute(new Attribute(1))
     get id () {
         return this._id;
+    }
+
+    // @attribute(new Attribute(2))
+    reset () {
+        this._id = "";
     }
 
     constructor () {
@@ -19,7 +31,9 @@ class Test {
 
 }
 
-// Until decorators are implemented, simulate manually
-attribute(new Attribute())(Test, "id");
-
 module.exports = {Attribute, Test};
+
+// Until decorators are implemented, simulate manually
+attribute(new gpf.attributes.MemberAttribute())(Attribute);
+attribute(new Attribute(1))(Test, "id");
+attribute(new Attribute(2))(Test, "reset");
