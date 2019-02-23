15420c28856a48bf0d075312df276787c44f9069
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-02-23 08:36:01 -0500

    Isolates entityDefinition management (#296)

--- src/define/build.js
+++ src/define/build.js
@@ -5,45 +5,10 @@
 /*#ifndef(UMD)*/
 "use strict";
 /*global _GpfEntityDefinition*/ // Entity definition
-/*global _gpfArrayForEachFalsy*/ // _gpfArrayForEach that returns first truthy value computed by the callback
 /*global _gpfContext*/ // Resolve contextual string
+/*global_gpfDefineEntitiesAdd*/
 /*global _gpfEmptyFunc*/ // An empty function
-/*exported _gpfDefineGetEntityFromBuilder*/ // Retrieves entity definition from instance instance builder
-/*exported _gpfDefinedEntities*/ // Array of defined entities
 /*#endif*/
-
-/**
- * Array of defined entities
- * @type {_GpfEntityDefinition[]}
- * @since 0.2.4
- */
-var _gpfDefinedEntities = [];
-
-/**
- * Retrieve entity definition from instance builder.
- * NOTE: This is an internal solution that has the advantage of not exposing the entity definitions.
- *       For performance reasons, this may change in the future.
- *
- * @param {Function} instanceBuilder Instance builder
- * @return {_GpfEntityDefinition|undefined} Entity definition (if found)
- * @since 0.2.4
- */
-function _gpfDefineGetEntityFromBuilder (instanceBuilder) {
-    var result = _gpfArrayForEachFalsy(_gpfDefinedEntities, function (entityDefinition) {
-        if (entityDefinition.getInstanceBuilder() === instanceBuilder) {
-            return entityDefinition;
-        }
-    });
-    if (!result) {
-        // Reversed lookup because testing inheritance
-        result = _gpfArrayForEachFalsy([].concat(_gpfDefinedEntities).reverse(), function (entityDefinition) {
-            if (instanceBuilder.prototype instanceof entityDefinition.getInstanceBuilder()) {
-                return entityDefinition;
-            }
-        });
-    }
-    return result;
-}
 
 Object.assign(_GpfEntityDefinition.prototype, {
 
@@ -76,7 +41,7 @@ Object.assign(_GpfEntityDefinition.prototype, {
             _gpfContext(this._namespace.split("."), true)[this._name] = value;
         }
         this._instanceBuilder = value;
-        _gpfDefinedEntities.push(this);
+        _gpfDefineEntitiesAdd(this);
     },
 
     /**
