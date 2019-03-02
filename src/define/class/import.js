fbda07b4acc82e8f47a7d2b20e9277d312926c89
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-02 09:17:37 -0500

    Fix backward compatibility (#296)

--- src/define/class/import.js
+++ src/define/class/import.js
@@ -7,6 +7,7 @@
 /*global _GpfClassDefinition*/ // Class definition
 /*global _gpfDefineEntitiesAdd*/ // Store the entity definition to be retreived later
 /*global _gpfDefineEntitiesFindByConstructor*/ // Retrieve entity definition from Constructor
+/*global _gpfEmptyFunc*/
 /*exported _gpfDefineClassImport*/ // Import a class as an entity definition
 /*exported _gpfDefineClassImported*/ // Base dictionary for all imported classes
 /*#endif*/
@@ -25,6 +26,9 @@ function _gpfDefineClassImportFrom (instanceBuilder) {
     var entityDefinition = new _GpfClassDefinition(_gpfDefineClassImportGetDictionary(instanceBuilder));
     entityDefinition._instanceBuilder = instanceBuilder;
     _gpfDefineEntitiesAdd(entityDefinition);
+    // Since it might not even have a name
+    entityDefinition._checkNameIsNotEmpty = _gpfEmptyFunc;
+    entityDefinition._checkName = _gpfEmptyFunc;
     entityDefinition.check();
     return entityDefinition;
 }
