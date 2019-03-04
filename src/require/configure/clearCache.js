c56f21c66ef8478ce9fabab2b3262269fc902045
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-04 13:35:45 -0500

    clearCache option (#306)

--- src/require/configure/clearCache.js
+++ src/require/configure/clearCache.js
@@ -1,14 +1,22 @@
 /**
  * @file Require clearCache configuration option
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfRequireConfigureAddOption*/
+/*global _gpfRequireConfigureAddOption*/ // Declare a configuration option
 /*#endif*/
 
+function _gpfRequireConfigureCheckClearCache (value) {
+    if (typeof value !== "boolean") {
+        gpf.Error.invalidRequireConfigureOptionValue();
+    }
+}
+
 function _gpfRequireConfigureClearCache (context, value) {
+    _gpfRequireConfigureCheckClearCache(value);
     if (value) {
-      context.cache = {};
+        context.cache = {};
     }
 }
 
