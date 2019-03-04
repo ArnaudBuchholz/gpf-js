c4f4f7c2a697a08b71356a332caaa15c43b7b8db
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-04 13:35:38 -0500

    preload option (#306)

--- src/require/configure/preload.js
+++ src/require/configure/preload.js
@@ -1,16 +1,25 @@
 
 /**
  * @file Require preload configuration option
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfRequireConfigureAddOption*/
+/*global _gpfArrayForEach*/ // Almost like [].forEach (undefined are also enumerated)
+/*global _gpfRequireConfigureAddOption*/ // Declare a configuration option
 /*#endif*/
 
+function _gpfRequireConfigureCheckPreload (value) {
+    if (typeof value !== "object") {
+        gpf.Error.invalidRequireConfigureOptionValue();
+    }
+}
+
 function _gpfRequireConfigurePreload (context, value) {
-  _gpfArrayForEach(Object.keys(value), function (name) {
-      context.preload[name] = value[name];
-  }, this);
+    _gpfRequireConfigureCheckPreload(value);
+    _gpfArrayForEach(Object.keys(value), function (name) {
+        context.preload[name] = value[name];
+    });
 }
 
 _gpfRequireConfigureAddOption("preload", _gpfRequireConfigurePreload);
