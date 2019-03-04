fdcd9615a25630ea30f54f2e58086a37e1daea90
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-04 13:35:52 -0500

    cache option (#306)

--- src/require/configure/cache.js
+++ src/require/configure/cache.js
@@ -1,15 +1,24 @@
 /**
  * @file Require cache configuration option
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfRequireConfigureAddOption*/
+/*global _gpfArrayForEach*/ // Almost like [].forEach (undefined are also enumerated)
+/*global _gpfRequireConfigureAddOption*/ // Declare a configuration option
 /*#endif*/
 
+function _gpfRequireConfigureCheckCache (value) {
+    if (typeof value !== "object") {
+        gpf.Error.invalidRequireConfigureOptionValue();
+    }
+}
+
 function _gpfRequireConfigureCache (context, value) {
-  _gpfArrayForEach(Object.keys(value), function (name) {
-      context.cache[name] = Promise.resolve(value[name]);
-  }, this);
+    _gpfRequireConfigureCheckCache(value);
+    _gpfArrayForEach(Object.keys(value), function (name) {
+        context.cache[name] = Promise.resolve(value[name]);
+    });
 }
 
 _gpfRequireConfigureAddOption("cache", _gpfRequireConfigureCache);
