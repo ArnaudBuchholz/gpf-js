8bf1c6c1f0739f808719997eecc3f3a32bfdbfff
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-04 13:35:59 -0500

    base option (#306)

--- src/require/configure/base.js
+++ src/require/configure/base.js
@@ -1,12 +1,20 @@
 /**
  * @file Require base configuration option
+ * @since 0.2.9
  */
 /*#ifndef(UMD)*/
 "use strict";
-/*global _gpfRequireConfigureAddOption*/
+/*global _gpfRequireConfigureAddOption*/ // Declare a configuration option
 /*#endif*/
 
+function _gpfRequireConfigureCheckBase (value) {
+    if (typeof value !== "string") {
+        gpf.Error.invalidRequireConfigureOptionValue();
+    }
+}
+
 function _gpfRequireConfigureBase (context, value) {
+    _gpfRequireConfigureCheckBase(value);
     context.base = value;
 }
 
