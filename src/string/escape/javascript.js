0b20f5a4e995c810ada2e4532f60c09950fa5376
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-07 08:49:56 -0500

    Generate explicit dependency (#295)

--- src/string/escape/javascript.js
+++ src/string/escape/javascript.js
@@ -4,15 +4,16 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
+/*global _gpfStringEscapeFor*/ // Make the string content compatible with lang
 /*global _gpfStringEscapes*/ // Dictionary of language to escapes
 /*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
+/*exported _gpfStringEscapeForJavascript*/ // String escape for JavaScript
 /*#endif*/
 
-_gpfStringEscapes.javascript = _gpfSyncReadSourceJSON("string/escape/javascript.json");
+var _GPF_STRING_ESCAPE_JAVASCRIPT = "javascript";
 
-/*#ifndef(UMD)*/
+_gpfStringEscapes[_GPF_STRING_ESCAPE_JAVASCRIPT] = _gpfSyncReadSourceJSON("string/escape/javascript.json");
 
-// Generates an empty function to reflect the null complexity of this module
-(function _gpfStringEscapeJavascript () {}());
-
-/*#endif*/
+function _gpfStringEscapeForJavascript (that) {
+    return _gpfStringEscapeFor(that, _GPF_STRING_ESCAPE_JAVASCRIPT);
+}
