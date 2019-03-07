d66bc15cee5a74639882d109b18bef179a86753b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-07 08:49:52 -0500

    Generate explicit dependency (#295)

--- src/string/escape/html.js
+++ src/string/escape/html.js
@@ -4,15 +4,18 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
+/*global _GPF_STRING_ESCAPE_XML*/ // xml escapes key
+/*global _gpfStringEscapeFor*/ // Make the string content compatible with lang
 /*global _gpfStringEscapes*/ // Dictionary of language to escapes
 /*global _gpfSyncReadSourceJSON*/ // Reads a source json file (only in source mode)
+/*exported _gpfStringEscapeForHtml*/ // String escape for Html
 /*#endif*/
 
-_gpfStringEscapes.html = Object.assign({}, _gpfStringEscapes.xml, _gpfSyncReadSourceJSON("string/escape/html.json"));
+var _GPF_STRING_ESCAPE_HTML = "html";
 
-/*#ifndef(UMD)*/
+_gpfStringEscapes.html = Object.assign({}, _gpfStringEscapes[_GPF_STRING_ESCAPE_XML],
+    _gpfSyncReadSourceJSON("string/escape/html.json"));
 
-// Generates an empty function to reflect the null complexity of this module
-(function _gpfStringEscapeHtml () {}());
-
-/*#endif*/
+function _gpfStringEscapeForHtml (that) {
+    return _gpfStringEscapeFor(that, _GPF_STRING_ESCAPE_HTML);
+}
