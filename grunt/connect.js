57acd0e20e25c1ec7bd9e79e8490060294e56a4a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-11 22:13:51 -0400

    Expose a serve-and-wait connect configuration

--- grunt/connect.js
+++ grunt/connect.js
@@ -1,21 +1,30 @@
 "use strict";
 
+const options = {
+    port: configuration.serve.httpPort,
+    hostname: "localhost",
+    middleware: function (connect, options, middlewares) {
+
+        var MIDDLEWARE_FOLDER = "connect-middleware",
+            fs = require("fs"),
+            path = require("path");
+        fs.readdirSync(path.join(__dirname, MIDDLEWARE_FOLDER)).forEach(function (fileName) {
+            middlewares.unshift(require("./" + MIDDLEWARE_FOLDER + "/" + fileName));
+        });
+
+        return middlewares;
+    }
+};
+
 module.exports = {
-    server: {
+    serve: {
+        options
+    },
+    "serve-and-wait": {
         options: {
-            port: configuration.serve.httpPort,
-            hostname: "*",
-            middleware: function (connect, options, middlewares) {
-
-                var MIDDLEWARE_FOLDER = "connect-middleware",
-                    fs = require("fs"),
-                    path = require("path");
-                fs.readdirSync(path.join(__dirname, MIDDLEWARE_FOLDER)).forEach(function (fileName) {
-                    middlewares.unshift(require("./" + MIDDLEWARE_FOLDER + "/" + fileName));
-                });
-
-                return middlewares;
-            }
+          useAvailablePort: true,
+          keepalive: true,
+          ...options
         }
     }
 };
