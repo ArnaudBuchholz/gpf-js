6ac416bfc64267f1ecfbaa693ef509b3f6528218
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-23 13:12:08 -0400

    Measure performance of async methods (#309)

--- test/perf/nodejs.js
+++ test/perf/nodejs.js
@@ -1,35 +1,68 @@
 "use strict";
 
-var testFile = process.argv[2],
-    testCases = require("./" + testFile + ".js"),
+var testCases = require("./" + process.argv[2] + ".js"),
     testResults = [];
 
 console.log("Please wait, evaluating test cases...");
-Object.keys(testCases).forEach(function (label) {
-    console.log("\t" + label);
-    var dtStart = new Date(),
-        count = 0,
-        testCase = testCases[label];
-    while (new Date() - dtStart < 1000) {
-        testCase();
-        ++count;
+
+function asyncLoop (testCase, startDate, count) {
+    if (new Date() - startDate > 1000) {
+        return Promise.resolve(count);
     }
+    return testCase().then(function () {
+        return asyncLoop (testCase, startDate, count + 1);
+    });
+}
+
+function done (label, count) {
     testResults.push({
         label: label,
         count: count
     });
-});
+    next();
+}
 
-console.log("Results:");
-var lastCount = 0;
-testResults.sort(function (a, b) {
-    return b.count - a.count;
-
-}).forEach(function (result, index) {
-    var message = "\t[" + (index + 1) + "] " + result.label + ": " + result.count.toLocaleString("en-US");
-    if (0 < index) {
-        message += " -" + Math.floor(1000 * (lastCount - result.count) / lastCount) / 10 + "%";
+function loop (label, startDate) {
+    console.log("\t" + label);
+    var testCase = testCases[label],
+        result = testCase(),
+        count = 1;
+    if (result.then) {
+        result.then(function () {
+            asyncLoop (testCase, startDate, count)
+                .then(function (totalCount) {
+                    done(label, totalCount);
+                });
+        });
+    } else {
+        while (new Date() - startDate < 1000) {
+            testCase();
+            ++count;
+        }
+        done(label, count);
     }
-    console.log(message);
-    lastCount = result.count;
-});
+}
+
+function next () {
+    var keys = Object.keys(testCases),
+        done = testResults.length;
+    if (keys.length > done) {
+        loop(keys[done], new Date());
+    } else {
+        console.log("Results:");
+        var lastCount = 0;
+        testResults.sort(function (a, b) {
+            return b.count - a.count;
+
+        }).forEach(function (result, index) {
+            var message = "\t[" + (index + 1) + "] " + result.label + ": " + result.count.toLocaleString("en-US");
+            if (0 < index) {
+                message += " -" + Math.floor(1000 * (lastCount - result.count) / lastCount) / 10 + "%";
+            }
+            console.log(message);
+            lastCount = result.count;
+        });
+    }
+}
+
+next();
