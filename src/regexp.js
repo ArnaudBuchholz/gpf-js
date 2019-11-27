3bdcc27ce04d590bcfa8bcaba41e72844ffeba4a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-27 13:19:42 -0500

    Tokenizer helper _gpfRegExpTokenize (#326)

--- src/regexp.js
+++ src/regexp.js
@@ -4,6 +4,7 @@
  */
 /*#ifndef(UMD)*/
 "use strict";
+/*global _GPF_START*/ // 0
 /*exported _gpfRegExpForEach*/ // Executes the callback for each match of the regular expression
 /*#endif*/
 
@@ -22,12 +23,12 @@ function _gpfRegExpGetNextMatch (regexp, string) {
 }
 
 function _gpfRegExpGetFirstMatch (regexp, string) {
-    regexp.lastIndex = 0;
+    regexp.lastIndex = _GPF_START;
     return _gpfRegExpGetNextMatch(regexp, string);
 }
 
 /**
- * Executes the callback for each match of the regular expression.
+ * Executes the regexp on the string until no match, returns the array of matches.
  * When configured with /g and used before,
  * the [lastIndex](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex)
  * must be first reset
@@ -47,3 +48,35 @@ function _gpfRegExpForEach (regexp, string) {
     }
     return matches;
 }
+
+function _gpfRegExpSetToken (match) {
+    match.every(function (value, index) {
+        if (index && value !== undefined) {
+            match.token = index;
+            return false;
+        }
+        return true;
+    });
+}
+
+/**
+ * Same as {@link _gpfRegExpForEach} but each match is augmented with a `token` property giving the index of the first
+ * capturing group that got extracted.
+ *
+ * @param {RegExp} regexp Regular expression to execute
+ * @param {String} string String to match
+ * @return {Array} Array of matches augmented with token information
+ * @since 0.2.1
+ * @version 0.2.2 Reset lastIndex and returns the array of matches
+ */
+function _gpfRegExpTokenize (regexp, string) {
+    var matches = _gpfRegExpForEach(regexp, string);
+    matches.forEach(_gpfRegExpSetToken);
+    return matches;
+}
+
+/*#ifndef(UMD)*/
+
+gpf.internals._gpfRegExpTokenize = _gpfRegExpTokenize;
+
+/*#endif*/
