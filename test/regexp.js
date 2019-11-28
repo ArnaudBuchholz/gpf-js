3b0c6ea443071b4f0f748e57aec700ca20c7ee9d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-28 13:13:47 -0500

    Expose an option to keep only tokenized (#326)

--- test/regexp.js
+++ test/regexp.js
@@ -32,6 +32,20 @@ describe("regexp", function () {
                     assert(matches[11].token === 6); // test
                 });
 
+                it("removes non tokenized matches", function () {
+                    var matches = _gpfRegExpTokenize(parsingRegExp, "//a | //ns:div/@test", true);
+                    assert(matches.length === 10);
+                    assert(matches[0].token === 1); // //
+                    assert(matches[1].token === 6); // a
+                    assert(matches[2].token === 3); // |
+                    assert(matches[3].token === 1); // //
+                    assert(matches[4].token === 6); // ns
+                    assert(matches[5].token === 5); // :
+                    assert(matches[6].token === 6); // div
+                    assert(matches[7].token === 2); // /
+                    assert(matches[8].token === 4); // @
+                    assert(matches[9].token === 6); // test
+                });
             });
 
         });
