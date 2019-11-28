c8bfcfef292dc03cea1d077b54a654dfb40f5e73
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-28 14:23:26 -0500

    XPATH parser (wip) (#326)

--- src/xml/xpath/parser.js
+++ src/xml/xpath/parser.js
@@ -47,14 +47,15 @@ var _GPF_XML_XPATH_TOKEN = {};
     "\\|		CONCAT",
     "\\/\\/		DEPTH",
     "\\/		SUB",
-    "\\.		ROOT",
+    "\\.		CURRENT",
     "@			ATTRIBUTE",
     "\\*		ANY",
     "\\w+:		NAMESPACE_PREFIX",
     "\\w+		NAME"
 ]));
 
-// <start> -> ROOT? (SUB|DEPTH) <match> (CONCAT <start>)?
+// <start> -> <level> (CONCAT <level>)?
+// <level> -> CURRENT? (SUB|DEPTH) <match>
 // <match> -> ATTRIBUTE? NAMESPACE_PREFIX? (NAME|ANY) ((SUB|DEPTH) <match>)?
 
 /**
@@ -66,4 +67,47 @@ var _GPF_XML_XPATH_TOKEN = {};
  */
 function _gpfXmlXPathParse (xpathExpression) {
     var tokens = _gpfRegExpTokenize(_GPF_XML_XPATH_TOKEN.regexp, xpathExpression);
+
+    function consumeIfTokenMatch () {
+        var expected = _gpfArraySlice(arguments),
+            current = tokens[0];
+        if (expected.include(current)) {
+            tokens.shift();
+            return current;
+        }
+    }
+
+    function checkAndConsumeIfTokenMatch () {
+        var token = consumeIfTokenMatch.apply(this, arguments);
+        if (!token) {
+            gpf.Error.invalidXPathSyntax();
+        }
+    }
+
+    function match () {
+        var
+    }
+
+    var levelClasses = {};
+    levelClasses[_GPF_XML_XPATH_TOKEN.SUB] = _gpfXmlXPathSub;
+    levelClasses[_GPF_XML_XPATH_TOKEN.DEEP] = _gpfXmlXPathDeep;
+
+    function level () {
+        var current = consumeIfTokenMatch(false, _GPF_XML_XPATH_TOKEN.CURRENT),
+            level = checkAndConsumeIfTokenMatch(_GPF_XML_XPATH_TOKEN.SUB, _GPF_XML_XPATH_TOKEN.DEEP),
+            Operator = levelClasses[level];
+        var operator = new Operator();
+        operator.addChild(match());
+        return operator;
+    }
+
+    function start () {
+        var concat = new _gpfXmlXPathConcat();
+        concat.addChild(level());
+        while (consumeIfTokenMatch(_GPF_XML_XPATH_TOKEN.CONCAT)) {
+            concat.addChild(operator());
+        }
+    }
+
+    return start();
 }
